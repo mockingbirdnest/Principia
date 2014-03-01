26909f02df41358ae6bf90023402d4d15ddc2a85
Robin Leroy <egg.robin.leroy+git@gmail.com>
2014-03-01 04:18:27 +0100

    Grassmann algebra.

--- NewtonianPhysics/Geometry.cs
+++ NewtonianPhysics/Geometry.cs
@@ -13,19 +13,55 @@ namespace NewtonianPhysics.Geometry {
 
   #region Linear Algebra
 
-  // Structs for strong typing in three dimensional real inner product spaces
-  // and their Grassmann algebras. Double-precision floating point numbers,
-  // representing reals, are wrapped in the struct Scalar to ensure strong
-  // typing.
-  // For k-vectors v, w, the inner product <v,w> is [k]Vector.InnerProduct(v, w)
+  // Structs for strong typing in a three dimensional real inner product space
+  // and its Grassmann algebra.
+  // Double-precision floating point numbers, representing reals, are wrapped in
+  // the struct Scalar to ensure strong typing. Three-dimensional data is stored
+  // as R3Element.
+  // We use the Grassman algebra in order to avoid the confusion between pseudo-
+  // vectors and vectors and between pseudoscalars and scalars. This allows for
+  // handling of indirect changes of coordinates. As a result, there is no cross
+  // product except for the underlying data type R3Element.
+  // We treat the isomorphism between a space and its dual as implicit, as we
+  // are dealing with inner product spaces.
+  // TODO(robin): We may want to change this when we get to rotating-pulsating
+  // reference frames.
+  // A reminder of the equivalents in the Grassman algebra of the cross
+  // product; We identify V ^ V with the Lie algebra so(V).
+  // . pseudovector = vector x vector, as in L = r x p, becomes the wedge
+  //   product,
+  //   bivector = vector ^ vector, L = r ^ p.
+  // . vector = pseudovector x vector, as in a_Coriolis = 2 Ω x v, becomes
+  //   the left action of the bivector,
+  //   vector = bivector . vector,  a_Coriolis = 2 Ω . v.
+  // . vector = vector x pseudovector, as in F = q v x B, becomes
+  //   the dual action,
+  //   vector = bivector . vector,  F* = q v* . B.
+  // . pseudovector = pseudovector x pseudovector, as in the composition of
+  //   rotation vectors Θ12 = α Θ1 + β Θ2 + γ Θ1 x Θ2, becomes the Lie bracket
+  //   of so(V).
+  //   bivector = [bivector, bivector], Θ12 = α Θ1 + β Θ2 + γ [Θ1, Θ2].
+  // . pseudoscalar = (pseudovector|vector), as in Φ = (B|S), becomes the wedge
+  //   product,
+  //   trivector = bivector ^ vector, Φ = B ^ S.
+  // NOTATION:
+  // For k-vectors v, w, the inner product (v|w) is [k]Vector.InnerProduct(v, w)
   // rather than v.InnerProduct(w) so that the enclosing space is explicit.
-  // Compare with v.Wedge(w) for a k-vector v and a l-vector w as there is no
-  // enclosing space.
+  // Compare with the notation v.Wedge(w) for the exterior product of a k-vector
+  // v and a l-vector w.
   // We choose to use operators for the vector space operations, thus keeping
-  // the enclosing space implicit, i.e., v + w rather than Vector.Plus(v, w).
+  // the enclosing space implicit, i.e., v + w rather than [k]Vector.Plus(v, w).
+  // The left action a . v of a bivector a on v is a.ActOn(v).
+  // The right action v* . a is v.ActedUpon(a). The commutator is
+  // Bivector.Commutator(a, b).
 
   public struct Bivector {
     public R3Element coordinates;
+    public static Bivector Commutator(Bivector left, Bivector right) {
+      return new Bivector {
+        coordinates = left.coordinates.Cross(right.coordinates)
+      };
+    }
     public static Scalar InnerProduct(Bivector left, Bivector right) {
       return left.coordinates.Dot(right.coordinates);
     }
@@ -47,12 +83,12 @@ namespace NewtonianPhysics.Geometry {
     public static Bivector operator +(Bivector left, Bivector right) {
       return new Bivector { coordinates = left.coordinates + right.coordinates };
     }
-    public Bivector Wedge(Vector right) {
-      return new Bivector {
+    public Vector ActOn(Vector right) {
+      return new Vector {
         coordinates = this.coordinates.Cross(right.coordinates)
       };
     }
-    public TriVector Wedge(Bivector right) {
+    public TriVector Wedge(Vector right) {
       return new TriVector {
         coordinate = this.coordinates.Dot(right.coordinates)
       };
@@ -198,6 +234,11 @@ namespace NewtonianPhysics.Geometry {
     }
     public static Vector operator +(Vector left, Vector right) {
       return new Vector { coordinates = left.coordinates + right.coordinates };
+    }
+    public Vector ActedUpon(Bivector right) {
+      return new Vector {
+        coordinates = this.coordinates.Cross(right.coordinates)
+      };
     }
     public Bivector Wedge(Vector right) {
       return new Bivector {
