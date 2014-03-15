4f43cbe5d7da2c0fbd041ebb1429f30f2ae6b0e8
Robin Leroy <egg.robin.leroy+git@gmail.com>
2014-03-15 22:18:09 +0100

    linewrap

--- Geometry/AffineSpace.cs
+++ Geometry/AffineSpace.cs
@@ -16,7 +16,8 @@ namespace Geometry {
     // A convex combination of positions is a position.
     public static Point<A> Barycenter(Point<A> q1, Scalar λ1,
                                       Point<A> q2, Scalar λ2) {
-      return new Point<A>((q1.Coordinates * λ1 + q2.Coordinates * λ2) / (λ1 + λ2));
+      return new Point<A>((q1.Coordinates * λ1 + q2.Coordinates * λ2) /
+                          (λ1 + λ2));
     }
   }
 }
