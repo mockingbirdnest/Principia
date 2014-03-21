872892a42a2e0f8c8f52e0176a69cbcf79b2d7e5
Robin Leroy <egg.robin.leroy@gmail.com>
2014-03-21 14:17:50 +0100

    spacing

--- PhysicalQuantities/NamedQuantities.h
+++ PhysicalQuantities/NamedQuantities.h
@@ -6,16 +6,16 @@
 
 namespace PhysicalQuantities {
 #pragma region General mechanics
-  typedef Quotient<Length, Time> Speed;
-  typedef Quotient<Speed, Time> Acceleration;
-  typedef Product<Mass, Speed> Momentum;
+  typedef Quotient<Length, Time>   Speed;
+  typedef Quotient<Speed, Time>    Acceleration;
+  typedef Product<Mass, Speed>     Momentum;
   typedef Quotient<Momentum, Time> Force;
-  typedef Product<Force, Length> Energy;
+  typedef Product<Force, Length>   Energy;
 #pragma endregion
 #pragma region Thermodynamics
-  typedef Product<Length, Length> Surface;
-  typedef Product<Surface, Length> Volume;
-  typedef Quotient<Force, Surface> Pressure;
+  typedef Product<Length, Length>       Surface;
+  typedef Product<Surface, Length>      Volume;
+  typedef Quotient<Force, Surface>      Pressure;
   typedef Quotient<Energy, Temperature> Entropy;
 #pragma endregion
 }
