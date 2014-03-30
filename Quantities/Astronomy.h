439baa5fc10eef8c3da588eda8579dc92e1af6c9
Robin Leroy <egg.robin.leroy+git@gmail.com>
2014-03-30 14:53:05 +0200

    spacing.

--- Quantities/Astronomy.h
+++ Quantities/Astronomy.h
@@ -10,11 +10,11 @@ namespace Principia {
 // This namespace contains units commonly used in astronomy that are not
 // accepted for use with the SI.
 namespace Astronomy {
-Quantities::Mass   const SolarMass    = 1.98855e30 * SI::Kilogram;
-Quantities::Mass   const JupiterMass  = 1.8986e27 * SI::Kilogram;
-Quantities::Mass   const EarthMass    = 5.9742e24 * SI::Kilogram;
-Quantities::Time   const JulianYear   = 365.25 * SI::Day;
-Quantities::Length const Parsec       = 648000 / π * SI::AstronomicalUnit;
-Quantities::Length const LightYear    = Constants::SpeedOfLight * JulianYear;
+Quantities::Mass   const SolarMass   = 1.98855e30 * SI::Kilogram;
+Quantities::Mass   const JupiterMass = 1.8986e27 * SI::Kilogram;
+Quantities::Mass   const EarthMass   = 5.9742e24 * SI::Kilogram;
+Quantities::Time   const JulianYear  = 365.25 * SI::Day;
+Quantities::Length const Parsec      = 648000 / π * SI::AstronomicalUnit;
+Quantities::Length const LightYear   = Constants::SpeedOfLight * JulianYear;
 }
 }
