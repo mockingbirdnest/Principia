18e232260be99a98c19531367965d9221cb9e37a
Robin Leroy <egg.robin.leroy+git@gmail.com>
2014-04-03 12:45:26 +0200

    Forgot to save... :-p

--- Geometry/Grassmann.hpp
+++ Geometry/Grassmann.hpp
@@ -33,37 +33,5 @@ template<typename T, typename Space, unsigned int Rank>
 Multivector<T, Space,
             Rank> operator/ (Multivector<T, Space, Rank> const& left,
                              Quantities::Dimensionless const& right);
-
-template<typename T, typename Space, unsigned int Rank, typename U>
-R3Element<Quantities::Product<U, T>> operator* (U const& left,
-                                                Multivector<T, Space,
-                                                            Rank> const& right);
-template<typename T, typename Space, unsigned int Rank, typename U>
-R3Element<Quantities::Product<T, U>> operator* (Multivector<T, Space,
-                                                            Rank> const& left,
-                                                U const& right);
-template<typename T, typename Space, unsigned int Rank, typename U>
-R3Element<Quantities::Quotient<T, U>> operator/ (Multivector<T, Space,
-                                                             Rank> const& left,
-                                                 U const& right);
-
-template<typename T, typename Space, unsigned int Rank>
-Multivector<T, Space,
-            Rank> operator+= (Multivector<T, Space, Rank>& left,
-                              Multivector<T, Space, Rank> const& right);
-template<typename T, typename Space, unsigned int Rank>
-Multivector<T, Space,
-            Rank> operator-= (Multivector<T, Space, Rank>& left,
-                              Multivector<T, Space, Rank> const& right);
-
-template<typename T, typename Space, unsigned int Rank>
-Multivector<T, Space,
-            Rank> operator*= (Multivector<T, Space, Rank>& left,
-                              Quantities::Dimensionless const& right);
-template<typename T, typename Space, unsigned int Rank>
-Multivector<T, Space,
-            Rank> operator/= (Multivector<T, Space, Rank>& left,
-                              Quantities::Dimensionless const& right);
-
 }
 }
