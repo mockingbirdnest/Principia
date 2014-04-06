01078815d7abb5a10078b1d7ac9138b1327ecb84
Robin Leroy <egg.robin.leroy+git@gmail.com>
2014-04-07 00:53:22 +0200

    Implement * and /, both dimensionful and dimensionless.

--- Geometry/Grassmann-body.hpp
+++ Geometry/Grassmann-body.hpp
@@ -27,5 +27,43 @@ inline Multivector<T, Frame,
   return Multivector<T, Frame, Rank>(left.coordinates + left.coordinates);
 }
 
+template<typename T, typename Frame, unsigned int Rank>
+inline Multivector<T, Frame,
+                   Rank> operator* (Quantities::Dimensionless const& left,
+                                    Multivector<T, Frame, Rank> const& right) {
+  return Multivector<T, Frame, Rank>(left * right.coordinates);
+}
+template<typename T, typename Frame, unsigned int Rank>
+inline Multivector<T, Frame,
+                   Rank> operator* (Multivector<T, Frame, Rank> const& left,
+                                    Quantities::Dimensionless const& right) {
+  return Multivector<T, Frame, Rank>(left.coordinates * right);
+}
+template<typename T, typename Frame, unsigned int Rank>
+inline Multivector<T, Frame,
+                   Rank> operator/ (Multivector<T, Frame, Rank> const& left,
+                                    Quantities::Dimensionless const& right) {
+  return Multivector<T, Frame, Rank>(left.coordinates / right);
+}
+
+template<typename T, typename U, typename Frame, unsigned int Rank>
+inline Multivector<Quantities::Product<U, T> , Frame,
+                   Rank> operator* (U const& left,
+                                    Multivector<T, Frame, Rank> const& right) {
+  return Multivector<T, Frame, Rank>(left * right.coordinates);
+}
+template<typename T, typename U, typename Frame, unsigned int Rank>
+inline Multivector<Quantities::Product<T, U>, Frame,
+                   Rank> operator* (Multivector<T, Frame, Rank> const& left,
+                                    Quantities::Dimensionless const& right) {
+  return Multivector<T, Frame, Rank>(left.coordinates * right);
+}
+template<typename T, typename U, typename Frame, unsigned int Rank>
+inline Multivector<Quantities::Quotient<T, U>, Frame,
+                   Rank> operator/ (Multivector<T, Frame, Rank> const& left,
+                                    Quantities::Dimensionless const& right) {
+  return Multivector<T, Frame, Rank>(left.coordinates / right);
+}
+
 }
 }
