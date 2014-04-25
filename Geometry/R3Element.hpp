71cba18579ddc18c1b86ab3f395ab5b7c7a8d85f
Robin Leroy <egg.robin.leroy@gmail.com>
2014-04-26 01:32:08 +0200

    Styleguide omnipotens.

--- Geometry/R3Element.hpp
+++ Geometry/R3Element.hpp
@@ -6,22 +6,22 @@
 
 namespace Principia {
 namespace Geometry {
-  // R3Element is an element of a 3-dimensional dimensionful vector space on the
-  // field R, represented by Dimensionless. It is the underlying data type for
-  // the more advanced strongly typed structures of the Grassmann algebras and
-  // affine spaces.
-  template<typename Scalar>
-  struct R3Element {
-   public:
-    R3Element(Scalar const& x, Scalar const& y, Scalar const& z);
+// R3Element is an element of a 3-dimensional dimensionful vector space on the
+// field R, represented by Dimensionless. It is the underlying data type for
+// the more advanced strongly typed structures of the Grassmann algebras and
+// affine spaces.
+template<typename Scalar>
+struct R3Element {
+ public:
+  R3Element(Scalar const& x, Scalar const& y, Scalar const& z);
 
-    Scalar&       operator[] (int const index);
-    Scalar const& operator[] (int const index) const;
+  Scalar&       operator[] (int const index);
+  Scalar const& operator[] (int const index) const;
 
-    Scalar x;
-    Scalar y;
-    Scalar z;
-  };
+  Scalar x;
+  Scalar y;
+  Scalar z;
+};
 
 template<typename Scalar>
 R3Element<Scalar> operator+(R3Element<Scalar> const& right);
