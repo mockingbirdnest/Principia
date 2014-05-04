35255421896deef35cee99cfc2861067c7a249ba
Robin Leroy <egg.robin.leroy@gmail.com>
2014-05-04 22:53:30 +0200

    Full coverage of R3Element.

--- GeometryTests/R3ElementTests.cpp
+++ GeometryTests/R3ElementTests.cpp
@@ -54,6 +54,33 @@ TEST_CLASS(R3ElementTests) {
     TestSymmetricPositiveDefiniteBilinearMap(Dot<Speed, Speed>,
                                              u, v, w, a, Dimensionless(42));
   }
+  
+  TEST_METHOD(MixedProduct) {
+    R3Element<Speed> nullVector(0 * Metre / Second,
+                                0 * Metre / Second,
+                                0 * Metre / Second);
+    R3Element<Speed> u(1 * Metre / Second,
+                       120 * Kilo(Metre) / Hour,
+                       -SpeedOfLight);
+    R3Element<Speed> v(-20 * Knot,
+                       2 * π * AstronomicalUnit / JulianYear,
+                       1 * admiralty::NauticalMile / Hour);
+    R3Element<Speed> w(-1 * Mile / Hour, -2 * Foot / Second, -3 * Knot);
+    R3Element<Speed> a(88 * Mile / Hour, 300 * Metre / Second, 46 * Knot);
+    auto leftTimeMultiplication = [](Time left, R3Element<Speed> right) {
+      return left * right;
+    };
+    auto rightTimeMultiplication = [](R3Element<Speed> left, Time right) {
+      return left * right;
+    };
+    TestBilinearMap(leftTimeMultiplication, 1 * Second, 1 * JulianYear, u, v,
+                    Dimensionless(42));
+    TestBilinearMap(rightTimeMultiplication, w, a, -1 * Day,
+                    1 * Parsec / SpeedOfLight, Dimensionless(-π));
+    Time t = -3 * Second;
+    AssertEqual(t * u, u * t);
+    AssertEqual((u * t) / t, u);
+  }
 };
 
 }  // namespace geometry
