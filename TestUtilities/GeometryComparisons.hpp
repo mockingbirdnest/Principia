da8cf680815276962a6acb1100960d9f4a9fdbe3
Robin Leroy <egg.robin.leroy@gmail.com>
2014-05-07 13:25:58 +0200

    Propagate εs everywhere. Tests still broken as expected.

--- TestUtilities/GeometryComparisons.hpp
+++ TestUtilities/GeometryComparisons.hpp
@@ -1,7 +1,8 @@
-#pragma once
+﻿#pragma once
 
 #include "Geometry/Grassmann.hpp"
 #include "Geometry/R3Element.hpp"
+#include "Quantities/Dimensionless.hpp"
 #include "TestUtilities/QuantityComparisons.hpp"
 #include "TestUtilities/TestUtilities.hpp"
 
@@ -9,12 +10,14 @@ namespace principia {
 namespace test_utilities {
 
 template<typename Scalar, typename Frame, unsigned int Rank>
-void AssertEqual(geometry::Multivector<Scalar, Frame, Rank> left,
-                 geometry::Multivector<Scalar, Frame, Rank> right);
+void AssertEqual(geometry::Multivector<Scalar, Frame, Rank> const& left,
+                 geometry::Multivector<Scalar, Frame, Rank> const& right,
+                 quantities::Dimensionless const& ε = tolerance);
 
 template<typename Scalar>
-void AssertEqual(geometry::R3Element<Scalar> left,
-                 geometry::R3Element<Scalar> right);
+void AssertEqual(geometry::R3Element<Scalar> const& left,
+                 geometry::R3Element<Scalar> const& right,
+                 quantities::Dimensionless const& ε = tolerance);
 
 }  // namespace test_utilities
 }  // namespace principia
