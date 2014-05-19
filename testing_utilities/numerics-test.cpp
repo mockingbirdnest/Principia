78c7989171f6d3884fd30d1bd82b42da565ffeb6
Robin Leroy <egg.robin.leroy@gmail.com>
2014-05-19 12:44:56 +0200

    Test ULPs.

--- testing_utilities/numerics-test.cpp
+++ testing_utilities/numerics-test.cpp
@@ -1,15 +1,16 @@
-﻿#include "geometry/grassmann.hpp"
+﻿#include <cfloat>
+#include <cmath>
+
 #include "glog/logging.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "quantities/dimensionless.hpp"
-#include "quantities/named_quantities.hpp"
-#include "quantities/quantities.hpp"
-#include "quantities/uk.hpp"
-#include "testing_utilities/almost_equals.hpp"
+#include "testing_utilities/numerics.hpp"
 
 namespace principia {
 namespace testing_utilities {
+
+using testing::Eq;
+using testing::Ne;
 
 namespace {
 struct World;
@@ -17,7 +18,21 @@ struct World;
 
 class NumericsTest : public testing::Test {};
 
+// The smallest positive double, a denormal.
+double const SmallestPositive = DBL_MIN * DBL_EPSILON;
+
 TEST_F(NumericsTest, ULPs) {
+  EXPECT_THAT(ULPDistance(1, 1), Eq(0));
+  EXPECT_THAT(ULPDistance(+0.0, +0.0), Eq(0));
+  EXPECT_THAT(ULPDistance(+0.0, -0.0), Eq(0));
+  // DBL_MIN is the smallest positive normalised number.
+  // 52 bits of mantissa stand between it and 0, in the form of denormals.
+  EXPECT_THAT(ULPDistance(+0.0, DBL_MIN), Eq(std::pow(2, DBL_MANT_DIG - 1)));
+  EXPECT_THAT(ULPDistance(-0.0, DBL_MIN), Eq(std::pow(2, DBL_MANT_DIG - 1)));
+  EXPECT_THAT(ULPDistance(+0.0, -SmallestPositive), Eq(1));
+  EXPECT_THAT(ULPDistance(-0.0, -SmallestPositive), Eq(1));
+  EXPECT_THAT(ULPDistance(-1, 1), Ne(0));
+  EXPECT_THAT(ULPDistance(-1, 1), 2 * ULPDistance(0, 1));
 }
 
 }  // namespace testing_utilities
