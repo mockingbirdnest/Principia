e4d6d420f77d8c9b6e8b5659b14a75629a7220fb
Robin Leroy <egg.robin.leroy@gmail.com>
2014-05-19 12:07:07 +0200

    foo

---
+++ testing_utilities/numerics-test.cpp
@@ -0,0 +1,24 @@
+﻿#include "geometry/grassmann.hpp"
+#include "glog/logging.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "quantities/dimensionless.hpp"
+#include "quantities/named_quantities.hpp"
+#include "quantities/quantities.hpp"
+#include "quantities/uk.hpp"
+#include "testing_utilities/almost_equals.hpp"
+
+namespace principia {
+namespace testing_utilities {
+
+namespace {
+struct World;
+}  // namespace
+
+class NumericsTest : public testing::Test {};
+
+TEST_F(NumericsTest, ULPs) {
+}
+
+}  // namespace testing_utilities
+}  // namespace principia
