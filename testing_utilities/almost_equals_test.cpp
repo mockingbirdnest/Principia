098e68bcb7c7ded3067f65574a1873e1cd575834
Robin Leroy <egg.robin.leroy@gmail.com>
2014-05-19 11:53:47 +0200

    Test most of AlmostEquals.

--- testing_utilities/almost_equals_test.cpp
+++ testing_utilities/almost_equals_test.cpp
@@ -3,8 +3,11 @@
 #include "glog/logging.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "quantities/bipm.hpp"
+#include "quantities/cgs.hpp"
 #include "quantities/dimensionless.hpp"
 #include "quantities/named_quantities.hpp"
+#include "quantities/numbers.hpp"
 #include "quantities/quantities.hpp"
 #include "quantities/uk.hpp"
 #include "testing_utilities/almost_equals.hpp"
@@ -12,14 +15,20 @@
 namespace principia {
 namespace testing_utilities {
 
+using bipm::Knot;
+using geometry::Bivector;
 using geometry::R3Element;
 using geometry::Vector;
+using geometry::Trivector;
 using quantities::Dimensionless;
 using quantities::Length;
+using quantities::MagneticFlux;
+using quantities::Speed;
 using testing::Ne;
 using testing::Eq;
 using testing::Not;
 using uk::Foot;
+using cgs::Maxwell;
 
 namespace {
 struct World;
@@ -27,19 +36,89 @@ struct World;
 
 class AlmostEqualsTest : public testing::Test {};
 
-TEST_F(AlmostEqualsTest, Vectors) {
-  auto const v1 = Vector<Length, World>(
-      R3Element<Length>(1 * Foot, 2 * Foot, 3 * Foot));
+TEST_F(AlmostEqualsTest, Dimensionless) {
+  Dimensionless const y = e;
+  EXPECT_THAT(y, AlmostEquals(e));
+  EXPECT_THAT(y, AlmostEquals(e, 0));
+  EXPECT_THAT(2 * y, Not(AlmostEquals(y)));
+  Dimensionless const δv = e / 100;
+  Dimensionless e_accumulated;
+  for (int i = 1; i <= 100; ++i) {
+    e_accumulated += δv;
+  }
+  EXPECT_THAT(e_accumulated, Ne(e));
+  EXPECT_THAT(e_accumulated, Not(AlmostEquals(e, 0)));
+  EXPECT_THAT(e_accumulated, AlmostEquals(e));
+}
+
+TEST_F(AlmostEqualsTest, Quantity) {
+  Speed v1 = 1 * Knot;
+  Speed const v2 = v1;
+  EXPECT_THAT(v2, AlmostEquals(v1));
+  EXPECT_THAT(2 * v2, Not(AlmostEquals(v1)));
+  Speed const δv = v1 / 100;
+  Speed v_accumulated;
+  for (int i = 1; i <= 100; ++i) {
+    v_accumulated += δv;
+  }
+  EXPECT_THAT(v_accumulated, Ne(v1));
+  EXPECT_THAT(v_accumulated, AlmostEquals(v1));
+}
+
+TEST_F(AlmostEqualsTest, R3Element) {
+  R3Element<Speed> const v1 = {1 * Knot, 2 * Knot, 3 * Knot};
+  R3Element<Speed> const v2 = v1;
+  EXPECT_THAT(v2, AlmostEquals(v1));
+  EXPECT_THAT(2 * v2, Not(AlmostEquals(v1)));
+  R3Element<Speed> const δv = v1 / 100;
+  R3Element<Speed> v_accumulated;
+  for (int i = 1; i <= 100; ++i) {
+    v_accumulated += δv;
+  }
+  EXPECT_THAT(v_accumulated, Ne(v1));
+  EXPECT_THAT(v_accumulated, AlmostEquals(v1, 8));
+}
+
+TEST_F(AlmostEqualsTest, Vector) {
+  Vector<Length, World> const v1({1 * Foot, 2 * Foot, 3 * Foot});
   Vector<Length, World> const v2 = v1;
   EXPECT_THAT(v2, AlmostEquals(v1));
   EXPECT_THAT(2 * v2, Not(AlmostEquals(v1)));
   Vector<Length, World> const δv = v1 / 100;
-  auto v_accumulated = Vector<Length, World>(R3Element<Length>());
+  Vector<Length, World> v_accumulated;
   for (int i = 1; i <= 100; ++i) {
     v_accumulated += δv;
   }
   EXPECT_THAT(v_accumulated, Ne(v1));
   EXPECT_THAT(v_accumulated, AlmostEquals(v1, 14));
+}
+
+TEST_F(AlmostEqualsTest, Bivector) {
+  Bivector<Dimensionless, World> const v1({4, -5, 6});
+  Bivector<Dimensionless, World> const v2 = v1;
+  EXPECT_THAT(v2, AlmostEquals(v1));
+  EXPECT_THAT(2 * v2, Not(AlmostEquals(v1)));
+  Bivector<Dimensionless, World> const δv = v1 / 100;
+  Bivector<Dimensionless, World> v_accumulated;
+  for (int i = 1; i <= 100; ++i) {
+    v_accumulated += δv;
+  }
+  EXPECT_THAT(v_accumulated, Ne(v1));
+  EXPECT_THAT(v_accumulated, AlmostEquals(v1, 11));
+}
+
+TEST_F(AlmostEqualsTest, Trivector) {
+  Trivector<MagneticFlux, World> const v1(2 * Maxwell);
+  Trivector<MagneticFlux, World> const v2 = v1;
+  EXPECT_THAT(v2, AlmostEquals(v1));
+  EXPECT_THAT(2 * v2, Not(AlmostEquals(v1)));
+  Trivector<MagneticFlux, World> const δv = v1 / 100;
+  Trivector<MagneticFlux, World> v_accumulated;
+  for (int i = 1; i <= 100; ++i) {
+    v_accumulated += δv;
+  }
+  EXPECT_THAT(v_accumulated, Ne(v1));
+  EXPECT_THAT(v_accumulated, AlmostEquals(v1, 9));
 }
 
 }  // namespace testing_utilities
