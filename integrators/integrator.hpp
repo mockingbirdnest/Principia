baa2d1b06958b90816c58fac8db4dc0c0b27d93b
pleroy <phl+personal@google.com>
2014-05-24 00:39:03 +0200

    Interface

---
+++ integrators/integrator.hpp
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include <vector>
+
+class Integrator {
+ public:
+  Integrator();
+  virtual ~Integrator();
+
+  // TODO(phl): I'd prefer this API to be typed with the untyping done
+  // internally.
+
+  typedef void (*AutonomousRightHandSideComputation)(
+                     std::vector<double> const& y,
+                     std::vector<double>* result);
+  typedef void (*RightHandSideComputation)(double const t,
+                                           std::vector<double> const& y,
+                                           std::vector<double>* result);
+
+  // TODO(phl): Rework the struct names, maybe promote them to classes.
+  struct Parameters {
+    std::vector<double> q0;
+    std::vector<double> p0;
+    std::vector<double> q_error;
+    std::vector<double> p_error;
+    double t0;
+    double tmax;
+    double Δt;
+    double t_error;
+    std::vector<std::vector<double>> coefficients;
+    int sampling_period;
+  };
+
+  struct QuantitiesAndError {
+    std::vector<double> quantities;
+    double error;
+  };
+
+  struct Solution {
+    std::vector<QuantitiesAndError> momentum;
+    std::vector<QuantitiesAndError> position;
+    QuantitiesAndError time;
+  };
+
+  virtual Solution Increment(
+      RightHandSideComputation const compute_force,
+      AutonomousRightHandSideComputation const compute_velocity,
+      Parameters const& parameters) = 0;
+
+};
