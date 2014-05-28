0f522d855076669a53088f8ef02df3c0d85f319d
Robin Leroy <egg.robin.leroy@gmail.com>
2014-05-28 17:08:34 +0200

    1 != 0 in a field

--- testing_utilities/algebra_test.cpp
+++ testing_utilities/algebra_test.cpp
@@ -11,7 +11,7 @@ class AlgebraTest : public testing::Test {};
 
 TEST_F(AlgebraTest, Group) {
   TestGroup(0, 42, -3, 2, Plus<int, int, int>, Minus<int, int>, 0);
-  TestGroup<double>(0.0, 42.0, -3.0, 2.0, Times<double, double, double>,
+  TestGroup<double>(1.0, 42.0, -3.0, 2.0, Times<double, double, double>,
                     [](double const& x) { return 1 / x; }, 0);
 }
 
