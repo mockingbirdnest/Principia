292fdfd7bc1a0d79f20a2bec39cb81481df35229
Robin Leroy <egg.robin.leroy@gmail.com>
2014-05-28 18:35:54 +0200

    Remove obsolete C#

--- CTSGeometryTests/stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Geometry Tests.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.hpp"
