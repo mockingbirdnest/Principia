02b27c90798879b99cb40c5133df92b80485111b
Robin Leroy <egg.robin.leroy@gmail.com>
2014-05-29 17:40:31 +0200

    Use the stack

--- integrators/symplectic_partitioned_runge_kutta_integrator_test.cpp
+++ integrators/symplectic_partitioned_runge_kutta_integrator_test.cpp
@@ -33,47 +33,42 @@ inline void compute_harmonice_oscillator_velocity(std::vector<double> const& p,
 
 class SPRKTest : public testing::Test {
  protected:
-  void SetUp() override {
-    integrator_.reset(new SPRKIntegrator);
-    parameters_.reset(new SPRKIntegrator::Parameters);
-    solution_.reset(new SPRKIntegrator::Solution);
-  }
+  void SetUp() override {}
 
-  std::unique_ptr<SPRKIntegrator> integrator_;
-  std::unique_ptr<SPRKIntegrator::Parameters> parameters_;
-  std::unique_ptr<SPRKIntegrator::Solution> solution_;
+  SPRKIntegrator             integrator_;
+  SPRKIntegrator::Parameters parameters_;
+  SPRKIntegrator::Solution   solution_;
 };
 
 TEST_F(SPRKTest, HarmonicOscillator) {
-  parameters_->q0 = {1.0};
-  parameters_->p0 = {0.0};
-  parameters_->t0 = 0.0;
+  parameters_.q0 = {1.0};
+  parameters_.p0 = {0.0};
+  parameters_.t0 = 0.0;
 #ifdef _DEBUG
-  parameters_->tmax = 100.0;
+  parameters_.tmax = 100.0;
 #else
-  parameters_->tmax = 1000.0;
+  parameters_.tmax = 1000.0;
 #endif
-  parameters_->Δt = 1.0E-4;
-  parameters_->coefficients = integrator_->Order5Optimal();
-  parameters_->sampling_period = 1;
-  integrator_->Solve(&compute_harmonic_oscillator_force,
-                         &compute_harmonice_oscillator_velocity,
-                         *parameters_,
-                         solution_.get());
+  parameters_.Δt = 1.0E-4;
+  parameters_.coefficients = integrator_.Order5Optimal();
+  parameters_.sampling_period = 1;
+  integrator_.Solve(&compute_harmonic_oscillator_force,
+                    &compute_harmonice_oscillator_velocity,
+                    parameters_, &solution_);
   double q_error = 0;
   double p_error = 0;
-  for (size_t i = 0; i < solution_->time.quantities.size(); ++i) {
+  for (size_t i = 0; i < solution_.time.quantities.size(); ++i) {
     q_error = std::max(q_error,
-                       std::abs(solution_->position[0].quantities[i] -
-                                std::cos(solution_->time.quantities[i])));
+                       std::abs(solution_.position[0].quantities[i] -
+                                std::cos(solution_.time.quantities[i])));
     p_error = std::max(p_error,
-                       std::abs(solution_->momentum[0].quantities[i] +
-                                std::sin(solution_->time.quantities[i])));
+                       std::abs(solution_.momentum[0].quantities[i] +
+                                std::sin(solution_.time.quantities[i])));
   }
   LOG(ERROR) << "q_error = " << q_error;
   LOG(ERROR) << "p_error = " << p_error;
-  EXPECT_THAT(AbsoluteError(0, q_error), Lt(2E-16 * parameters_->tmax));
-  EXPECT_THAT(AbsoluteError(0, p_error), Lt(2E-16 * parameters_->tmax));
+  EXPECT_THAT(AbsoluteError(0, q_error), Lt(2E-16 * parameters_.tmax));
+  EXPECT_THAT(AbsoluteError(0, p_error), Lt(2E-16 * parameters_.tmax));
 }
 
 }  // namespace integrators
