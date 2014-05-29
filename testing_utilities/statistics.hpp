2841fe78fd47bea0cf2cd857f5755c2251c833c1
Robin Leroy <egg.robin.leroy@gmail.com>
2014-05-29 20:05:27 +0200

    Stats spec.

---
+++ testing_utilities/statistics.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+
+#include "quantities/quantities.hpp"
+
+namespace principia {
+namespace testing_utilities {
+
+template<typename T>
+T Mean(std::vector<T> x);
+
+template<typename T>
+quantities::Product<T, T> Variance(std::vector<T> x);
+
+template<typename T>
+quantities::Product<T, U> Covariance(std::vector<T> x, std::vector<U> y);
+
+template<typename T>
+T StandardDeviation(std::vector<T> x);
+
+template<typename T, typename U>
+Dimensionless PearsonProductMomentCorrelation(std::vector<T> x,
+                                              std::vector<U> y);
+
+}  // testing_utilities
+}  // principia
