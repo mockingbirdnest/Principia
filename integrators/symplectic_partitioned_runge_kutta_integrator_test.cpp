34145dd3eb9356aba9d1c66e6387f15921b4a682
Robin Leroy <egg.robin.leroy@gmail.com>
2014-05-30 15:20:40 +0200

    test stuff

--- integrators/symplectic_partitioned_runge_kutta_integrator_test.cpp
+++ integrators/symplectic_partitioned_runge_kutta_integrator_test.cpp
@@ -10,15 +10,23 @@
 #include "glog/logging.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "quantities/dimensionless.hpp"
 #include "testing_utilities/numerics.hpp"
+#include "testing_utilities/statistics.hpp"
 
 using principia::testing_utilities::AbsoluteError;
+using testing::AllOf;
+using testing::Gt;
 using testing::Lt;
+using principia::testing_utilities::Slope;
+using principia::testing_utilities::PearsonProductMomentCorrelationCoefficient;
 
 namespace principia {
 namespace integrators {
 
 namespace {
+
+using quantities::Dimensionless;
 
 inline void compute_harmonic_oscillator_force(double const t,
                                               std::vector<double> const& q,
@@ -74,15 +82,19 @@ TEST_F(SPRKTest, HarmonicOscillator) {
 }
 
 TEST_F(SPRKTest, Convergence) {
+  parameters_.q0 = {1.0};
+  parameters_.p0 = {0.0};
+  parameters_.t0 = 0.0;
   parameters_.tmax = 100;
   parameters_.coefficients = integrator_.Order5Optimal();
   parameters_.sampling_period = 0;
-  int const step_sizes = 5;
-  std::vector<double> log_step_sizes(step_sizes);
-  std::vector<double> log_q_errors(step_sizes);
-  std::vector<double> log_p_errors(step_sizes);
-  parameters_.Δt = 1;
-  for(int i = 0; i < step_sizes; ++i, parameters_.Δt /= 10) {
+  int const step_sizes = 50;
+  std::vector<Dimensionless> log_step_sizes(step_sizes);
+  std::vector<Dimensionless> log_q_errors(step_sizes);
+  std::vector<Dimensionless> log_p_errors(step_sizes);
+  parameters_.Δt = 3;
+  for(int i = 0; i < step_sizes; ++i, parameters_.Δt /= 1.1) {
+    solution_ = SPRKIntegrator::Solution();
     integrator_.Solve(&compute_harmonic_oscillator_force,
                       &compute_harmonice_oscillator_velocity,
                       parameters_, &solution_);
@@ -91,10 +103,51 @@ TEST_F(SPRKTest, Convergence) {
         std::abs(solution_.position[0].quantities[0] -
                  std::cos(solution_.time.quantities[0])));
     log_p_errors[i] = std::log(
-        std::abs(solution_.position[0].quantities[0] -
+        std::abs(solution_.momentum[0].quantities[0] +
                  std::sin(solution_.time.quantities[0])));
   }
-
+  std::string mathematica_out = "{";
+  for (int i = 0; i < step_sizes; ++i) {
+    mathematica_out += "{";
+    mathematica_out += ToString(log_step_sizes[i]);
+    mathematica_out += ",";
+    mathematica_out += ToString(log_q_errors[i]);
+    mathematica_out += "}";
+    if (i + 1 < step_sizes) {
+      mathematica_out += ",\n ";
+    }
+  }
+  mathematica_out += "}";
+  FLAGS_log_prefix = false;
+  google::LogToStderr();
+  LOG(INFO) << "Convergence data for q:\n" << mathematica_out;
+  Dimensionless const q_convergence_order = Slope(log_step_sizes, log_q_errors);
+  Dimensionless const q_correlation =
+      PearsonProductMomentCorrelationCoefficient(log_step_sizes, log_q_errors);
+  LOG(INFO) << "Convergence order in q : " << q_convergence_order;
+  LOG(INFO) << "Correlation            : " << q_correlation;
+  EXPECT_THAT(q_convergence_order, AllOf(Gt(4.5), Lt(6)));
+  EXPECT_THAT(q_correlation, AllOf(Gt(0.9), Lt(1.1)));
+  mathematica_out = "{";
+  for (int i = 0; i < step_sizes; ++i) {
+    mathematica_out += "{";
+    mathematica_out += ToString(log_step_sizes[i]);
+    mathematica_out += ",";
+    mathematica_out += ToString(log_p_errors[i]);
+    mathematica_out += "}";
+    if (i + 1 < step_sizes) {
+      mathematica_out += ",\n ";
+    }
+  }
+  mathematica_out += "}";
+  LOG(INFO) << "Convergence data for p:\n" << mathematica_out;
+  Dimensionless const p_convergence_order = Slope(log_step_sizes, log_p_errors);
+  Dimensionless const p_correlation =
+      PearsonProductMomentCorrelationCoefficient(log_step_sizes, log_p_errors);
+  LOG(INFO) << "Convergence order in p : " << p_convergence_order;
+  LOG(INFO) << "Correlation            : " << p_correlation;
+  EXPECT_THAT(p_convergence_order, AllOf(Gt(4.5), Lt(6)));
+  EXPECT_THAT(p_correlation, AllOf(Gt(0.9), Lt(1.1)));
 }
 
 }  // namespace integrators
