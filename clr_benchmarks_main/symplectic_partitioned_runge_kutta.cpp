b320d30673bebb8e1523cb3261015f205795d786
Robin Leroy <egg.robin.leroy@gmail.com>
2014-05-31 02:41:37 +0200

    Yog-Sothoth knows the gate. Yog-Sothoth is the gate. Yog-Sothoth is the key and guardian of the gate.

--- clr_benchmarks_main/symplectic_partitioned_runge_kutta.cpp
+++ clr_benchmarks_main/symplectic_partitioned_runge_kutta.cpp
@@ -1,10 +1,9 @@
 #define GLOG_NO_ABBREVIATED_SEVERITIES
 
-#include "clr_benchmarks/symplectic_partitioned_runge_kutta_integrator.hpp"
 // Must come last to avoid conflicts when defining the CHECK macros.
 #include "benchmark/benchmark.h"
 
-using principia::clr_benchmarks::SPRKIntegratorCLRBenchmark;
+// using principia::clr_benchmarks::SPRKIntegratorCLRBenchmark;
 
 namespace principia {
 namespace benchmarks {
@@ -12,7 +11,7 @@ namespace benchmarks {
 static void BM_SolveHarmonicOscillator(
     benchmark::State& state) {  // NOLINT(runtime/references)
   while (state.KeepRunning()) {
-    SPRKIntegratorCLRBenchmark::SolveHarmonicOscillator();
+    principia::clr_benchmarks::SPRKIntegratorCLRBenchmark::SolveHarmonicOscillator();
   }
 }
 
