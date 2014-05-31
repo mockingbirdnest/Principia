e8af0900c808a6bb132315d8504623ca656f9b68
Robin Leroy <egg.robin.leroy@gmail.com>
2014-05-31 14:02:22 +0200

    H'EE-L'GEB

---
+++ clr_benchmarks_adapter/symplectic_partitioned_runge_kutta_integrator.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace principia {
+namespace clr_benchmarks {
+
+public ref class SPRKIntegratorCLRBenchmark abstract sealed {
+ public:
+  static void SolveHarmonicOscillator();
+};
+
+}  // namespace clr_benchmarks
+}  // namespace principia
