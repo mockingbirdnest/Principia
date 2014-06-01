28b18f93ff81adb36d103cca33fa88d620adf8e0
pleroy <phl+personal@google.com>
2014-06-01 20:23:50 +0200

    Benchmark results.

--- benchmarks/symplectic_partitioned_runge_kutta_integrator.cpp
+++ benchmarks/symplectic_partitioned_runge_kutta_integrator.cpp
@@ -1,20 +1,20 @@
-﻿#include "benchmarks/symplectic_partitioned_runge_kutta_integrator.hpp"
-
-// .\Release\benchmarks.exe --benchmark_repetitions=5 --benchmark_min_time=300                                                    // NOLINT(whitespace/line_length)
+﻿
+// .\Release\benchmarks.exe --benchmark_repetitions=5 --benchmark_min_time=30                                                     // NOLINT(whitespace/line_length)
 // Benchmarking on 1 X 3310 MHz CPU
-// 2014/06/01-10:56:16
+// 2014/06/01-20:15:33
 // Benchmark                           Time(ns)    CPU(ns) Iterations
 // ------------------------------------------------------------------
-// BM_SolveHarmonicOscillator        1158462428 1157407419         52                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
-// BM_SolveHarmonicOscillator        1164918290 1163707460         52                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
-// BM_SolveHarmonicOscillator        1161977190 1163107456         52                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
-// BM_SolveHarmonicOscillator        1161909218 1160707440         52                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
-// BM_SolveHarmonicOscillator        1163742772 1162207450         52                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
-// BM_SolveHarmonicOscillator_mean   1162201980 1161427445        260                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
-// BM_SolveHarmonicOscillator_stddev    2185080    2249814        260                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator        1127986726 1131007250          6                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator        1134211041 1138807300          6                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator        1130702115 1131007250          6                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator        1130180137 1131007250          6                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator        1134746807 1131007250          6                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator_mean   1131565365 1132567260         30                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator_stddev    2553111    3120020         30                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
+
+#include "benchmarks/symplectic_partitioned_runge_kutta_integrator.hpp"
 
 #define GLOG_NO_ABBREVIATED_SEVERITIES
-
 #include <algorithm>
 
 // Must come last to avoid conflicts when defining the CHECK macros.
--- clr_benchmarks/symplectic_partitioned_runge_kutta.cpp
+++ clr_benchmarks/symplectic_partitioned_runge_kutta.cpp
@@ -1,4 +1,17 @@
 
+// .\Release\clr_benchmarks.exe --benchmark_repetitions=5 --benchmark_min_time=30  // NOLINT(whitespace/line_length)
+// Benchmarking on 1 X 3310 MHz CPU
+// 2014/06/01-20:19:58
+// Benchmark                               Time(ns)    CPU(ns) Iterations
+// ----------------------------------------------------------------------
+// BM_CLR_SolveHarmonicOscillator        2877869106 2880818467          3
+// BM_CLR_SolveHarmonicOscillator        2868951775 2865218367          3
+// BM_CLR_SolveHarmonicOscillator        2871617294 2875618433          3
+// BM_CLR_SolveHarmonicOscillator        2872283515 2870418400          3
+// BM_CLR_SolveHarmonicOscillator        2864615815 2865218367          3
+// BM_CLR_SolveHarmonicOscillator_mean   2871067501 2871458407         15
+// BM_CLR_SolveHarmonicOscillator_stddev    4339202    6064229         15
+
 // Must come last to avoid conflicts when defining the CHECK macros.
 #include "benchmark/benchmark.h"
 
