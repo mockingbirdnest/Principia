e3b6a153ea7fbda914a3d3c12a98eb8a7373a203
Robin Leroy <egg.robin.leroy@gmail.com>
2014-06-03 20:24:36 +0200

    After pleroy's 2nd review.

--- benchmarks/quantities_body.hpp
+++ benchmarks/quantities_body.hpp
@@ -16,7 +16,11 @@ namespace benchmarks {
 
 #define TRIGGER_DEAD_CODE_ELIMINATION
 
-std::size_t const dimension = 100;
+namespace {
+
+std::size_t const kDimension = 100;
+
+}  // namespace
 
 inline void DimensionfulDiscreteCosineTransform(
     std::vector<quantities::Momentum>* result) {
@@ -24,44 +28,44 @@ inline void DimensionfulDiscreteCosineTransform(
   using quantities::Dimensionless;
   using quantities::Momentum;
   using si::Radian;
-  std::vector<Momentum> input(dimension);
-  for (std::size_t i = 0; i < dimension; ++i) {
+  std::vector<Momentum> input(kDimension);
+  for (std::size_t i = 0; i < kDimension; ++i) {
     input[i] = i * Momentum::SIUnit();
   }
-  result->resize(dimension);
+  result->resize(kDimension);
   Dimensionless sign = 1;
   Momentum sum;
-  for (std::size_t k = 0; k < dimension; ++k, sign *= -1) {
+  for (std::size_t k = 0; k < kDimension; ++k, sign *= -1) {
     sum = Momentum();
-    for (std::size_t n = 1; n < dimension - 1; ++n) {
-      sum += input[n] * quantities::Cos(π * Radian / (dimension - 1) * n * k);
+    for (std::size_t n = 1; n < kDimension - 1; ++n) {
+      sum += input[n] * quantities::Cos(π * Radian / (kDimension - 1) * n * k);
     }
 #ifdef TRIGGER_DEAD_CODE_ELIMINATION
-    (*result)[k] = 0.5 * (input[0] + sign * input[dimension - 1]);
+    (*result)[k] = 0.5 * (input[0] + sign * input[kDimension - 1]);
 #else
-    (*result)[k] = 0.5 * (input[0] + sign * input[dimension - 1]) + sum;
+    (*result)[k] = 0.5 * (input[0] + sign * input[kDimension - 1]) + sum;
 #endif
   }
 }
 
 inline void DoubleDiscreteCosineTransform(
     std::vector<double>* result) {
-  std::vector<double> input(dimension);
-  for (std::size_t i = 0; i < dimension; ++i) {
+  std::vector<double> input(kDimension);
+  for (std::size_t i = 0; i < kDimension; ++i) {
     input[i] = i;
   }
-  result->resize(dimension);
+  result->resize(kDimension);
   double sign = 1;
   double sum;
-  for (std::size_t k = 0; k < dimension; ++k, sign *= -1) {
+  for (std::size_t k = 0; k < kDimension; ++k, sign *= -1) {
     sum = 0;
-    for (std::size_t n = 1; n < dimension - 1; ++n) {
-      sum += input[n] * std::cos(M_PI / (dimension - 1) * n * k);
+    for (std::size_t n = 1; n < kDimension - 1; ++n) {
+      sum += input[n] * std::cos(M_PI / (kDimension - 1) * n * k);
     }
 #ifdef TRIGGER_DEAD_CODE_ELIMINATION
-    (*result)[k] = 0.5 * (input[0] + sign * input[dimension - 1]);
+    (*result)[k] = 0.5 * (input[0] + sign * input[kDimension - 1]);
 #else
-    (*result)[k] = 0.5 * (input[0] + sign * input[dimension - 1]) + sum;
+    (*result)[k] = 0.5 * (input[0] + sign * input[kDimension - 1]) + sum;
 #endif
   }
 }
