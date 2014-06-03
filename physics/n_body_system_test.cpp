37c8cca94dc7a3f3759b6d27e74bb0a91b28d2c1
pleroy <phl+personal@google.com>
2014-06-03 22:35:48 +0200

    Trying to understand the 2-body problem.

--- physics/n_body_system_test.cpp
+++ physics/n_body_system_test.cpp
@@ -1,14 +1,20 @@
-#include "physics/n_body_system.hpp"
+﻿#include "physics/n_body_system.hpp"
 
 #include <memory>
 #include <string>
 
 #include "geometry/grassmann.hpp"
+#include "geometry/point.hpp"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "physics/body.hpp"
 #include "physics/frame.hpp"
+#include "quantities/constants.hpp"
+#include "quantities/numbers.hpp"
 
+using principia::constants::GravitationalConstant;
+using principia::geometry::Barycentre;
+using principia::geometry::Point;
 using principia::geometry::Vector;
 
 namespace principia {
@@ -22,21 +28,50 @@ class NBodySystemTest : public testing::Test {
     // The Earth-Moon system, roughly.
     body1_ = new Body<InertialFrame>(6E24 * Mass::SIUnit());
     body2_ = new Body<InertialFrame>(7E22 * Mass::SIUnit());
-    Vector<Length, InertialFrame> q1({0 * Length::SIUnit(),
-                                      0 * Length::SIUnit(),
-                                      0 * Length::SIUnit()});
-    Vector<Length, InertialFrame> q2({0 * Length::SIUnit(),
-                                      4E8 * Length::SIUnit(),
-                                      0 * Length::SIUnit()});
-    Vector<Speed, InertialFrame> p1({0 * Speed::SIUnit(),
-                                     0 * Speed::SIUnit(),
-                                     0 * Speed::SIUnit()});
-    //TODO(phl): Despite the name, this wants a speed...
-    Vector<Speed, InertialFrame> p2({1E3 * Speed::SIUnit(),
-                                     0 * Speed::SIUnit(),
-                                     0 * Speed::SIUnit()});
-    body1_->AppendToTrajectory({q1}, {p1}, {0 * Time::SIUnit()});
-    body2_->AppendToTrajectory({q2}, {p2}, {0 * Time::SIUnit()});
+    Point<Vector<Length, InertialFrame>> const
+        q1(Vector<Length, InertialFrame>({0 * Length::SIUnit(),
+                                          0 * Length::SIUnit(),
+                                          0 * Length::SIUnit()}));
+    Point<Vector<Length, InertialFrame>> const
+        q2(Vector<Length, InertialFrame>({0 * Length::SIUnit(),
+                                          4E8 * Length::SIUnit(),
+                                          0 * Length::SIUnit()}));
+    Point<Vector<Length, InertialFrame>> const centre_of_mass =
+        Barycentre(q1, body1_->mass(), q2, body2_->mass());
+    Point<Vector<Speed, InertialFrame>> const
+        v1(Vector<Speed, InertialFrame>({0 * Speed::SIUnit(),
+                                         0 * Speed::SIUnit(),
+                                         0 * Speed::SIUnit()}));
+    // 1E3 m/s puts us at the apogee at the beginning, because the speed for a
+    // circular orbit with this separation would be 1006.36 m/s.
+    Point<Vector<Speed, InertialFrame>> const
+        v2(Vector<Speed, InertialFrame>({1E3 * Speed::SIUnit(),
+                                         0 * Speed::SIUnit(),
+                                         0 * Speed::SIUnit()}));
+    Point<Vector<Speed, InertialFrame>> const overall_velocity =
+        Barycentre(v1, body1_->mass(), v2, body2_->mass());
+    body1_->AppendToTrajectory({q1 - centre_of_mass},
+                               {v1 - overall_velocity},
+                               {0 * Time::SIUnit()});
+    body2_->AppendToTrajectory({q2 - centre_of_mass},
+                               {v2 - overall_velocity},
+                               {0 * Time::SIUnit()});
+    Length const r2 = (q2 - centre_of_mass).Norm();
+    Length const semi_major_axis =
+        1 / (2 / r2 -
+                 (v2 - overall_velocity).Norm().Pow<2>() /
+                      (body1_->gravitational_parameter() +
+                       body2_->gravitational_parameter()));
+    Length const r1 = (q1 - centre_of_mass).Norm();
+    Length const semi_major_axis1 =
+        1 / (2 / r1 -
+                 (v1 - overall_velocity).Norm().Pow<2>() /
+                      (body1_->gravitational_parameter() +
+                       body2_->gravitational_parameter()));
+    LOG(ERROR)<<semi_major_axis<<" "<<semi_major_axis1;
+    period_ = 2 * π * Sqrt(
+        (semi_major_axis1 + semi_major_axis).Pow<3>() /
+            (GravitationalConstant * (body1_->mass() + body2_->mass())));
     system_.reset(new NBodySystem(
         new std::vector<Body<InertialFrame>*>({body1_, body2_})));
   }
@@ -76,6 +111,7 @@ class NBodySystemTest : public testing::Test {
   Body<InertialFrame>* body1_;
   Body<InertialFrame>* body2_;
   SPRKIntegrator integrator_;
+  Time period_;
   std::unique_ptr<NBodySystem> system_;
 };
 
@@ -83,7 +119,8 @@ TEST_F(NBodySystemTest, T) {
   std::vector<Vector<Length, InertialFrame>> positions;
   std::vector<Vector<Speed, InertialFrame>> momenta;
   std::vector<Time> times;
-  system_->Integrate(integrator_, 3E6 * Time::SIUnit(), 3E4 * Time::SIUnit(), 1);
+  LOG(ERROR)<<period_;
+  system_->Integrate(integrator_, 1.1*period_, period_ / 1000, 1);
   body1_->GetTrajectory(&positions, &momenta, &times);
   LOG(ERROR) << ToMathematicaString(positions);
   body2_->GetTrajectory(&positions, &momenta, &times);
