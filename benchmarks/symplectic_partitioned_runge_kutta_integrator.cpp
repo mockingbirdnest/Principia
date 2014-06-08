578356e823886a02ac4c5c0d52dba67a8f799f2a
pleroy <phl+personal@google.com>
2014-06-09 00:25:19 +0200

    Insert benchmark numbers.

--- benchmarks/symplectic_partitioned_runge_kutta_integrator.cpp
+++ benchmarks/symplectic_partitioned_runge_kutta_integrator.cpp
@@ -1,16 +1,16 @@
 ﻿
-// .\Release\benchmarks.exe --benchmark_repetitions=5 --benchmark_min_time=30                                                     // NOLINT(whitespace/line_length)
+// .\Release\benchmarks.exe --benchmark_repetitions=5 --benchmark_min_time=30 --benchmark_filter=HarmonicOscillator                                                 // NOLINT(whitespace/line_length)
 // Benchmarking on 1 X 3310 MHz CPU
-// 2014/06/01-20:15:33
+// 2014/06/09-00:22:28
 // Benchmark                           Time(ns)    CPU(ns) Iterations
 // ------------------------------------------------------------------
-// BM_SolveHarmonicOscillator        1127986726 1131007250          6                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
-// BM_SolveHarmonicOscillator        1134211041 1138807300          6                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
-// BM_SolveHarmonicOscillator        1130702115 1131007250          6                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
-// BM_SolveHarmonicOscillator        1130180137 1131007250          6                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
-// BM_SolveHarmonicOscillator        1134746807 1131007250          6                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
-// BM_SolveHarmonicOscillator_mean   1131565365 1132567260         30                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
-// BM_SolveHarmonicOscillator_stddev    2553111    3120020         30                                 1.37019e-013, 1.37057e-013  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator        1257378650 1260488080          5                                 1.3701886847350409e-013 m, 1.3705703238997557e-013 m kg s^-1  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator        1257609773 1260488080          5                                 1.3701886847350409e-013 m, 1.3705703238997557e-013 m kg s^-1  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator        1260331750 1263608100          5                                 1.3701886847350409e-013 m, 1.3705703238997557e-013 m kg s^-1  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator        1264436937 1263608100          5                                 1.3701886847350409e-013 m, 1.3705703238997557e-013 m kg s^-1  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator        1265400577 1266728120          5                                 1.3701886847350409e-013 m, 1.3705703238997557e-013 m kg s^-1  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator_mean   1261031538 1262984096          5                                 1.3701886847350409e-013 m, 1.3705703238997557e-013 m kg s^-1  // NOLINT(whitespace/line_length)
+// BM_SolveHarmonicOscillator_stddev    3353416    2334809          0                                 1.3701886847350409e-013 m, 1.3705703238997557e-013 m kg s^-1  // NOLINT(whitespace/line_length)
 
 #include "benchmarks/symplectic_partitioned_runge_kutta_integrator.hpp"
 
