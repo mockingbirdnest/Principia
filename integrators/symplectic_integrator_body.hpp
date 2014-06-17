d9eecff23bb0ee3d647d00a540fbf40557c896ef
pleroy <phl+personal@google.com>
2014-06-17 21:45:48 +0200

    Lint.

--- integrators/symplectic_integrator_body.hpp
+++ integrators/symplectic_integrator_body.hpp
@@ -3,8 +3,9 @@ namespace integrators {
 
 template<typename Position, typename Momentum>
 template<typename Scalar>
-inline SymplecticIntegrator<Position, Momentum>::ValueAndError<Scalar>::ValueAndError(
-    Scalar const& value) : value(value) {}
+inline SymplecticIntegrator<Position, Momentum>::
+ValueAndError<Scalar>::ValueAndError(Scalar const& value)
+    : value(value) {}
 
 }  // namespace integrators
 }  // namespace principia
