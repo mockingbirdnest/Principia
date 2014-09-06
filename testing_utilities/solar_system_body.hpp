9ad40874b6dd779e5008e8f9ec32b72cd81aed95
pleroy <phl+personal@google.com>
2014-09-06 23:29:14 +0200

    All good.

--- testing_utilities/solar_system_body.hpp
+++ testing_utilities/solar_system_body.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "geometry/grassmann.hpp"
+#include "geometry/point.hpp"
 #include "physics/body.hpp"
 #include "physics/n_body_system.hpp"
 #include "physics/trajectory.hpp"
@@ -12,6 +13,7 @@
 #include "quantities/quantities.hpp"
 #include "quantities/si.hpp"
 
+using principia::geometry::Point;
 using principia::geometry::Vector;
 using principia::physics::Body;
 using principia::physics::NBodySystem;
@@ -67,12 +69,14 @@ std::unique_ptr<SolarSystem> SolarSystem::AtСпутникLaunch() {
       new Trajectory<ICRFJ2000EclipticFrame>(sun));
   sun_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({ 1.138350928138014E+06 * Kilo(Metre),
-                  6.177753685036716E+05 * Kilo(Metre),
-                 -3.770941657504326E+04 * Kilo(Metre)}),
-       Velocity({-5.067456621846211E-03 * Kilo(Metre) / Second,
-                  1.259599196445122E-02 * Kilo(Metre) / Second,
-                  9.778588606052481E-05 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({ 1.138350928138014E+06 * Kilo(Metre),
+                      6.177753685036716E+05 * Kilo(Metre),
+                     -3.770941657504326E+04 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({-5.067456621846211E-03 * Kilo(Metre) / Second,
+                      1.259599196445122E-02 * Kilo(Metre) / Second,
+                      9.778588606052481E-05 * Kilo(Metre) / Second}))});
 
   // Planets.
 
@@ -81,84 +85,100 @@ std::unique_ptr<SolarSystem> SolarSystem::AtСпутникLaunch() {
       new Trajectory<ICRFJ2000EclipticFrame>(jupiter));
   jupiter_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-7.950209667306620E+08 * Kilo(Metre),
+      {Point<Position>(
+           Position({-7.950209667306620E+08 * Kilo(Metre),
                  -1.784285526424396E+08 * Kilo(Metre),
-                  1.853825132237791E+07 * Kilo(Metre)}),
-       Velocity({ 2.709330231918198E+00 * Kilo(Metre) / Second,
+                  1.853825132237791E+07 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({ 2.709330231918198E+00 * Kilo(Metre) / Second,
                  -1.213073724288562E+01 * Kilo(Metre) / Second,
-                 -1.088748435062713E-02 * Kilo(Metre) / Second})});
+                 -1.088748435062713E-02 * Kilo(Metre) / Second}))});
   Trajectory<ICRFJ2000EclipticFrame>* saturn_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(saturn));
   saturn_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-3.774715321901159E+08 * Kilo(Metre),
-                 -1.451892263379818E+09 * Kilo(Metre),
-                  4.040621083792380E+07 * Kilo(Metre)}),
-       Velocity({ 8.817029873536633E+00 * Kilo(Metre) / Second,
-                 -2.466058486223613E+00 * Kilo(Metre) / Second,
-                 -3.068419809533604E-01 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({-3.774715321901159E+08 * Kilo(Metre),
+                     -1.451892263379818E+09 * Kilo(Metre),
+                     4.040621083792380E+07 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({ 8.817029873536633E+00 * Kilo(Metre) / Second,
+                     -2.466058486223613E+00 * Kilo(Metre) / Second,
+                     -3.068419809533604E-01 * Kilo(Metre) / Second}))});
   Trajectory<ICRFJ2000EclipticFrame>* neptune_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(neptune));
   neptune_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-3.810689792831146E+09 * Kilo(Metre),
-                 -2.456423858579051E+09 * Kilo(Metre),
-                  1.383694320077938E+08 * Kilo(Metre)}),
-       Velocity({ 2.913267720085410E+00 * Kilo(Metre) / Second,
-                 -4.535247383721019E+00 * Kilo(Metre) / Second,
-                  2.589759251085161E-02 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({-3.810689792831146E+09 * Kilo(Metre),
+                    -2.456423858579051E+09 * Kilo(Metre),
+                      1.383694320077938E+08 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({ 2.913267720085410E+00 * Kilo(Metre) / Second,
+                     -4.535247383721019E+00 * Kilo(Metre) / Second,
+                      2.589759251085161E-02 * Kilo(Metre) / Second}))});
   Trajectory<ICRFJ2000EclipticFrame>* uranus_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(uranus));
   uranus_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-1.729995609344851E+09 * Kilo(Metre),
-                  2.159967050539728E+09 * Kilo(Metre),
-                  3.048735047038063E+07 * Kilo(Metre)}),
-       Velocity({-5.366539669972795E+00 * Kilo(Metre) / Second,
-                 -4.575802196749351E+00 * Kilo(Metre) / Second,
-                  5.261322980347850E-02 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({-1.729995609344851E+09 * Kilo(Metre),
+                      2.159967050539728E+09 * Kilo(Metre),
+                      3.048735047038063E+07 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({-5.366539669972795E+00 * Kilo(Metre) / Second,
+                     -4.575802196749351E+00 * Kilo(Metre) / Second,
+                      5.261322980347850E-02 * Kilo(Metre) / Second}))});
 
   // Telluric planets.
   Trajectory<ICRFJ2000EclipticFrame>* earth_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(earth));
   earth_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({ 1.475150112055673E+08 * Kilo(Metre),
-                  3.144435102288270E+07 * Kilo(Metre),
-                 -3.391764309344300E+04 * Kilo(Metre)}),
-       Velocity({-6.635753510543799E+00 * Kilo(Metre) / Second,
-                  2.904321639216012E+01 * Kilo(Metre) / Second,
-                  3.125252418990812E-03 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({ 1.475150112055673E+08 * Kilo(Metre),
+                      3.144435102288270E+07 * Kilo(Metre),
+                     -3.391764309344300E+04 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({-6.635753510543799E+00 * Kilo(Metre) / Second,
+                      2.904321639216012E+01 * Kilo(Metre) / Second,
+                      3.125252418990812E-03 * Kilo(Metre) / Second}))});
   Trajectory<ICRFJ2000EclipticFrame>* venus_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(venus));
   venus_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({ 6.084974577091119E+07 * Kilo(Metre),
-                 -9.037413730207849E+07 * Kilo(Metre),
-                 -4.719158908401959E+06 * Kilo(Metre)}),
-       Velocity({ 2.903958257174759E+01 * Kilo(Metre) / Second,
-                  1.910383147602264E+01 * Kilo(Metre) / Second,
-                 -1.418780340302349E+00 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({ 6.084974577091119E+07 * Kilo(Metre),
+                     -9.037413730207849E+07 * Kilo(Metre),
+                     -4.719158908401959E+06 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({ 2.903958257174759E+01 * Kilo(Metre) / Second,
+                      1.910383147602264E+01 * Kilo(Metre) / Second,
+                     -1.418780340302349E+00 * Kilo(Metre) / Second}))});
   Trajectory<ICRFJ2000EclipticFrame>* mars_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(mars));
   mars_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-2.440047184660406E+08 * Kilo(Metre),
-                 -2.002994580992744E+07 * Kilo(Metre),
-                  5.577600092368793E+06 * Kilo(Metre)}),
-       Velocity({ 2.940381268511949E+00 * Kilo(Metre) / Second,
-                 -2.206625841382794E+01 * Kilo(Metre) / Second,
-                 -5.348179460834037E-01 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({-2.440047184660406E+08 * Kilo(Metre),
+                     -2.002994580992744E+07 * Kilo(Metre),
+                      5.577600092368793E+06 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({ 2.940381268511949E+00 * Kilo(Metre) / Second,
+                     -2.206625841382794E+01 * Kilo(Metre) / Second,
+                     -5.348179460834037E-01 * Kilo(Metre) / Second}))});
   Trajectory<ICRFJ2000EclipticFrame>* mercury_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(mercury));
   mercury_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-3.013851560892715E+07 * Kilo(Metre),
-                  3.823388939456400E+07 * Kilo(Metre),
-                  5.907240907643730E+06 * Kilo(Metre)}),
-       Velocity({-4.731017449071709E+01 * Kilo(Metre) / Second,
-                 -2.918747853895398E+01 * Kilo(Metre) / Second,
-                  1.963450229872517E+00 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({-3.013851560892715E+07 * Kilo(Metre),
+                      3.823388939456400E+07 * Kilo(Metre),
+                      5.907240907643730E+06 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({-4.731017449071709E+01 * Kilo(Metre) / Second,
+                     -2.918747853895398E+01 * Kilo(Metre) / Second,
+                      1.963450229872517E+00 * Kilo(Metre) / Second}))});
 
   // End of planets.
 
@@ -167,82 +187,96 @@ std::unique_ptr<SolarSystem> SolarSystem::AtСпутникLaunch() {
       new Trajectory<ICRFJ2000EclipticFrame>(ganymede));
   ganymede_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-7.942681422941415E+08 * Kilo(Metre),
-                 -1.776681035234876E+08 * Kilo(Metre),
-                  1.857215495334835E+07 * Kilo(Metre)}),
-       Velocity({-5.026319376504355E+00 * Kilo(Metre) / Second,
-                 -4.481735740234995E+00 * Kilo(Metre) / Second,
-                  1.326192167761359E-01 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({-7.942681422941415E+08 * Kilo(Metre),
+                     -1.776681035234876E+08 * Kilo(Metre),
+                      1.857215495334835E+07 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({-5.026319376504355E+00 * Kilo(Metre) / Second,
+                     -4.481735740234995E+00 * Kilo(Metre) / Second,
+                      1.326192167761359E-01 * Kilo(Metre) / Second}))});
 
   // Satellite of Saturn.
   Trajectory<ICRFJ2000EclipticFrame>* titan_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(titan));
   titan_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-3.771930512714775E+08 * Kilo(Metre),
-                 -1.452931696594699E+09 * Kilo(Metre),
-                  4.091643033375849E+07 * Kilo(Metre)}),
-       Velocity({ 1.433381483669744E+01 * Kilo(Metre) / Second,
-                 -1.422590492527597E+00 * Kilo(Metre) / Second,
-                 -1.375826555026097E+00 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({-3.771930512714775E+08 * Kilo(Metre),
+                     -1.452931696594699E+09 * Kilo(Metre),
+                      4.091643033375849E+07 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({ 1.433381483669744E+01 * Kilo(Metre) / Second,
+                     -1.422590492527597E+00 * Kilo(Metre) / Second,
+                     -1.375826555026097E+00 * Kilo(Metre) / Second}))});
 
   // Satellites of Jupiter.
   Trajectory<ICRFJ2000EclipticFrame>* callisto_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(callisto));
   callisto_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-7.951805452047400E+08 * Kilo(Metre),
-                 -1.802957437059298E+08 * Kilo(Metre),
-                  1.847154088070625E+07 * Kilo(Metre)}),
-       Velocity({ 1.091928199422218E+01 * Kilo(Metre) / Second,
-                 -1.278098875182818E+01 * Kilo(Metre) / Second,
-                  5.878649120351949E-02 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({-7.951805452047400E+08 * Kilo(Metre),
+                     -1.802957437059298E+08 * Kilo(Metre),
+                      1.847154088070625E+07 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({ 1.091928199422218E+01 * Kilo(Metre) / Second,
+                     -1.278098875182818E+01 * Kilo(Metre) / Second,
+                      5.878649120351949E-02 * Kilo(Metre) / Second}))});
   Trajectory<ICRFJ2000EclipticFrame>* io_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(io));
   io_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-7.946073188298367E+08 * Kilo(Metre),
-                 -1.783491436977172E+08 * Kilo(Metre),
-                  1.854699192614355E+07 * Kilo(Metre)}),
-       Velocity({-5.049684272040893E-01 * Kilo(Metre) / Second,
-                  4.916473261567652E+00 * Kilo(Metre) / Second,
-                  5.469177855959977E-01 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({-7.946073188298367E+08 * Kilo(Metre),
+                     -1.783491436977172E+08 * Kilo(Metre),
+                      1.854699192614355E+07 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({-5.049684272040893E-01 * Kilo(Metre) / Second,
+                      4.916473261567652E+00 * Kilo(Metre) / Second,
+                      5.469177855959977E-01 * Kilo(Metre) / Second}))});
 
   // Satellite of Earth.
   Trajectory<ICRFJ2000EclipticFrame>* moon_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(moon));
   moon_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({ 1.478545271460863E+08 * Kilo(Metre),
-                  3.122566749814625E+07 * Kilo(Metre),
-                  1.500491219719345E+03 * Kilo(Metre)}),
-       Velocity({-6.099833968412930E+00 * Kilo(Metre) / Second,
-                  2.985006033154299E+01 * Kilo(Metre) / Second,
-                 -1.952438319420470E-02 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({ 1.478545271460863E+08 * Kilo(Metre),
+                      3.122566749814625E+07 * Kilo(Metre),
+                      1.500491219719345E+03 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({-6.099833968412930E+00 * Kilo(Metre) / Second,
+                      2.985006033154299E+01 * Kilo(Metre) / Second,
+                     -1.952438319420470E-02 * Kilo(Metre) / Second}))});
 
   // Satellite of Jupiter.
   Trajectory<ICRFJ2000EclipticFrame>* europa_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(europa));
   europa_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-7.944180333947762E+08 * Kilo(Metre),
-                 -1.787346439588362E+08 * Kilo(Metre),
-                  1.853675837527557E+07 * Kilo(Metre)}),
-       Velocity({ 8.811255547505889E+00 * Kilo(Metre) / Second,
-                  5.018147960240774E-02 * Kilo(Metre) / Second,
-                  6.162195631257494E-01 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({-7.944180333947762E+08 * Kilo(Metre),
+                     -1.787346439588362E+08 * Kilo(Metre),
+                      1.853675837527557E+07 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({ 8.811255547505889E+00 * Kilo(Metre) / Second,
+                      5.018147960240774E-02 * Kilo(Metre) / Second,
+                      6.162195631257494E-01 * Kilo(Metre) / Second}))});
 
   // Satellite of Neptune.
   Trajectory<ICRFJ2000EclipticFrame>* triton_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(triton));
   triton_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-3.810797098554279E+09 * Kilo(Metre),
-                 -2.456691608348630E+09 * Kilo(Metre),
-                  1.381629136719314E+08 * Kilo(Metre)}),
-       Velocity({-1.047462448797063E+00 * Kilo(Metre) / Second,
-                 -4.404556713303486E+00 * Kilo(Metre) / Second,
-                  1.914469843538767E+00 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({-3.810797098554279E+09 * Kilo(Metre),
+                     -2.456691608348630E+09 * Kilo(Metre),
+                      1.381629136719314E+08 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({-1.047462448797063E+00 * Kilo(Metre) / Second,
+                     -4.404556713303486E+00 * Kilo(Metre) / Second,
+                      1.914469843538767E+00 * Kilo(Metre) / Second}))});
 
   // Dwarf planet (scattered disc object).
   // Mass from Brown, Michael E.; Schaller, Emily L. (15 June 2007).
@@ -251,24 +285,28 @@ std::unique_ptr<SolarSystem> SolarSystem::AtСпутникLaunch() {
       new Trajectory<ICRFJ2000EclipticFrame>(eris));
   eris_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({ 1.317390066862979E+10 * Kilo(Metre),
-                  2.221403321600002E+09 * Kilo(Metre),
-                 -5.736076877456254E+09 * Kilo(Metre)}),
-       Velocity({ 4.161883594267296E-01 * Kilo(Metre) / Second,
-                  1.872714752602233E+00 * Kilo(Metre) / Second,
-                  1.227093842948539E+00 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({ 1.317390066862979E+10 * Kilo(Metre),
+                      2.221403321600002E+09 * Kilo(Metre),
+                     -5.736076877456254E+09 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({ 4.161883594267296E-01 * Kilo(Metre) / Second,
+                      1.872714752602233E+00 * Kilo(Metre) / Second,
+                      1.227093842948539E+00 * Kilo(Metre) / Second}))});
 
   // Dwarf planet (Kuiper belt object).
   Trajectory<ICRFJ2000EclipticFrame>* pluto_trajectory(
       new Trajectory<ICRFJ2000EclipticFrame>(pluto));
   pluto_trajectory->Append(
       kСпутникLaunchTime,
-      {Position({-4.406985590968750E+09 * Kilo(Metre),
-                  2.448731153209013E+09 * Kilo(Metre),
-                  1.012525975599311E+09 * Kilo(Metre)}),
-       Velocity({-1.319871918266467E+00 * Kilo(Metre) / Second,
-                 -5.172112237151897E+00 * Kilo(Metre) / Second,
-                  9.407707128142039E-01 * Kilo(Metre) / Second})});
+      {Point<Position>(
+           Position({-4.406985590968750E+09 * Kilo(Metre),
+                      2.448731153209013E+09 * Kilo(Metre),
+                      1.012525975599311E+09 * Kilo(Metre)})),
+       Point<Velocity>(
+           Velocity({-1.319871918266467E+00 * Kilo(Metre) / Second,
+                     -5.172112237151897E+00 * Kilo(Metre) / Second,
+                      9.407707128142039E-01 * Kilo(Metre) / Second}))});
 
   // End of celestial bodies.
 
