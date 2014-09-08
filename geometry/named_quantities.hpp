3754c1b97cd03bef4e218f68feabd48406bd344c
Robin Leroy <egg.robin.leroy@gmail.com>
2014-09-08 14:43:07 +0200

    Add named vector and point quantities.

---
+++ geometry/named_quantities.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "geometry/grassmann.hpp"
+#include "geometry/point.hpp"
+#include "quantities/named_quantities.hpp"
+#include "quantities/quantities.hpp"
+
+namespace principia {
+namespace geometry {
+
+typedef Point<quantities::Time> Instant;
+template<typename Frame>
+using Displacement = Vector<quantities::Length, Frame>;
+template<typename Frame>
+using Position = Point<Displacement<Frame>>;
+template<typename Frame>
+using Velocity = Vector<quantities::Speed, Frame>;
+
+}  // namespace quantities
+}  // namespace principia
