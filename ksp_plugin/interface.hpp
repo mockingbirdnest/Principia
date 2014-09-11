1aedcfad44a349144502c2a4a7a02eaa3ea05012
Robin Leroy <egg.robin.leroy@gmail.com>
2014-09-11 16:16:25 +0200

    preprocessor nonsense

--- ksp_plugin/interface.hpp
+++ ksp_plugin/interface.hpp
@@ -2,11 +2,18 @@
 
 // DLL-exported functions for interfacing with Platform Invocation Services.
 
+#if defined(_WIN32)
+#define DLLEXPORT __declspec(dllexport)
+#elif defined(__INTEL_COMPILER) || defined(__GNUC__) || defined(__clang__)
+#define DLLEXPORT __attribute__((visibility("default")))
+#else
+#error "What compiler do you think you're using?"
+#endif
+
 namespace principia {
 namespace ksp_plugin {
 
-extern "C"
-__declspec(dllexport)
+extern "C" DLLEXPORT
 int Say33();
 
 extern "C"
