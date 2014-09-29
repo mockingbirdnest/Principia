e2f22edbf1a7983443e0073569dcd5726218fec2
pleroy <phl+personal@google.com>
2014-09-29 23:25:01 +0200

    Add a todo.

--- ksp_plugin_test/plugin_test.cpp
+++ ksp_plugin_test/plugin_test.cpp
@@ -11,6 +11,7 @@ namespace ksp_plugin {
 class PluginTest : public testing::Test {
  protected:
   void SetUp() override {
+    //TODO(phl): Should get these data from SolarSystem.
     initial_time_ = Instant(2 * SIUnit<Time>());
     sun_index_ = 42;
     sun_gravitational_parameter_ = 3 * SIUnit<GravitationalParameter>();
