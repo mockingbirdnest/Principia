4af8f22c2cea90fc13f3371f9536bae45dfa29c6
pleroy <phl+personal@google.com>
2014-10-05 19:24:35 +0200

    Benchmark numbers.

--- benchmarks/n_body_system.cpp
+++ benchmarks/n_body_system.cpp
@@ -1,16 +1,23 @@
 ﻿
 // .\Release\benchmarks.exe  --benchmark_repetitions=5 --benchmark_filter=Solar
 // Benchmarking on 1 X 3310 MHz CPU
-// 2014/10/05-13:01:28
-// Benchmark               Time(ns)    CPU(ns) Iterations
-// ------------------------------------------------------
-// BM_SolarSystem        48122102714 47954707400          1                                 1.0002759263053012e+00 ua  // NOLINT(whitespace/line_length)
-// BM_SolarSystem        48154779410 48126308500          1                                 1.0002759263053012e+00 ua  // NOLINT(whitespace/line_length)
-// BM_SolarSystem        52067173219 52041933600          1                                 1.0002759263053012e+00 ua  // NOLINT(whitespace/line_length)
-// BM_SolarSystem        48369799829 48188708900          1                                 1.0002759263053012e+00 ua  // NOLINT(whitespace/line_length)
-// BM_SolarSystem        48255792594 48079508200          1                                 1.0002759263053012e+00 ua  // NOLINT(whitespace/line_length)
-// BM_SolarSystem_mean   48993929553 48878233320          1                                 1.0002759263053012e+00 ua  // NOLINT(whitespace/line_length)
-// BM_SolarSystem_stddev 1539055926 1583710596          0                                 1.0002759263053012e+00 ua    // NOLINT(whitespace/line_length)
+// 2014/10/05-19:16:12
+// Benchmark                                  Time(ns)    CPU(ns) Iterations
+// -------------------------------------------------------------------------
+// BM_SolarSystemMajorBodiesOnly            24203858590 23649751600          1                                 1.0002759262592109e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMajorBodiesOnly            24101375556 24024154000          1                                 1.0002759262592109e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMajorBodiesOnly            24065379835 23696551900          1                                 1.0002759262592109e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMajorBodiesOnly            24012373186 23712152000          1                                 1.0002759262592109e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMajorBodiesOnly            24073379016 23712152000          1                                 1.0002759262592109e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMajorBodiesOnly_mean       24091273237 23758952300          1                                 1.0002759262592109e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMajorBodiesOnly_stddev       63235130  134559341          0                                 1.0002759262592109e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMinorAndMajorBodies        50587372995 49951520200          1                                 1.0002759263053576e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMinorAndMajorBodies        51049073196 50403923100          1                                 1.0002759263053576e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMinorAndMajorBodies        50057970024 49343116300          1                                 1.0002759263053576e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMinorAndMajorBodies        50023569084 49046714400          1                                 1.0002759263053576e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMinorAndMajorBodies        51184083438 50653524700          1                                 1.0002759263053576e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMinorAndMajorBodies_mean   50580413747 49879759740          1                                 1.0002759263053576e+00 ua  // NOLINT(whitespace/line_length)
+// BM_SolarSystemMinorAndMajorBodies_stddev  483141931  610009791          0                                 1.0002759263053576e+00 ua  // NOLINT(whitespace/line_length)
 #include <memory>
 #include <vector>
 
