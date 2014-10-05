71a9b932c80be5413d99e85234344b9e4e7a2053
pleroy <phl+personal@google.com>
2014-10-05 13:54:51 +0200

    Body changes.

--- physics/body.hpp
+++ physics/body.hpp
@@ -4,9 +4,12 @@
 
 #include "geometry/grassmann.hpp"
 #include "quantities/named_quantities.hpp"
+#include "quantities/quantities.hpp"
 
 using principia::quantities::GravitationalParameter;
+using principia::quantities::Length;
 using principia::quantities::Mass;
+using principia::quantities::Order2ZonalCoefficient;
 
 namespace principia {
 namespace physics {
@@ -17,18 +20,38 @@ class Body {
   // unit roundoffs from repeated multiplications by G.
   explicit Body(GravitationalParameter const& gravitational_parameter);
   explicit Body(Mass const& mass);
+
+  // A body with oblateness.  The body must not be massless.
+  Body(GravitationalParameter const& gravitational_parameter,
+       double const j2,
+       Length const& radius);
+  Body(Mass const& mass,
+       double const j2,
+       Length const& radius);
+  Body(GravitationalParameter const& gravitational_parameter,
+       Order2ZonalCoefficient const& j2);
+  Body(Mass const& mass,
+       Order2ZonalCoefficient const& j2);
+
   ~Body() = default;
 
   // Returns the construction parameter.
   GravitationalParameter const& gravitational_parameter() const;
   Mass const& mass() const;
 
+  // Returns the j2 coefficient.  Returns 0 for a non-oblate body.
+  Order2ZonalCoefficient const& j2() const;
+
   // Returns true iff |gravitational_parameter| (or |mass|) returns 0.
   bool is_massless() const;
+
+  // Returns true iff |j2| returns a non-0 value.
+  bool is_oblate() const;
 
  private:
   GravitationalParameter const gravitational_parameter_;
   Mass const mass_;
+  Order2ZonalCoefficient const j2_;
 };
 
 }  // namespace physics
