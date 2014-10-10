21fb8f8d6f1e04f64af8a357248dc1feb1c2ffe1
pleroy <phl+personal@google.com>
2014-10-10 23:21:53 +0200

    More tests.

---
+++ ksp_plugin_test/interface_test.cpp
@@ -0,0 +1,72 @@
+#include "ksp_plugin/interface.hpp"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "quantities/si.hpp"
+#include "ksp_plugin/mock_plugin.hpp"
+
+using principia::quantities::GravitationalParameter;
+using principia::si::Radian;
+using principia::si::Second;
+using testing::IsNull;
+using testing::StrictMock;
+
+namespace principia {
+namespace ksp_plugin {
+
+namespace {
+
+Index const kCelestialIndex = 1;
+Index const kParentIndex = 2;
+
+double const kGravitationalParameter = 3;
+
+XYZ kParentPosition = {4, 5, 6};
+XYZ kParentVelocity = {7, 8, 9};
+
+}  // namespace
+
+class InterfaceTest : public testing::Test {
+ protected:
+  InterfaceTest()
+      : plugin_(new StrictMock<MockPlugin>) {}
+
+  std::unique_ptr<StrictMock<MockPlugin>> plugin_;
+};
+
+TEST_F(InterfaceTest, DeletePlugin) {
+  Plugin const* plugin = plugin_.release();
+  DeletePlugin(&plugin);
+  EXPECT_THAT(plugin, IsNull());
+}
+
+TEST_F(InterfaceTest, InsertCelestial) {
+  EXPECT_CALL(*plugin_,
+              InsertCelestial(
+                  kCelestialIndex,
+                  kGravitationalParameter * SIUnit<GravitationalParameter>(),
+                  kParentIndex,
+                  Displacement<AliceSun>(
+                      {kParentPosition.x * SIUnit<Length>(),
+                       kParentPosition.y * SIUnit<Length>(),
+                       kParentPosition.z * SIUnit<Length>()}),
+                  Velocity<AliceSun>(
+                      {kParentVelocity.x * SIUnit<Speed>(),
+                       kParentVelocity.y * SIUnit<Speed>(),
+                       kParentVelocity.z * SIUnit<Speed>()})));
+  InsertCelestial(plugin_.get(),
+                  kCelestialIndex,
+                  kGravitationalParameter,
+                  kParentIndex,
+                  kParentPosition,
+                  kParentVelocity);
+}
+
+TEST_F(InterfaceTest, UpdateCelestialHierarchy) {
+  EXPECT_CALL(*plugin_,
+              UpdateCelestialHierarchy(kCelestialIndex, kParentIndex));
+  UpdateCelestialHierarchy(plugin_.get(), kCelestialIndex, kParentIndex);
+}
+
+}  // namespace ksp_plugin
+}  // namespace principia
