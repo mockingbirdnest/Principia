fad023bd4a94571fad8d2a5b5a4af1f568c49d61
pleroy <phl+personal@google.com>
2014-10-13 19:21:05 +0200

    Forwarding constructor.

--- ksp_plugin/celestial_body.hpp
+++ ksp_plugin/celestial_body.hpp
@@ -6,18 +6,13 @@ namespace principia {
 namespace ksp_plugin {
 
 template<typename Frame>
-Celestial<Frame>::Celestial(
-    GravitationalParameter const& gravitational_parameter)
-    : body_(new Body<Frame>(gravitational_parameter)) {}
+template<typename... Args>
+Celestial<Frame>::Celestial(Args&&... args)
+    : body_(new Body<Frame>(std::forward<Args>(args)...)) {}
 
 template<typename Frame>
 bool Celestial<Frame>::has_parent() const {
   return parent_ != nullptr;
-}
-
-template<typename Frame>
-Body<Frame> const& Celestial<Frame>::body() const {
-  return *body_;
 }
 
 template<typename Frame>
