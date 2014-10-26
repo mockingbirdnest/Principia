d670a957957b94e4470766b7e217d5d30f84f743
Robin Leroy <egg.robin.leroy@gmail.com>
2014-10-26 23:47:36 +0100

    comments

--- base/not_null.hpp
+++ base/not_null.hpp
@@ -11,33 +11,36 @@ namespace base {
 template<typename Pointer>
 class not_null {
  public:
+  // Creates a |not_null<Pointer>| whose |pointer_| equals the given |pointer|,
+  // dawg, and checks (using a glog |CHECK| macro) that |pointer| is not null.
   explicit not_null(Pointer const& pointer);
-  explicit not_null(Pointer&& pointer);
+  explicit not_null(Pointer&& pointer);  // NOLINT(build/c++11)
 
   not_null() = delete;
   not_null(not_null const&) = default;
   // We really want a move constructor. Moving the |pointer_| might make the
   // argument an invalid |not_null<Pointer>| by making its |pointer_| null.  The
-  // standard says we must leave the argument in a leave the argument in some
-  // valid but otherwise indeterminate state, so we infrige on that.  On the
-  // other hand, if you rely on the value after a move, you're asking for
-  // trouble.
+  // standard says we must leave the argument in some  valid but otherwise
+  // indeterminate state, so we infrige on that.  On the other hand, if you rely
+  // on the value after a move, you're asking for trouble.
   // NOTE(egg): We would use |= default|, but VS2013 does not implement that for
   // the move constructor;
-  not_null(not_null&&);
+  not_null(not_null&&);  // NOLINT(build/c++11)
   ~not_null() = default;
+
+  not_null& operator=(not_null const&) = default;
+  // Implemented as a swap, so the argument remains valid.
+  not_null& operator=(not_null&& other);  // NOLINT(build/c++11)
 
   // Returns |pointer_|, by const reference to avoid issues with |unique_ptr|.
   operator Pointer const&() const;
 
-  not_null& operator=(not_null const&) = default;
-  // Implemented as a swap, so the argument remains valid.
-  not_null& operator=(not_null&& other);
-
   // The following operators can be inlined and will be turned into constexprs
   // eventually, so having them explicitly should improve optimization.
-  // They return |false|.
+
+  // Returns |false|.
   bool operator==(nullptr_t other) const;
+  // Returns |true|.
   operator bool() const;
 
  private:
@@ -48,18 +51,21 @@ template<typename Pointer>
 std::ostream& operator<<(std::ostream& stream,
                          not_null<Pointer> const& pointer);
 
-// Factories taking advantage of template argument deduction.
-
+// Factories taking advantage of template argument deduction.  They call the
+// corresponding constructors for |not_null<Pointer>|.
 template<typename Pointer>
-not_null<Pointer> check_not_null(Pointer&& pointer);
+not_null<Pointer> check_not_null(Pointer&& pointer);  // NOLINT(build/c++11)
 template<typename Pointer>
 not_null<Pointer> check_not_null(Pointer const& pointer);
+
 // While the above would cover this case using the implicit conversion, this
-// results in a redundant |CHECK|.
+// results in a redundant |CHECK|.  These return the argument.
 template<typename Pointer>
 not_null<Pointer> check_not_null(not_null<Pointer> const& pointer);
+// Returns |pointer| by moving it, which may invalidate it as described above.
 template<typename Pointer>
-not_null<Pointer> check_not_null(not_null<Pointer>&& pointer);
+not_null<Pointer> check_not_null(
+    not_null<Pointer>&& pointer);  // NOLINT(build/c++11)
 
 }  // namespace base
 }  // namespace principia
