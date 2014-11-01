c267e7a23fad44d71a2afbf0b2e6ee9fb34ffbf8
Robin Leroy <egg.robin.leroy@gmail.com>
2014-11-01 15:28:42 +0100

    use property sheet

--- generate_version_header.ps1
+++ generate_version_header.ps1
@@ -1,4 +1,5 @@
-cd $args[0]
+$solutiondir = resolve-path $args[0]
 $env:Path += ";$env:localappdata\GitHub\Portab~1\bin"
 [string]::format("#pragma once`n#define PRINCIPIA_VERSION_ID ""{0}""`n",
-                 (git describe --tags --always --dirty --abbrev=40 --long)) > "base/version.hpp"
+                 (git describe --tags --always --dirty --abbrev=40 --long)) > `
+    (join-path $solutiondir "base/version.hpp")
