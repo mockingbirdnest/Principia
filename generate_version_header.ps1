059e736ff30586cf46f117637a21025ca0443747
Robin Leroy <egg.robin.leroy@gmail.com>
2014-11-01 16:04:30 +0100

    1980 called, they want their macros back

--- generate_version_header.ps1
+++ generate_version_header.ps1
@@ -1,5 +1,14 @@
 $solutiondir = resolve-path $args[0]
 $env:Path += ";$env:localappdata\GitHub\Portab~1\bin"
-[string]::format("#pragma once`n#define PRINCIPIA_VERSION_ID ""{0}""`n",
-                 (git describe --tags --always --dirty --abbrev=40 --long)) > `
-    (join-path $solutiondir "base/version.hpp")
+[string]::format(
+    "#pragma once`n"                                             `
+        + "`n"                                                   `
+        + "namespace principia {{`n"                             `
+        + "namespace base {{`n"                                  `
+        + "`n"                                                   `
+        + "char const kVersion[] = `"{0}`";`n"                   `
+        + "`n"                                                   `
+        + "}}  // namespace base`n"                              `
+        + "}}  // namespace principia`n",                        `
+    (git describe --tags --always --dirty --abbrev=40 --long)) > `
+        (join-path $solutiondir "base/version.hpp")
