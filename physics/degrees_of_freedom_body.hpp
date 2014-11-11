45462f7c5679504741bbe8ebd1ef083f6797a47a
pleroy <phl+personal@google.com>
2014-11-11 21:11:59 +0100

    Cleanup.

--- physics/degrees_of_freedom_body.hpp
+++ physics/degrees_of_freedom_body.hpp
@@ -22,22 +22,22 @@ template<typename Frame, typename Weight>
 DegreesOfFreedom<Frame> Barycentre(
     std::vector<DegreesOfFreedom<Frame>> const& degrees_of_freedom,
     std::vector<Weight> const& weights) {
-  // This weird expression is 0, but it defines the type of the variable.
+  Position<Frame> reference_position;
   auto positions_weighted_sum =
       (degrees_of_freedom[0].position -
-       degrees_of_freedom[0].position).coordinates() * weights[0];
+       reference_position).coordinates() * weights[0];
   auto velocities_weighted_sum =
       degrees_of_freedom[0].velocity.coordinates() * weights[0];
   Weight weight = weights[0];
   for (size_t i = 1; i < degrees_of_freedom.size(); ++i) {
     positions_weighted_sum +=
         (degrees_of_freedom[i].position -
-         degrees_of_freedom[0].position).coordinates() * weights[i];
+         reference_position).coordinates() * weights[i];
     velocities_weighted_sum +=
         degrees_of_freedom[i].velocity.coordinates() * weights[i];
     weight += weights[i];
   }
-  return {degrees_of_freedom[0].position +
+  return {reference_position +
               Displacement<Frame>(positions_weighted_sum / weight),
           Velocity<Frame>(velocities_weighted_sum / weight)};
 }
