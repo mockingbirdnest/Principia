2100b33e28503c1b6142fcd35690d0234e048f71
pleroy <phl+personal@google.com>
2014-11-15 18:47:51 +0100

    Comments.

--- physics/transforms.hpp
+++ physics/transforms.hpp
@@ -5,12 +5,22 @@
 namespace principia {
 namespace physics {
 
-//TODO(phl):Comment
+// This class represent a pair of transformations of a trajectory from
+// |FromFrame| to |ToFrame| with an intermediate representation in
+// |ThroughFrame|.
 template<typename FromFrame, typename ThroughFrame, typename ToFrame>
 class Transforms {
  public:
+  // A factory method where the intermediate frame is in translation with the
+  // body of |centre_trajectory|.
   static Transforms BodyCentredNonRotating(
       Trajectory<FromFrame> const& centre_trajectory);
+
+  // A factory method where the intermediate frame is in translation with the
+  // barycentre of the two bodies, whose X axis goes from the primary to the
+  // secondary, whose Y axis is in the plane of the velocities of the bodies in
+  // their barycentric frame, along the velocity of the primary body, and which
+  // is right-handed.
   static Transforms BarycentricRotating(
       Trajectory<FromFrame> const& primary_trajectory,
       Trajectory<FromFrame> const& secondary_trajectory);
