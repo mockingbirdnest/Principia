85b07c0e67f2ed884758342f95fa6514cc37320c
pleroy <phl+personal@google.com>
2014-11-17 19:43:13 +0100

    HPP.

---
+++ geometry/identity.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "geometry/grassmann.hpp"
+#include "geometry/linear_map.hpp"
+#include "geometry/sign.hpp"
+
+namespace principia {
+namespace geometry {
+
+template<typename FromFrame, typename ToFrame>
+class OrthogonalMap;
+
+// The identity map.
+template<typename FromFrame, typename ToFrame>
+class Identity : public LinearMap<FromFrame, ToFrame> {
+  Identity() = default;
+  ~Identity() override = default;
+
+  Sign Determinant() const override;
+
+  Identity<ToFrame, FromFrame> Inverse() const;
+
+  template<typename Scalar>
+  Vector<Scalar, ToFrame> operator()(
+      Vector<Scalar, FromFrame> const& vector) const;
+
+  template<typename Scalar>
+  Bivector<Scalar, ToFrame> operator()(
+      Bivector<Scalar, FromFrame> const& bivector) const;
+
+  template<typename Scalar>
+  Trivector<Scalar, ToFrame> operator()(
+      Trivector<Scalar, FromFrame> const& trivector) const;
+
+  OrthogonalMap<FromFrame, ToFrame> Forget() const;
+
+  static Identity Identity();
+
+ private:
+  template<typename Scalar>
+  R3Element<Scalar> operator()(R3Element<Scalar> const& r3_element) const;
+};
+
+template<typename FromFrame, typename ThroughFrame, typename ToFrame>
+Identity<FromFrame, ToFrame> operator*(
+    Identity<ThroughFrame, ToFrame> const& left,
+    Identity<FromFrame, ThroughFrame> const& right);
+
+}  // namespace geometry
+}  // namespace principia
+
+#include "geometry/permutation_body.hpp"
