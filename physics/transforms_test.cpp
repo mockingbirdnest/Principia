15951a07c246b8fd192e620837b56140250e40b9
pleroy <phl+personal@google.com>
2014-11-22 18:46:28 +0100

    Bodies test.

--- physics/transforms_test.cpp
+++ physics/transforms_test.cpp
@@ -9,14 +9,17 @@
 #include "quantities/named_quantities.hpp"
 #include "quantities/quantities.hpp"
 #include "testing_utilities/almost_equals.hpp"
+#include "testing_utilities/numerics.hpp"
 
 using principia::quantities::Length;
 using principia::quantities::Mass;
 using principia::quantities::SIUnit;
 using principia::quantities::Speed;
 using principia::quantities::Time;
+using principia::testing_utilities::AbsoluteError;
 using principia::testing_utilities::AlmostEquals;
 using testing::Eq;
+using testing::Lt;
 
 namespace principia {
 namespace physics {
@@ -39,23 +42,15 @@ protected:
   void SetUp() override {
     body1_ = std::make_unique<MassiveBody>(1 * SIUnit<Mass>());
     body2_ = std::make_unique<MassiveBody>(3 * SIUnit<Mass>());
-    body_from_ = std::make_unique<Trajectory<From>>(body_);
+    satellite1_from_ = std::make_unique<Trajectory<From>>(satellite1_);
+    satellite2_from_ = std::make_unique<Trajectory<From>>(satellite2_);
+    satellite3_from_ = std::make_unique<Trajectory<From>>(satellite3_);
     body1_from_ = std::make_unique<Trajectory<From>>(*body1_);
     body2_from_ = std::make_unique<Trajectory<From>>(*body2_);
     body1_to_ = std::make_unique<Trajectory<To>>(*body1_);
     body2_to_ = std::make_unique<Trajectory<To>>(*body2_);
 
     for (int i = 1; i <= kNumberOfPoints; ++i) {
-      body_from_->Append(
-          Instant(i * SIUnit<Time>()),
-                  DegreesOfFreedom<From>(
-                      Position<From>(
-                          Displacement<From>({10 * i * SIUnit<Length>(),
-                                              -20 * i * SIUnit<Length>(),
-                                              30 * i * SIUnit<Length>()})),
-                      Velocity<From>({40 * i * SIUnit<Speed>(),
-                                      -80 * i * SIUnit<Speed>(),
-                                      160 * i * SIUnit<Speed>()})));
       body1_from_->Append(
           Instant(i * SIUnit<Time>()),
                   DegreesOfFreedom<From>(
@@ -76,17 +71,42 @@ protected:
                       Velocity<From>({4 * i * SIUnit<Speed>(),
                                       8 * i * SIUnit<Speed>(),
                                       -16 * i * SIUnit<Speed>()})));
+      satellite1_from_->Append(
+          Instant(i * SIUnit<Time>()),
+                  DegreesOfFreedom<From>(
+                      Position<From>(
+                          Displacement<From>({10 * i * SIUnit<Length>(),
+                                              -20 * i * SIUnit<Length>(),
+                                              30 * i * SIUnit<Length>()})),
+                      Velocity<From>({40 * i * SIUnit<Speed>(),
+                                      -80 * i * SIUnit<Speed>(),
+                                      160 * i * SIUnit<Speed>()})));
+      // At the midpoint between body1_ and body2_.
+      satellite2_from_->Append(
+          Instant(i * SIUnit<Time>()),
+                  DegreesOfFreedom<From>(
+                      Position<From>(
+                          Displacement<From>({0 * i * SIUnit<Length>(),
+                                              0 * i * SIUnit<Length>(),
+                                              6 * i * SIUnit<Length>()})),
+                      Velocity<From>({8 * i * SIUnit<Speed>(),
+                                      16 * i * SIUnit<Speed>(),
+                                      0 * i * SIUnit<Speed>()})));
     }
   }
 
-  MasslessBody body_;
   std::unique_ptr<MassiveBody> body1_;
   std::unique_ptr<MassiveBody> body2_;
-  std::unique_ptr<Trajectory<From>> body_from_;
+  MasslessBody satellite1_;
+  MasslessBody satellite2_;
+  MasslessBody satellite3_;
   std::unique_ptr<Trajectory<From>> body1_from_;
   std::unique_ptr<Trajectory<From>> body2_from_;
   std::unique_ptr<Trajectory<To>> body1_to_;
   std::unique_ptr<Trajectory<To>> body2_to_;
+  std::unique_ptr<Trajectory<From>> satellite1_from_;
+  std::unique_ptr<Trajectory<From>> satellite2_from_;
+  std::unique_ptr<Trajectory<From>> satellite3_from_;
 
   std::unique_ptr<Transforms<From, Through, To>> transforms_;
 };
@@ -97,7 +117,7 @@ TEST_F(TransformsTest, BodyCentredNonRotating) {
   Trajectory<Through> body1_through(*body1_);
 
   int i = 1;
-  for (auto it = transforms_->first(body_from_.get());
+  for (auto it = transforms_->first(satellite1_from_.get());
        !it.at_end();
        ++it, ++i) {
     DegreesOfFreedom<Through> const degrees_of_freedom =
@@ -141,14 +161,14 @@ TEST_F(TransformsTest, BodyCentredNonRotating) {
   }
 }
 
-TEST_F(TransformsTest, BarycentricRotating) {
+TEST_F(TransformsTest, SatelliteBarycentricRotating) {
   transforms_ = Transforms<From, Through, To>::BarycentricRotating(
                     *body1_from_, *body1_to_,
                     *body2_from_, *body2_to_);
-  Trajectory<Through> body1_through(*body1_);
+  Trajectory<Through> satellite1_through(satellite1_);
 
   int i = 1;
-  for (auto it = transforms_->first(body_from_.get());
+  for (auto it = transforms_->first(satellite1_from_.get());
        !it.at_end();
        ++it, ++i) {
     DegreesOfFreedom<Through> const degrees_of_freedom =
@@ -163,11 +183,11 @@ TEST_F(TransformsTest, BarycentricRotating) {
                                  {-28 * sqrt(5.0) * i * SIUnit<Speed>(),
                                   168 * i * SIUnit<Speed>(),
                                   32 * sqrt(5.0) * i * SIUnit<Speed>()}))) << i;
-    body1_through.Append(Instant(i * SIUnit<Time>()), degrees_of_freedom);
+    satellite1_through.Append(Instant(i * SIUnit<Time>()), degrees_of_freedom);
   }
 
   i = 1;
-  for (auto it = transforms_->second(&body1_through);
+  for (auto it = transforms_->second(&satellite1_through);
        !it.at_end();
        ++it, ++i) {
       body1_to_->Append(
@@ -203,6 +223,61 @@ TEST_F(TransformsTest, BarycentricRotating) {
                                  {168 * i * SIUnit<Speed>(),
                                   36 * i * SIUnit<Speed>(),
                                   -88 * i * SIUnit<Speed>()}))) << i;
+  }
+}
+
+TEST_F(TransformsTest, BodiesBarycentricRotating) {
+  // TODO(phl): We need two transforms to avoid getting confused by the cache.
+  auto transforms1 = Transforms<From, Through, To>::BarycentricRotating(
+                         *body1_from_, *body1_to_,
+                         *body2_from_, *body2_to_);
+  auto transforms2 = Transforms<From, Through, To>::BarycentricRotating(
+                         *body1_from_, *body1_to_,
+                         *body2_from_, *body2_to_);
+  Trajectory<Through> body1_through(*body1_);
+  Trajectory<Through> body2_through(*body2_);
+
+  int i = 1;
+  for (auto it1 = transforms1->first(body1_from_.get()),
+            it2 = transforms2->first(body2_from_.get());
+       !it1.at_end() && !it2.at_end();
+       ++it1, ++it2, ++i) {
+    DegreesOfFreedom<Through> const degrees_of_freedom1 =
+        it1.degrees_of_freedom();
+    DegreesOfFreedom<Through> const degrees_of_freedom2 =
+        it2.degrees_of_freedom();
+
+    EXPECT_THAT(
+        AbsoluteError(degrees_of_freedom1.position - Position<Through>(),
+                      Displacement<Through>(
+                          {1.5 * sqrt(5.0) * i * SIUnit<Length>(),
+                           0 * SIUnit<Length>(),
+                           0 * SIUnit<Length>()})),
+        Lt(40 * DBL_EPSILON * SIUnit<Length>()));
+    EXPECT_THAT(
+        AbsoluteError(degrees_of_freedom2.position - Position<Through>(),
+                      Displacement<Through>(
+                          {-0.5 * sqrt(5.0) * i * SIUnit<Length>(),
+                           0 * SIUnit<Length>(),
+                           0 * SIUnit<Length>()})),
+        Lt(50 * DBL_EPSILON * SIUnit<Length>()));
+    // TODO(phl): Add a test that the velocities are along X once we have fixed
+    // them.
+
+    DegreesOfFreedom<Through> const barycenter_degrees_of_freedom =
+        Barycentre<Through, Mass>({degrees_of_freedom1, degrees_of_freedom2},
+                                  {body1_->mass(), body2_->mass()});
+    EXPECT_THAT(AbsoluteError(barycenter_degrees_of_freedom.position -
+                                  Position<Through>(),
+                              Displacement<Through>()),
+                Lt(40 * DBL_EPSILON * SIUnit<Length>())) << i;
+    EXPECT_THAT(AbsoluteError(barycenter_degrees_of_freedom.velocity,
+                              Velocity<Through>()),
+                Lt(140 * DBL_EPSILON * SIUnit<Speed>())) << i;
+
+    Length const length = (degrees_of_freedom1.position -
+                           degrees_of_freedom2.position).Norm();
+    EXPECT_THAT(length, AlmostEquals(2.0 * sqrt(5.0) * i * SIUnit<Length>()));
   }
 }
 
