2e8a94d8fcda6e3ba06d7e0d46cf4a03331ddc8c
Robin Leroy <egg.robin.leroy@gmail.com>
2014-11-29 17:27:41 +0100

    vessel changes

--- ksp_plugin/vessel.hpp
+++ ksp_plugin/vessel.hpp
@@ -37,12 +37,25 @@ class Vessel {
   Trajectory<Barycentric>* mutable_prolongation();
   void set_parent(Celestial const* parent);
 
-  // Creates a |history_| for this body and appends a point with the given
-  // |time| and |degrees_of_freedom|.
-  void CreateHistory(Instant const& time,
-                     DegreesOfFreedom<Barycentric> const& degrees_of_freedom);
+  // Creates an |owned_prolongation_| for this vessel and appends a point with
+  // the given |time| and |degrees_of_freedom|.  The vessel must have neither a
+  // |history_|, an |owned_prolongation_| nor a |prolongation_|.
+  void CreateProlongation(
+      Instant const& time,
+      DegreesOfFreedom<Barycentric> const& degrees_of_freedom);
 
-  // Deletes the |prolongation_| if there is one and forks a new one at |time|.
+  // Creates a |history_| for this vessel and appends a point with  with the
+  // given |time| and |degrees_of_freedom|, then forks a |prolongation_| at
+  // |time|.  Nulls |owned_prolongation_|.  The vessel must not have a
+  // |history_|. |*owned_prolongation_| is destroyed *after* |history_| has been
+  // constructed.
+  void CreateHistoryAndForkProlongation(
+      Instant const& time,
+      DegreesOfFreedom<Barycentric> const& degrees_of_freedom);
+
+  // Deletes the |prolongation_| and forks a new one at |time|.
+  // |history_| and |prolongation_| must not be null, |owned_prolongation_|
+  // must be null.
   void ResetProlongation(Instant const& time);
 
  private:
@@ -52,19 +65,18 @@ class Vessel {
   Celestial const* parent_;
   // The past and present trajectory of the body. It ends at |HistoryTime()|
   // unless |*this| was created after |HistoryTime()|, in which case it ends
-  // at |current_time_|.
+  // at |current_time_|.  It is advanced with a constant time step.
   std::unique_ptr<Trajectory<Barycentric>> history_;
-  // A child trajectory of |*history|. It is forked at |history->last_time()|
-  // and continues it until |current_time_|. It is computed with a
-  // non-constant timestep, which breaks symplecticity. |history| is advanced
-  // with a constant timestep as soon as possible, and |prolongation| is then
-  // restarted from this new end of |history|.
-  // Not owning, is null when the vessel is added and becomes non-null when
-  // |history| is next advanced for all vessels and celestials. In the
-  // meantime, |history| is advanced with small, non-constant timesteps to
-  // catch up with the synchronous constant-timestep integration.
-  // |this| is in |new_vessels_| if and only if |prolongation| is null.
+  // Most of the time, this is a child trajectory of |*history_|. It is forked
+  // at/ |history_->last_time()| and continues it until |current_time_|. It is
+  // computed with a non-constant timestep, which breaks symplecticity.
+  // If |history_| is null, this points to |owned_prolongation_| instead.
+  // Not owning.
   Trajectory<Barycentric>* prolongation_ = nullptr;
+  // When the vessel is added, before it is synchonized with the other vessels
+  // and celestials, there is no |history|.  The prolongation is directly owned
+  // during that time.  Null if, and only if, |history_| is not null.
+  std::unique_ptr<Trajectory<Barycentric>> owned_prolongation_;
 };
 
 }  // namespace ksp_plugin
--- ksp_plugin/vessel_body.hpp
+++ ksp_plugin/vessel_body.hpp
@@ -29,10 +29,6 @@ inline Trajectory<Barycentric> const& Vessel::prolongation() const {
   return *prolongation_;
 }
 
-inline Trajectory<Barycentric> const& Vessel::prolongation_or_history() const {
-    return prolongation_ == nullptr ? *history_ : *prolongation_;
-}
-
 inline Trajectory<Barycentric>* Vessel::mutable_history() {
   return history_.get();
 }
@@ -45,17 +41,32 @@ inline void Vessel::set_parent(Celestial const* parent) {
   parent_ = CHECK_NOTNULL(parent);
 }
 
-inline void Vessel::CreateHistory(
+inline void Vessel::CreateProlongation(
     Instant const& time,
     DegreesOfFreedom<Barycentric> const& degrees_of_freedom) {
+  CHECK(history_ == nullptr);
+  CHECK(prolongation_ == nullptr);
+  CHECK(owned_prolongation_ == nullptr);
+  owned_prolongation_ = std::make_unique<Trajectory<Barycentric>>(*body_);
+  owned_prolongation_->Append(time, degrees_of_freedom);
+  prolongation_ = owned_prolongation_.get();
+}
+
+inline void Vessel::CreateHistoryAndForkProlongation(
+    Instant const& time,
+    DegreesOfFreedom<Barycentric> const& degrees_of_freedom) {
+  CHECK(history_ == nullptr);
   history_ = std::make_unique<Trajectory<Barycentric>>(*body_);
   history_->Append(time, degrees_of_freedom);
+  prolongation_ = history_->Fork(time);
+  owned_prolongation_.reset();
 }
 
 inline void Vessel::ResetProlongation(Instant const& time) {
-  if (prolongation_ != nullptr) {
-    history_->DeleteFork(&prolongation_);
-  }
+  CHECK(history_ != nullptr);
+  CHECK(prolongation_ == nullptr);
+  CHECK(owned_prolongation_ == nullptr);
+  history_->DeleteFork(&prolongation_);
   prolongation_ = history_->Fork(time);
 }
 
