9b5059c24a218d84832c0c45b5a952f7fa588645
pleroy <phl+personal@google.com>
2014-11-29 16:32:04 +0100

    Test.

--- geometry/r3_element_body.hpp
+++ geometry/r3_element_body.hpp
@@ -3,7 +3,9 @@
 #include <string>
 
 #include "glog/logging.h"
-#include "quantities/elementary_functions.hpp"
+#include "quantities/quantities.hpp"
+
+using principia::quantities::SIUnit;
 
 namespace principia {
 namespace geometry {
@@ -88,8 +90,12 @@ template<typename Scalar>
 void R3Element<Scalar>::Orthogonalize(R3Element* r3_element) const {
   CHECK_NOTNULL(r3_element);
   Scalar const this_norm = this->Norm();
-  CHECK_NE(0, this_norm);
-  R3Element const this_normalized = *this / this_norm;
+  LOG(ERROR)<<this_norm;
+  CHECK_NE(0 * SIUnit<Scalar>(), this_norm);
+  R3Element<double> const this_normalized = *this / this_norm;
+  LOG(ERROR)<<this_normalized;
+  LOG(ERROR)<<Dot(*r3_element, this_normalized);
+  LOG(ERROR)<<Dot(*r3_element, this_normalized) * this_normalized;
   *r3_element -= Dot(*r3_element, this_normalized) * this_normalized;
 }
 
