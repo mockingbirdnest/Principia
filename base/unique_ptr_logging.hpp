026eb338651006bbe45d6dc5c550d67f91da1f7c
Robin Leroy <egg.robin.leroy@gmail.com>
2014-12-05 11:21:32 +0100

    comments

--- base/unique_ptr_logging.hpp
+++ base/unique_ptr_logging.hpp
@@ -2,6 +2,19 @@
 
 #include <memory>
 
+// We need to polllute the |::| namespace in order for argument-dependent lookup
+// to work, like glog/stl_logging.h.  The following caveat from
+// glog/stl_logging.h applies here too.
+
+// Note that if you want to use these operators from the non-global namespace,
+// you may get an error since they are not in namespace std (and they are not
+// in namespace std since that would result in undefined behavior).  You may
+// need to write
+//
+//   using ::operator<<;
+//
+// to fix these errors.
+
 // Logs the pointer.  No transfer of ownership.
 template <typename T, typename U>
 std::ostream& operator<<(std::ostream& out,
