45295a646151b1babbfd8e89a5bdfe5081aea4c8
pleroy <phl+personal@google.com>
2014-12-16 21:53:37 +0100

    A bit of cleanup.

--- ksp_plugin/physics_bubble.hpp
+++ ksp_plugin/physics_bubble.hpp
@@ -19,51 +19,47 @@ class PhysicsBubble {
   using PlanetariumRotationXXX = geometry::Rotation<Barycentric, WorldSun>;
 
   //TODO(phl): Fix \o/ ALL \o/ the comments.
-  PhysicsBubble();
-  ~PhysicsBubble();
+  PhysicsBubble() = default;
+  ~PhysicsBubble() = default;
 
-  // If |next_physics_bubble_| is not null, computes the world centre of mass,
-  // trajectory (including intrinsic acceleration) of |*next_physics_bubble_|.
-  // Moves |next_physics_bubble_| into |current_physics_bubble_|.
+  // If |next_| is not null, computes the world centre of mass, trajectory
+  // (including intrinsic acceleration) of |*next_|. Moves |next_| into
+  // |current_|.
+  // TODO(phl): Document the parameters!
   void Prepare(PlanetariumRotationXXX const& planetarium_rotation,
                Instant const& current_time,
                Instant const& next_time);
 
  private:
   // Computes the world degrees of freedom of the centre of mass of
-  // |next_physics_bubble_| using the contents of |next_physics_bubble_->parts|.
-  // |next_physics_bubble_| must not be null.
+  // |next_| using the contents of |next_->parts|.  |next_| must not be null.
   void ComputeNextCentreOfMassWorldDegreesOfFreedom();
 
-  // Computes |next_physics_bubble_->displacements_from_centre_of_mass| and
-  // |next_physics_bubble_->velocities_from_centre_of_mass|.
-  // |next_physics_bubble_| must not be null.
+  // Computes |next_->displacements_from_centre_of_mass| and
+  // |next_->velocities_from_centre_of_mass|.  |next_| must not be null.
   void ComputeNextVesselOffsets(
       PlanetariumRotationXXX const& planetarium_rotation);
 
-  // Creates |next_physics_bubble_->centre_of_mass_trajectory| and appends to it
-  // the barycentre of the degrees of freedom of the vessels in
-  // |next_physics_bubble_->vessels|.  There is no intrinsic acceleration.
-  // |next_physics_bubble_| must not be null.
+  // Creates |next_->centre_of_mass_trajectory| and appends to it the barycentre
+  // of the degrees of freedom of the vessels in |next_->vessels|.  There is no
+  // intrinsic acceleration.  |next_| must not be null.
   void RestartNext(Instant const& current_time);
 
   // Returns the intrinsic acceleration measured on the parts that are common to
-  // the current and next physics bubbles.  Stores a pair of pointers to parts
+  // the current and next bubbles.  Stores a pair of pointers to parts
   // (current, next) in |common_parts| for all parts common to the current and
-  // next physics bubble.
-  // |common_parts| must not be null.  |*common_parts| must be empty.  No
-  // transfer of ownership.
+  // next bubbles.  |common_parts| must not be null.  |*common_parts| must be
+  // empty.  No transfer of ownership.
   Vector<Acceleration, World> IntrinsicAcceleration(
       Instant const& current_time,
       Instant const& next_time,
       std::vector<PartCorrespondence>* const common_parts);
 
   // Given the vector of common parts produced by |IntrinsicAcceleration|,
-  // constructs |*next_physics_bubble_->centre_of_mass_trajectory| and appends
-  // degrees of freedom at |current_time_| that conserve the degrees of freedom
-  // of the centre of mass of the parts in |common_parts|.
-  // |common_parts| must not be null.  |next_physics_bubble_| must not be null.
-  // No transfer of ownership.
+  // constructs |*next_->centre_of_mass_trajectory| and appends degrees of
+  // freedom at |current_time| that conserve the degrees of freedom of the
+  // centre of mass of the parts in |common_parts|. |common_parts| must not be
+  // null.  |next_| must not be null.  No transfer of ownership.
   void Shift(PlanetariumRotationXXX const& planetarium_rotation,
              Instant const& current_time,
              std::vector<PartCorrespondence> const* const common_parts);
@@ -83,8 +79,8 @@ class PhysicsBubble {
     std::unique_ptr<
         std::map<Vessel const* const,
                  Velocity<Barycentric>>> velocities_from_centre_of_mass;
-    std::unique_ptr<Displacement<World>> displacement_correction;
-    std::unique_ptr<Velocity<World>> velocity_correction;
+    std::unique_ptr<Displacement<World>> displacement_correction;  // Only for current_.
+    std::unique_ptr<Velocity<World>> velocity_correction;  // Only for current_.
   };
 
   //TODO(phl): See what's used exactly for next_.
