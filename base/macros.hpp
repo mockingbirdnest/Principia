106bb880cd6625d9419fc553316f6d43561e9818
Robin Leroy <egg.robin.leroy@gmail.com>
2014-12-17 16:32:52 +0100

    compiler, compiler version, os, arch strings

--- base/macros.hpp
+++ base/macros.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 namespace principia {
 namespace base {
 
@@ -48,6 +50,36 @@ inline void noreturn() { exit(0); }
 #define FORCE_INLINE [[gnu::always_inline]]  // NOLINT(whitespace/braces)
 #elif _MSC_VER
 #define FORCE_INLINE __forceinline
+#endif
+
+#if defined(__clang__)
+char const* const kCompilerName = "clang";
+char const* const kCompilerVersion = __clang_version__;
+#elif defined(_MSC_VER)
+char const* const kCompilerName = "Microsoft C/C++";
+char const* const kCompilerVersion = std::to_string(_MSC_FULL_VER).c_str();
+#endif
+
+#if defined(__APPLE__)
+char const* const kOperatingSystem = "OS X";
+#elif defined(__linux__)
+// We don't care what Stallman calls it.
+char const* const kOperatingSystem = "Linux";
+#elif defined(__FreeBSD__)
+char const* const kOperatingSystem = "FreeBSD";
+#elif defined(__OpenBSD__)
+// There should be a BoringBSD...
+char const* const kOperatingSystem = "OpenBSD";
+#elif defined(_WIN32)
+char const* const kOperatingSystem = "Windows";
+#endif
+
+#if defined(__i386) || defined(_M_IX86)
+char const* const kArchitecture = "x86";
+#elif defined(_M_X64) || defined(__x86_64__)
+char const* const kArchitecture = "x86-64";
+#else
+#error "Have you tried a PDP-11?"
 #endif
 
 #define VLOG_AND_RETURN(verboselevel, expression)                  \
