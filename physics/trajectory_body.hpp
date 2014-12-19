7c4b4a51b64d8509d555dd7e62761a4edae5bc31
Robin Leroy <egg.robin.leroy@gmail.com>
2014-12-19 01:25:50 +0100

    Aaand we need a TEMPLATE too.

--- physics/trajectory_body.hpp
+++ physics/trajectory_body.hpp
@@ -46,7 +46,7 @@ typename Trajectory<Frame>::NativeIterator Trajectory<Frame>::last() const {
 
 template<typename Frame>
 template<typename ToFrame>
-typename Trajectory<Frame>::template TransformingIterator<ToFrame>
+typename Trajectory<Frame>::TEMPLATE TransformingIterator<ToFrame>
 Trajectory<Frame>::first_with_transform(
     Transform<ToFrame> const& transform) const {
   TransformingIterator<ToFrame> it(transform);
@@ -56,7 +56,7 @@ Trajectory<Frame>::first_with_transform(
 
 template<typename Frame>
 template<typename ToFrame>
-typename Trajectory<Frame>::template TransformingIterator<ToFrame>
+typename Trajectory<Frame>::TEMPLATE TransformingIterator<ToFrame>
 Trajectory<Frame>::on_or_after_with_transform(
     Instant const& time,
     Transform<ToFrame> const& transform) const {
@@ -67,7 +67,7 @@ Trajectory<Frame>::on_or_after_with_transform(
 
 template<typename Frame>
 template<typename ToFrame>
-typename Trajectory<Frame>::template TransformingIterator<ToFrame>
+typename Trajectory<Frame>::TEMPLATE TransformingIterator<ToFrame>
 Trajectory<Frame>::last_with_transform(
     Transform<ToFrame> const& transform) const {
   TransformingIterator<ToFrame> it(transform);
