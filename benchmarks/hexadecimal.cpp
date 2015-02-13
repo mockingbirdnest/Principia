ee7c8f6c67f18d4f3c1a0403519cd5f266dd8495
Robin Leroy <egg.robin.leroy@gmail.com>
2015-02-14 00:11:05 +0100

    lorem ipsum, says the bear.

--- benchmarks/hexadecimal.cpp
+++ benchmarks/hexadecimal.cpp
@@ -13,51 +13,53 @@ using principia::base::HexadecimalEncode;
 namespace principia {
 namespace benchmarks {
 
-static char const kPiHexadecimalDigits[] =
-"3243f6a8885a308d313198a2e03707344a4093822299f31d0082efa98ec4e6c89452821e63"
-"8d01377be5466cf34e90c6cc0ac29b7c97c50dd3f84d5b5b54709179216d5d98979fb1bd13"
-"10ba698dfb5ac2ffd72dbd01adfb7b8e1afed6a267e96ba7c9045f12c7f9924a19947b3916"
-"cf70801f2e2858efc16636920d871574e69a458fea3f4933d7e0d95748f728eb658718bcd5"
-"882154aee7b54a41dc25a59b59c30d5392af26013c5d1b023286085f0ca417918b8db38ef8"
-"e79dcb0603a180e6c9e0e8bb01e8a3ed71577c1bd314b2778af2fda55605c60e65525f3aa5"
-"5ab945748986263e8144055ca396a2aab10b6b4cc5c341141e8cea15486af7c72e993b3ee1"
-"411636fbc2a2ba9c55d741831f6ce5c3e169b87931eafd6ba336c24cf5c7a3253812895867"
-"73b8f48986b4bb9afc4bfe81b6628219361d809ccfb21a991487cac605dec8032ef845d5de"
-"98575b1dc262302eb651b8823893e81d396acc50f6d6ff383f442392e0b4482a484200469c"
-"8f04a9e1f9b5e21c66842f6e96c9a670c9c61abd388f06a51a0d2d8542f68960fa728ab513"
-"3a36eef0b6c137a3be4ba3bf0507efb2a98a1f1651d39af017666ca593e82430e888cee861"
-"9456f9fb47d84a5c33b8b5ebee06f75d885c12073401a449f56c16aa64ed3aa62363f77061"
-"bfedf72429b023d37d0d724d00a1248db0fead3";
+static char const kPi1000HexadecimalDigits[] =
+    "3243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89452821E63"
+    "8D01377BE5466CF34E90C6CC0AC29B7C97C50DD3F84D5B5B54709179216D5D98979FB1BD13"
+    "10BA698DFB5AC2FFD72DBD01ADFB7B8E1AFED6A267E96BA7C9045F12C7F9924A19947B3916"
+    "CF70801F2E2858EFC16636920D871574E69A458FEA3F4933D7E0D95748F728EB658718BCD5"
+    "882154AEE7B54A41DC25A59B59C30D5392AF26013C5D1B023286085F0CA417918B8DB38EF8"
+    "E79DCB0603A180E6C9E0E8BB01E8A3ED71577C1BD314B2778AF2FDA55605C60E65525F3AA5"
+    "5AB945748986263E8144055CA396A2AAB10B6B4CC5C341141E8CEA15486AF7C72E993B3EE1"
+    "411636FBC2A2BA9C55D741831F6CE5C3E169B87931EAFD6BA336C24CF5C7A3253812895867"
+    "73B8F48986B4BB9AFC4BFE81B6628219361D809CCFB21A991487CAC605DEC8032EF845D5DE"
+    "98575B1DC262302EB651B8823893E81D396ACC50F6D6FF383F442392E0B4482A484200469C"
+    "8F04A9E1F9B5E21C66842F6E96C9A670C9C61ABD388F06A51A0D2D8542F68960FA728AB513"
+    "3A36EEF0B6C137A3BE4BA3BF0507EFB2A98A1F1651D39AF017666CA593E82430E888CEE861"
+    "9456F9FB47D84A5C33B8B5EBEE06F75D885C12073401A449F56C16AA64ED3AA62363F77061"
+    "BFEDF72429B023D37D0D724D00A1248DB0FEAD";
 
-static char const kPiBytes[] =
-"\x32\x43\xf6\xa8\x88\x5a\x30\x8d\x31\x31\x98\xa2\xe0\x37\x07\x34\x4a\x40"
-"\x93\x82\x22\x99\xf3\x1d\x00\x82\xef\xa9\x8e\xc4\xe6\xc8\x94\x52\x82\x1e"
-"\x63\x8d\x01\x37\x7b\xe5\x46\x6c\xf3\x4e\x90\xc6\xcc\x0a\xc2\x9b\x7c\x97"
-"\xc5\x0d\xd3\xf8\x4d\x5b\x5b\x54\x70\x91\x79\x21\x6d\x5d\x98\x97\x9f\xb1"
-"\xbd\x13\x10\xba\x69\x8d\xfb\x5a\xc2\xff\xd7\x2d\xbd\x01\xad\xfb\x7b\x8e"
-"\x1a\xfe\xd6\xa2\x67\xe9\x6b\xa7\xc9\x04\x5f\x12\xc7\xf9\x92\x4a\x19\x94"
-"\x7b\x39\x16\xcf\x70\x80\x1f\x2e\x28\x58\xef\xc1\x66\x36\x92\x0d\x87\x15"
-"\x74\xe6\x9a\x45\x8f\xea\x3f\x49\x33\xd7\xe0\xd9\x57\x48\xf7\x28\xeb\x65"
-"\x87\x18\xbc\xd5\x88\x21\x54\xae\xe7\xb5\x4a\x41\xdc\x25\xa5\x9b\x59\xc3"
-"\x0d\x53\x92\xaf\x26\x01\x3c\x5d\x1b\x02\x32\x86\x08\x5f\x0c\xa4\x17\x91"
-"\x8b\x8d\xb3\x8e\xf8\xe7\x9d\xcb\x06\x03\xa1\x80\xe6\xc9\xe0\xe8\xbb\x01"
-"\xe8\xa3\xed\x71\x57\x7c\x1b\xd3\x14\xb2\x77\x8a\xf2\xfd\xa5\x56\x05\xc6"
-"\x0e\x65\x52\x5f\x3a\xa5\x5a\xb9\x45\x74\x89\x86\x26\x3e\x81\x44\x05\x5c"
-"\xa3\x96\xa2\xaa\xb1\x0b\x6b\x4c\xc5\xc3\x41\x14\x1e\x8c\xea\x15\x48\x6a"
-"\xf7\xc7\x2e\x99\x3b\x3e\xe1\x41\x16\x36\xfb\xc2\xa2\xba\x9c\x55\xd7\x41"
-"\x83\x1f\x6c\xe5\xc3\xe1\x69\xb8\x79\x31\xea\xfd\x6b\xa3\x36\xc2\x4c\xf5"
-"\xc7\xa3\x25\x38\x12\x89\x58\x67\x73\xb8\xf4\x89\x86\xb4\xbb\x9a\xfc\x4b"
-"\xfe\x81\xb6\x62\x82\x19\x36\x1d\x80\x9c\xcf\xb2\x1a\x99\x14\x87\xca\xc6"
-"\x05\xde\xc8\x03\x2e\xf8\x45\xd5\xde\x98\x57\x5b\x1d\xc2\x62\x30\x2e\xb6"
-"\x51\xb8\x82\x38\x93\xe8\x1d\x39\x6a\xcc\x50\xf6\xd6\xff\x38\x3f\x44\x23"
-"\x92\xe0\xb4\x48\x2a\x48\x42\x00\x46\x9c\x8f\x04\xa9\xe1\xf9\xb5\xe2\x1c"
-"\x66\x84\x2f\x6e\x96\xc9\xa6\x70\xc9\xc6\x1a\xbd\x38\x8f\x06\xa5\x1a\x0d"
-"\x2d\x85\x42\xf6\x89\x60\xfa\x72\x8a\xb5\x13\x3a\x36\xee\xf0\xb6\xc1\x37"
-"\xa3\xbe\x4b\xa3\xbf\x05\x07\xef\xb2\xa9\x8a\x1f\x16\x51\xd3\x9a\xf0\x17"
-"\x66\x6c\xa5\x93\xe8\x24\x30\xe8\x88\xce\xe8\x61\x94\x56\xf9\xfb\x47\xd8"
-"\x4a\x5c\x33\xb8\xb5\xeb\xee\x06\xf7\x5d\x88\x5c\x12\x07\x34\x01\xa4\x49"
-"\xf5\x6c\x16\xaa\x64\xed\x3a\xa6\x23\x63\xf7\x70\x61\xbf\xed\xf7\x24\x29"
-"\xb0\x23\xd3\x7d\x0d\x72\x4d\x00\xa1\x24\x8d\xb0\xfe\xad";
+static char const kPi500Bytes[] =
+    "\x32\x43\xF6\xA8\x88\x5A\x30\x8D\x31\x31\x98\xA2\xE0\x37\x07\x34\x4A\x40"
+    "\x93\x82\x22\x99\xF3\x1D\x00\x82\xEF\xA9\x8E\xC4\xE6\xC8\x94\x52\x82\x1E"
+    "\x63\x8D\x01\x37\x7B\xE5\x46\x6C\xF3\x4E\x90\xC6\xCC\x0A\xC2\x9B\x7C\x97"
+    "\xC5\x0D\xD3\xF8\x4D\x5B\x5B\x54\x70\x91\x79\x21\x6D\x5D\x98\x97\x9F\xB1"
+    "\xBD\x13\x10\xBA\x69\x8D\xFB\x5A\xC2\xFF\xD7\x2D\xBD\x01\xAD\xFB\x7B\x8E"
+    "\x1A\xFE\xD6\xA2\x67\xE9\x6B\xA7\xC9\x04\x5F\x12\xC7\xF9\x92\x4A\x19\x94"
+    "\x7B\x39\x16\xCF\x70\x80\x1F\x2E\x28\x58\xEF\xC1\x66\x36\x92\x0D\x87\x15"
+    "\x74\xE6\x9A\x45\x8F\xEA\x3F\x49\x33\xD7\xE0\xD9\x57\x48\xF7\x28\xEB\x65"
+    "\x87\x18\xBC\xD5\x88\x21\x54\xAE\xE7\xB5\x4A\x41\xDC\x25\xA5\x9B\x59\xC3"
+    "\x0D\x53\x92\xAF\x26\x01\x3C\x5D\x1B\x02\x32\x86\x08\x5F\x0C\xA4\x17\x91"
+    "\x8B\x8D\xB3\x8E\xF8\xE7\x9D\xCB\x06\x03\xA1\x80\xE6\xC9\xE0\xE8\xBB\x01"
+    "\xE8\xA3\xED\x71\x57\x7C\x1B\xD3\x14\xB2\x77\x8A\xF2\xFD\xA5\x56\x05\xC6"
+    "\x0E\x65\x52\x5F\x3A\xA5\x5A\xB9\x45\x74\x89\x86\x26\x3E\x81\x44\x05\x5C"
+    "\xA3\x96\xA2\xAA\xB1\x0B\x6B\x4C\xC5\xC3\x41\x14\x1E\x8C\xEA\x15\x48\x6A"
+    "\xF7\xC7\x2E\x99\x3B\x3E\xE1\x41\x16\x36\xFB\xC2\xA2\xBA\x9C\x55\xD7\x41"
+    "\x83\x1F\x6C\xE5\xC3\xE1\x69\xB8\x79\x31\xEA\xFD\x6B\xA3\x36\xC2\x4C\xF5"
+    "\xC7\xA3\x25\x38\x12\x89\x58\x67\x73\xB8\xF4\x89\x86\xB4\xBB\x9A\xFC\x4B"
+    "\xFE\x81\xB6\x62\x82\x19\x36\x1D\x80\x9C\xCF\xB2\x1A\x99\x14\x87\xCA\xC6"
+    "\x05\xDE\xC8\x03\x2E\xF8\x45\xD5\xDE\x98\x57\x5B\x1D\xC2\x62\x30\x2E\xB6"
+    "\x51\xB8\x82\x38\x93\xE8\x1D\x39\x6A\xCC\x50\xF6\xD6\xFF\x38\x3F\x44\x23"
+    "\x92\xE0\xB4\x48\x2A\x48\x42\x00\x46\x9C\x8F\x04\xA9\xE1\xF9\xB5\xE2\x1C"
+    "\x66\x84\x2F\x6E\x96\xC9\xA6\x70\xC9\xC6\x1A\xBD\x38\x8F\x06\xA5\x1A\x0D"
+    "\x2D\x85\x42\xF6\x89\x60\xFA\x72\x8A\xB5\x13\x3A\x36\xEE\xF0\xB6\xC1\x37"
+    "\xA3\xBE\x4B\xA3\xBF\x05\x07\xEF\xB2\xA9\x8A\x1F\x16\x51\xD3\x9A\xF0\x17"
+    "\x66\x6C\xA5\x93\xE8\x24\x30\xE8\x88\xCE\xE8\x61\x94\x56\xF9\xFB\x47\xD8"
+    "\x4A\x5C\x33\xB8\xB5\xEB\xEE\x06\xF7\x5D\x88\x5C\x12\x07\x34\x01\xA4\x49"
+    "\xF5\x6C\x16\xAA\x64\xED\x3A\xA6\x23\x63\xF7\x70\x61\xBF\xED\xF7\x24\x29"
+    "\xB0\x23\xD3\x7D\x0D\x72\x4D\x00\xA1\x24\x8D\xB0\xFE\xAD";
+
+int const kCopiesOfPi = 10000;
 
 template<typename Container>
 std::enable_if_t<
@@ -72,7 +74,6 @@ HexadecimalEncodeNoMemcpy(Container const& input, not_null<Container*> output) {
   // only works for |std::vector| (it is read-only in a |std::basic_string|).
   char* digit = &digits[0];
   for (uint8_t const byte : bytes) {
-    // The following is four times faster than copying both bytes by hand.
     const char* reference_digit = base::kByteToHexadecimalDigits[byte];
     *digit = *reference_digit;
     *++digit = *++reference_digit;
@@ -81,22 +82,35 @@ HexadecimalEncodeNoMemcpy(Container const& input, not_null<Container*> output) {
   *output = std::move(digits);
 }
 
-void EncodePiNoMemcpy(not_null<benchmark::State*> const state,
-              not_null<bool*> const correct) {
+void HexEncodeNoMemcpy(not_null<benchmark::State*> const state,
+                      not_null<bool*> const correct,
+                      std::string const& input_bytes,
+                      std::string const& expected_digits,
+                      std::string* const digits) {
+  HexadecimalEncodeNoMemcpy<std::string>(input_bytes, digits);
   state->PauseTiming();
-  std::string const pi_bytes(kPiBytes, 500);
-  std::string digits;
-  state->ResumeTiming();
-  HexadecimalEncodeNoMemcpy<std::string>(pi_bytes, &digits);
-  state->PauseTiming();
-  *correct = digits == kPiHexadecimalDigits;
+  *correct = *digits == expected_digits;
   state->ResumeTiming();
 }
 
 void BM_EncodePiNoMemcpy(benchmark::State& state) {  // NOLINT(runtime/references)
   bool correct;
+  state.PauseTiming();
+  std::string const pi_bytes(kPi500Bytes, 500);
+  std::string input_bytes;
+  std::string expected_digits;
+  input_bytes.reserve(500 * kCopiesOfPi);
+  expected_digits.reserve(1000 * kCopiesOfPi);
+  for (int i = kCopiesOfPi; i > 0; --i) {
+    input_bytes += pi_bytes;
+    expected_digits += kPi1000HexadecimalDigits;
+  }
+  state.ResumeTiming();
   while (state.KeepRunning()) {
-    EncodePiNoMemcpy(&state, &correct);
+    state.PauseTiming();
+    std::string buffer;
+    state.ResumeTiming();
+    HexEncodeNoMemcpy(&state, &correct, input_bytes, expected_digits, &buffer);
   }
   std::stringstream ss;
   ss << correct;
@@ -105,22 +119,35 @@ void BM_EncodePiNoMemcpy(benchmark::State& state) {  // NOLINT(runtime/reference
 
 BENCHMARK(BM_EncodePiNoMemcpy);
 
-void EncodePi(not_null<benchmark::State*> const state,
-              not_null<bool*> const correct) {
+void HexEncode(not_null<benchmark::State*> const state,
+              not_null<bool*> const correct,
+              std::string const& input_bytes,
+              std::string const& expected_digits,
+              std::string* const digits) {
+  HexadecimalEncode<std::string>(input_bytes, digits);
   state->PauseTiming();
-  std::string const pi_bytes(kPiBytes, 500);
-  std::string digits;
-  state->ResumeTiming();
-  HexadecimalEncode<std::string>(pi_bytes, &digits);
-  state->PauseTiming();
-  *correct = digits == kPiHexadecimalDigits;
+  *correct = *digits == expected_digits;
   state->ResumeTiming();
 }
 
 void BM_EncodePi(benchmark::State& state) {  // NOLINT(runtime/references)
   bool correct;
+  state.PauseTiming();
+  std::string const pi_bytes(kPi500Bytes, 500);
+  std::string input_bytes;
+  std::string expected_digits;
+  input_bytes.reserve(500 * kCopiesOfPi);
+  expected_digits.reserve(1000 * kCopiesOfPi);
+  for (int i = kCopiesOfPi; i > 0; --i) {
+    input_bytes += pi_bytes;
+    expected_digits += kPi1000HexadecimalDigits;
+  }
+  state.ResumeTiming();
   while (state.KeepRunning()) {
-    EncodePi(&state, &correct);
+    state.PauseTiming();
+    std::string buffer;
+    state.ResumeTiming();
+    HexEncode(&state, &correct, input_bytes, expected_digits, &buffer);
   }
   std::stringstream ss;
   ss << correct;
@@ -128,6 +155,58 @@ void BM_EncodePi(benchmark::State& state) {  // NOLINT(runtime/references)
 }
 
 BENCHMARK(BM_EncodePi);
+
+void BM_EncodePiNoMemcpyPreallocated(
+    benchmark::State& state) {  // NOLINT(runtime/references)
+  bool correct;
+  state.PauseTiming();
+  std::string const pi_bytes(kPi500Bytes, 500);
+  std::string input_bytes;
+  std::string expected_digits;
+  input_bytes.reserve(500 * kCopiesOfPi);
+  expected_digits.reserve(1000 * kCopiesOfPi);
+  for (int i = kCopiesOfPi; i > 0; --i) {
+    input_bytes += pi_bytes;
+    expected_digits += kPi1000HexadecimalDigits;
+  }
+  std::string buffer;
+  buffer.resize(expected_digits.size());
+  state.ResumeTiming();
+  while (state.KeepRunning()) {
+    HexEncodeNoMemcpy(&state, &correct, input_bytes, expected_digits, &buffer);
+  }
+  std::stringstream ss;
+  ss << correct;
+  state.SetLabel(ss.str());
+}
+
+BENCHMARK(BM_EncodePiNoMemcpyPreallocated);
+
+void BM_EncodePiPreallocated(
+    benchmark::State& state) {  // NOLINT(runtime/references)
+  bool correct;
+  state.PauseTiming();
+  std::string const pi_bytes(kPi500Bytes, 500);
+  std::string input_bytes;
+  std::string expected_digits;
+  input_bytes.reserve(500 * kCopiesOfPi);
+  expected_digits.reserve(1000 * kCopiesOfPi);
+  for (int i = kCopiesOfPi; i > 0; --i) {
+    input_bytes += pi_bytes;
+    expected_digits += kPi1000HexadecimalDigits;
+  }
+  std::string buffer;
+  buffer.resize(expected_digits.size());
+  state.ResumeTiming();
+  while (state.KeepRunning()) {
+    HexEncode(&state, &correct, input_bytes, expected_digits, &buffer);
+  }
+  std::stringstream ss;
+  ss << correct;
+  state.SetLabel(ss.str());
+}
+
+BENCHMARK(BM_EncodePiPreallocated);
 
 }  // namespace benchmarks
 }  // namespace principia
