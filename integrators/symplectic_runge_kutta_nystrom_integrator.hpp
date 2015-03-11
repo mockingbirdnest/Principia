1d7ca8ee3561cef5257b10df58cb535cc0e55d75
Robin Leroy <egg.robin.leroy@gmail.com>
2015-03-11 03:16:07 +0100

    add the file

---
+++ integrators/symplectic_runge_kutta_nystrom_integrator.hpp
@@ -0,0 +1,106 @@
+﻿
+#pragma once
+
+#include <vector>
+
+#include "base/not_null.hpp"
+#include "integrators/symplectic_integrator.hpp"
+
+namespace principia {
+
+using base::not_null;
+
+namespace integrators {
+
+// The integrator defined in this class integrates Hamilton's equations for
+// Hamiltonians of the form
+//   H(q, p, t) = T(q, p) + V(q, t),
+// with T quadratic in p, i.e.,
+//  T(q, p) = M(q)⁻¹(p, p)
+// where M(q)⁻¹ is a quadratic form.
+// The flows of T and V must be known.
+// A special case is provided for the case where M(q)⁻¹ is the standard metric
+// and ∂V/∂qᵢ(q, t) is known.
+
+// NOTE(egg): only the latter is implemented at this time.
+
+// TODO(egg): a blurb on non-autonomy, it's slightly trickier than in the SPRK
+// case.
+
+// We follow the convention of McLachlan & Atela, calling the position nodes
+// aᵢ and the momentum nodes bᵢ.  The following notations appear in the
+// litterature:
+//   (a, b) in McLachlan & Atela, as well as Candy & Rozmus;
+//   (d, c) in Ruth, Yoshida, as well as Forest & Ruth;
+//   (B, b) in Sofroniou & Spaletta.
+// Moreover, we follow the convention of Sofroniou & Spaletta in calling the
+// weights used for the time argument of the force computation cᵢ, with
+// c₁ = 0, cᵢ = cᵢ₋₁ + aᵢ₋₁ for i > 1.
+
+enum VanishingCoefficients {
+  kNone,
+  kFirstBVanishes,
+  kLastAVanishes,
+};
+
+class SRKNIntegrator : public SymplecticIntegrator {
+ protected:
+  enum VanishingCoefficients {
+   kNone,
+   kFirstBVanishes,
+   kLastAVanishes,
+ };
+
+ public:
+
+  // The functor |compute_force| computes ∂V/∂qᵢ(q, t).
+  template<typename Position, typename Momentum,
+           typename RightHandSideComputation>
+  void SolveQuadraticKineticEnergy(
+      RightHandSideComputation compute_force,
+      Parameters<Position, Momentum> const& parameters,
+      not_null<Solution<Position, Momentum>*> const solution) const;
+
+  // The functor |compute_force| computes ∂V/∂qᵢ(q, t).
+  template<typename Position, typename Velocity,
+           typename RightHandSideComputation>
+  void SolveTrivialKineticEnergyIncrement(
+      RightHandSideComputation compute_force,
+      Parameters<Position, Velocity> const& parameters,
+      not_null<Solution<Position, Velocity>*> const solution) const;
+
+ protected:
+  struct FirstSameAsLast {
+    double first;
+    double last;
+  };
+
+  VanishingCoefficients vanishing_coefficients_;
+  // Null if, and only if, |vanishing_coefficients_ == kNone|.
+  // If |vanishing_coefficients_ == kFirstBVanishes|, this contains the first
+  // and last a coefficients.
+  // If |vanishing_coefficients_ == FirstAVanishes|, this contains the first
+  // and last b coefficients.
+  // These are used to desynchronize and synchronize first-same-as-last
+  // integrators.
+  std::unique_ptr<FirstSameAsLast> first_same_as_last_
+
+  int const stages_;
+
+  // The position and momentum nodes.
+  // If |vanishing_coefficients_ == kFirstBVanishes|, we do not store the first
+  // b coefficient, and the last entry of |a_| is the sum of the first and last
+  // a coefficients.
+  std::vector<double> a_;
+  std::vector<double> b_;
+
+  // The weights.  Note that the first c coefficient is not stored if
+  // |vanishing_coefficients_ == kFirstBVanishes|.
+  std::vector<double> c_;
+ private:
+};
+
+}  // namespace integrators
+}  // namespace principia
+
+#include "integrators/symplectic_partitioned_runge_kutta_integrator_body.hpp"
