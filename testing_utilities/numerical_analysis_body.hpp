fa995ddde630339b703570a30d8a951ba2e54649
Robin Leroy <egg.robin.leroy@gmail.com>
2015-03-22 01:26:48 +0100

    rhs

--- testing_utilities/numerical_analysis_body.hpp
+++ testing_utilities/numerical_analysis_body.hpp
@@ -2,17 +2,25 @@
 
 #include<vector>
 
+#include "geometry/named_quantities.hpp"
 #include "quantities/quantities.hpp"
 #include "quantities/named_quantities.hpp"
 
 namespace principia {
 
+using geometry::Displacement;
+using geometry::InnerProduct;
+using geometry::Position;
+using geometry::Vector;
+using geometry::Velocity;
 using quantities::Force;
+using quantities::GravitationalParameter;
 using quantities::Length;
 using quantities::Mass;
 using quantities::Momentum;
 using quantities::SIUnit;
 using quantities::Speed;
+using quantities::Sqrt;
 using quantities::Stiffness;
 using quantities::Time;
 
@@ -36,6 +44,18 @@ inline void ComputeHarmonicOscillatorAcceleration(
     std::vector<Length> const& q,
     std::vector<Acceleration>* const result) {
   (*result)[0] = -q[0] * (SIUnit<Stiffness>() / SIUnit<Mass>());
+}
+
+template<typename Frame>
+inline void ComputeKeplerAcceleration(
+    Time const& t,
+    std::vector<Position<Frame>> const& q,
+    std::vector<Vector<Acceleration, Frame>>* const) {
+  Displacement<Frame> const r = q[1] - q[0];
+  Length const r_squared = InnerProduct(r, r);
+  Length const r_norm = Sqrt(r_squared);
+  (*result)[0] = SIUnit<GravitationalParameter>() * r / (r_squared * r_norm);
+  (*result)[1] = -(*result)[0];
 }
 
 }  // namespace testing_utilities
