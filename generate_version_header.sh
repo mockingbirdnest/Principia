6e6f6562156cfac727f4a941ebb45e0c5ead3f9e
John Turner <groups@norgg.org>
2015-03-26 15:35:42 +0000

    Ensure utc for build date.

--- generate_version_header.sh
+++ generate_version_header.sh
@@ -4,13 +4,14 @@ namespace principia {
     namespace base {
 
     char const kBuildDate[] = \"%%DATE%%\";
-    char const kVersion[] = \"%%VERSION%%\";
+    char const kVersion[] = 
+        \"%%VERSION%%\";
 
     }  // namespace base
 }  // namespace principia"
 
 echo -e "$VERSION_TEMPLATE" | 
-sed "s/%%DATE%%/`date`/" |
+sed "s/%%DATE%%/`date -u`/" |
 sed "s/%%VERSION%%/`git describe --tags --always --dirty --abbrev=40 --long`/" > base/version.hpp
 #(get-date).ToUniversalTime(),
 #(git describe --tags --always --dirty --abbrev=40 --long))
