6fbfaa153d06749fce6d63feaa63e5e8f6c8094e
pleroy <phl+personal@google.com>
2015-05-02 14:23:38 +0200

    Plugin error test.

--- ksp_plugin_test/plugin_test.cpp
+++ ksp_plugin_test/plugin_test.cpp
@@ -395,6 +395,17 @@ TEST_F(PluginDeathTest, AdvanceTimeError) {
   }, "Check failed: !initializing");
 }
 
+TEST_F(PluginDeathTest, ForgetAllHistoriesBeforeError) {
+  EXPECT_DEATH({
+    EXPECT_CALL(*n_body_system_, Integrate(_, _, _, _, _, _)).Times(2);
+    Instant const t(1 * Second);
+    InsertAllSolarSystemBodies();
+    plugin_->EndInitialization();
+    plugin_->AdvanceTime(t, Angle());
+    plugin_->ForgetAllHistoriesBefore(t);
+  }, "Check failed: t < HistoryTime");
+}
+
 TEST_F(PluginDeathTest, VesselFromParentError) {
   GUID const guid = "Test Satellite";
   EXPECT_DEATH({
