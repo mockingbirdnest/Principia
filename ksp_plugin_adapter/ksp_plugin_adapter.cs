f6baa8027f26919f056f67222add424d061cfa4b
Robin Leroy <egg.robin.leroy@gmail.com>
2015-05-05 20:54:57 +0200

    also lengthen the history

--- ksp_plugin_adapter/ksp_plugin_adapter.cs
+++ ksp_plugin_adapter/ksp_plugin_adapter.cs
@@ -67,7 +67,7 @@ public partial class PrincipiaPluginAdapter : ScenarioModule {
       {1 << 10, 1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15, 1 << 16, 1 << 17,
        1 << 18, 1 << 19, 1 << 20, 1 << 21, 1 << 22, 1 << 23, 1 << 24, 1 << 25,
        1 << 26, 1 << 27, 1 << 28, 1 << 29, double.PositiveInfinity};
-  private int history_length_index_ = 6;
+  private int history_length_index_ = 10;
 
   private bool show_reference_frame_selection_ = true;
   private bool show_prediction_settings_ = true;
