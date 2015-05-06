e2a219eefdadf5985cb4d86476141d06bf73d2f3
John Turner <groups@norgg.org>
2015-05-06 14:21:03 +0100

    Cleanup of version header generator.

--- generate_version_header.sh
+++ generate_version_header.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 VERSION_TEMPLATE="#pragma once
 namespace principia {
-    namespace base {
-
-    char const kBuildDate[] = \"%%DATE%%\";
-    char const kVersion[] = 
-        \"%%VERSION%%\";
-
-    }  // namespace base
+namespace base {
+char const kBuildDate[] = \"%%DATE%%\";
+char const kVersion[] = 
+    \"%%VERSION%%\";
+}  // namespace base
 }  // namespace principia"
 
 echo -e "$VERSION_TEMPLATE" | 
 sed "s/%%DATE%%/`date -u`/" |
 sed "s/%%VERSION%%/`git describe --tags --always --dirty --abbrev=40 --long`/" > base/version.hpp
-#(get-date).ToUniversalTime(),
-#(git describe --tags --always --dirty --abbrev=40 --long))
-
