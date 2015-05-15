f33d704806a7fdb7a2c6b441c7c6c4d99257a7b5
pleroy <phl+personal@google.com>
2015-05-15 18:22:39 +0200

    Numbers.

--- benchmarks/чебышёв_series.cpp
+++ benchmarks/чебышёв_series.cpp
@@ -1,4 +1,16 @@
-﻿
+﻿// C:\Users\phl\Projects\GitHub\Principia [Series +0 ~1 -0]> .\Release\benchmarks.exe --benchmark_filter=Evaluate  // NOLINT(whitespace/line_length)
+// Benchmarking on 1 X 3310 MHz CPU
+// 2015/05/15-18:21:37
+// Benchmark              Time(ns)    CPU(ns) Iterations
+// -----------------------------------------------------
+// BM_EvaluateDouble/4       11859      12009      42869
+// BM_EvaluateDouble/8       18978      19040      27038
+// BM_EvaluateDouble/15      40024      40119      12832
+// BM_EvaluateDouble/16      38326      38407      13404
+// BM_EvaluateDouble/17      43830      44042      11689
+// BM_EvaluateDouble/18      49399      49519      10396
+// BM_EvaluateDouble/19      54013      54121       9512
+
 #include <random>
 
 #include "quantities/si.hpp"
@@ -48,7 +60,8 @@ void BM_EvaluateDouble(benchmark::State& state) {
   state.SetLabel(std::to_string(result).substr(0, 0));
 }
 
-BENCHMARK(BM_EvaluateDouble)->Arg(4)->Arg(8)->Arg(16);
+BENCHMARK(BM_EvaluateDouble)->
+    Arg(4)->Arg(8)->Arg(15)->Arg(16)->Arg(17)->Arg(18)->Arg(19);
 
 }  // namespace benchmarks
 }  // namespace principia
