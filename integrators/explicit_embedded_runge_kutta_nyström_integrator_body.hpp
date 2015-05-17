15dade0f68c0138c69d763ae96656028d704bdc8
Robin Leroy <egg.robin.leroy@gmail.com>
2015-05-17 23:47:44 +0200

    zzz

--- integrators/explicit_embedded_runge_kutta_nyström_integrator_body.hpp
+++ integrators/explicit_embedded_runge_kutta_nyström_integrator_body.hpp
@@ -27,9 +27,60 @@ void ExplicitEmbeddedRungeKuttaNyströmIntegrator::Solve(
     Variation<Position> const& velocity_tolerance,
     double const safety_factor,
     not_null<Solution<Position, Variation<Position>>*> const solution) const {
+  int const dimension = initial_value.positions.size();
+  CHECK_EQ(initial_value.momenta.size(), dimension);
+  CHECK_NE(first_time_step, Time());
+  bool const forward = first_time_step > Time();
+  if (forward) {
+    CHECK_LT(initial_value.time, t_final);
+  } else {
+    // Integrating backward.
+    CHECK_GT(initial_value.time, t_final);
+  }
+  using Displacement = Difference<Position>;
+  using Velocity = Variation<Position>;
+  using Acceleration = Variation<Velocity>;
+  // The timestep.
+  Time h = first_time_step;
+  DoublePrecision<Time> t;
+  std::vector<Displacement> ∆q(dimension);
+  std::vector<Velocity> ∆v(dimension);
+  std::vector<DoublePrecision<Position>> q(dimension);
+  std::vector<DoublePrecision<Position>> v(dimension);
+  // The low-order integration is not performed using compensated summation.
+  std::vector<Position> q_low_order(dimension);
+  std::vector<Velocity> v_low_order(dimension);
+  // TODO(egg): this is a rectangular container, use something more appropriate.
+  std::vector<std::vector<Acceleration>> g(stages_);
+  for (auto& g_stage : g) {
+    g_stage.resize(dimension);
+  }
+  Time t_stage;
+  std::vector<Displacement> q_stage(dimension);
+
+  for (int k = 0; k < dimension; ++k) {
+    ∆q[k] *= h;
+  }
   for (int i = 0; i < stages_; ++i) {
-    for (int j = 0; j < stages_; ++j) {
+    t_stage = t.value + c[i] * h;
+    for (int k = 0; k < dimension; ++k) {
+      for (int j = 0; j < i; ++j) {
+        q_stage[k] += g[j][k] * a[i][j]
+      }
+      q_stage[k] *= h;
+      q_stage[k] += c[i] * v[k].value;
+      q_stage[k] *= h;
+      q_stage[k] += q[k].value;
     }
+    compute_acceleration(t_stage, q_stage, &g[i]);
+  }
+  for (int k = 0; k < dimension; ++k) {
+    for (int i = 0; i < stages_; ++i) {
+      ∆q[k] += b_hat_[i] * g[i][k];
+    }
+    ∆q[k] *= h;
+    ∆q[k] += v[k].value;
+    ∆q[k] *= h;
   }
 }
 
