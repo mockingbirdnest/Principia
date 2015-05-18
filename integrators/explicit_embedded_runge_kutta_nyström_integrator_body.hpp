df6bbe53f7e432b4155d86adfed184a000ea6eed
Robin Leroy <egg.robin.leroy@gmail.com>
2015-05-18 02:40:12 +0200

    append to the solution

--- integrators/explicit_embedded_runge_kutta_nyström_integrator_body.hpp
+++ integrators/explicit_embedded_runge_kutta_nyström_integrator_body.hpp
@@ -55,52 +55,72 @@ void ExplicitEmbeddedRungeKuttaNyströmIntegrator::Solve(
   }
   std::vector<Position> q_stage(dimension);
   double control_factor = 1.0;
+  bool at_end = false;
+  while (!at_end) {
+    do {
+      // TODO(egg): This has to be the silliest way to compute an nth root.
+      h *= safety_factor * std::pow(control_factor, 1.0 / (lower_order_ + 1));
 
-  do {
-    // TODO(egg): This has to be the silliest way to compute an nth root.
-    h = safety_factor * std::pow(control_factor, 1.0 / (lower_order_ + 1)) * h;
-    for (int i = 0; i < stages_; ++i) {
-      Time const t_stage = t.value + c_[i] * h;
-      for (int k = 0; k < dimension; ++k) {
-        Acceleration ∑_a_ij_g_j;
-        for (int j = 0; j < i; ++j) {
-          ∑_a_ij_g_j += a_[i][j] * g[j][k];
-        }
-        q_stage[k] = q_hat[k].value +
-                         h * (c_[i] * v_hat[k].value + h * ∑_a_ij_g_j);
+      // Termination.
+      Time const time_to_end = t_final - t.value - t.error;
+      if (forward && h > time_to_end ||
+          !forward && h < time_to_end) {
+        // The chosen step size will overshoot.  Clip it to just reach the end,
+        // and terminate if the step is accepted.
+        h = time_to_end;
+        at_end = true;
+      } else {
+        at_end = false;
       }
-      compute_acceleration(t_stage, q_stage, &g[i]);
-    }
 
-    for (int k = 0; k < dimension; ++k) {
-      Acceleration ∑_b_hat_i_g_i;
-      Acceleration ∑_b_i_g_i;
-      Acceleration ∑_b_prime_hat_i_g_i;
-      Acceleration ∑_b_prime_i_g_i;
-      Displacement ∆q_k;
-      Velocity ∆v_k;
-      // Please keep eight assigments below aligned, they become illegible
-      // otherwise.
+      // Runge-Kutta-Nyström iteration.
       for (int i = 0; i < stages_; ++i) {
-        ∑_b_hat_i_g_i       += b_hat_[i] * g[i][k];
-        ∑_b_i_g_i           += b_[i] * g[i][k];
-        ∑_b_prime_hat_i_g_i += b_prime_hat_[i] * g[i][k];
-        ∑_b_prime_i_g_i     += b_prime_[i] * g[i][k];
+        Time const t_stage = t.value + c_[i] * h;
+        for (int k = 0; k < dimension; ++k) {
+          Acceleration ∑_a_ij_g_j;
+          for (int j = 0; j < i; ++j) {
+            ∑_a_ij_g_j += a_[i][j] * g[j][k];
+          }
+          q_stage[k] = q_hat[k].value +
+                           h * (c_[i] * v_hat[k].value + h * ∑_a_ij_g_j);
+        }
+        compute_acceleration(t_stage, q_stage, &g[i]);
       }
-      ∆q_hat[k] = h * (h * (∑_b_hat_i_g_i) + v_hat[k].value);
-      ∆q_k      = h * (h * (∑_b_i_g_i) + v_hat[k].value);
-      ∆v_hat[k] = h * ∑_b_prime_hat_i_g_i;
-      ∆v_k      = h * ∑_b_prime_i_g_i;
 
-      q_error_estimate[k] = ∆q_k - ∆q_hat[k];
-      v_error_estimate[k] = ∆v_k - ∆v_hat[k];
+      // Increment computation and step size control.
+      for (int k = 0; k < dimension; ++k) {
+        Acceleration ∑_b_hat_i_g_i;
+        Acceleration ∑_b_i_g_i;
+        Acceleration ∑_b_prime_hat_i_g_i;
+        Acceleration ∑_b_prime_i_g_i;
+        Displacement ∆q_k;
+        Velocity ∆v_k;
+        // Please keep eight assigments below aligned, they become illegible
+        // otherwise.
+        for (int i = 0; i < stages_; ++i) {
+          ∑_b_hat_i_g_i       += b_hat_[i] * g[i][k];
+          ∑_b_i_g_i           += b_[i] * g[i][k];
+          ∑_b_prime_hat_i_g_i += b_prime_hat_[i] * g[i][k];
+          ∑_b_prime_i_g_i     += b_prime_[i] * g[i][k];
+        }
+        ∆q_hat[k] = h * (h * (∑_b_hat_i_g_i) + v_hat[k].value);
+        ∆q_k      = h * (h * (∑_b_i_g_i) + v_hat[k].value);
+        ∆v_hat[k] = h * ∑_b_prime_hat_i_g_i;
+        ∆v_k      = h * ∑_b_prime_i_g_i;
+
+        q_error_estimate[k] = ∆q_k - ∆q_hat[k];
+        v_error_estimate[k] = ∆v_k - ∆v_hat[k];
+      }
+      control_factor = step_size_controller(q_error_estimate, v_error_estimate);
+    } while (control_factor < 1.0);
+
+    // Increment the solution with the high-order approximation.
+    t.Increment(h);
+    for(int k = 0; k < dimension; ++k) {
+      q_hat[k].Increment(∆q_hat[k]);
+      v_hat[k].Increment(∆v_hat[k]);
     }
-    control_factor = step_size_controller(q_error_estimate, v_error_estimate);
-  } while (control_factor < 1.0);
-  t.Increment(h);
-  for(int k = 0; k < dimension; ++k) {
-    q_hat[k].Increment(∆q_hat[k]);
-    v_hat[k].Increment(∆v_hat[k]);
+    solution->push_back({q_hat, v_hat, t});
   }
 }
 
