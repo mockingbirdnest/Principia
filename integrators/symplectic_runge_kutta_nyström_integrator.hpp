1e64f404ad97dbfff34afec96e49a412a03916cb
Robin Leroy <egg.robin.leroy@gmail.com>
2015-05-26 19:00:14 +0200

    remove the comments that can be moved to a separate file

--- integrators/symplectic_runge_kutta_nyström_integrator.hpp
+++ integrators/symplectic_runge_kutta_nyström_integrator.hpp
@@ -20,9 +20,9 @@ namespace integrators {
 //   (exp hA)(q, p) = (q + h M⁻¹ p, p), (exp hB)(q, p) = (q, p - h ∇q V(q)).
 // It corresponds to Hamilton's equations with quadratic kinetic energy,
 //   H(q, p, t) = ½ pᵀM⁻¹p + V(q, t).    (4)
-// See below for a proof that these A and B do indeed satisfy
+// See the documentation for a proof that these A and B do indeed satisfy
 // [B, [B, [B, A]]] = 0.
-//
+
 // Each step of size h is computed using the composition of evolutions
 //   exp(b₀ h B) exp(a₀ h A) ... exp(bᵣ h B) exp(aᵣ h A),
 // so that the integrator is a composition method.  If the appropriate
@@ -33,51 +33,20 @@ namespace integrators {
 // used in Blanes, Casas and Ros (2001),
 // New Families of Symplectic Runge-Kutta-Nyström Integration Methods,
 // http://www.gicas.uji.es/Fernando/Proceedings/2000NAA.pdf.
+
 // Types ABA and BAB have the first-same-as-last property: the last and first
 // applications of the evolution operators in two consecutive steps can be
 // merged when output is not needed, so for sparse outputs r-1 evolutions of B
 // and r of A are required in the ABA case, and vice versa in the BAB case.
-//
 // When solving equations of type (2), the integrator makes full use of the FSAL
-// property even for dense output: in the BAB case, the two consecutive
+// property, even for dense output: in the BAB case, the two consecutive
 // applications of exp(bᵢ h B) require only one evaluation of B (and similarly
 // in the ABA case).
-//
-// A remark on non-autonomy:
-// Most treatments of these integrators write (1) as (q, p)′ = X, with
-// X = A(q, p) + B(q, p), and (4) as H(q, p, t) = ½ pᵀM⁻¹p + V(q).
-// It is however possible to incorporate time, by considering it as an
-// additional variable:
-//   (q, p, t)′ = X, with X = (A(q, p), 1) + (B(q, t, p), 0).
-// Since B does not advance t, in cases of the form (2) we retain the defining
-// property on the extended phase space,
-//   (exp hB)(q, p, t) = (q, p, t) + (h B(q, p, t), 0).
-//
-// For equations of the form (3) it remains to show that Hamilton's equations
-// with quadratic kinetic energy and a time-dependent potential satisfy
-// [B, [B, [B, A]]] = 0.  Introducing t and its conjugate momentum ϖ to the
-// phase space, (3) follows from Hamilton's equations with
-//   H(q, p, t, ϖ) = ½ pᵀM⁻¹p + ϖ + V(q, t)
-// since we then get t′ = 1.
-// Writing Q = (q, t), P = (p, ϖ), and L(P) = ½ pᵀM⁻¹p + ϖ, the
-// Hamiltonian becomes H(Q, P) = L(P) + V(Q), where L is a quadratic
-// polynomial in P.
-// Here A = {·, L}, and B = {·, V}, so that [B, [B, [B, A]]] = 0 is equivalent
-// to {V, {V, {L, V}}} = 0, where {·, ·} is the Poisson bracket.  It is
-// immediate every term in that expression will contain a third order partial
-// derivative of L, and since L is quadratic all such derivatives vanish.  □
-//
-// See McLachlan and Quispel (2006), Geometric Integrators for ODEs, page 26,
-// http://www.massey.ac.nz/~rmclachl/JPAReview.pdf for a detailed treatment
-// of non-autonomous Hamiltonians using an extended phase space.
-// See McLachlan (1993), Symplectic Integration of Wave Equations, page 8,
-// http://www.massey.ac.nz/~rmclachl/wave.ps for a proof that
-// {V, {V, {L, V}}} = 0 for arbitrary Poisson tensors and L quadratic in Q as
-// well as P.
+// In the implementation, we call r the number of |stages_|.  The number of
+// |evaluations| is r-1 in the ABA and BAB cases, and r otherwise.
 
-// Notations:
-// As above, we the most widespread notation, calling the position weights aᵢ
-// and the momentum weights bᵢ.  Note that our indices are 0-based.
+// As above, we follow the most widespread notation, calling the position
+// weights a and the momentum weights b.  Note that our indices are 0-based.
 // The following notations appear in the litterature:
 //   (a, b) in most treatments of the subject;
 //   (d, c) in Ruth, Yoshida, as well as Forest and Ruth;
@@ -85,36 +54,32 @@ namespace integrators {
 //   (<unnamed>, B) in Okunbor and Skeel;
 //   (<unnamed>, b) in Calvo and Sanz-Serna.
 // Moreover, we follow the convention of Sofroniou and Spaletta (2002),
-// Symplectic Methods for Separable Hamiltonian Systems, in calling cᵢ the
+// Symplectic Methods for Separable Hamiltonian Systems, in calling c the
 // nodes used for the time argument of the evolution of B, with
 //   c₀ = 0, cᵢ = cᵢ₋₁ + aᵢ₋₁ for i > 0.
-// The notation γᵢ is used for these nodes in Calvo and Sanz-Serna.
-//
-// Following common usage, we call the number of stages r - 1 in the ABA and BAB
-// cases, and r otherwise.  Note that this is at odds with the traditions for
-// nonsymplectic Runge-Kutta-Nyström methods, where the number of stages is
-// considered to be the same regardless of the FSAL property.
-//
-// Note that the derivative weights b′ of general (nonsymplectic)
-// Runge-Kutta-Nyström methods correspond to our momentum weights b; the
-// position weights of the general case (usually denoted b) correspond to our
-// bᵢ(1 - cᵢ), and the Runge-Kutta matrix is given by (usually denoted aᵤᵥ)
-// is given by bᵥ (cᵤ - cᵥ).
+// The notation γ is used for these nodes in Calvo and Sanz-Serna.
+// See the documentation for a description of the correspondence between
+// these coefficients and those of a general Runge-Kutta-Nyström method.
 
-enum CompositionKind {
-  kNotFirstSameAsLast,
-  kABA,
-  kBAB,
+enum CompositionMethod {
+  kBA,   // Neither b₀ nor aᵣ vanishes.
+  kABA,  // b₀ = 0.
+  kBAB,  // aᵣ = 0.
 };
 
-template<typename Position, int order, int stages,
-         CompositionKind first_same_as_last>
+template<typename Position, int order, int evaluations,
+         CompositionMethod composition>
 class SymplecticRungeKuttaNyströmIntegrator
     : public FixedStepSizeIntegrator<
                  SpecialSecondOrderDifferentialEquation<Position>> {
   
   void Solve(IntegrationProblem<ODE> const& problem,
              Time const& step) const override;
+ private:
+  static int const stages_ = composition == kBA ? evaluations : evaluations + 1;
+  FixedVector<double, stages_> a_;
+  FixedVector<double, stages_> b_;
+  FixedVector<double, stages_> c_;
 };
 
 
