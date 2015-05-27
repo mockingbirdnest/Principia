6924b99b7251a8a5526cdeebd5a60ec537e91259
Robin Leroy <egg.robin.leroy@gmail.com>
2015-05-27 13:14:40 +0200

    seminar time

--- integrators/symplectic_runge_kutta_nyström_integrator.hpp
+++ integrators/symplectic_runge_kutta_nyström_integrator.hpp
@@ -14,14 +14,13 @@ namespace integrators {
 //       (exp hA)(q, p) = (q, p) + h A(q, p), and A and B are known;
 // (3).  q″ = f(q, t), with known f = -M⁻¹ ∇q V(q, t);
 // Only (3) is implemented at this time.
-
 // See the documentation for a proof that (3) is a special case of (2), and thus
-// of (1), and for the relation to Hamiltonian mechanics.
+// of (1), and for an explanation of the relation to Hamiltonian mechanics.
 
 // Each step of size h is computed using the composition of evolutions
-//   exp(b₀ h B) exp(a₀ h A) ... exp(bᵣ h B) exp(aᵣ h A),
-// so that the integrator is a composition method.  If the appropriate
-// coefficients vanish, this can be reformulated as either
+//   exp(b₀ h B) exp(a₀ h A) ... exp(bᵣ h B) exp(aᵣ h A);
+// the integrator thus is a composition method.  If the appropriate coefficients
+// vanish, the above can be reformulated as either
 //   exp(a₀ h A) exp(b₁ h B) ... exp(bᵣ h B) exp(aᵣ h A) or
 //   exp(b₀ h B) exp(a₀ h A) ... exp(aᵣ₋₁ h A) exp(bᵣ h B).
 // The former is called type ABA, the latter type BAB, following the conventions
@@ -31,7 +30,7 @@ namespace integrators {
 // In the implementation, we call r the number of |stages_|.  The number of
 // |evaluations| is r-1 in the ABA and BAB cases, and r otherwise.
 // See the documentation for an explanation of how types ABA and BAB reduce the
-// computational cost, especially in cases (2) and (3).
+// number of evaluations required, especially in cases (2) and (3).
 
 // As above, we follow the most widespread notation, calling the position
 // weights a and the momentum weights b.  Note that our indices are 0-based.
@@ -63,8 +62,7 @@ class SymplecticRungeKuttaNyströmIntegrator
   static int const stages_ = composition == kBA ? evaluations : evaluations + 1;
  public:
   SymplecticRungeKuttaNyströmIntegrator(FixedVector<double, stages_> const& a,
-                                        FixedVector<double, stages_> const& b,
-                                        FixedVector<double, stages_> const& c);
+                                        FixedVector<double, stages_> const& b);
 
   void Solve(IntegrationProblem<ODE> const& problem,
              Time const& step) const override;
@@ -73,6 +71,81 @@ class SymplecticRungeKuttaNyströmIntegrator
   FixedVector<double, stages_> b_;
   FixedVector<double, stages_> c_;
 };
+
+// This method minimizes the error constant.
+// Coefficients from Robert I. McLachlan and Pau Atela (1992),
+// The accuracy of symplectic integrators, table 2.
+// http://eaton.math.rpi.edu/CSUMS/Papers/Symplectic/McLachlan_Atela_92.pdf.
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position,
+                                      4 /*order*/,
+                                      4 /*evaluations*/,
+                                      kBA> const&
+McLachlanAtela1992Order4Optimal();
+// Coefficients from McLachlan (1995),
+// On the numerical integration of ordinary differential equations by symmetric
+// composition methods, http://www.massey.ac.nz/~rmclachl/sisc95.pdf.
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position,
+                                      4 /*order*/,
+                                      4 /*evaluations*/,
+                                      kABA> const& McLachlan1995SB3A4();
+// Coefficients from McLachlan (1995),
+// On the numerical integration of ordinary differential equations by symmetric
+// composition methods, http://www.massey.ac.nz/~rmclachl/sisc95.pdf.
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position,
+                                      4 /*order*/,
+                                      5 /*evaluations*/,
+                                      kABA> const& McLachlan1995SB3A5();
+// Coefficients from Blanes and Moan (2002),
+// Practical symplectic partitioned Runge–Kutta and Runge–Kutta–Nyström methods,
+// http://personales.upv.es/serblaza/2002JCAM.pdf.
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position,
+                                      4 /*order*/,
+                                      6 /*evaluations*/,
+                                      kBAB> const& BlanesMoan2002SRKN6B();
+// This method minimizes the error constant.
+// Coefficients from Robert I. McLachlan and Pau Atela (1992),
+// The accuracy of symplectic integrators, table 2.
+// http://eaton.math.rpi.edu/CSUMS/Papers/Symplectic/McLachlan_Atela_92.pdf.
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position,
+                                      5 /*order*/,
+                                      6 /*evaluations*/,
+                                      kBA> const&
+McLachlanAtela1992Order5Optimal();
+// Coefficients from Okunbor and Skeel (1994),
+// Canonical Runge-Kutta-Nyström methods of orders 5 and 6,
+// http://bionum.cs.purdue.edu/94OkSk.pdf.
+// NOTE(egg): The coefficients were actually copied from McLachlan (1995), they
+// seem to differ after a dozen significant figures or so.  Okunbor and Skeel
+// remark "we did not use HYBRJ1 to improve the accuracy of method coefficients
+// as we did in section 3.1".  We assume McLachlan's version is accurate.
+// TODO(egg): Derive the coefficients with Mathematica.
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position,
+                                      6 /*order*/,
+                                      7 /*evaluations*/,
+                                      kABA> const&
+OkunborSkeel1994Order6Method13();
+// Coefficients from Blanes and Moan (2002),
+// Practical symplectic partitioned Runge–Kutta and Runge–Kutta–Nyström methods,
+// http://personales.upv.es/serblaza/2002JCAM.pdf.
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position,
+                                      6 /*order*/,
+                                      11 /*evaluations*/,
+                                      kBAB> const& BlanesMoan2002SRKN11B();
+// Coefficients from Blanes and Moan (2002),
+// Practical symplectic partitioned Runge–Kutta and Runge–Kutta–Nyström methods,
+// http://personales.upv.es/serblaza/2002JCAM.pdf.
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position,
+                                      6 /*order*/,
+                                      14 /*evaluations*/,
+                                      kABA> const& BlanesMoan2002SRKN14A();
 
 }  // namespace integrators
 }  // namespace principia
--- integrators/symplectic_runge_kutta_nyström_integrator_body.hpp
+++ integrators/symplectic_runge_kutta_nyström_integrator_body.hpp
@@ -10,9 +10,16 @@ template<typename Position, int order, int evaluations,
 SymplecticRungeKuttaNyströmIntegrator<Position, order, evaluations,
                                       composition>::
 SymplecticRungeKuttaNyströmIntegrator(FixedVector<double, stages_> const& a,
-                                      FixedVector<double, stages_> const& b,
-                                      FixedVector<double, stages_> const& c)
-    : a_(a), b_(b), c_(c) {}
+                                      FixedVector<double, stages_> const& b)
+    : a_(a),
+      b_(b) {
+  DoublePrecision<double> c_i = 0.0;
+  for (int i = 0; i < stages_; ++i) {
+    c_[i] = c_i.value;
+    c_i.Increment(a_i);
+  }
+  CHECK_EQ(1.0, c_i.value);
+}
 
 template<typename Position, int order, int evaluations,
          CompositionMethod composition>
@@ -97,7 +104,189 @@ void SymplecticRungeKuttaNyströmIntegrator<Position, order, evaluations,
     }
     problem.append_state(current_state);
   }
+}
 
+
+
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position, 4, 4, kBA> const&
+McLachlanAtela1992Order4Optimal() {
+static SymplecticRungeKuttaNyströmIntegrator<
+           Position, 4, 4, kBA> const integrator({ 0.5153528374311229364,
+                                                  -0.085782019412973646,
+                                                   0.4415830236164665242,
+                                                   0.1288461583653841854},
+                                                 { 0.1344961992774310892,
+                                                  -0.2248198030794208058,
+                                                   0.7563200005156682911,
+                                                   0.3340036032863214255});
+  return integrator;
+}
+
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position, 4, 4, kABA> const&
+McLachlan1995SB3A4() {
+  static SymplecticRungeKuttaNyströmIntegrator<
+             Position, 4, 4, kABA> const integrator({ 0.18819521776883821787,
+                                                     -0.021528551102171551201,
+                                                      0.66666666666666666667,
+                                                     -0.021528551102171551201,
+                                                      0.18819521776883821787},
+                                                    { 0.0,
+                                                      1.0,
+                                                     -0.5,
+                                                     -0.5,
+                                                      1.0});
+  return integrator;
+}
+
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position, 4, 5, kABA> const&
+McLachlan1995SB3A5() {
+  static SymplecticRungeKuttaNyströmIntegrator<
+             Position, 4, 5, kABA> const integrator({ 0.4051886183952522772,
+                                                     -0.2871440408165240890,
+                                                      0.3819554224212718118,
+                                                      0.3819554224212718118,
+                                                     -0.2871440408165240890,
+                                                      0.4051886183952522772},
+                                                    { 0.0,
+                                                     -0.041095890410958904110,
+                                                      0.28813559322033898305,
+                                                      0.50592059438123984212,
+                                                      0.28813559322033898305,
+                                                     -0.041095890410958904110});
+  return integrator;
+}
+
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position, , , > const& BlanesMoan2002SRKN6B() {
+  static SymplecticRungeKuttaNyströmIntegrator<
+             Position, , , > const integrator({ 0.24529895718427100,
+                                           0.60487266571108000,
+                                          -0.35017162289535100,
+                                          -0.35017162289535100,
+                                           0.60487266571108000,
+                                           0.24529895718427100,
+                                           0.0},
+                                         { 0.082984406417405200,
+                                           0.39630980149836800,
+                                          -0.039056304922348600,
+                                           0.1195241940131508,
+                                          -0.039056304922348600,
+                                           0.39630980149836800,
+                                           0.082984406417405200});
+  return integrator;
+}
+
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position, , , > const&
+ McLachlanAtela1992Order5Optimal() {
+  static SymplecticRungeKuttaNyströmIntegrator<
+             Position, , , > const integrator({ 0.339839625839110000,
+                                          -0.088601336903027329,
+                                           0.5858564768259621188,
+                                          -0.603039356536491888,
+                                           0.3235807965546976394,
+                                           0.4423637942197494587},
+                                         { 0.1193900292875672758,
+                                           0.6989273703824752308,
+                                          -0.1713123582716007754,
+                                           0.4012695022513534480,
+                                           0.0107050818482359840,
+                                          -0.0589796254980311632});
+  return integrator;
+}
+
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position, , , > const&
+ OkunborSkeel1994Order6Method13() {
+  static SymplecticRungeKuttaNyströmIntegrator<
+             Position, , , > const integrator({-1.0130879789171747298,
+                                           1.1874295737325427070,
+                                          -0.018335852096460590340,
+                                           0.34399425728109261313,
+                                           0.34399425728109261313,
+                                          -0.018335852096460590340,
+                                           1.1874295737325427070,
+                                          -1.0130879789171747298},
+                                         { 0.0,
+                                           0.00016600692650009894,
+                                          -0.37962421426377360608,
+                                           0.68913741185181063674,
+                                           0.38064159097092574080,
+                                           0.68913741185181063674,
+                                          -0.37962421426377360608,
+                                           0.00016600692650009894});
+  return integrator;
+}
+
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position, , , > const&
+ BlanesMoan2002SRKN11B() {
+  static SymplecticRungeKuttaNyströmIntegrator<
+             Position, , , > const integrator({ 0.12322977594627100,
+                                           0.29055379779955800,
+                                          -0.12704921262541700,
+                                          -0.24633176106207500,
+                                           0.35720887279592800,
+                                           0.2047770542914700,
+                                           0.35720887279592800,
+                                          -0.24633176106207500,
+                                          -0.12704921262541700,
+                                           0.29055379779955800,
+                                           0.12322977594627100,
+                                           0.0},
+                                         { 0.041464998518262400,
+                                           0.19812867191806700,
+                                          -0.040006192104153300,
+                                           0.075253984301580700,
+                                          -0.011511387420687900,
+                                           0.23666992478693110,
+                                           0.23666992478693110,
+                                          -0.011511387420687900,
+                                           0.075253984301580700,
+                                          -0.040006192104153300,
+                                           0.19812867191806700,
+                                           0.041464998518262400});
+  return integrator;
+}
+
+template<typename Position>
+SymplecticRungeKuttaNyströmIntegrator<Position, , , > const&
+ BlanesMoan2002SRKN14A() {
+  static SymplecticRungeKuttaNyströmIntegrator<
+             Position, , , > const integrator({ 0.037859319840611600,
+                                           0.10263563310243500,
+                                          -0.025867888266558700,
+                                           0.31424140307144700,
+                                          -0.13014445951741500,
+                                           0.10641770036954300,
+                                          -0.0087942431285105800,
+                                           0.2073050690568954,
+                                          -0.0087942431285105800,
+                                           0.10641770036954300,
+                                          -0.13014445951741500,
+                                           0.31424140307144700,
+                                          -0.025867888266558700,
+                                           0.10263563310243500,
+                                           0.037859319840611600},
+                                         { 0.0,
+                                           0.091719152624461650,
+                                           0.18398317000500600,
+                                          -0.056534365832888270,
+                                           0.0049146887747128540,
+                                           0.14376112716835800,
+                                           0.32856769374680400,
+                                          -0.19641146648645423,
+                                          -0.19641146648645423,
+                                           0.32856769374680400,
+                                           0.14376112716835800,
+                                           0.0049146887747128540,
+                                          -0.056534365832888270,
+                                           0.18398317000500600,
+                                           0.091719152624461650});
+  return integrator;
 }
 
 }  // namespace integrators
