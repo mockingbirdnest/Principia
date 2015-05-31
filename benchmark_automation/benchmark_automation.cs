3f729d6306ac0100188e325ccf39fd7a3f15ad98
Robin Leroy <egg.robin.leroy@gmail.com>
2015-05-31 21:29:55 +0200

    csv

--- benchmark_automation/benchmark_automation.cs
+++ benchmark_automation/benchmark_automation.cs
@@ -13,11 +13,13 @@ class BenchmarkAutomation {
   private const String benchmark_executable = @".\Release\benchmarks.exe";
 
   private static void Main(string[] args) {
-    DirectoryInfo directory = new DirectoryInfo(args[0]);
+    DirectoryInfo benchmark_directory = new DirectoryInfo(args[0]);
+    DirectoryInfo mathematica_directory = new DirectoryInfo(args[1]);
+    DirectoryInfo jenkins_directory = new DirectoryInfo(args[2]);
     DateTime date = DateTime.UtcNow;
     String mathematica_date = date.ToString("{yyyy, M, d, H, m, s.fffffff}");
     String mathematica_output_file =
-        Path.Combine(args[1],
+        Path.Combine(mathematica_directory.FullName,
                      "principia_benchmark_results_" +
                          date.ToString("yyyy_M_d__H_m_s_fffffff.wl"));
     Console.WriteLine("DateList[TimeZone->0]: " + mathematica_date);
@@ -25,22 +27,26 @@ class BenchmarkAutomation {
     StreamWriter mathematica_stream =
         new StreamWriter(
             File.Open(mathematica_output_file, FileMode.CreateNew));
+    String csv_benchmark_names = "";
+    String csv_means = "";
     mathematica_stream.WriteLine("{");
     mathematica_stream.WriteLine(mathematica_date);
-    FileInfo[] files = directory.GetFiles("*.cpp");
+    FileInfo[] files = benchmark_directory.GetFiles("*.cpp");
     foreach (FileInfo file in files) {
       StreamReader stream = file.OpenText();
       String command_line;
       do {
         command_line = stream.ReadLine();
       } while (command_line == "");
-      if (command_line.StartsWith(@"// " + benchmark_executable)) {
+      if (command_line.StartsWith("// " + benchmark_executable)) {
         // Get rid of the // NOLINT comments and of the actual command, leaving
         // only the arguments.
         command_line =
             command_line.Split(
                 separator : new String[]{benchmark_executable, "//"},
                 options   : StringSplitOptions.None)[2].Trim();
+        Console.WriteLine(
+            "Running benchmarks with arguments from " + file.Name);
         Console.WriteLine(command_line);
         Process process = new Process {
           StartInfo = new ProcessStartInfo {
@@ -54,7 +60,6 @@ class BenchmarkAutomation {
         process.Start();
         while (!process.StandardOutput.EndOfStream) {
           String line = process.StandardOutput.ReadLine();
-          Console.WriteLine(line);
           String[] words =
               line.Split(separator : new Char[]{' '},
                          options   : StringSplitOptions.RemoveEmptyEntries);
@@ -66,6 +71,8 @@ class BenchmarkAutomation {
             mathematica_stream.WriteLine("{");
             mathematica_stream.WriteLine("\"" + benchmark_name + "\",");
             mathematica_stream.WriteLine(μ + ",");
+            CommaSeparatedAppend(ref csv_benchmark_names, benchmark_name);
+            CommaSeparatedAppend(ref csv_means, μ.ToString());
           } else if (words[0].EndsWith("_stddev")) {
             String benchmark_name = words[0].Substring(0, words[0].Length - 7);
             Int64 σ = Int64.Parse(words[1]);
@@ -82,6 +89,18 @@ class BenchmarkAutomation {
     }
     mathematica_stream.WriteLine("}");
     mathematica_stream.Close();
+    File.WriteAllText(
+        Path.Combine(jenkins_directory.FullName,
+                     "jenkins_benchmark_results.csv"),
+        csv_benchmark_names + "\n" + csv_means);
+  }
+
+  private static void CommaSeparatedAppend(ref string csv, string value) {
+    if (csv == "") {
+      csv = value;
+    } else {
+      csv = csv + ", " + value;
+    }
   }
 }
 
