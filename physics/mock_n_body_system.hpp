f14cf86381f2c9344e79971f98737638a0ba504c
Robin Leroy <egg.robin.leroy@gmail.com>
2015-07-23 22:46:48 +0200

    remove the tracing destructors

--- physics/mock_n_body_system.hpp
+++ physics/mock_n_body_system.hpp
@@ -12,11 +12,6 @@ class MockNBodySystem : public Ephemeris<InertialFrame> {
  public:
   MockNBodySystem() : Ephemeris() {}
 
-  ~MockNBodySystem() {
-    LOG(ERROR)<<FUNCTION_SIGNATURE;
-    LOG(ERROR)<<NAMED(this);
-  }
-
   // TODO(egg): MOCK_ALL_THE_THINGS
   MOCK_METHOD1(Prolong, void(Instant const& t));
 };
