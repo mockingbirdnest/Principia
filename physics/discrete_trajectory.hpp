1438168957313b451ba128fe6c8336682376cc3c
pleroy <phl+personal@google.com>
2015-09-02 22:36:23 +0200

    Clone and remove apparently dead code.

---
+++ physics/discrete_trajectory.hpp
@@ -0,0 +1,233 @@
+#pragma once
+
+#include <functional>
+#include <list>
+#include <map>
+#include <memory>
+
+#include "base/not_null.hpp"
+#include "geometry/grassmann.hpp"
+#include "geometry/named_quantities.hpp"
+#include "physics/degrees_of_freedom.hpp"
+#include "physics/forkable.hpp"
+#include "quantities/named_quantities.hpp"
+#include "serialization/physics.pb.h"
+
+namespace principia {
+
+using base::not_null;
+using geometry::Instant;
+using geometry::Vector;
+using geometry::Velocity;
+using quantities::Acceleration;
+using quantities::Length;
+using quantities::Speed;
+
+namespace physics {
+
+class Body;
+
+template<typename Frame>
+class DiscreteTrajectory;
+
+template<typename Frame>
+struct ForkableTraits<DiscreteTrajectory<Frame>> {
+  using TimelineConstIterator = 
+      std::map<Instant, DegreesOfFreedom<Frame>>::const_iterator;
+  static Instant const& time(TimelineConstIterator const it);
+};
+
+template<typename Frame>
+class DiscreteTrajectory : public Forkable<DiscreteTrajectory<Frame>> {
+  using Timeline = std::map<Instant, DegreesOfFreedom<Frame>>;
+ public:
+  class NativeIterator;
+  template<typename ToFrame>
+  class TransformingIterator;
+
+  // A function that transforms the coordinates to a different frame.
+  template<typename ToFrame>
+  using Transform = std::function<DegreesOfFreedom<ToFrame>(
+                        Instant const&,
+                        DegreesOfFreedom<Frame> const&,
+                        not_null<DiscreteTrajectory<Frame> const*> const)>;
+
+  // No transfer of ownership.  |body| must live longer than the trajectory as
+  // the trajectory holds a reference to it.  If |body| is oblate it must be
+  // expressed in the same frame as the trajectory.
+  explicit DiscreteTrajectory(not_null<Body const*> const body);
+  ~DiscreteTrajectory();
+
+  DiscreteTrajectory(DiscreteTrajectory const&) = delete;
+  DiscreteTrajectory(DiscreteTrajectory&&) = delete;
+  DiscreteTrajectory& operator=(DiscreteTrajectory const&) = delete;
+  DiscreteTrajectory& operator=(DiscreteTrajectory&&) = delete;
+
+  // Sets a callback to be run before this trajectory gets destroyed.
+  void set_on_destroy(
+      std::function<void(not_null<DiscreteTrajectory<Frame>const*> const)>
+          on_destroy);
+
+  // Returns an iterator at the first point of the trajectory.  Complexity is
+  // O(|depth|).  The result may be at end if the trajectory is empty.
+  NativeIterator first() const;
+
+  // Returns at the first point of the trajectory which is on or after |time|.
+  // Complexity is O(|depth| + Ln(|length|)).  The result may be at end if the
+  // |time| is after the end of the trajectory.
+  NativeIterator on_or_after(Instant const& time) const;
+
+  // Returns an iterator at the last point of the trajectory.  Complexity is
+  // O(1).  The trajectory must not be empty.
+  NativeIterator last() const;
+
+  // Same as |first| above, but returns an iterator that performs a coordinate
+  // tranformation to ToFrame.
+  template<typename ToFrame>
+  TransformingIterator<ToFrame> first_with_transform(
+      Transform<ToFrame> const& transform) const;
+
+  // Returns at the first point of the trajectory which is on or after |time|.
+  // Complexity is O(|depth| + Ln(|length|)).  The result may be at end if the
+  // |time| is after the end of the trajectory.
+  template<typename ToFrame>
+  TransformingIterator<ToFrame> on_or_after_with_transform(
+      Instant const& time,
+      Transform<ToFrame> const& transform) const;
+
+  // Same as |last| above, but returns an iterator that performs a coordinate
+  // tranformation to ToFrame.
+  template<typename ToFrame>
+  TransformingIterator<ToFrame> last_with_transform(
+      Transform<ToFrame> const& transform) const;
+
+  // These functions return the series of positions/velocities/times for the
+  // trajectory of the body.  All three containers are guaranteed to have the
+  // same size.  These functions are O(|depth| + |length|).
+  std::map<Instant, Position<Frame>> Positions() const;
+  std::map<Instant, Velocity<Frame>> Velocities() const;
+  std::list<Instant> Times() const;
+
+  // Appends one point to the trajectory.
+  void Append(Instant const& time,
+              DegreesOfFreedom<Frame> const& degrees_of_freedom);
+
+  // Removes all data for times (strictly) greater than |time|, as well as all
+  // child trajectories forked at times (strictly) greater than |time|.  |time|
+  // must be at or after the fork time, if any.
+  void ForgetAfter(Instant const& time);
+
+  // Removes all data for times less than or equal to |time|, as well as all
+  // child trajectories forked at times less than or equal to |time|.  This
+  // trajectory must be a root.
+  void ForgetBefore(Instant const& time);
+
+  // The body to which this trajectory pertains.  The body is cast to the type
+  // B.  An error occurs in debug mode if the cast fails.
+  template<typename B>
+  std::enable_if_t<std::is_base_of<Body, B>::value,
+                   not_null<B const*>> body() const;
+
+  // This function represents the intrinsic acceleration of a body, irrespective
+  // of any external field.  It can be due e.g., to an engine burn.
+  using IntrinsicAcceleration =
+      std::function<Vector<Acceleration, Frame>(Instant const& time)>;
+
+  // Sets the intrinsic acceleration for the trajectory of a massless body.
+  // For a nonroot trajectory the intrinsic acceleration only applies to times
+  // (strictly) greater than |fork_time()|.  In other words, the function
+  // |acceleration| is never called with times less than or equal to
+  // |fork_time()|.  It may, however, be called with times beyond |last_time()|.
+  // For a root trajectory the intrinsic acceleration applies to times greater
+  // than or equal to the first time of the trajectory.  Again, it may apply
+  // beyond |last_time()|.
+  // It is an error to call this function for a trajectory that already has an
+  // intrinsic acceleration, or for the trajectory of a massive body.
+  void set_intrinsic_acceleration(IntrinsicAcceleration const acceleration);
+
+  // Removes any intrinsic acceleration for the trajectory.
+  void clear_intrinsic_acceleration();
+
+  // Returns true if this trajectory has an intrinsic acceleration.
+  bool has_intrinsic_acceleration() const;
+
+  // Computes the intrinsic acceleration for this trajectory at time |time|.  If
+  // |has_intrinsic_acceleration()| return false, or if |time| is before the
+  // |fork_time()| (or initial time) of this trajectory, the returned
+  // acceleration is zero.
+  Vector<Acceleration, Frame> evaluate_intrinsic_acceleration(
+      Instant const& time) const;
+
+  // This trajectory must be a root.  The intrinsic acceleration is not
+  // serialized.  The body is not owned, and therefore is not serialized.
+  void WriteToMessage(not_null<serialization::Trajectory*> const message) const;
+
+  // NOTE(egg): This should return a |not_null|, but we can't do that until
+  // |not_null<std::unique_ptr<T>>| is convertible to |std::unique_ptr<T>|, and
+  // that requires a VS 2015 feature (rvalue references for |*this|).
+  static std::unique_ptr<DiscreteTrajectory> ReadFromMessage(
+      serialization::Trajectory const& message,
+      not_null<Body const*> const body);
+
+  void WritePointerToMessage(
+      not_null<serialization::Trajectory::Pointer*> const message) const;
+
+  // |trajectory| must be a root.
+  static not_null<DiscreteTrajectory*> ReadPointerFromMessage(
+      serialization::Trajectory::Pointer const& message,
+      not_null<DiscreteTrajectory*> const trajectory);
+
+  // A base class for iterating over the timeline of a trajectory, taking forks
+  // into account.  Objects of this class cannot be created.
+  // An iterator which returns the coordinates in the native frame of the
+  // trajectory, i.e., |Frame|.
+  class NativeIterator : public Iterator {
+   public:
+    bool at_end() const;
+    Instant const& time() const;
+    DegreesOfFreedom<Frame> const& degrees_of_freedom() const;
+
+   private:
+    NativeIterator() = default;
+    friend class DiscreteTrajectory;
+  };
+
+  // An iterator which returns the coordinates in another frame.
+  template<typename ToFrame>
+  class TransformingIterator : public Iterator {
+   public:
+    bool at_end() const;
+    Instant const& time() const;
+    DegreesOfFreedom<ToFrame> degrees_of_freedom() const;
+
+   private:
+    explicit TransformingIterator(Transform<ToFrame> const& transform);
+    Transform<ToFrame> transform_;
+    friend class DiscreteTrajectory;
+  };
+
+ private:
+  // This trajectory need not be a root.
+  void WriteSubTreeToMessage(
+      not_null<serialization::Trajectory*> const message) const;
+
+  void FillSubTreeFromMessage(serialization::Trajectory const& message);
+
+  not_null<Body const*> const body_;
+
+  Timeline timeline_;
+
+  std::unique_ptr<IntrinsicAcceleration> intrinsic_acceleration_;
+
+  std::function<void(not_null<DiscreteTrajectory<Frame>const *> const)>
+      on_destroy_;
+
+  // For using the private constructor in maps.
+  template<typename, typename>
+  friend struct std::pair;
+};
+
+}  // namespace physics
+}  // namespace principia
+
+#include "trajectory_body.hpp"
---
+++ physics/discrete_trajectory_body.hpp
@@ -0,0 +1,400 @@
+#pragma once
+
+#include "physics/discrete_trajectory.hpp"
+
+#include <algorithm>
+#include <list>
+#include <map>
+
+#include "geometry/named_quantities.hpp"
+#include "glog/logging.h"
+#include "physics/oblate_body.hpp"
+
+namespace principia {
+
+using base::make_not_null_unique;
+using geometry::Instant;
+
+namespace physics {
+
+template<typename Frame>
+DiscreteTrajectory<Frame>::DiscreteTrajectory(not_null<Body const*> const body)
+    : body_(body) {
+  CHECK(body_->is_compatible_with<Frame>())
+      << "Oblate body not in the same frame as the trajectory";
+}
+
+template<typename Frame>
+DiscreteTrajectory<Frame>::~DiscreteTrajectory() {
+  if (on_destroy_) {
+    on_destroy_(this);
+  }
+}
+
+template<typename Frame>
+void DiscreteTrajectory<Frame>::set_on_destroy(
+    std::function<void(not_null<DiscreteTrajectory<Frame>const *> const)>
+        on_destroy) {
+  on_destroy_ = on_destroy;
+}
+
+template<typename Frame>
+typename DiscreteTrajectory<Frame>::NativeIterator
+DiscreteTrajectory<Frame>::first() const {
+  NativeIterator it = Begin();
+  return it;
+}
+
+template<typename Frame>
+typename DiscreteTrajectory<Frame>::NativeIterator
+DiscreteTrajectory<Frame>::on_or_after(
+    Instant const& time) const {
+  NativeIterator it = Find(time);//TODO(phl):Not quite the same.
+  return it;
+}
+
+template<typename Frame>
+typename DiscreteTrajectory<Frame>::NativeIterator
+DiscreteTrajectory<Frame>::last() const {
+  NativeIterator it = End();
+  return --it;
+}
+
+template<typename Frame>
+template<typename ToFrame>
+typename DiscreteTrajectory<Frame>::TEMPLATE TransformingIterator<ToFrame>
+DiscreteTrajectory<Frame>::first_with_transform(
+    Transform<ToFrame> const& transform) const {
+  TransformingIterator<ToFrame> it(transform);
+  it.InitializeFirst(this);
+  return it;
+}
+
+template<typename Frame>
+template<typename ToFrame>
+typename DiscreteTrajectory<Frame>::TEMPLATE TransformingIterator<ToFrame>
+DiscreteTrajectory<Frame>::on_or_after_with_transform(
+    Instant const& time,
+    Transform<ToFrame> const& transform) const {
+  TransformingIterator<ToFrame> it(transform);
+  it.InitializeOnOrAfter(time, this);
+  return it;
+}
+
+template<typename Frame>
+template<typename ToFrame>
+typename DiscreteTrajectory<Frame>::TEMPLATE TransformingIterator<ToFrame>
+DiscreteTrajectory<Frame>::last_with_transform(
+    Transform<ToFrame> const& transform) const {
+  TransformingIterator<ToFrame> it(transform);
+  it.InitializeLast(this);
+  return it;
+}
+
+template<typename Frame>
+std::map<Instant, Position<Frame>>
+DiscreteTrajectory<Frame>::Positions() const {
+  std::map<Instant, Position<Frame>> result;
+  for (auto it = Begin(); it != End(); ++it) {
+    auto timeline_it = it.current();
+    Instant const& time = timeline_it->first;
+    DegreesOfFreedom<Frame> const& degrees_of_freedom = timeline_it->second;
+    result.emplace_hint(result.end(), time, degrees_of_freedom.position());
+  }
+  return result;
+}
+
+template<typename Frame>
+std::map<Instant, Velocity<Frame>> DiscreteTrajectory<Frame>::Velocities() const {
+  std::map<Instant, Velocity<Frame>> result;
+  for (auto it = Begin(); it != End(); ++it) {
+    auto timeline_it = it.current();
+    Instant const& time = timeline_it->first;
+    DegreesOfFreedom<Frame> const& degrees_of_freedom = timeline_it->second;
+    result.emplace_hint(result.end(), time, degrees_of_freedom.velocity());
+  }
+  return result;
+}
+
+template<typename Frame>
+std::list<Instant> DiscreteTrajectory<Frame>::Times() const {
+  std::list<Instant> result;
+  for (auto it = Begin(); it != End(); ++it) {
+    auto timeline_it = it.current();
+    Instant const& time = timeline_it->first;
+    result.push_back(time);
+  }
+  return result;
+}
+
+template<typename Frame>
+void DiscreteTrajectory<Frame>::Append(
+    Instant const& time,
+    DegreesOfFreedom<Frame> const& degrees_of_freedom) {
+  if (!first().at_end() && last().time() == time) {
+    LOG(WARNING) << "Append at existing time " << time
+                 << ", time range = [" << Times().front() << ", "
+                 << Times().back() << "]";
+    return;
+  }
+  auto it = timeline_.emplace_hint(timeline_.end(),
+                                   time,
+                                   degrees_of_freedom);
+  CHECK(timeline_.end() == ++it) << "Append out of order";
+}
+
+template<typename Frame>
+void DiscreteTrajectory<Frame>::ForgetAfter(Instant const& time) {
+  // Each of these blocks gets an iterator denoting the first entry with
+  // time > |time|.  It then removes that entry and all the entries that follow
+  // it.  This preserve any entry with time == |time|.
+  {
+    auto const it = timeline_.upper_bound(time);
+    CHECK(is_root() || time >= ForkTime())
+        << "ForgetAfter before the fork time";
+    timeline_.erase(it, timeline_.end());
+  }
+  {
+    auto const it = children_.upper_bound(time);
+    children_.erase(it, children_.end());
+  }
+}
+
+template<typename Frame>
+void DiscreteTrajectory<Frame>::ForgetBefore(Instant const& time) {
+  // Check that this is a root.
+  CHECK(is_root()) << "ForgetBefore on a nonroot trajectory";
+  // Each of these blocks gets an iterator denoting the first entry with
+  // time > |time|.  It then removes that all the entries that precede it.  This
+  // removes any entry with time == |time|.
+  {
+    auto it = timeline_.upper_bound(time);
+    timeline_.erase(timeline_.begin(), it);
+  }
+  {
+    auto it = children_.upper_bound(time);
+    children_.erase(children_.begin(), it);
+  }
+}
+
+template<typename Frame>
+template<typename B>
+std::enable_if_t<std::is_base_of<Body, B>::value, not_null<B const*>>
+DiscreteTrajectory<Frame>::body() const {
+// Dynamic casting is expensive, as in 3x slower for the benchmarks.  Do that in
+// debug mode to catch bugs, but not in optimized mode where we want all the
+// performance we can get.
+#ifdef _DEBUG
+  return dynamic_cast<B const*>(static_cast<Body const*>(body_));
+#else
+  return static_cast<not_null<B const*>>(body_);
+#endif
+}
+
+template<typename Frame>
+void DiscreteTrajectory<Frame>::set_intrinsic_acceleration(
+    IntrinsicAcceleration const acceleration) {
+  CHECK(body_->is_massless()) << "DiscreteTrajectory is for a massive body";
+  CHECK(intrinsic_acceleration_ == nullptr)
+      << "DiscreteTrajectory already has an intrinsic acceleration";
+  intrinsic_acceleration_ =
+      std::make_unique<IntrinsicAcceleration>(acceleration);
+}
+
+template<typename Frame>
+void DiscreteTrajectory<Frame>::clear_intrinsic_acceleration() {
+  intrinsic_acceleration_.reset();
+}
+
+template<typename Frame>
+bool DiscreteTrajectory<Frame>::has_intrinsic_acceleration() const {
+  return intrinsic_acceleration_ != nullptr;
+}
+
+template<typename Frame>
+Vector<Acceleration, Frame>
+DiscreteTrajectory<Frame>::evaluate_intrinsic_acceleration(
+    Instant const& time) const {
+  if (intrinsic_acceleration_ != nullptr &&
+      (fork_ == nullptr || time > fork_->timeline->first)) {
+    return (*intrinsic_acceleration_)(time);
+  } else {
+    return Vector<Acceleration, Frame>({0 * SIUnit<Acceleration>(),
+                                        0 * SIUnit<Acceleration>(),
+                                        0 * SIUnit<Acceleration>()});
+  }
+}
+
+template<typename Frame>
+void DiscreteTrajectory<Frame>::WriteToMessage(
+    not_null<serialization::DiscreteTrajectory*> const message) const {
+  LOG(INFO) << __FUNCTION__;
+  CHECK(is_root());
+  WriteSubTreeToMessage(message);
+  LOG(INFO) << NAMED(this);
+  LOG(INFO) << NAMED(message->SpaceUsed());
+  LOG(INFO) << NAMED(message->ByteSize());
+}
+
+template<typename Frame>
+std::unique_ptr<DiscreteTrajectory<Frame>>
+DiscreteTrajectory<Frame>::ReadFromMessage(
+    serialization::DiscreteTrajectory const& message,
+    not_null<Body const*> const body) {
+  auto trajectory = std::make_unique<DiscreteTrajectory>(body);
+  trajectory->FillSubTreeFromMessage(message);
+  return trajectory;
+}
+
+template<typename Frame>
+void DiscreteTrajectory<Frame>::WritePointerToMessage(
+    not_null<serialization::DiscreteTrajectory::Pointer*> const message) const {
+  not_null<DiscreteTrajectory const*> ancestor = this;
+  while (ancestor->parent_ != nullptr) {
+    Fork const& fork = *ancestor->fork_;
+    ancestor = ancestor->parent_;
+    int const children_distance =
+        std::distance(ancestor->children_.begin(), fork.children);
+    int const timeline_distance =
+        std::distance(ancestor->timeline_.begin(), fork.timeline);
+    auto* const fork_message = message->add_fork();
+    fork_message->set_children_distance(children_distance);
+    fork_message->set_timeline_distance(timeline_distance);
+  }
+}
+
+template<typename Frame>
+not_null<DiscreteTrajectory<Frame>*>
+DiscreteTrajectory<Frame>::ReadPointerFromMessage(
+    serialization::DiscreteTrajectory::Pointer const& message,
+    not_null<DiscreteTrajectory*> const trajectory) {
+  CHECK(trajectory->is_root());
+  not_null<DiscreteTrajectory*> descendant = trajectory;
+  for (int i = 0; i < message.fork_size(); ++i) {
+    auto const& fork_message = message.fork(i);
+    int const children_distance = fork_message.children_distance();
+    int const timeline_distance = fork_message.timeline_distance();
+    auto children_it = descendant->children_.begin();
+    auto timeline_it = descendant->timeline_.begin();
+    std::advance(children_it, children_distance);
+    std::advance(timeline_it, timeline_distance);
+    descendant = &children_it->second;
+  }
+  return descendant;
+}
+
+template<typename Frame>
+typename DiscreteTrajectory<Frame>::Iterator&
+DiscreteTrajectory<Frame>::Iterator::operator++() {
+  if (!forks_.empty() && current_ == forks_.front().timeline) {
+    // Skip over any timeline where the fork is at |end()|.  These are the ones
+    // that were forked at the fork point of their parent.  Looking at the
+    // |begin()| of the parent would be wrong (the fork would see changes to its
+    // parent after the fork point).
+    do {
+      ancestry_.pop_front();
+      forks_.pop_front();
+    } while (!forks_.empty() &&
+             forks_.front().timeline == ancestry_.front()->timeline_.end());
+    current_ = ancestry_.front()->timeline_.begin();
+  } else {
+    CHECK(current_ != ancestry_.front()->timeline_.end())
+        << "Incrementing beyond end of trajectory";
+    ++current_;
+  }
+  CHECK(!current_is_misplaced());
+  return *this;
+}
+
+template<typename Frame>
+bool DiscreteTrajectory<Frame>::Iterator::at_end() const {
+  return forks_.empty() && current_ == ancestry_.front()->timeline_.end();
+}
+
+template<typename Frame>
+Instant const& DiscreteTrajectory<Frame>::Iterator::time() const {
+  return current_->first;
+}
+
+template<typename Frame>
+not_null<DiscreteTrajectory<Frame> const*>
+DiscreteTrajectory<Frame>::Iterator::trajectory() const {
+  return ancestry_.back();
+}
+
+template<typename Frame>
+DegreesOfFreedom<Frame> const&
+DiscreteTrajectory<Frame>::NativeIterator::degrees_of_freedom() const {
+  return this->current()->second;
+}
+
+template<typename Frame>
+template<typename ToFrame>
+DegreesOfFreedom<ToFrame>
+DiscreteTrajectory<Frame>::
+TransformingIterator<ToFrame>::degrees_of_freedom() const {
+  auto it = this->current();
+  return transform_(it->first, it->second, this->trajectory());
+}
+
+template<typename Frame>
+template<typename ToFrame>
+DiscreteTrajectory<Frame>::TransformingIterator<ToFrame>::TransformingIterator(
+    Transform<ToFrame> const& transform)
+    : Iterator(),
+      transform_(transform) {}
+
+template<typename Frame>
+void DiscreteTrajectory<Frame>::WriteSubTreeToMessage(
+    not_null<serialization::DiscreteTrajectory*> const message) const {
+  Instant last_instant;
+  bool is_first = true;
+  serialization::DiscreteTrajectory::Litter* litter = nullptr;
+  for (auto const& pair : children_) {
+    Instant const& fork_time = pair.first;
+    DiscreteTrajectory const& child = pair.second;
+    if (is_first || fork_time != last_instant) {
+      is_first = false;
+      last_instant = fork_time;
+      litter = message->add_children();
+      fork_time.WriteToMessage(litter->mutable_fork_time());
+    }
+    child.WriteSubTreeToMessage(litter->add_trajectories());
+  }
+  for (auto const& pair : timeline_) {
+    Instant const& instant = pair.first;
+    DegreesOfFreedom<Frame> const& degrees_of_freedom = pair.second;
+    auto const instantaneous_degrees_of_freedom = message->add_timeline();
+    instant.WriteToMessage(instantaneous_degrees_of_freedom->mutable_instant());
+    degrees_of_freedom.WriteToMessage(
+        instantaneous_degrees_of_freedom->mutable_degrees_of_freedom());
+  }
+}
+
+template<typename Frame>
+void DiscreteTrajectory<Frame>::FillSubTreeFromMessage(
+    serialization::DiscreteTrajectory const& message) {
+  auto timeline_it = message.timeline().begin();
+  for (serialization::DiscreteTrajectory::Litter const& litter : message.children()) {
+    Instant const fork_time = Instant::ReadFromMessage(litter.fork_time());
+    for (;
+         timeline_it != message.timeline().end() &&
+         Instant::ReadFromMessage(timeline_it->instant()) <= fork_time;
+         ++timeline_it) {
+      Append(Instant::ReadFromMessage(timeline_it->instant()),
+             DegreesOfFreedom<Frame>::ReadFromMessage(
+                 timeline_it->degrees_of_freedom()));
+    }
+    for (serialization::DiscreteTrajectory const& child : litter.trajectories()) {
+      NewFork(fork_time)->FillSubTreeFromMessage(child);
+    }
+  }
+  for (; timeline_it != message.timeline().end(); ++timeline_it) {
+    Append(Instant::ReadFromMessage(timeline_it->instant()),
+           DegreesOfFreedom<Frame>::ReadFromMessage(
+               timeline_it->degrees_of_freedom()));
+  }
+}
+
+}  // namespace physics
+}  // namespace principia
