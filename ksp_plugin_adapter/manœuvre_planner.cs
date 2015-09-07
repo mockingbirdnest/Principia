b628155a685be14653ed798d91ddd10338b0ee24
Robin Leroy <egg.robin.leroy@gmail.com>
2015-09-07 22:50:34 +0200

    new C# class

---
+++ ksp_plugin_adapter/manœuvre_planner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace principia {
+namespace ksp_plugin_adapter {
+
+class manœuvre_planner {
+  
+}
+
+}  // namespace ksp_plugin_adapter
+}  // namespace principia
