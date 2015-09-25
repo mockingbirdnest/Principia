a05a944d97eeada5b0e93eb452cb3e8519031e8d
Robin Leroy <egg.robin.leroy@gmail.com>
2015-09-25 19:01:17 +0200

    rigid motion

--- physics/dynamic_frame.hpp
+++ physics/dynamic_frame.hpp
@@ -11,6 +11,7 @@
 namespace principia {
 
 using geometry::AffineMap;
+using geometry::AngularVelocity;
 using geometry::Instant;
 using geometry::Position;
 using geometry::Rotation;
@@ -20,19 +21,36 @@ using quantities::Length;
 
 namespace physics {
 
+// TODO(egg): AffineMap needs an accessor for its linear map.
+// TODO(egg): why not OrthogonalMap?
 template<typename FromFrame, typename ToFrame>
-class InstantaneousFrameMap {
+using RigidTransformation = AffineMap<FromFrame, ToFrame, Length, Rotation>;
+
+// The instantaneous motion of |ToFrame| with respect to |FromFrame|.
+// This is the derivative of a |RigidTransformation<FromFrame, ToFrame>|.
+// In order to invert, the |RigidTransformation| is needed, and we need its
+// linear part anyway, so we store it (and we forward its action on positions).
+template<typename FromFrame, typename ToFrame>
+class RigidMotion {
  public:
-  explicit InstantaneousFrameMap(
-      AffineMap<FromFrame, ToFrame, Length, Rotation> const& map);
+  RigidTransformation<FromFrame, ToFrame> const& rigid_transformation();
+  // Returns |rigid_transformation().linear_map()|.
+  Rotation<FromFrame, ToFrame> const& rotation() const;
 
   DegreesOfFreedom<ToFrame> operator()(
       DegreesOfFreedom<FromFrame> const& degrees_of_freedom) const;
-  Rotation<FromFrame, ToFrame> const& linear_map() const;
-  AffineMap<FromFrame, ToFrame, Length, Rotation> const& affine_map() const;
+
+  RigidMotion<ToFrame, FromFrame> Inverse() const;
 
  private:
-  AffineMap<FromFrame, ToFrame, Length, Rotation> map_;
+  RigidTransformation<FromFrame, ToFrame> rigid_tranformation_;
+  // d/dt rigid_transformation?¹(basis of ToFrame). The positively oriented
+  // orthogonal bases of FromFrame are acted upon faithfully and transitively by
+  // |Rotation<FromFrame, FromFrame>|, so this lies in the tangent space, i.e.,
+  // the Lie algebra.
+  AngularVelocity<FromFrame> rotation_;
+  // d/dt rigid_transformation?¹(ToFrame::origin).
+  Velocity<FromFrame> translation_;
 };
 
 // The definition of a reference frame |Frame| in arbitrary motion with respect
@@ -40,12 +58,14 @@ class InstantaneousFrameMap {
 template<typename BaseFrame, typename Frame>
 class DynamicFrame {
  public:
-  virtual InstantaneousFrameMap<BaseFrame, Frame> ToFrameAtTime(
+  virtual RigidMotion<BaseFrame, Frame> ToFrameAtTime(
       Instant const& t) = 0;
-  virtual InstantaneousFrameMap<Frame, BaseFrame> FromFrameAtTime(
+  virtual RigidMotion<Frame, BaseFrame> FromFrameAtTime(
       Instant const& t) = 0;
 
   // The acceleration due to the non-inertial motion of |Frame| and gravity.
+  // A particle in free fall follows a trajectory whose second derivative
+  // is |GeometricAcceleration|.
   virtual Vector<Acceleration, Frame> GeometricAcceleration(
       Instant const& t,
       Position const& q) = 0;
