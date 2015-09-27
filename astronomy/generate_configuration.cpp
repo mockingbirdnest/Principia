ab22816ade3a6e14b6e1ed97b976c8f3d354620e
pleroy <phl+personal@google.com>
2015-09-27 13:11:57 +0200

    The result looks fine.

--- astronomy/generate_configuration.cpp
+++ astronomy/generate_configuration.cpp
@@ -3,13 +3,18 @@
 #include <experimental/filesystem>
 
 #include "astronomy/frames.hpp"
+#include "geometry/epoch.hpp"
+#include "geometry/named_quantities.hpp"
 #include "glog/logging.h"
 #include "physics/solar_system.hpp"
+#include "quantities/parser.hpp"
+#include "quantities/si.hpp"
 #include "serialization/astronomy.pb.h"
 
 namespace principia {
 
 using physics::SolarSystem;
+using quantities::si::Second;
 
 namespace {
 constexpr char kCfg[] = "cfg";
@@ -18,7 +23,8 @@ constexpr char kProtoTxt[] = "proto.txt";
 
 namespace astronomy {
 
-void GenerateConfiguration(std::string const& gravity_model_stem,
+void GenerateConfiguration(Instant const& game_epoch,
+                           std::string const& gravity_model_stem,
                            std::string const& initial_state_stem) {
   std::experimental::filesystem::path const directory =
       SOLUTION_DIR / "astronomy";
@@ -67,6 +73,8 @@ void GenerateConfiguration(std::string const& gravity_model_stem,
   initial_state_cfg << "principia_initial_state:NEEDS["
                     << solar_system.initial_state_needs()
                     << "] {\n";
+  initial_state_cfg << "  epoch = "
+                    << (solar_system.epoch() - game_epoch) / Second << "\n";
   for (std::string const& name : solar_system.names()) {
     serialization::InitialState::Body const& body =
         solar_system.initial_state_message(name);
@@ -89,14 +97,18 @@ void GenerateConfiguration(std::string const& gravity_model_stem,
 int main(int argc, char const* argv[]) {
   google::InitGoogleLogging(argv[0]);
   google::LogToStderr();
-  if (argc < 3) {
+  if (argc < 4) {
     std::cerr << "Usage: generate_configuration "
-                 "gravity_model_stem initial_state_stem";
+                 "game_epoch_jd gravity_model_stem initial_state_stem";
     return 1;
   }
-  std::string const gravity_model_stem = argv[1];
-  std::string const initial_state_stem = argv[2];
-  principia::astronomy::GenerateConfiguration(gravity_model_stem,
+  principia::geometry::Instant game_epoch =
+      principia::geometry::JulianDate(
+          principia::quantities::ParseQuantity<double>(argv[1]));
+  std::string const gravity_model_stem = argv[2];
+  std::string const initial_state_stem = argv[3];
+  principia::astronomy::GenerateConfiguration(game_epoch,
+                                              gravity_model_stem,
                                               initial_state_stem);
   return 0;
 }
