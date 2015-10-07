55fc99d3d2e4cf4fed3adc04733a463ea2e2ad0b
pleroy <phl+personal@google.com>
2015-10-07 09:36:29 +0200

    Test solar system.

---
+++ physics/body_centered_non_rotating_dynamic_frame_body_test.cpp
@@ -0,0 +1,15 @@
+#include "physics/body_centered_non_rotating_dynamic_frame.hpp"
+
+#include "gtest/gtest.h"
+
+namespace principia {
+namespace physics {
+
+class BodyCentredNonRotatingDynamicFrameTest : public ::testing::Test {
+};
+
+TEST_F(BodyCentredNonRotatingDynamicFrameTest, ) {
+}
+
+}  // namespace physics
+}  // namespace principia
