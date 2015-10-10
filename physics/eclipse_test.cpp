348af4e7ea6486aea8f1bd9f5ae7f54afa8820c5
pdn4kd <pdn4kd@mail.umkc.edu>
2015-10-10 23:40:27 +0000

    Unity lighting quality eclipse test

---
+++ physics/eclipse_test.cpp
@@ -0,0 +1,19 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace Principia {
+
+namespace physics {
+
+    class EclipseTest : public testing::Test {
+    protected:
+        EclipseTest();
+    };
+    
+    TEST_F(EclipseTest, Dummy){
+        // ...
+    };
+        
+} // physics
+
+} // Principia
