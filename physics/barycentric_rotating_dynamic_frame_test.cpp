68540daf347013c4a6e75e60965b02ae3b404075
pleroy <phl+personal@google.com>
2015-10-15 22:43:47 +0200

    Improved test.

--- physics/barycentric_rotating_dynamic_frame_test.cpp
+++ physics/barycentric_rotating_dynamic_frame_test.cpp
@@ -103,8 +103,12 @@ class BarycentricRotatingDynamicFrameTest : public ::testing::Test {
 TEST_F(BarycentricRotatingDynamicFrameTest, ToBigSmallFrameAtTime) {
   int const kSteps = 100;
 
+  ContinuousTrajectory<ICRFJ2000Equator>::Hint big_hint;
+  ContinuousTrajectory<ICRFJ2000Equator>::Hint small_hint;
   for (Instant t = t0_; t < t0_ + 1 * period_; t += period_ / kSteps) {
     auto const to_big_small_frame_at_t = big_small_frame_->ToThisFrameAtTime(t);
+
+    // Check that the centre of mass is at the origin and doesn't move.
     DegreesOfFreedom<BigSmall> const centre_of_mass_in_big_small_at_t =
         to_big_small_frame_at_t(centre_of_mass_initial_state_);
     EXPECT_THAT(AbsoluteError(centre_of_mass_in_big_small_at_t.position() -
@@ -114,7 +118,39 @@ TEST_F(BarycentricRotatingDynamicFrameTest, ToBigSmallFrameAtTime) {
     EXPECT_THAT(AbsoluteError(centre_of_mass_in_big_small_at_t.velocity(),
                               Velocity<BigSmall>()),
                 Lt(1.0E-11 * Metre / Second));
+
+    // Check that the bodies don't move and are at the right locations.
+    DegreesOfFreedom<ICRFJ2000Equator> const big_in_inertial_frame_at_t =
+        solar_system_.trajectory(*ephemeris_, kBig).
+            EvaluateDegreesOfFreedom(t, &big_hint);
+    DegreesOfFreedom<ICRFJ2000Equator> const small_in_inertial_frame_at_t =
+        solar_system_.trajectory(*ephemeris_, kSmall).
+            EvaluateDegreesOfFreedom(t, &small_hint);
+
+    DegreesOfFreedom<BigSmall> const big_in_big_small_at_t =
+        to_big_small_frame_at_t(big_in_inertial_frame_at_t);
+    DegreesOfFreedom<BigSmall> const small_in_big_small_at_t =
+        to_big_small_frame_at_t(small_in_inertial_frame_at_t);
+    EXPECT_THAT(AbsoluteError(big_in_big_small_at_t.position() -
+                                  BigSmall::origin,
+                              Displacement<BigSmall>({15.0 / 7.0 * Kilo(Metre),
+                                                      0 * Kilo(Metre),
+                                                      0 * Kilo(Metre)})),
+                Lt(1.0E-6 * Metre));
+    EXPECT_THAT(AbsoluteError(big_in_big_small_at_t.velocity(),
+                              Velocity<BigSmall>()),
+                Lt(1.0E-4 * Metre / Second));
+    EXPECT_THAT(AbsoluteError(small_in_big_small_at_t.position() -
+                                  BigSmall::origin,
+                              Displacement<BigSmall>({-20.0 / 7.0 * Kilo(Metre),
+                                                      0 * Kilo(Metre),
+                                                      0 * Kilo(Metre)})),
+                Lt(1.0E-5 * Metre));
+    EXPECT_THAT(AbsoluteError(small_in_big_small_at_t.velocity(),
+                              Velocity<BigSmall>()),
+                Lt(1.0E-4 * Metre / Second));
   }
+
 }
 
 }  // namespace physics
