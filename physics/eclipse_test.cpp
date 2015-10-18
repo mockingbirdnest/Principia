366abca44e483a7e0cc7d10c01dba425972f5452
pdn4kd (raptop) <pdn4kd@noreply.github.com>
2015-10-18 20:29:15 +0000

    Saner var placement

--- physics/eclipse_test.cpp
+++ physics/eclipse_test.cpp
@@ -12,11 +12,12 @@ namespace physics {
     class EclipseTest : public testing::Test {
     protected:
         EclipseTest() {
-			SolarSystem<ICRFJ2000Equator> solar_system_1950_;
 			solar_system_1950_.Initialize(
 				SOLUTION_DIR / "astronomy" / "gravity_model.proto.txt", 
 				SOLUTION_DIR / "astronomy" / "initial_state_jd_2433282_500000000.proto.txt");
 		}
+
+		SolarSystem<ICRFJ2000Equator> solar_system_1950_;
     };
     
     TEST_F(EclipseTest, Dummy){
