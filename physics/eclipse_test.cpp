0087ac81748383d1f32ae5cbc131491b77535b52
pdn4kd (raptop) <pdn4kd@noreply.github.com>
2015-10-18 19:48:36 +0000

    Well, it compiles.

--- physics/eclipse_test.cpp
+++ physics/eclipse_test.cpp
@@ -5,15 +5,18 @@
 
 namespace principia {
 
+using astronomy::ICRFJ2000Equator;
+
 namespace physics {
 
     class EclipseTest : public testing::Test {
     protected:
-        EclipseTest();
-		SolarSystem<astronomy::ICRFJ2000Equator> solar_system_1950_;
-		solar_system_1950_->Initialize(
-			SOLUTION_DIR / "astronomy" / "gravity_model.proto.txt", 
-			SOLUTION_DIR / "astronomy" / "initial_state_jd_2433282_500000000.proto.txt");
+        EclipseTest() {
+			SolarSystem<ICRFJ2000Equator> solar_system_1950_;
+			solar_system_1950_.Initialize(
+				SOLUTION_DIR / "astronomy" / "gravity_model.proto.txt", 
+				SOLUTION_DIR / "astronomy" / "initial_state_jd_2433282_500000000.proto.txt");
+		}
     };
     
     TEST_F(EclipseTest, Dummy){
