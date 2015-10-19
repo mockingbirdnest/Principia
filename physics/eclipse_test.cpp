96156c5933137b446011e79b436cbb8d9e4e6311
pdn4kd (raptop) <pdn4kd@noreply.github.com>
2015-10-19 00:00:11 +0000

    Almost a compiling test.

--- physics/eclipse_test.cpp
+++ physics/eclipse_test.cpp
@@ -2,10 +2,16 @@
 #include "gtest/gtest.h"
 
 #include "physics/solar_system.hpp"
+#include "quantities/quantities.hpp"
+#include "quantities/si.hpp"
+#include "integrators/symplectic_runge_kutta_nyström_integrator.hpp"
+#include "geometry/epoch.hpp"
 
 namespace principia {
 
 using astronomy::ICRFJ2000Equator;
+using geometry::JulianDate;
+using integrators::McLachlanAtela1992Order5Optimal;
 
 namespace physics {
 
@@ -20,8 +26,14 @@ namespace physics {
 		SolarSystem<ICRFJ2000Equator> solar_system_1950_;
     };
     
-    TEST_F(EclipseTest, Dummy){
-        // ...
+    TEST_F(EclipseTest, Dummy) {
+        auto ephemeris = solar_system_1950_.MakeEphemeris(McLachlanAtela1992Order5Optimal<Position<ICRFJ000Equator>>(),
+		45 * Minute,
+		5 * Milli(Metre));
+
+		ephemeris->Prolong(JulianDate(2433374.5));
+		// Prolong until date of eclipse
+		// pass body to Ephemeris.trajectory
     };
         
 } // physics
