baa70558954d98c1faf2f9bbd329b49301964d53
pleroy <phl+personal@google.com>
2015-11-15 23:32:02 +0100

    Implementation sketch, but I don't like these static Fill.

--- ksp_plugin/journal.hpp
+++ ksp_plugin/journal.hpp
@@ -1,11 +1,22 @@
+#pragma once
+
+#include <experimental/optional>
+#include <functional>
+#include <memory>
+
+#include "base/not_null.hpp"
 #include "serialization/journal.pb.h"
 
 namespace principia {
+
+using base::not_null;
+
 namespace ksp_plugin {
 
 class Plugin;
 
 struct DeletePlugin {
+  using Message = serialization::DeletePlugin;
   struct In {
     Plugin const* plugin;
   };
@@ -13,15 +24,24 @@ struct DeletePlugin {
     Plugin const** const plugin;
   };
   struct Return {};
+
+  static void Fill(In const& in, not_null<Message*> const message);
+  static void Fill(Out const& out, not_null<Message*> const message);
+  static void Fill(Return const& r3turn, not_null<Message*> const message);
 };
 
 struct NewPlugin {
+  using Message = serialization::NewPlugin;
   struct In {
     double initial_time;
     double planetarium_rotation_in_degrees;
   };
   struct Out {};
   using Return = Plugin*;
+
+  static void Fill(In const& in, not_null<Message*> const message);
+  static void Fill(Out const& out, not_null<Message*> const message);
+  static void Fill(Return const& r3turn, not_null<Message*> const message);
 };
 
 class Journal {
@@ -30,9 +50,16 @@ class Journal {
   class Method {
    public:
     Method(typename Profile::In const& in, typename Profile::Out const& out);
-    Method(typename Profile::In const& in);
+    explicit Method(typename Profile::In const& in);
+    ~Method();
+
     typename Profile::Return Return(typename Profile::Return const& r3turn);
     void Return();
+
+   private:
+    std::unique_ptr<typename Profile::Message> const message_;
+    std::experimental::optional<typename Profile::Out> out_;
+    bool returned_ = false;
   };
 };
 
--- ksp_plugin/journal_body.hpp
+++ ksp_plugin/journal_body.hpp
@@ -1,3 +1,6 @@
+#pragma once
+
+#include "glog/logging.h"
 #include "ksp_plugin/journal.hpp"
 
 namespace principia {
@@ -5,20 +8,39 @@ namespace ksp_plugin {
 
 template<typename Profile>
 Journal::Method<Profile>::Method(typename Profile::In const& in,
-                                 typename Profile::Out const& out) {
+                                 typename Profile::Out const& out)
+    : message_(std::make_unique<Profile::Message>()),
+      out_(out) {
+  Profile::Fill(in, message_.get());
 }
 
 template<typename Profile>
-Journal::Method<Profile>::Method(typename Profile::In const& in) {
+Journal::Method<Profile>::Method(typename Profile::In const& in)
+    : message_(std::make_unique<typename Profile::Message>()) {
+  Profile::Fill(in, message_.get());
+}
+
+template<typename Profile>
+Journal::Method<Profile>::~Method() {
+  CHECK(returned_);
+  if (out_) {
+    Profile::Fill(*out_, message_.get());
+  }
 }
 
 template<typename Profile>
 typename Profile::Return Journal::Method<Profile>::Return(
     typename Profile::Return const& r3turn) {
+  CHECK(!returned_);
+  returned_ = true;
+  Profile::Fill(r3turn, message_.get());
+  return r3turn;
 }
 
 template<typename Profile>
 void Journal::Method<Profile>::Return() {
+  CHECK(!returned_);
+  returned_ = true;
 }
 
 }  // namespace ksp_plugin
