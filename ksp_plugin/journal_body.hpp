25562f89e147a4db972f818600a32f37d0a37f49
pleroy <phl+personal@google.com>
2015-11-22 10:30:58 +0100

    Check active_ when logging.

--- ksp_plugin/journal_body.hpp
+++ ksp_plugin/journal_body.hpp
@@ -10,34 +10,44 @@ namespace principia {
 namespace ksp_plugin {
 
 template<typename Profile>
-Journal::Method<Profile>::Method()
-    : message_(std::make_unique<typename Profile::Message>()) {}
+Journal::Method<Profile>::Method() {
+  if (active_ != nullptr) {
+    message_ = std::make_unique<typename Profile::Message>();
+  }
+}
 
 template<typename Profile>
 template<typename P, typename>
-Journal::Method<Profile>::Method(typename P::In const& in)
-    : message_(std::make_unique<typename Profile::Message>()) {
-  Profile::Fill(in, message_.get());
+Journal::Method<Profile>::Method(typename P::In const& in) {
+  if (active_ != nullptr) {
+    message_ = std::make_unique<typename Profile::Message>();
+    Profile::Fill(in, message_.get());
+  }
 }
 
 template<typename Profile>
 template<typename P, typename, typename>
 Journal::Method<Profile>::Method(typename P::In const& in,
-                                 typename P::Out const& out)
-    : message_(std::make_unique<typename Profile::Message>()),
-      out_filler_([this, out]() { Profile::Fill(out, message_.get()); }) {
-  Profile::Fill(in, message_.get());
+                                 typename P::Out const& out) {
+  if (active_ != nullptr) {
+    message_ = std::make_unique<typename Profile::Message>();
+    out_filler_ = [this, out]() { Profile::Fill(out, message_.get()); };
+    Profile::Fill(in, message_.get());
+  }
 }
 
 template<typename Profile>
 Journal::Method<Profile>::~Method() {
   CHECK(returned_);
-  if (out_filler_ != nullptr) {
-    out_filler_();
+  if (active_ != nullptr) {
+    if (out_filler_ != nullptr) {
+      out_filler_();
+    }
+    serialization::Method method;
+    method.SetAllocatedExtension(
+        Profile::Message::extension, message_.release());
+    active_->Write(method);
   }
-  serialization::Method method;
-  method.SetAllocatedExtension(Profile::Message::extension, message_.release());
-  active_->Write(method);
 }
 
 template<typename Profile>
@@ -52,7 +62,9 @@ typename P::Return Journal::Method<Profile>::Return(
     typename P::Return const& result) {
   CHECK(!returned_);
   returned_ = true;
-  Profile::Fill(result, message_.get());
+  if (active_ != nullptr) {
+    Profile::Fill(result, message_.get());
+  }
   return result;
 }
 
