2e036ce727c55e7ae81c8bba4b3fc82e988a2171
pleroy <phl+personal@google.com>
2015-12-05 16:37:16 +0100

    Botched indent after merge.

--- ksp_plugin/interface.cpp
+++ ksp_plugin/interface.cpp
@@ -285,15 +285,15 @@ void principia__DirectlyInsertCelestial(
     char const* const vy,
     char const* const vz) {
   journal::Method<journal::DirectlyInsertCelestial> m({plugin,
-                                              celestial_index,
-                                              parent_index,
-                                              gravitational_parameter,
-                                              axis_right_ascension,
-                                              axis_declination,
-                                              j2,
-                                              reference_radius,
-                                              x, y, z,
-                                              vx, vy, vz});
+                                                       celestial_index,
+                                                       parent_index,
+                                                       gravitational_parameter,
+                                                       axis_right_ascension,
+                                                       axis_declination,
+                                                       j2,
+                                                       reference_radius,
+                                                       x, y, z,
+                                                       vx, vy, vz});
   serialization::GravityModel::Body gravity_model;
   serialization::InitialState::Body initial_state;
   gravity_model.set_gravitational_parameter(gravitational_parameter);
@@ -330,10 +330,10 @@ void principia__InsertCelestial(Plugin* const plugin,
                                 int const parent_index,
                                 QP const from_parent) {
   journal::Method<journal::InsertCelestial> m({plugin,
-                                      celestial_index,
-                                      gravitational_parameter,
-                                      parent_index,
-                                      from_parent});
+                                               celestial_index,
+                                               gravitational_parameter,
+                                               parent_index,
+                                               from_parent});
   CHECK_NOTNULL(plugin)->InsertCelestial(
       celestial_index,
       gravitational_parameter * SIUnit<GravitationalParameter>(),
@@ -348,8 +348,8 @@ void principia__InsertSun(Plugin* const plugin,
                           int const celestial_index,
                           double const gravitational_parameter) {
   journal::Method<journal::InsertSun> m({plugin,
-                                celestial_index,
-                                gravitational_parameter});
+                                         celestial_index,
+                                         gravitational_parameter});
   CHECK_NOTNULL(plugin)->InsertSun(
       celestial_index,
       gravitational_parameter * SIUnit<GravitationalParameter>());
@@ -360,8 +360,8 @@ void principia__UpdateCelestialHierarchy(Plugin const* const plugin,
                                          int const celestial_index,
                                          int const parent_index) {
   journal::Method<journal::UpdateCelestialHierarchy> m({plugin,
-                                               celestial_index,
-                                               parent_index});
+                                                        celestial_index,
+                                                        parent_index});
   CHECK_NOTNULL(plugin)->UpdateCelestialHierarchy(celestial_index,
                                                   parent_index);
   return m.Return();
@@ -462,7 +462,7 @@ void principia__SetPlottingFrame(Plugin* const plugin,
 void principia__DeleteNavigationFrame(
     NavigationFrame** const navigation_frame) {
   journal::Method<journal::DeleteNavigationFrame> m({navigation_frame},
-                                           {navigation_frame});
+                                                    {navigation_frame});
   TakeOwnership(navigation_frame);
   return m.Return();
 }
@@ -480,9 +480,9 @@ LineAndIterator* principia__RenderedVesselTrajectory(
     NavigationFrame* const navigation_frame,
     XYZ const sun_world_position) {
   journal::Method<journal::RenderedVesselTrajectory> m({plugin,
-                                               vessel_guid,
-                                               navigation_frame,
-                                               sun_world_position});
+                                                        vessel_guid,
+                                                        navigation_frame,
+                                                        sun_world_position});
   RenderedTrajectory<World> rendered_trajectory = CHECK_NOTNULL(plugin)->
       RenderedVesselTrajectory(
           vessel_guid,
@@ -506,9 +506,9 @@ LineAndIterator* principia__RenderedPrediction(
     NavigationFrame* const navigation_frame,
     XYZ const sun_world_position) {
   journal::Method<journal::RenderedPrediction> m({plugin,
-                                         vessel_guid,
-                                         navigation_frame,
-                                         sun_world_position});
+                                                  vessel_guid,
+                                                  navigation_frame,
+                                                  sun_world_position});
   RenderedTrajectory<World> rendered_trajectory = CHECK_NOTNULL(plugin)->
       RenderedPrediction(
           vessel_guid,
@@ -533,10 +533,10 @@ LineAndIterator* principia__RenderedFlightPlan(
     NavigationFrame* const navigation_frame,
     XYZ const sun_world_position) {
   journal::Method<journal::RenderedFlightPlan> m({plugin,
-                                         vessel_guid,
-                                         plan_phase,
-                                         navigation_frame,
-                                         sun_world_position});
+                                                  vessel_guid,
+                                                  plan_phase,
+                                                  navigation_frame,
+                                                  sun_world_position});
   RenderedTrajectory<World> rendered_trajectory =
       CHECK_NOTNULL(plugin)->RenderedFlightPlan(
           vessel_guid,
@@ -603,7 +603,7 @@ bool principia__AtEnd(LineAndIterator const* const line_and_iterator) {
 void principia__DeleteLineAndIterator(
     LineAndIterator** const line_and_iterator) {
   journal::Method<journal::DeleteLineAndIterator> m({line_and_iterator},
-                                           {line_and_iterator});
+                                                    {line_and_iterator});
   TakeOwnership(line_and_iterator);
   return m.Return();
 }
@@ -613,9 +613,9 @@ void principia__AddVesselToNextPhysicsBubble(Plugin* const plugin,
                                              KSPPart const* const parts,
                                              int count) {
   journal::Method<journal::AddVesselToNextPhysicsBubble> m({plugin,
-                                                   vessel_guid,
-                                                   parts,
-                                                   count});
+                                                            vessel_guid,
+                                                            parts,
+                                                            count});
   VLOG(1) << __FUNCTION__ << '\n' << NAMED(count);
   std::vector<principia::ksp_plugin::IdAndOwnedPart> vessel_parts;
   vessel_parts.reserve(count);
@@ -757,7 +757,7 @@ char const* principia__SerializePlugin(Plugin const* const plugin,
 
 void principia__DeletePluginSerialization(char const** const serialization) {
   journal::Method<journal::DeletePluginSerialization> m({serialization},
-                                               {serialization});
+                                                        {serialization});
   LOG(INFO) << __FUNCTION__;
   TakeOwnershipArray(reinterpret_cast<uint8_t const**>(serialization));
   return m.Return();
@@ -768,10 +768,10 @@ void principia__DeserializePlugin(char const* const serialization,
                                   PushDeserializer** const deserializer,
                                   Plugin const** const plugin) {
   journal::Method<journal::DeserializePlugin> m({serialization,
-                                        serialization_size,
-                                        deserializer,
-                                        plugin},
-                                       {deserializer, plugin});
+                                                 serialization_size,
+                                                 deserializer,
+                                                 plugin},
+                                                {deserializer, plugin});
   LOG(INFO) << __FUNCTION__;
   CHECK_NOTNULL(serialization);
   CHECK_NOTNULL(deserializer);
