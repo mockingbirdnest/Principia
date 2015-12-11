f704826696ce37b9c150bf96cd015affd6626a9c
pdn4kd (raptop) <pdn4kd@noreply.github.com>
2015-12-11 02:25:33 +0000

    reformated to be within guidelines, removed excessive comments

--- physics/eclipse_test.cpp
+++ physics/eclipse_test.cpp
@@ -21,7 +21,7 @@ using quantities::si::Metre;
 using quantities::si::Nano;
 using quantities::si::Milli;
 using quantities::si::Kilo;
-using quantities::ArcCos; // This feels very cargocultish, especially since I don't need it for ArcSin.
+using quantities::ArcCos;
 using testing_utilities::AbsoluteError;
 using ::testing::Gt;
 using ::testing::Lt;
@@ -29,28 +29,28 @@ using ::testing::AllOf;
 
 namespace physics {
 
-  class EclipseTest : public testing::Test {
-  protected:
+class EclipseTest : public testing::Test {
+ protected:
     EclipseTest() {
       solar_system_1950_.Initialize(
-        SOLUTION_DIR / "astronomy" / "gravity_model.proto.txt", 
+        SOLUTION_DIR / "astronomy" / "gravity_model.proto.txt",
         SOLUTION_DIR / "astronomy" / "initial_state_jd_2433282_500000000.proto.txt");
     }
 
     SolarSystem<ICRFJ2000Equator> solar_system_1950_;
-  };
-    
+};
+
   TEST_F(EclipseTest, Dummy) {
     auto ephemeris = solar_system_1950_.MakeEphemeris(McLachlanAtela1992Order5Optimal<Position<ICRFJ2000Equator>>(),
     45 * Minute,
     5 * Milli(Metre));
 
-    ephemeris->Prolong(JulianDate(2433374.5)); // Prolong until date of eclipse (Eclipse was 1950-04-02 but JD is 1950-04-03:00:00:00)
+    ephemeris->Prolong(JulianDate(2433374.5));  // Prolong until date of eclipse (Eclipse was 1950-04-02 but JD is 1950-04-03:00:00:00)
     // pass body to Ephemeris.trajectory
     auto sun = ephemeris->bodies()[solar_system_1950_.index("Sun")];
     auto earth = ephemeris->bodies()[solar_system_1950_.index("Earth")];
     auto moon = ephemeris->bodies()[solar_system_1950_.index("Moon")];
-    
+
     // Massive_body eventually needs radius information. Or non-hardcoded data pulled from https://github.com/mockingbirdnest/Principia/blob/master/astronomy/gravity_model.proto.txt
     auto r_sun = 696000.0 * Kilo(Metre);
     auto r_earth = 6378.1363 * Kilo(Metre);
@@ -73,8 +73,8 @@ namespace physics {
     // Earth/Sun lineup
     auto alpha = ArcSin((r_sun - r_earth)/(q_sun - q_earth).Norm());
     auto q_U14 = q_earth + Normalize(q_earth - q_sun) * (r_earth + r_moon) / Sin(alpha);
-    // Earth/Moon lineup
-    auto gamma = ArcCos(InnerProduct(q_U14 - q_earth, q_U14 - q_moon) / ((q_U14 - q_moon).Norm() * (q_U14 - q_earth).Norm())); // Angle for U14
+    // Earth/Moon lineup for U14
+    auto gamma = ArcCos(InnerProduct(q_U14 - q_earth, q_U14 - q_moon) / ((q_U14 - q_moon).Norm() * (q_U14 - q_earth).Norm()));
     // Do the angles work?
     LOG(ERROR) << alpha;
     LOG(ERROR) << gamma;
@@ -89,8 +89,8 @@ namespace physics {
     // Earth/Sun lineup
     alpha = ArcSin((r_sun - r_earth)/(q_sun - q_earth).Norm());
     auto q_U23 = q_earth + Normalize(q_earth - q_sun) * (r_earth - r_moon) / Sin(alpha);
-    // Earth/Moon lineup
-    auto beta = ArcCos(InnerProduct(q_U23 - q_earth, q_U23 - q_moon) / ((q_U23 - q_moon).Norm() * (q_U23 - q_earth).Norm())); // Angle for U23
+    // Earth/Moon lineup for U23
+    auto beta = ArcCos(InnerProduct(q_U23 - q_earth, q_U23 - q_moon) / ((q_U23 - q_moon).Norm() * (q_U23 - q_earth).Norm()));
     // Do the angles work?
     LOG(ERROR) << alpha;
     LOG(ERROR) << beta;
@@ -105,8 +105,8 @@ namespace physics {
     // Earth/Sun lineup
     alpha = ArcSin((r_sun - r_earth)/(q_sun - q_earth).Norm());
     q_U23 = q_earth + Normalize(q_earth - q_sun) * (r_earth - r_moon) / Sin(alpha);
-    // Earth/Moon lineup
-    beta = ArcCos(InnerProduct(q_U23 - q_earth, q_U23 - q_moon) / ((q_U23 - q_moon).Norm() * (q_U23 - q_earth).Norm())); // Angle for U23
+    // Earth/Moon lineup for U23
+    beta = ArcCos(InnerProduct(q_U23 - q_earth, q_U23 - q_moon) / ((q_U23 - q_moon).Norm() * (q_U23 - q_earth).Norm()));
     // Do the angles work?
     LOG(ERROR) << alpha;
     LOG(ERROR) << beta;
@@ -121,39 +121,13 @@ namespace physics {
     // Earth/Sun lineup
     alpha = ArcSin((r_sun - r_earth)/(q_sun - q_earth).Norm());
     q_U14 = q_earth + Normalize(q_earth - q_sun) * (r_earth + r_moon) / Sin(alpha);
-    // Earth/Moon lineup
-    gamma = ArcCos(InnerProduct(q_U14 - q_earth, q_U14 - q_moon) / ((q_U14 - q_moon).Norm() * (q_U14 - q_earth).Norm())); // Angle for U14
+    // Earth/Moon lineup for U14
+    gamma = ArcCos(InnerProduct(q_U14 - q_earth, q_U14 - q_moon) / ((q_U14 - q_moon).Norm() * (q_U14 - q_earth).Norm()));
     // Do the angles work?
     LOG(ERROR) << alpha;
     LOG(ERROR) << gamma;
     EXPECT_THAT(AbsoluteError(alpha, gamma), AllOf(Lt(1.0 * Milli(Radian)), Gt(1.0 * Nano(Radian))));
 
-/*
-    etimes = {2433374.25788409, 2433374.29850909, 2433374.354979, 2433374.37367113, 2433374.43016419, 2433374.47075446};
-    for(int i = 0; i <= 5; i++) {
-      auto some_instant = JulianDate(etimes[i]);
-      auto q_sun = ephemeris->trajectory(sun)->EvaluatePosition(some_instant, nullptr);
-      auto q_moon = ephemeris->trajectory(moon)->EvaluatePosition(some_instant, nullptr);
-      auto q_earth = ephemeris->trajectory(earth)->EvaluatePosition(some_instant, nullptr);
-
-      // check body angles at target times
-      // Lunar eclipse
-      // Earth/Sun lineup
-      auto alpha = ArcSin((r_sun - r_earth)/(q_sun - q_earth).Norm());
-      auto q_U23 = q_earth + Normalize(q_earth - q_sun) * (r_earth - r_moon) / Sin(alpha);
-      auto q_U14 = q_earth + Normalize(q_earth - q_sun) * (r_earth + r_moon) / Sin(alpha);
-      // Earth/Moon lineup
-      auto beta = ArcCos(InnerProduct(q_U23 - q_earth, q_U23 - q_moon) / ((q_U23 - q_moon).Norm() * (q_U23 - q_earth).Norm())); // Angle for U23
-      auto gamma = ArcCos(InnerProduct(q_U14 - q_earth, q_U14 - q_moon) / ((q_U14 - q_moon).Norm() * (q_U14 - q_earth).Norm())); // Angle for U14
-      // Still need to compare angles
-      LOG(ERROR) << alpha;
-      LOG(ERROR) << beta;
-      LOG(ERROR) << gamma;
-
-      EXPECT_THAT(AbsoluteError(alpha, beta), AllOf(Lt(1.0 * Milli(Radian)), Gt(1.0 * Nano(Radian)))); // Should work for 3rd & 4th times
-      EXPECT_THAT(AbsoluteError(alpha, gamma), AllOf(Lt(1.0 * Milli(Radian)), Gt(1.0 * Nano(Radian)))); // Should work for 2nd & 5th times
-      }
-*/    
     // Later on for additional accuracy: 2 * ArcTan((x_norm_y - y_normx).Norm(),(x_norm_y + y_norm_x).Norm())
     // x_norm_y = x * y.Norm() and y_norm_x = y * x.Norm()
 
@@ -167,7 +141,7 @@ namespace physics {
     // 2469076.66235167  2469076.71279148  2469076.76776833  2469076.80661092  2469076.86158778  2469076.91195815
     // etimes = {2469076.66235167, 2469076.71279148, 2469076.76776833, 2469076.80661092, 2469076.86158778, 2469076.91195815};
   };
-        
-} // physics
 
-} // principia
+}  // namespace physics
+
+}  // namespace principia
