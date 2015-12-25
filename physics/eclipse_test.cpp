43e3530e264bdf9e1addcf9d938249cea0c0843d
pdn4kd (raptop) <pdn4kd@noreply.github.com>
2015-12-25 04:52:53 +0000

    Accidentally a word, removed redundant phrase.

--- physics/eclipse_test.cpp
+++ physics/eclipse_test.cpp
@@ -82,13 +82,12 @@ class EclipseTest : public testing::Test {
                ((apex_of_moon_locus_at_umbral_contact - q_moon).Norm() *
                 (apex_of_moon_locus_at_umbral_contact - q_earth).Norm()));
     // We are at the desired contact if the angle between Earth and Moon from
-    // the apex of locus of the moon at that contact is the same value (though
-    // offset from) the half-aperture of the umbra (Earth-Sun cone).
+    // the apex of locus of the moon at that contact is the same value as the
+    // half-aperture of the umbra (Earth-Sun cone).
     EXPECT_THAT(AbsoluteError(umbral_half_aperture, earth_moon_angle),
                 AllOf(Lt(1.0 * Milli(Radian)), Gt(1.0 * Nano(Radian))))
         << NAMED(umbral_half_aperture) << ", " << NAMED(earth_moon_angle)
         << ", " << NAMED(current_time);
-    return;
   }
 
   SolarSystem<ICRFJ2000Equator> solar_system_1950_;
