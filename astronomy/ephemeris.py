e1beea6bedf216b0bb6e8810cc6144e0ce85e1fa
pdn4kd (raptop) <pdn4kd@noreply.github.com>
2015-12-29 23:06:45 +0000

    More partials, corrected penumbral.

--- astronomy/ephemeris.py
+++ astronomy/ephemeris.py
@@ -12,7 +12,7 @@ from astropy.time import Time
 # U4 = 22:18:54
 # P4 = 23:17:21
 
-# Additional total and 2x partial eclipses in 1950/51
+# Additional total and 2x penumbral eclipses in 1950/51
 # 1950-09-26
 # P1 = 01:21:43 UT
 # U1 = 02:31:48
@@ -20,12 +20,24 @@ from astropy.time import Time
 # U3 = 04:38:49
 # U4 = 06:01:33
 # P4 = 07:11:47
-# 1951-04-23
+# 1951-03-23
 # P1 = 08:50:00
 # P4 = 12:24:19
 # 1951-09-15
 # P1 = 10:29:16
 # P4 = 14:23:52
+
+# 2x partial eclipses in 1952
+# 1952-02-11
+# P1 = 22:08:20 UT
+# U1 = 00:04:17
+# U4 = 01:14:24
+# P4 = 03:10:15
+# 1952-08-05
+# P1 = 17:28:13 UT
+# U1 = 18:33:49
+# U4 = 21:01:00
+# P4 = 22:06:35
 
 # Last Lunar Eclipse will be 2048-01-01 
 # P1 = 03:52:39 UT
@@ -52,7 +64,7 @@ et2 = eclipseTimes.tdb
 csv.write(str(et2.jd))
 csv.write('\n')
 
-P1P4 = ['1951-04-23T08:50:00', '1951-04-23T12:24:19']
+P1P4 = ['1951-03-23T08:50:00', '1951-03-23T12:24:19']
 eclipseTimes = Time(P1P4, format='isot', scale='utc')
 et2 = eclipseTimes.tdb
 csv.write(str(et2.jd))
@@ -60,6 +72,20 @@ csv.write('\n')
 
 P1P4 = ['1951-09-15T10:29:16', '1951-09-15T14:23:52']
 eclipseTimes = Time(P1P4, format='isot', scale='utc')
+et2 = eclipseTimes.tdb
+csv.write(str(et2.jd))
+csv.write('\n')
+
+P1U14P4 = ['1952-02-11T22:08:20', '1952-02-12T00:04:17', \
+           '1952-02-12T01:14:24', '1952-02-12T03:10:15']
+eclipseTimes = Time(P1U14P4, format='isot', scale='utc')
+et2 = eclipseTimes.tdb
+csv.write(str(et2.jd))
+csv.write('\n')
+
+P1U14P4 = ['1952-08-05T17:28:13', '1952-08-05T18:33:49', \
+           '1952-08-05T21:01:00', '1952-08-05T22:06:35']
+eclipseTimes = Time(P1U14P4, format='isot', scale='utc')
 et2 = eclipseTimes.tdb
 csv.write(str(et2.jd))
 csv.write('\n')
