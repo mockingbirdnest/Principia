28101715032dc3a619ea4da79d527e425cb18fd3
pleroy <phl+personal@google.com>
2016-01-03 13:56:38 +0100

    Order.

--- ksp_plugin/interface.hpp
+++ ksp_plugin/interface.hpp
@@ -89,48 +89,52 @@ bool operator==(NavigationFrameParameters const& left,
                 NavigationFrameParameters const& right);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__InitGoogleLogging();
+void CDECL principia__AddVesselToNextPhysicsBubble(
+    Plugin* const plugin,
+    char const* const vessel_guid,
+    KSPPart const* const parts,
+    int count);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__ActivateRecorder(bool const activate);
+void CDECL principia__AdvanceTime(Plugin* const plugin,
+                                  double const t,
+                                  double const planetarium_rotation);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__SetBufferedLogging(int const max_severity);
-extern "C" PRINCIPIA_DLL
-int CDECL principia__GetBufferedLogging();
-extern "C" PRINCIPIA_DLL
-void CDECL principia__SetBufferDuration(int const seconds);
-extern "C" PRINCIPIA_DLL
-int CDECL principia__GetBufferDuration();
-extern "C" PRINCIPIA_DLL
-void CDECL principia__SetSuppressedLogging(int const min_severity);
-extern "C" PRINCIPIA_DLL
-int CDECL principia__GetSuppressedLogging();
-extern "C" PRINCIPIA_DLL
-void CDECL principia__SetVerboseLogging(int const level);
-extern "C" PRINCIPIA_DLL
-int CDECL principia__GetVerboseLogging();
-extern "C" PRINCIPIA_DLL
-void CDECL principia__SetStderrLogging(int const min_severity);
-extern "C" PRINCIPIA_DLL
-int CDECL principia__GetStderrLogging();
+bool CDECL principia__AtEnd(LineAndIterator const* const line_and_iterator);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__LogInfo(char const* const text);
-extern "C" PRINCIPIA_DLL
-void CDECL principia__LogWarning(char const* const text);
-extern "C" PRINCIPIA_DLL
-void CDECL principia__LogError(char const* const text);
-extern "C" PRINCIPIA_DLL
-void CDECL principia__LogFatal(char const* const text);
+XYZ CDECL principia__BubbleDisplacementCorrection(Plugin const* const plugin,
+                                                  XYZ const sun_position);
 
 extern "C" PRINCIPIA_DLL
-Plugin* CDECL principia__NewPlugin(
-    double const initial_time,
-    double const planetarium_rotation_in_degrees);
+XYZ CDECL principia__BubbleVelocityCorrection(Plugin const* const plugin,
+                                              int const reference_body_index);
+
+extern "C" PRINCIPIA_DLL
+QP CDECL principia__CelestialFromParent(Plugin const* const plugin,
+                                        int const celestial_index);
+
+extern "C" PRINCIPIA_DLL
+double CDECL principia__CurrentTime(Plugin const* const plugin);
+
+extern "C" PRINCIPIA_DLL
+void CDECL principia__DeleteLineAndIterator(
+    LineAndIterator** const line_and_iterator);
 
 extern "C" PRINCIPIA_DLL
 void CDECL principia__DeletePlugin(Plugin const** const plugin);
+
+extern "C" PRINCIPIA_DLL
+void CDECL principia__DeletePluginSerialization(
+    char const** const serialization);
+
+extern "C" PRINCIPIA_DLL
+void CDECL principia__DeserializePlugin(
+    char const* const serialization,
+    int const serialization_size,
+    PushDeserializer** const deserializer,
+    Plugin const** const plugin);
 
 extern "C" PRINCIPIA_DLL
 void CDECL principia__DirectlyInsertCelestial(
@@ -150,6 +154,55 @@ void CDECL principia__DirectlyInsertCelestial(
   char const* const vz);
 
 extern "C" PRINCIPIA_DLL
+void CDECL principia__EndInitialization(Plugin* const plugin);
+
+extern "C" PRINCIPIA_DLL
+XYZSegment CDECL principia__FetchAndIncrement(
+    LineAndIterator* const line_and_iterator);
+
+extern "C" PRINCIPIA_DLL
+int principia__FlightPlanSize(Plugin const* const plugin,
+                              char const* const vessel_guid);
+
+extern "C" PRINCIPIA_DLL
+void CDECL principia__ForgetAllHistoriesBefore(Plugin* const plugin,
+                                               double const t);
+
+int CDECL principia__GetBufferDuration();
+extern "C" PRINCIPIA_DLL
+
+extern "C" PRINCIPIA_DLL
+int CDECL principia__GetBufferedLogging();
+
+extern "C" PRINCIPIA_DLL
+NavigationFrameParameters CDECL principia__GetNavigationFrameParameters(
+    NavigationFrame const* const navigation_frame);
+
+extern "C" PRINCIPIA_DLL
+NavigationFrame const* CDECL principia__GetPlottingFrame(
+    Plugin const* const plugin);
+
+extern "C" PRINCIPIA_DLL
+int CDECL principia__GetStderrLogging();
+
+extern "C" PRINCIPIA_DLL
+int CDECL principia__GetSuppressedLogging();
+
+extern "C" PRINCIPIA_DLL
+int CDECL principia__GetVerboseLogging();
+
+extern "C" PRINCIPIA_DLL
+bool principia__HasPrediction(Plugin const* const plugin,
+                              char const* const vessel_guid);
+
+extern "C" PRINCIPIA_DLL
+bool CDECL principia__HasVessel(Plugin* const plugin,
+                                char const* const vessel_guid);
+
+extern "C" PRINCIPIA_DLL
+void CDECL principia__InitGoogleLogging();
+
+extern "C" PRINCIPIA_DLL
 void CDECL principia__InsertCelestial(Plugin* const plugin,
                                       int const celestial_index,
                                       double const gravitational_parameter,
@@ -157,49 +210,32 @@ void CDECL principia__InsertCelestial(Plugin* const plugin,
                                       QP const from_parent);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__InsertSun(Plugin* const plugin,
-                                int const celestial_index,
-                                double const gravitational_parameter);
-
-extern "C" PRINCIPIA_DLL
-void CDECL principia__UpdateCelestialHierarchy(Plugin const* const plugin,
-                                               int const celestial_index,
-                                               int const parent_index);
-
-extern "C" PRINCIPIA_DLL
-void CDECL principia__EndInitialization(Plugin* const plugin);
-
-extern "C" PRINCIPIA_DLL
 bool CDECL principia__InsertOrKeepVessel(Plugin* const plugin,
                                          char const* const vessel_guid,
                                          int const parent_index);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__SetVesselStateOffset(Plugin* const plugin,
-                                           char const* const vessel_guid,
-                                           QP const from_parent);
+void CDECL principia__InsertSun(Plugin* const plugin,
+                                int const celestial_index,
+                                double const gravitational_parameter);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__AdvanceTime(Plugin* const plugin,
-                                  double const t,
-                                  double const planetarium_rotation);
+void CDECL principia__LogError(char const* const text);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__ForgetAllHistoriesBefore(Plugin* const plugin,
-                                               double const t);
+void CDECL principia__LogFatal(char const* const text);
 
 extern "C" PRINCIPIA_DLL
-QP CDECL principia__VesselFromParent(Plugin const* const plugin,
-                                     char const* const vessel_guid);
+void CDECL principia__LogInfo(char const* const text);
 
 extern "C" PRINCIPIA_DLL
-QP CDECL principia__CelestialFromParent(Plugin const* const plugin,
-                                        int const celestial_index);
+void CDECL principia__LogWarning(char const* const text);
 
 extern "C" PRINCIPIA_DLL
-NavigationFrame* CDECL principia__NewBodyCentredNonRotatingNavigationFrame(
+WXYZ CDECL principia__NavballOrientation(
     Plugin const* const plugin,
-    int const reference_body_index);
+    XYZ const sun_world_position,
+    XYZ const ship_world_position);
 
 extern "C" PRINCIPIA_DLL
 NavigationFrame* CDECL principia__NewBarycentricRotatingNavigationFrame(
@@ -208,36 +244,33 @@ NavigationFrame* CDECL principia__NewBarycentricRotatingNavigationFrame(
     int const secondary_index);
 
 extern "C" PRINCIPIA_DLL
+NavigationFrame* CDECL principia__NewBodyCentredNonRotatingNavigationFrame(
+    Plugin const* const plugin,
+    int const reference_body_index);
+
+extern "C" PRINCIPIA_DLL
 NavigationFrame* CDECL principia__NewNavigationFrame(
     Plugin const* const plugin,
     NavigationFrameParameters const parameters);
 
 extern "C" PRINCIPIA_DLL
-NavigationFrameParameters CDECL principia__GetNavigationFrameParameters(
-    NavigationFrame const* const navigation_frame);
+Plugin* CDECL principia__NewPlugin(
+    double const initial_time,
+    double const planetarium_rotation_in_degrees);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__SetPlottingFrame(
+int CDECL principia__NumberOfSegments(
+    LineAndIterator const* const line_and_iterator);
+
+extern "C" PRINCIPIA_DLL
+bool CDECL principia__PhysicsBubbleIsEmpty(Plugin const* const plugin);
+
+extern "C" PRINCIPIA_DLL
+LineAndIterator* CDECL principia__RenderedFlightPlan(
     Plugin* const plugin,
-    NavigationFrame** const navigation_frame);
-
-extern "C" PRINCIPIA_DLL
-NavigationFrame const* CDECL principia__GetPlottingFrame(
-    Plugin const* const plugin);
-
-extern "C" PRINCIPIA_DLL
-void principia__UpdatePrediction(Plugin const* const plugin,
-                                 char const* const vessel_guid);
-
-extern "C" PRINCIPIA_DLL
-LineAndIterator* CDECL principia__RenderedVesselTrajectory(
-    Plugin const* const plugin,
     char const* const vessel_guid,
+    int const plan_phase,
     XYZ const sun_world_position);
-
-extern "C" PRINCIPIA_DLL
-bool principia__HasPrediction(Plugin const* const plugin,
-                              char const* const vessel_guid);
 
 extern "C" PRINCIPIA_DLL
 LineAndIterator* CDECL principia__RenderedPrediction(
@@ -246,15 +279,29 @@ LineAndIterator* CDECL principia__RenderedPrediction(
     XYZ const sun_world_position);
 
 extern "C" PRINCIPIA_DLL
-int principia__FlightPlanSize(Plugin const* const plugin,
-                              char const* const vessel_guid);
+LineAndIterator* CDECL principia__RenderedVesselTrajectory(
+    Plugin const* const plugin,
+    char const* const vessel_guid,
+    XYZ const sun_world_position);
 
 extern "C" PRINCIPIA_DLL
-LineAndIterator* CDECL principia__RenderedFlightPlan(
+char const* CDECL principia__SayHello();
+
+extern "C" PRINCIPIA_DLL
+char const* CDECL principia__SerializePlugin(
+    Plugin const* const plugin,
+    PullSerializer** const serializer);
+
+extern "C" PRINCIPIA_DLL
+void CDECL principia__SetBufferDuration(int const seconds);
+
+extern "C" PRINCIPIA_DLL
+void CDECL principia__SetBufferedLogging(int const max_severity);
+
+extern "C" PRINCIPIA_DLL
+void CDECL principia__SetPlottingFrame(
     Plugin* const plugin,
-    char const* const vessel_guid,
-    int const plan_phase,
-    XYZ const sun_world_position);
+    NavigationFrame** const navigation_frame);
 
 extern "C" PRINCIPIA_DLL
 void CDECL principia__SetPredictionLength(Plugin* const plugin,
@@ -269,81 +316,46 @@ void CDECL principia__SetPredictionSpeedTolerance(Plugin* const plugin,
                                                   double const v);
 
 extern "C" PRINCIPIA_DLL
-bool CDECL principia__HasVessel(Plugin* const plugin,
-                                char const* const vessel_guid);
+void CDECL principia__SetStderrLogging(int const min_severity);
 
 extern "C" PRINCIPIA_DLL
-int CDECL principia__NumberOfSegments(
-    LineAndIterator const* const line_and_iterator);
+void CDECL principia__SetSuppressedLogging(int const min_severity);
 
 extern "C" PRINCIPIA_DLL
-XYZSegment CDECL principia__FetchAndIncrement(
-    LineAndIterator* const line_and_iterator);
+void CDECL principia__SetVerboseLogging(int const level);
 
 extern "C" PRINCIPIA_DLL
-bool CDECL principia__AtEnd(LineAndIterator const* const line_and_iterator);
+void CDECL principia__SetVesselStateOffset(Plugin* const plugin,
+                                           char const* const vessel_guid,
+                                           QP const from_parent);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__DeleteLineAndIterator(
-    LineAndIterator** const line_and_iterator);
+void CDECL principia__UpdateCelestialHierarchy(Plugin const* const plugin,
+                                               int const celestial_index,
+                                               int const parent_index);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__AddVesselToNextPhysicsBubble(
-    Plugin* const plugin,
-    char const* const vessel_guid,
-    KSPPart const* const parts,
-    int count);
-
-extern "C" PRINCIPIA_DLL
-bool CDECL principia__PhysicsBubbleIsEmpty(Plugin const* const plugin);
-
-extern "C" PRINCIPIA_DLL
-XYZ CDECL principia__BubbleDisplacementCorrection(Plugin const* const plugin,
-                                                  XYZ const sun_position);
-
-extern "C" PRINCIPIA_DLL
-XYZ CDECL principia__BubbleVelocityCorrection(Plugin const* const plugin,
-                                              int const reference_body_index);
-
-extern "C" PRINCIPIA_DLL
-WXYZ CDECL principia__NavballOrientation(
-    Plugin const* const plugin,
-    XYZ const sun_world_position,
-    XYZ const ship_world_position);
-
-extern "C" PRINCIPIA_DLL
-XYZ CDECL principia__VesselTangent(Plugin const* const plugin,
-                                   char const* const vessel_guid);
-
-extern "C" PRINCIPIA_DLL
-XYZ CDECL principia__VesselNormal(Plugin const* const plugin,
-                                  char const* const vessel_guid);
+void principia__UpdatePrediction(Plugin const* const plugin,
+                                 char const* const vessel_guid);
 
 extern "C" PRINCIPIA_DLL
 XYZ CDECL principia__VesselBinormal(Plugin const* const plugin,
                                     char const* const vessel_guid);
 
 extern "C" PRINCIPIA_DLL
-double CDECL principia__CurrentTime(Plugin const* const plugin);
+QP CDECL principia__VesselFromParent(Plugin const* const plugin,
+                                     char const* const vessel_guid);
 
 extern "C" PRINCIPIA_DLL
-char const* CDECL principia__SerializePlugin(
-    Plugin const* const plugin,
-    PullSerializer** const serializer);
+XYZ CDECL principia__VesselNormal(Plugin const* const plugin,
+                                  char const* const vessel_guid);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__DeletePluginSerialization(
-    char const** const serialization);
+XYZ CDECL principia__VesselTangent(Plugin const* const plugin,
+                                   char const* const vessel_guid);
 
 extern "C" PRINCIPIA_DLL
-void CDECL principia__DeserializePlugin(
-    char const* const serialization,
-    int const serialization_size,
-    PushDeserializer** const deserializer,
-    Plugin const** const plugin);
-
-extern "C" PRINCIPIA_DLL
-char const* CDECL principia__SayHello();
+void CDECL principia__ActivateRecorder(bool const activate);
 
 }  // namespace ksp_plugin
 }  // namespace principia
