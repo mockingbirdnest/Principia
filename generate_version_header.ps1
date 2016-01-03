51317a4dffec1e64b08fc472f71791718e7530eb
Robin Leroy <egg.robin.leroy@gmail.com>
2016-01-03 17:55:52 +0100

    Don't touch the version header if git describe hasn't changed

--- generate_version_header.ps1
+++ generate_version_header.ps1
@@ -1,21 +1,28 @@
 $solutiondir = resolve-path $args[0]
 $env:Path += ";$env:programfiles\Git\bin;$env:localappdata\GitHub\Portab~1\bin"
-
-$text = [string]::format(
-    "#pragma once`n"                                `
-        + "`n"                                      `
-        + "namespace principia {{`n"                `
-        + "namespace base {{`n"                     `
-        + "`n"                                      `
-        + "char const kBuildDate[] = `"{0:O}`";`n"  `
-        + "char const kVersion[] =`n"               `
-        + "    `"{1}`";`n"                          `
-        + "`n"                                      `
-        + "}}  // namespace base`n"                 `
-        + "}}  // namespace principia`n",           `
-    (get-date).ToUniversalTime(),
-    (git describe --tags --always --dirty --abbrev=40 --long))
-[system.io.file]::writealltext(
-    (join-path $solutiondir "base/version.hpp"),
-    $text,
-    [system.text.encoding]::utf8)
+[IO.File]::ReadAllText((join-path $solutiondir "base/version.hpp"))  `
+    -match '(?m)^\s+"([^"]+)";$.*'
+$oldversion = $matches[1]
+$newversion =  (git describe --tags --always --dirty --abbrev=40 --long)
+if ($oldversion = $newversion) {
+  echo "No change to git describe, leaving base/version.hpp unmodified"
+} else {
+  $text = [string]::format(
+      "#pragma once`n"                                `
+          + "`n"                                      `
+          + "namespace principia {{`n"                `
+          + "namespace base {{`n"                     `
+          + "`n"                                      `
+          + "char const kBuildDate[] = `"{0:O}`";`n"  `
+          + "char const kVersion[] =`n"               `
+          + "    `"{1}`";`n"                          `
+          + "`n"                                      `
+          + "}}  // namespace base`n"                 `
+          + "}}  // namespace principia`n",           `
+      (get-date).ToUniversalTime(),
+      $newversion)
+  [system.io.file]::writealltext(
+      (join-path $solutiondir "base/version.hpp"),
+      $text,
+      [system.text.encoding]::utf8)
+}
