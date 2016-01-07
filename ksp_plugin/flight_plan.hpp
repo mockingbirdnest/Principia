a0926d9d21443777c771b6ee7f0fd604fe4f815c
Robin Leroy <egg.robin.leroy@gmail.com>
2016-01-07 12:48:56 +0100

    after phl's review

--- ksp_plugin/flight_plan.hpp
+++ ksp_plugin/flight_plan.hpp
@@ -20,6 +20,12 @@ namespace ksp_plugin {
 // the corresponding |NavigationManœuvre|s.
 class FlightPlan {
  public:
+  // Creates a |FlightPlan| with no burns whose chain of trajectories is forked
+  // from |root| at the latest time prior to |initial_time|, and which starts
+  // with the given |initial_mass|.  The trajectories are computed using the
+  // given |integrator| in the given |ephemeris|.
+  // The given |final_time| and tolerances are used, and may be modified by
+  // the mutators |SetFinalTime| and |SetTolerances|.
   FlightPlan(
       not_null<DiscreteTrajectory<Barycentric>*> root,
       Instant const& initial_time,
@@ -33,33 +39,47 @@ class FlightPlan {
   ~FlightPlan() = default;
 
   int size() const;
+  // |index| must be in [0, size()[.
   NavigationManœuvre const& Get(int index);
 
+  // |size()| must be greater than 0.
   void RemoveLast();
 
   // The following two functions return false and have no effect if the given
   // |burn| would start before |initial_time_| or before the end of the previous
   // burn, or end after |final_time_|.
   bool Append(Burn burn);
+  // |size()| must be greater than 0.
   bool ReplaceLast(Burn burn);
 
   // Returns false and has no effect if |final_time| is before the end of the
   // last manœuvre or before |initial_time_|.
   bool SetFinalTime(Instant const& final_time);
 
+  // Sets the tolerances used to compute the trajectories.  The trajectories are
+  // recomputed.
   void SetTolerances(
       Length const& length_integration_tolerance,
       Speed const& speed_integration_tolerance);
 
  private:
+  // Appends |manœuvre| to |manœuvres_|, recomputes the last coast segment until
+  // |manœuvre.initial_time()|, and adds a burn and a coast segment.
+  // |manœuvre| must fit between |start_of_last_coast()| and |final_time_|.
   void Append(NavigationManœuvre manœuvre);
 
+  // Recomputes all trajectories in |segments_|.
   void RecomputeSegments();
 
+  // Flows the last segment for the duration of |manœuvre| using its intrinsic
+  // acceleration.
   void BurnLastSegment(NavigationManœuvre const& manœuvre);
+  // Flows the last segment until |final_time| with no intrinsic acceleration.
   void CoastLastSegment(Instant const& final_time);
 
+  // Adds a trajectory to |segment_|, forked at the end of the last one.
   void AddSegment();
+  // Forgets the last segment after its fork.
   void ResetLastSegment();
 
   Instant const start_of_last_coast() const;
