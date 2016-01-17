8052f0f4df8a5207def81c089fb35681c8538a6a
Robin Leroy <egg.robin.leroy@gmail.com>
2016-01-17 03:12:08 +0100

    ./ksp_plugin/plugin.hpp:248:37: error: extra qualification on member 'GetVessel'

--- ksp_plugin/plugin.hpp
+++ ksp_plugin/plugin.hpp
@@ -245,7 +245,7 @@ class Plugin {
   virtual void SetPredictionSpeedTolerance(Speed const& v);
 
   virtual bool HasVessel(GUID const& vessel_guid) const;
-  virtual not_null<Vessel*> Plugin::GetVessel(GUID const & vessel_guid) const;
+  virtual not_null<Vessel*> GetVessel(GUID const & vessel_guid) const;
 
   virtual not_null<std::unique_ptr<NavigationFrame>>
   NewBodyCentredNonRotatingNavigationFrame(
