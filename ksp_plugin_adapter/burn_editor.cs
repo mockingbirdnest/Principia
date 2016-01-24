073313b8123a2d56182cb3f9ce5b828354379cca
Robin Leroy <egg.robin.leroy@gmail.com>
2016-01-24 17:28:29 +0100

    magic thrusts

--- ksp_plugin_adapter/burn_editor.cs
+++ ksp_plugin_adapter/burn_editor.cs
@@ -9,7 +9,8 @@ namespace ksp_plugin_adapter {
 class BurnEditor {
   public BurnEditor(WindowRenderer.ManagerInterface manager,
                     IntPtr plugin,
-                    Vessel vessel) {
+                    Vessel vessel,
+                    double initial_time) {
     Δv_tangent_ = new DifferentialSlider(label            : "Δv tangent",
                                          unit             : "m / s",
                                          log10_lower_rate : Log10ΔvLowerRate,
@@ -30,12 +31,15 @@ class BurnEditor {
                 log10_upper_rate : Log10TimeUpperRate,
                 min_value        : 0,
                 max_value        : double.PositiveInfinity,
-                formatter        : value => FormatTimeSpan(
-                                                TimeSpan.FromSeconds(value)));
+                formatter        : value =>
+                    FormatTimeSpan(
+                        TimeSpan.FromSeconds(
+                            Planetarium.GetUniversalTime() - value)));
     reference_frame_selector_ =
         new ReferenceFrameSelector(manager, plugin, ReferenceFrameChanged);
     plugin_ = plugin;
     vessel_ = vessel;
+    ComputeEngineCharacterestics();
   }
 
   // Renders the |BurnEditor|.  Returns true if and only if the settings were
@@ -44,12 +48,28 @@ class BurnEditor {
     var old_skin = UnityEngine.GUI.skin;
     UnityEngine.GUI.skin = null;
     UnityEngine.GUILayout.BeginVertical();
+    bool changed = false;
     if (enabled) {
       reference_frame_selector_.RenderButton();
+      UnityEngine.GUILayout.BeginHorizontal();
+      if (UnityEngine.GUILayout.Button("Active Engines")) {
+        engine_warning_ = "";
+        ComputeEngineCharacterestics();
+        changed = true;
+      } else if (UnityEngine.GUILayout.Button("Active RCS")) {
+        engine_warning_ = "";
+        ComputeRCSCharacterestics();
+        changed = true;
+      } else if (UnityEngine.GUILayout.Button("Instant Impulse")) {
+        engine_warning_ = "";
+        UseMagicThrust();
+        changed = true;
+      }
+      UnityEngine.GUILayout.EndHorizontal();
+      UnityEngine.GUILayout.TextArea(engine_warning_);
     } else {
       reference_frame_selector_.Hide();
     }
-    bool changed = false;
     changed |= Δv_tangent_.Render(enabled);
     changed |= Δv_normal_.Render(enabled);
     changed |= Δv_binormal_.Render(enabled);
@@ -77,7 +97,7 @@ class BurnEditor {
         thrust_in_kilonewtons = thrust_in_kilonewtons_,
         specific_impulse_in_seconds_g0 = specific_impulse_in_seconds_g0_,
         frame = reference_frame_selector_.FrameParameters(),
-        initial_time = initial_time_.value + flight_plan_initial_time,
+        initial_time = initial_time_.value,
         delta_v = new XYZ{x = Δv_tangent_.value,
                           y = Δv_normal_.value,
                           z = Δv_binormal_.value } };
@@ -89,6 +109,80 @@ class BurnEditor {
 
   public void Close() {
     reference_frame_selector_.Dispose();
+  }
+
+  private void ComputeEngineCharacterestics() {
+    ModuleEngines[] active_engines =
+        (from part in vessel_.parts
+         select (from PartModule module in part.Modules
+                 where module is ModuleEngines &&
+                       (module as ModuleEngines).EngineIgnited
+                 select module as ModuleEngines)).SelectMany(x => x).ToArray();
+    Vector3d reference_direction = vessel_.ReferenceTransform.up;
+    double[] thrusts =
+        (from engine in active_engines
+         select engine.maxThrust *
+             (from transform in engine.thrustTransforms
+              select Math.Max(0,
+                              Vector3d.Dot(reference_direction,
+                                           -transform.forward))).Average()).
+            ToArray();
+    thrust_in_kilonewtons_ = thrusts.Sum();
+
+    // This would use zip if we had 4.0 or later.  We loop for now.
+    double Σ_f_over_i_sp = 0;
+    for (int i = 0; i < active_engines.Count(); ++i) {
+      Σ_f_over_i_sp +=
+          thrusts[i] / active_engines[i].atmosphereCurve.Evaluate(0);
+    }
+    specific_impulse_in_seconds_g0_ = thrust_in_kilonewtons_ / Σ_f_over_i_sp;
+
+    // If there are no engines, fall back onto RCS.
+    if (thrust_in_kilonewtons_ == 0) {
+      engine_warning_ = "No active engines, falling back to RCS";
+      ComputeRCSCharacterestics();
+    }
+  }
+
+  private void ComputeRCSCharacterestics() {
+    ModuleRCS[] active_rcs =
+        (from part in vessel_.parts
+         select (from PartModule module in part.Modules
+                 where module is ModuleRCS &&
+                       (module as ModuleRCS).rcsEnabled
+                 select module as ModuleRCS)).SelectMany(x => x).ToArray();
+    Vector3d reference_direction = vessel_.ReferenceTransform.up;
+    List<double> thrusts = new List<double>();
+    foreach (ModuleRCS rcs in active_rcs) {
+      thrusts.Add(0);
+      for (int i = 0; i < rcs.thrusterTransforms.Count; ++i) {
+        thrusts[thrusts.Count - 1] +=
+            Math.Max(0,
+                     Vector3d.Dot(-rcs.thrusterTransforms[i].forward *
+                                      rcs.thrustForces[i],
+                                  reference_direction));
+      }
+    }
+    thrust_in_kilonewtons_ = thrusts.Sum();
+
+    // This would use zip if we had 4.0 or later.  We loop for now.
+    double Σ_f_over_i_sp = 0;
+    for (int i = 0; i < active_rcs.Count(); ++i) {
+      Σ_f_over_i_sp +=
+          thrusts[i] / active_rcs[i].atmosphereCurve.Evaluate(0);
+    }
+    specific_impulse_in_seconds_g0_ = thrust_in_kilonewtons_ / Σ_f_over_i_sp;
+
+    // If RCS provides no thrust, model a virtually instant burn.
+    if (thrust_in_kilonewtons_ == 0) {
+      engine_warning_ = "No active RCS, modeling as instant burn";
+      UseMagicThrust();
+    }
+  }
+
+  private void UseMagicThrust() {
+    thrust_in_kilonewtons_ = 1E15;
+    specific_impulse_in_seconds_g0_ = 3E7;
   }
 
   // Returns the equivalent of the .NET >= 4 format
@@ -118,6 +212,7 @@ class BurnEditor {
   private readonly Vessel vessel_;
 
   private bool changed_reference_frame_ = false;
+  private string engine_warning_ = "";
 }
 
 }  // namespace ksp_plugin_adapter
--- ksp_plugin_adapter/flight_planner.cs
+++ ksp_plugin_adapter/flight_planner.cs
@@ -9,61 +9,102 @@ namespace ksp_plugin_adapter {
 
 class FlightPlanner {
   public FlightPlanner(WindowRenderer.ManagerInterface manager,
-                       IntPtr plugin,
-                       Vessel vessel) {
+                       IntPtr plugin) {
     manager_ = manager;
     plugin_ = plugin;
-    vessel_ = vessel;
-    string vessel_guid = vessel.id.ToString();
-    if (plugin_.HasVessel(vessel_guid) &&
-        plugin_.FlightPlanExists(vessel_guid)) {
-      burns_ = new List<BurnEditor>();
-      for (int i = 0;
-           i < plugin_.FlightPlanNumberOfManoeuvres(vessel_guid);
-           ++i) {
-        burns_.Add(new BurnEditor(manager_, plugin_, vessel_));
-        burns_.Last().Reset(
-            plugin_.FlightPlanGetManoeuvre(vessel.id.ToString(), i).burn);
-      }
-    }
   }
 
   public void Render() {
     var old_skin = UnityEngine.GUI.skin;
     UnityEngine.GUI.skin = null;
     UnityEngine.GUILayout.BeginVertical();
-    if (burns_ == null) {
-    } else {
-      for (int i = 0; i < burns_.Count - 1; ++i) {
-        burns_[i].Render(enabled : false);
-      }
-      burns_.Last().Render(enabled : true);
-      if (burns_.Count > 0) {
-        if (UnityEngine.GUILayout.Button(
-                "Delete",
-                UnityEngine.GUILayout.ExpandWidth(true))) {
-          plugin_.FlightPlanRemoveLast(vessel_.id.ToString());
-          burns_.Last().Close();
-          burns_.RemoveAt(burns_.Count - 1);
+
+    if (vessel_ == null || vessel_ != FlightGlobals.ActiveVessel ||
+        !plugin_.HasVessel(vessel_.id.ToString())) {
+      Reset();
+    }
+
+    if (vessel_ != null) {
+      string vessel_guid = vessel_.id.ToString();
+      if (burn_editors_ == null) {
+        if (plugin_.HasVessel(vessel_guid)) {
+          if (plugin_.FlightPlanExists(vessel_guid)) {
+            burn_editors_ = new List<BurnEditor>();
+            for (int i = 0;
+                 i < plugin_.FlightPlanNumberOfManoeuvres(vessel_guid);
+                 ++i) {
+              // Dummy initial time, we call |Reset| immediately afterwards.
+              burn_editors_.Add(
+                  new BurnEditor(manager_, plugin_, vessel_, initial_time : 0));
+              burn_editors_.Last().Reset(
+                  plugin_.FlightPlanGetManoeuvre(vessel_guid, i).burn);
+            }
+          } else {
+            if (UnityEngine.GUILayout.Button("Create flight plan")) {
+              plugin_.FlightPlanCreate(vessel_guid,
+                                       plugin_.CurrentTime() + 1000,
+                                       vessel_.GetTotalMass());
+            }
+          }
         }
-      }
-      if (UnityEngine.GUILayout.Button(
-              "Add",
-              UnityEngine.GUILayout.ExpandWidth(true))) {
-        burns_.Add(new BurnEditor(manager_, plugin_, vessel_));
+      } else {
+        if (UnityEngine.GUILayout.Button("Delete flight plan")) {
+          plugin_.FlightPlanDelete(vessel_guid);
+        } else {
+          for (int i = 0; i < burn_editors_.Count - 1; ++i) {
+            burn_editors_[i].Render(enabled : false);
+          }
+          burn_editors_.Last().Render(enabled : true);
+          if (burn_editors_.Count > 0) {
+            if (UnityEngine.GUILayout.Button(
+                    "Delete",
+                    UnityEngine.GUILayout.ExpandWidth(true))) {
+              plugin_.FlightPlanRemoveLast(vessel_guid);
+              burn_editors_.Last().Close();
+              burn_editors_.RemoveAt(burn_editors_.Count - 1);
+            }
+          }
+          if (UnityEngine.GUILayout.Button(
+                  "Add",
+                  UnityEngine.GUILayout.ExpandWidth(true))) {
+            double initial_time =
+                (burn_editors_.Count == 0
+                     ? plugin_.CurrentTime()
+                     : plugin_.FlightPlanGetManoeuvre(
+                           vessel_guid,
+                           burn_editors_.Count - 1).final_time) + 60;
+            burn_editors_.Add(
+                new BurnEditor(manager_, plugin_, vessel_, initial_time));
+            bool inserted = plugin_.FlightPlanAppend(
+                                vessel_guid,
+                                burn_editors_.Last().Burn());
+            if (!inserted) {
+              burn_editors_.RemoveAt(burn_editors_.Count - 1);
+            }
+          }
+        }
       }
     }
     UnityEngine.GUILayout.EndVertical();
     UnityEngine.GUI.skin = old_skin;
   }
 
-  private void ComputeEngineProperties() { }
+  private void Reset() {
+    foreach (BurnEditor editor in burn_editors_) {
+      editor.Close();
+    }
+    burn_editors_ = null;
+    vessel_ = FlightGlobals.ActiveVessel;
+  }
 
   // Not owned.
   private readonly IntPtr plugin_;
   private readonly WindowRenderer.ManagerInterface manager_;
-  private readonly Vessel vessel_;
-  private List<BurnEditor> burns_;
+  private Vessel vessel_;
+  private List<BurnEditor> burn_editors_;
+
+  // TODO(egg): make mutable.
+  private const double excess_time_ = 60;
 }
 
 }  // namespace ksp_plugin_adapter
