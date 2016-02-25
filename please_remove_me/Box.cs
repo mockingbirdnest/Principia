a8b62162189e212281197e6cfa58f31f7302c905
Robin Leroy <egg.robin.leroy@gmail.com>
2016-02-25 01:36:43 +0100

    experimenting and understanding

---
+++ please_remove_me/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace principia {
+namespace ksp_plugin_adapter {
+
+// Strongly-typed boxing.  This is used for marshaling, since custom marshalers
+// are only allowed on classes, strings, arrays, and boxed value types, so that
+// |T?| cannot be marshaled, and |object| is not statically typed.
+internal class Box<T> where T : struct {
+  public static implicit operator Box<T>(T all) {
+    return new Box<T>(all);
+  }
+
+  public T all { get; }
+
+  private Box(T all) {
+    this.all = all;
+  }
+}
+
+}  // namespace ksp_plugin_adapter
+}  // namespace principia
---
+++ please_remove_me/logging.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace principia {
+namespace ksp_plugin_adapter {
+
+internal static class Log {
+  internal static String[] kSeverityNames = {"INFO", "WARNING", "ERROR",
+                                             "FATAL"};
+
+  internal static void InitGoogleLogging() {
+    Interface.InitGoogleLogging();
+  }
+
+  internal static void ActivateRecorder(bool activate, bool verbose) {
+    Interface.ActivateRecorder(activate, verbose);
+  }
+
+  internal static void SetBufferedLogging(int max_severity) {
+    Interface.SetBufferedLogging(max_severity);
+  }
+
+  internal static int GetBufferedLogging() {
+    return Interface.GetBufferedLogging();
+  }
+
+  internal static void SetBufferDuration(int seconds) {
+    Interface.SetBufferDuration(seconds);
+  }
+
+  internal static int GetBufferDuration() {
+    return Interface.GetBufferDuration();
+  }
+
+  internal static void SetSuppressedLogging(int min_severity) {
+    Interface.SetSuppressedLogging(min_severity);
+  }
+
+  internal static int GetSuppressedLogging() {
+    return Interface.GetSuppressedLogging();
+  }
+
+  internal static void SetVerboseLogging(int level) {
+    Interface.SetVerboseLogging(level);
+  }
+
+  internal static int GetVerboseLogging() {
+    return Interface.GetVerboseLogging();
+  }
+
+  internal static void SetStderrLogging(int min_severity) {
+    Interface.SetStderrLogging(min_severity);
+  }
+
+  internal static int GetStderrLogging() {
+    return Interface.GetStderrLogging();
+  }
+
+  internal static void Info(String message) {
+    Interface.LogInfo(message);
+  }
+
+  internal static void Warning(String message) {
+    Interface.LogWarning(message);
+  }
+
+  internal static void Error(String message) {
+    Interface.LogError(message);
+  }
+
+  // Returns an exception so it can be thrown so that the compiler doesn't
+  // complain about non-returning code.
+  internal static Exception Fatal(String message) {
+    Interface.LogFatal(message);
+    return new Exception();
+  }
+}
+
+}  // namespace ksp_plugin_adapter
+}  // namespace principia
