8c04d06afaf371bf7b9bf2b0a0037b72907603e6
Robin Leroy <egg.robin.leroy@gmail.com>
2016-04-01 23:11:52 +0200

    after pleroy's review

--- journal/method.hpp
+++ journal/method.hpp
@@ -29,23 +29,27 @@ namespace journal {
 //                    not_null<Player::PointerMap*> const pointer_map);
 //  };
 
+namespace internal {
+
 template<typename T>
-using specialize = void;
+using void_if_exists = void;
 
 template<typename P, typename = void>
 struct has_in : std::false_type {};
 template<typename P>
-struct has_in<P, specialize<typename P::In>> : std::true_type {};
+struct has_in<P, void_if_exists<typename P::In>> : std::true_type {};
 
 template<typename P, typename = void>
 struct has_out : std::false_type {};
 template<typename P>
-struct has_out<P, specialize<typename P::Out>> : std::true_type {};
+struct has_out<P, void_if_exists<typename P::Out>> : std::true_type {};
 
 template<typename P, typename = void>
 struct has_return : std::false_type {};
 template<typename P>
-struct has_return<P, specialize<typename P::Return>> : std::true_type {};
+struct has_return<P, void_if_exists<typename P::Return>> : std::true_type {};
+
+}  // namespace internal
 
 template<typename Profile>
 class Method {
@@ -55,31 +59,34 @@ class Method {
   // Only declare this constructor if the profile has an |In| type and no |Out|
   // type.
   template<typename P = Profile,
-           typename = std::enable_if_t<has_in<P>::value && !has_out<P>::value>>
+           typename = std::enable_if_t<internal::has_in<P>::value &&
+                                       !internal::has_out<P>::value>>
   explicit Method(typename P::In const& in);
 
   // Only declare this constructor if the profile has an |Out| type and no |In|
   // type.
   template<typename P = Profile,
-           typename = std::enable_if_t<has_out<P>::value && !has_in<P>::value>>
+           typename = std::enable_if_t<internal::has_out<P>::value &&
+                                       !internal::has_in<P>::value>>
   explicit Method(typename P::Out const& out);
 
   // Only declare this constructor if the profile has an |In| and an |Out|
   // type.
   template<typename P = Profile,
-           typename = std::enable_if_t<has_in<P>::value && has_out<P>::value>>
+           typename = std::enable_if_t<internal::has_in<P>::value &&
+                                       internal::has_out<P>::value>>
   Method(typename P::In const& in, typename P::Out const& out);
 
   ~Method();
 
   // Only declare this method if the profile has no |Return| type.
   template<typename P = Profile,
-           typename = std::enable_if_t<!has_return<P>::value>>
+           typename = std::enable_if_t<!internal::has_return<P>::value>>
   void Return();
 
   // Only declare this method if the profile has a |Return| type.
   template<typename P = Profile,
-           typename = std::enable_if_t<has_return<P>::value>>
+           typename = std::enable_if_t<internal::has_return<P>::value>>
   typename P::Return Return(typename P::Return const& result);
 
  private:
