37290a55cee904cc9f09806667103f0808948b2c
Robin Leroy <egg.robin.leroy@gmail.com>
2016-04-13 18:35:27 +0200

    don't delete

--- ksp_plugin/vessel_body.hpp
+++ ksp_plugin/vessel_body.hpp
@@ -125,8 +125,8 @@ inline void Vessel::AdvanceTimeInBubble(
 
 inline void Vessel::ForgetBefore(Instant const& time) {
   CHECK(is_initialized());
-  history_->DeleteFork(&prediction_);
   if (prediction_->Fork().time() < time) {
+    history_->DeleteFork(&prediction_);
     prediction_ = history_->NewForkAtLast();
   }
   if (flight_plan_ != nullptr) {
