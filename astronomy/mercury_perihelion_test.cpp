360d0781106ffe418e3db2b719d8df20f043d4ae
pleroy <phl+personal@google.com>
2016-05-23 23:23:59 +0200

    Position error.

--- astronomy/mercury_perihelion_test.cpp
+++ astronomy/mercury_perihelion_test.cpp
@@ -69,17 +69,34 @@ SolarSystem<ICRFJ2000Equator> MercuryPerihelionTest::solar_system_1950_;
 std::unique_ptr<Ephemeris<ICRFJ2000Equator>> MercuryPerihelionTest::ephemeris_;
 
 TEST_F(MercuryPerihelionTest, PrintPerihelion) {
-  // From Horizons by dichotomy, around 1950-JAN-11 03:12:30.0000 (TDB).
-  Instant const first_perihelion_low = JulianDate(2433292.633686343);
-  Instant const first_perihelion_high = JulianDate(2433292.633692130);
-  Instant const first_perihelion_mid = Barycentre<Instant, double>(
-      {first_perihelion_low, first_perihelion_high}, {1, 1});
+  // From Horizons by dichotomy, around 1950-01-11T03:12:30.0000Z (TDB).
+  Instant const first_perihelion_time_low = JulianDate(2433292.633686343);
+  Instant const first_perihelion_time_high = JulianDate(2433292.633692130);
+  Instant const first_perihelion_time_mid = Barycentre<Instant, double>(
+      {first_perihelion_time_low, first_perihelion_time_high}, {1, 1});
 
-  // From Horizons by dichotomy, around 1959-Nov-26 21:00:28.0000 (TDB).
-  Instant const last_perihelion_low = JulianDate(2436899.375324074);
-  Instant const last_perihelion_high = JulianDate(2436899.375329861 );
-  Instant const last_perihelion_mid = Barycentre<Instant, double>(
-      {last_perihelion_low, last_perihelion_high}, {1, 1});
+  // From Horizons by dichotomy, around 1959-11-26T21:00:28.0000Z (TDB).
+  Instant const last_perihelion_time_low = JulianDate(2436899.375324074);
+  Instant const last_perihelion_time_high = JulianDate(2436899.375329861 );
+  Instant const last_perihelion_time_mid = Barycentre<Instant, double>(
+      {last_perihelion_time_low, last_perihelion_time_high}, {1, 1});
+
+  Position<ICRFJ2000Equator> const last_perihelion_position_low =
+      ICRFJ2000Equator::origin +
+      Displacement<ICRFJ2000Equator>(
+          {7.084639885659656e-02 * AstronomicalUnit,
+           2.748967000176096e-01 * AstronomicalUnit,
+           1.388562317323265e-01 * AstronomicalUnit});
+  Position<ICRFJ2000Equator> const last_perihelion_position_high =
+      ICRFJ2000Equator::origin +
+      Displacement<ICRFJ2000Equator>(
+          {7.084620740779489e-02 * AstronomicalUnit,
+           2.748967303484739e-01 * AstronomicalUnit,
+           1.388562678052315e-01 * AstronomicalUnit});
+  Position<ICRFJ2000Equator> const last_perihelion_position_mid =
+      Barycentre<Position<ICRFJ2000Equator>, double>(
+          {last_perihelion_position_low, last_perihelion_position_high},
+          {1, 1});
 
   DiscreteTrajectory<ICRFJ2000Equator> sun_apoapsides;
   DiscreteTrajectory<ICRFJ2000Equator> sun_periapsides;
@@ -93,10 +110,16 @@ TEST_F(MercuryPerihelionTest, PrintPerihelion) {
                              mercury_apoapsides,
                              mercury_periapsides);
 
-  EXPECT_LT(AbsoluteError(sun_periapsides.Begin().time(), first_perihelion_mid),
+  EXPECT_LT(AbsoluteError(sun_periapsides.Begin().time(),
+                          first_perihelion_time_mid),
             0.26 * Second);
-  EXPECT_LT(AbsoluteError(sun_periapsides.last().time(), last_perihelion_mid),
+  EXPECT_LT(AbsoluteError(sun_periapsides.last().time(),
+                          last_perihelion_time_mid),
             99.3 * Second);
+  EXPECT_LT(
+      AbsoluteError(mercury_periapsides.last().degrees_of_freedom().position(),
+                    last_perihelion_position_mid),
+      946 * Kilo(Metre));
 
   std::experimental::optional<Instant> previous_time;
   std::experimental::optional<KeplerianElements<ICRFJ2000Equator>>
