7ee4293e253347e884b1bc40f552bdf65bc91ae2
pleroy <phl+personal@google.com>
2016-05-27 20:27:42 +0200

    1960

--- astronomy/mercury_perihelion_test.cpp
+++ astronomy/mercury_perihelion_test.cpp
@@ -40,6 +40,10 @@ using quantities::si::Metre;
 using quantities::si::Milli;
 using quantities::si::Second;
 using testing_utilities::AbsoluteError;
+using testing_utilities::RelativeError;
+using ::testing::AllOf;
+using ::testing::Gt;
+using ::testing::Lt;
 
 namespace astronomy {
 
@@ -60,18 +64,100 @@ class MercuryPerihelionTest : public testing::Test {
 
   MercuryPerihelionTest()
       : sun_(solar_system_1950_.massive_body(*ephemeris_, "Sun")),
-        mercury_(solar_system_1950_.massive_body(*ephemeris_, "Mercury")) {}
+        mercury_(solar_system_1950_.massive_body(*ephemeris_, "Mercury")),
+        t_1950_(JulianDate(2433282.500000000)),
+        t_1960_(JulianDate(2436934.500000000)),
+        t_2050_(JulianDate(2469807.500000000)) {
+    keplerian_elements_1950_.eccentricity = 2.056187274905493E-01;
+    keplerian_elements_1950_.semimajor_axis =
+        5.790897350196702E+07 * Kilo(Metre);
+    keplerian_elements_1950_.mean_motion =
+        4.736523381721572E-05 * Degree / Second;
+    keplerian_elements_1950_.inclination = 2.854970888858858E+01 * Degree;
+    keplerian_elements_1950_.longitude_of_ascending_node =
+        1.100421919049157E+01 * Degree;
+    keplerian_elements_1950_.argument_of_periapsis =
+        6.747518782664667E+01 * Degree;
+    keplerian_elements_1950_.mean_anomaly = 3.185292722214373E+02 * Degree;
+
+    keplerian_elements_1960_.eccentricity = 2.056305163902087E-01;
+    keplerian_elements_1960_.semimajor_axis =
+        5.790908221204869E+07 * Kilo(Metre);
+    keplerian_elements_1960_.mean_motion =
+        4.736510044238512E-05 * Degree / Second;
+    keplerian_elements_1960_.inclination = 2.855025264340049E+01 * Degree;
+    keplerian_elements_1960_.longitude_of_ascending_node =
+        1.100120089390144E+01 * Degree;
+    keplerian_elements_1960_.argument_of_periapsis =
+        6.748880915164143E+01 * Degree;
+    keplerian_elements_1960_.mean_anomaly = 1.437427180144607E+02 * Degree;
+
+    keplerian_elements_2050_.eccentricity = 2.056443534543679E-01;
+    keplerian_elements_2050_.semimajor_axis =
+        5.790909484661692E+07 * Kilo(Metre);
+    keplerian_elements_2050_.mean_motion =
+        4.736508494125626E-05 * Degree / Second;
+    keplerian_elements_2050_.inclination = 2.855459021021251E+01 * Degree;
+    keplerian_elements_2050_.longitude_of_ascending_node =
+        1.097153493770216E+01 * Degree;
+    keplerian_elements_2050_.argument_of_periapsis =
+        6.765993779473686E+01 * Degree;
+    keplerian_elements_2050_.mean_anomaly = 3.105229688140852E+01 * Degree;
+  }
 
   static SolarSystem<ICRFJ2000Equator> solar_system_1950_;
   static std::unique_ptr<Ephemeris<ICRFJ2000Equator>> ephemeris_;
+
   not_null<MassiveBody const*> sun_;
   not_null<MassiveBody const*> mercury_;
+  Instant t_1950_;
+  Instant t_1960_;
+  Instant t_2050_;
+  KeplerianElements<ICRFJ2000Equator> keplerian_elements_1950_;
+  KeplerianElements<ICRFJ2000Equator> keplerian_elements_1960_;
+  KeplerianElements<ICRFJ2000Equator> keplerian_elements_2050_;
 };
 
 SolarSystem<ICRFJ2000Equator> MercuryPerihelionTest::solar_system_1950_;
 std::unique_ptr<Ephemeris<ICRFJ2000Equator>> MercuryPerihelionTest::ephemeris_;
 
+TEST_F(MercuryPerihelionTest, Year1960) {
+  ephemeris_->Prolong(t_1960_);
+
+  auto const& sun_trajectory =
+      solar_system_1950_.trajectory(*ephemeris_, "Sun");
+  auto const& mercury_trajectory =
+      solar_system_1950_.trajectory(*ephemeris_, "Mercury");
+
+  RelativeDegreesOfFreedom<ICRFJ2000Equator> const relative_degrees_of_freedom =
+      mercury_trajectory.EvaluateDegreesOfFreedom(t_1960_, /*hint=*/nullptr) -
+      sun_trajectory.EvaluateDegreesOfFreedom(t_1960_, /*hint=*/nullptr);
+  KeplerOrbit<ICRFJ2000Equator> orbit(
+      *sun_, *mercury_, relative_degrees_of_freedom, t_1960_);
+  KeplerianElements<ICRFJ2000Equator> const keplerian_elements =
+      orbit.elements_at_epoch();
+
+  EXPECT_LT(RelativeError(keplerian_elements.eccentricity,
+                          keplerian_elements_1960_.eccentricity), 6e-7);
+  EXPECT_LT(RelativeError(*keplerian_elements.semimajor_axis,
+                          *keplerian_elements_1960_.semimajor_axis), 1.1e-7);
+  EXPECT_LT(RelativeError(*keplerian_elements.mean_motion,
+                          *keplerian_elements_1960_.mean_motion), 1.7e-7);
+  EXPECT_LT(RelativeError(keplerian_elements.inclination,
+                          keplerian_elements_1960_.inclination), 1.1e-10);
+  EXPECT_LT(RelativeError(keplerian_elements.longitude_of_ascending_node,
+                          keplerian_elements_1960_.longitude_of_ascending_node),
+            1.3e-9);
+  EXPECT_THAT(keplerian_elements_1960_.argument_of_periapsis -
+                  keplerian_elements.argument_of_periapsis,
+              AllOf(Gt(4.202 * ArcSecond), Lt(4.203 * ArcSecond)));
+  EXPECT_THAT(keplerian_elements.mean_anomaly -
+                  keplerian_elements_1960_.mean_anomaly,
+              AllOf(Gt(17.0 * ArcSecond), Lt(17.1 * ArcSecond)));
+}
+
 TEST_F(MercuryPerihelionTest, KeplerianElements) {
+  LOG(ERROR) << JulianDate(2469807.500000000);
   ephemeris_->Prolong(solar_system_1950_.epoch() + 100 * JulianYear);
 
   auto const& sun_trajectory =
@@ -82,19 +168,28 @@ TEST_F(MercuryPerihelionTest, KeplerianElements) {
   typename ContinuousTrajectory<ICRFJ2000Equator>::Hint mercury_hint;
 
   std::vector<Angle> arguments_of_periapsis;
+  std::experimental::optional<Instant> previous_time;
+  std::experimental::optional<KeplerianElements<ICRFJ2000Equator>>
+      previous_keplerian_elements;
   for (Instant time = solar_system_1950_.epoch();
        time <= solar_system_1950_.epoch() + 100 * JulianYear;
        time += 1 * Day) {
     RelativeDegreesOfFreedom<ICRFJ2000Equator> const
         relative_degrees_of_freedom =
-            sun_trajectory.EvaluateDegreesOfFreedom(time, &sun_hint) -
-            mercury_trajectory.EvaluateDegreesOfFreedom(time, &mercury_hint);
+            mercury_trajectory.EvaluateDegreesOfFreedom(time, &mercury_hint) -
+            sun_trajectory.EvaluateDegreesOfFreedom(time, &sun_hint);
     KeplerOrbit<ICRFJ2000Equator> orbit(
         *sun_, *mercury_, relative_degrees_of_freedom, time);
     KeplerianElements<ICRFJ2000Equator> const keplerian_elements =
         orbit.elements_at_epoch();
     arguments_of_periapsis.push_back(keplerian_elements.argument_of_periapsis);
+    if (!previous_keplerian_elements) {
+      LOG(ERROR)<<time<<":\n"<<keplerian_elements;
+    }
+    previous_time = time;
+    previous_keplerian_elements = keplerian_elements;
   }
+  LOG(ERROR) << *previous_time << ":\n" << *previous_keplerian_elements;
 
   std::ofstream file;
   file.open("mercury_perihelion.generated.wl");
