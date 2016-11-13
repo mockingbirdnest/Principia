932b85627a6b1aaabcd7b24312d7e248344b008f
pleroy <phl+personal@google.com>
2016-11-13 13:06:10 +0100

    Compile.

--- integrators/symmetric_linear_multistep_integrator_body.hpp
+++ integrators/symmetric_linear_multistep_integrator_body.hpp
@@ -27,7 +27,9 @@ SymmetricLinearMultistepIntegrator(
     FixedVector<double, half_order_> const & ɑ,
     FixedVector<double, half_order_> const & β_numerators,
     double const β_denominator)
-    : ɑ_(ɑ), β_(Divide(β_numerators, β_denominator)) {}
+    : FixedStepSizeIntegrator(kind),
+      ɑ_(ɑ),
+      β_(Divide(β_numerators, β_denominator)) {}
 
 template<typename Position, int order_>
 void SymmetricLinearMultistepIntegrator<Position, order_>::Solve(
@@ -38,9 +40,9 @@ template <typename Position>
 SymmetricLinearMultistepIntegrator<Position, 8> const& Quinlan1999Order8A() {
   static SymmetricLinearMultistepIntegrator<Position, 8> const integrator(
       serialization::FixedStepSizeIntegrator::QUINLAN_1999_ORDER_8A,
-      {1, -2, 2, -2, 2},
-      {0, 22081, -29418, 75183, -75212},
-      15120);
+      {1.0, -2.0, 2.0, -2.0, 2.0},
+      {0.0, 22081.0, -29418.0, 75183.0, -75212.0},
+      15120.0);
   return integrator;
 }
 
@@ -48,9 +50,9 @@ template <typename Position>
 SymmetricLinearMultistepIntegrator<Position, 8> const& Quinlan1999Order8B() {
   static SymmetricLinearMultistepIntegrator<Position, 8> const integrator(
       serialization::FixedStepSizeIntegrator::QUINLAN_1999_ORDER_8B,
-      {1, 0, 0, -1 / 2, -1},
-      {0, 192481, 6582, 816783, -156812},
-      120960);
+      {1.0, 0.0, 0.0, -1 / 2.0, -1.0},
+      {0.0, 192481.0, 6582.0, 816783.0, -156812.0},
+      120960.0);
   return integrator;
 }
 
@@ -59,54 +61,54 @@ SymmetricLinearMultistepIntegrator<Position, 8> const&
 QuinlanTremaine1990Order8() {
   static SymmetricLinearMultistepIntegrator<Position, 8> const integrator(
       serialization::FixedStepSizeIntegrator::QUINLAN_TREMAINE_1990_ORDER_8,
-      {1, -2, 2, -1, 0},
-      {0, 17671, -23622, 61449, 50516},
-      12096);
+      {1.0, -2.0, 2.0, -1.0, 0.0},
+      {0.0, 17671.0, -23622.0, 61449.0, 50516.0},
+      12096.0);
   return integrator;
 }
 
 template <typename Position>
 SymmetricLinearMultistepIntegrator<Position, 10> const&
 QuinlanTremaine1990Order10() {
-  static SymmetricLinearMultistepIntegrator<Position, 8> const integrator(
+  static SymmetricLinearMultistepIntegrator<Position, 10> const integrator(
       serialization::FixedStepSizeIntegrator::QUINLAN_TREMAINE_1990_ORDER_10,
-      {1, -1, 1, -1, 1, 2},
-      {0, 399187, -485156, 2391436, -2816732, 4651330},
-      241920);
+      {1.0, -1.0, 1.0, -1.0, 1.0, 2.0},
+      {0.0, 399187.0, -485156.0, 2391436.0, -2816732.0, 4651330.0},
+      241920.0);
   return integrator;
 }
 
 template <typename Position>
 SymmetricLinearMultistepIntegrator<Position, 12> const&
 QuinlanTremaine1990Order12() {
-  static SymmetricLinearMultistepIntegrator<Position, 8> const integrator(
+  static SymmetricLinearMultistepIntegrator<Position, 12> const integrator(
       serialization::FixedStepSizeIntegrator::QUINLAN_TREMAINE_1990_ORDER_12,
-      {1, -2, 2, -1, 0, 0, 0},
-      {0,
-       90987349,
-       -229596838,
-       812627169,
-       -1628539944,
-       2714971338,
-       -3041896548},
-      53222400);
+      {1.0, -2.0, 2.0, -1.0, 0.0, 0.0, 0.0},
+      {0.0,
+       90987349.0,
+       -229596838.0,
+       812627169.0,
+       -1628539944.0,
+       2714971338.0,
+       -3041896548.0},
+      53222400.0);
   return integrator;
 }
 
 template <typename Position>
 SymmetricLinearMultistepIntegrator<Position, 14> const&
 QuinlanTremaine1990Order14() {
-  static SymmetricLinearMultistepIntegrator<Position, 8> const integrator(
+  static SymmetricLinearMultistepIntegrator<Position, 14> const integrator(
       serialization::FixedStepSizeIntegrator::QUINLAN_TREMAINE_1990_ORDER_14,
-      {1, -2, 2, -1, 0, 0, 0, 0},
-      {433489274083,
-       -1364031998256,
-       5583113380398,
-       -14154444148720,
-       28630585332045,
-       -42056933842656,
-       48471792742212},
-      237758976000);
+      {1.0, -2.0, 2.0, -1.0, 0.0, 0.0, 0.0, 0.0},
+      {433489274083.0,
+       -1364031998256.0,
+       5583113380398.0,
+       -14154444148720.0,
+       28630585332045.0,
+       -42056933842656.0,
+       48471792742212.0},
+      237758976000.0);
   return integrator;
 }
 
