0627d6c52127b3ef25687a9c04b7cead69eee481
Robin Leroy <egg.robin.leroy@gmail.com>
2016-11-22 14:30:21 +0100

    cooperative abort

--- base/bundle.cpp
+++ base/bundle.cpp
@@ -1,10 +1,11 @@
 #pragma once
 
+#include "base/bundle.hpp"
+
 #include <functional>
 #include <queue>
 
 #include "base/status.hpp"
-#include "bundle.hpp"
 
 namespace principia {
 namespace base {
@@ -16,14 +17,10 @@ Bundle::Bundle(int threads) {
   }
   for (int i = 0; i < threads; ++i) {
     threads_.emplace_back([this]() {
-      Task current;
-      Status status;
+      std::shared_ptr<Task> current_task = nullptr;
       for (;;) {
         {
           std::unique_lock<std::mutex> l(lock_);
-          if (!status.ok()) {
-            status_ = status;
-          }
           if (joining_ && tasks_.empty()) {
             return;
           }
@@ -33,26 +30,43 @@ Bundle::Bundle(int threads) {
           if (tasks_.empty()) {
             return;
           }
-          current = std::move(tasks_.front());
-          tasks_.pop();
+          current_task = std::move(tasks_.front());
+          tasks_.pop_front();
+          current_task->in_queue_ = std::experimental::nullopt;
+          current_task->abort_requested_ = &AbortRequested;
         }
-        status = current();
+        Status const status = current_task->body_();
+        {
+          std::unique_lock<std::mutex> l(lock_);
+          current_task->abort_requested_ = nullptr;
+          current_task->status_ = status;
+          if (!status.ok() && status_.ok()) {
+            status_ = status;
+          }
+        }
+        current_task->done_.notify_all();
+        current_task = nullptr;
       }
     });
   }
 }
 
-void Bundle::Add(Task task) {
+std::shared_ptr<Bundle::Task> Bundle::Add(std::function<Status()> task) {
+  auto result = std::make_shared<Task>(task);
   {
     std::unique_lock<std::mutex> l(lock_);
-    tasks_.emplace(std::move(task));
+    CHECK(!joining_);
+    tasks_.emplace_back(result);
+    tasks_.back();
   }
   tasks_not_empty_or_joining_.notify_one();
+  return result;
 }
 
 Status Bundle::Join() {
   {
     std::unique_lock<std::mutex> l(lock_);
+    CHECK(!joining_);
     joining_ = true;
   }
   tasks_not_empty_or_joining_.notify_all();
@@ -62,6 +76,28 @@ Status Bundle::Join() {
   std::unique_lock<std::mutex> l(lock_);
   return status_;
 }
+
+void Bundle::Abort(not_null<Task*> task) {
+  std::unique_lock<std::mutex> l(lock_);
+  if (task->in_queue_) {
+    tasks_.erase(*task->in_queue_);
+    task->in_queue_ = std::experimental::nullopt;
+  } else if (task->abort_requested_ != nullptr) {
+    *task->abort_requested_ = true;
+  }
+  // TODO(egg): Should we do something, e.g., set task->status_ to aborted if it
+  // terminated normally then was aborted?
+}
+
+Status Bundle::JoinTask(not_null<Task*> task) {
+  std::unique_lock<std::mutex> l(lock_);
+  // TODO(egg): this should use has_value rather than static_cast once we have
+  // a better optional.
+  task->done_.wait(l, [task]() { return static_cast<bool>(task->status_); });
+  return *task->status_;
+}
+
+Bundle::Task::Task(std::function<Status()> body) : body_(std::move(body)) {}
 
 }  // namespace base
 }  // namespace principia
--- base/bundle.hpp
+++ base/bundle.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <atomic>
+#include <experimental/optional>
 #include <functional>
-#include <queue>
+#include <list>
+#include <memory>
 #include <mutex>
 
 #include "base/macros.hpp"
@@ -12,19 +14,56 @@
 namespace principia {
 namespace base {
 
+thread_local std::atomic<bool> AbortRequested;
+
 class Bundle {
+  class Task;
+
+ public:
   Bundle(int threads);
 
-  using Task = std::function<Status()>;
-  void Add(Task task);
+  // Returns the first non-OK status encountered.  All threads are joined;
+  // no calls to |Add| or |Join| may follow this call.
   Status Join();
 
+  // The given |Task| may be used in calls to |Abort| or |JoinTask|; |this|
+  // shares ownership until |task| has returned.
+  std::shared_ptr<Task> Add(std::function<Status()> task);
+
+  // Removes the given |task| from the queue if it has not started, or instructs
+  // it to cooperatively abort if it is running.
+  void Abort(not_null<Task*> task);
+
+  // Waits until the given |task| is scheduled and completes, and returns its
+  // status.
+  Status JoinTask(not_null<Task*> task);
+
  private:
+  class Task {
+   public:
+    Task(std::function<Status()> body);
+
+    std::function<Status()> body_;
+
+    // |status_|, |in_queue_|, and |abort_requested_| are guarded by the |lock_|
+    // of the |Bundle|.
+
+    // Has a value if and only if the task has completed.
+    std::experimental::optional<Status> status_;
+    // Has a value if and only if the task has yet to start.
+    std::experimental::optional<std::list<std::shared_ptr<Task>>::iterator>
+        in_queue_;
+    // Is non-null if and only if the task is running.
+    std::atomic<bool>* abort_requested_ = nullptr;
+
+    std::condition_variable done_;
+  };
+
   std::mutex lock_;
   std::condition_variable tasks_not_empty_or_joining_;
 
   bool joining_ GUARDED_BY(lock_);
-  std::queue<Task> tasks_ GUARDED_BY(lock_);
+  std::list<std::shared_ptr<Task>> tasks_ GUARDED_BY(lock_);
   Status status_ GUARDED_BY(lock_);
 
   std::vector<std::thread> threads_;
