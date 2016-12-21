d5fb42ebe12a65d30b77c404793065225475bea5
Robin Leroy <egg.robin.leroy@gmail.com>
2016-12-21 16:48:11 +0100

    tpyo

--- testing_utilities/solar_system_factory.hpp
+++ testing_utilities/solar_system_factory.hpp
@@ -29,7 +29,7 @@ namespace testing_utilities {
 // A helper class for constructing physics::SolarSystem objects for testing.
 // TODO(egg): should this be a namespace instead?  It contains only static
 // things, and there's no reason for it to be a type, it's not used in fancy
-// template thnigs.
+// template things.
 class SolarSystemFactory final {
  public:
   // The bodies are in decreasing order of mass.
