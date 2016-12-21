cec7ef0ae12719049714d71c0177fb2a4efda761
Robin Leroy <egg.robin.leroy@gmail.com>
2016-12-21 22:08:33 +0100

    delete the constructor too

--- base/type_traits.hpp
+++ base/type_traits.hpp
@@ -7,6 +7,7 @@ namespace base {
 // Type traits should inherit from this, so that they may use inheritance
 // without virtual destructors.
 struct not_constructible {
+  not_constructible() = delete;
   ~not_constructible() = delete;
 };
 
