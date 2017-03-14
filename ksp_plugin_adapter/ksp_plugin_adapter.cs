37ebce6160e3a5480c49d81c2b2a5405e367183b
Robin Leroy <egg.robin.leroy@gmail.com>
2017-03-14 16:54:32 +0100

    revert

--- ksp_plugin_adapter/ksp_plugin_adapter.cs
+++ ksp_plugin_adapter/ksp_plugin_adapter.cs
@@ -697,6 +697,8 @@ public partial class PrincipiaPluginAdapter
       // the FashionablyLate callbacks, including ReportNonConservativeForces,
       // then the FlightIntegrator's FixedUpdate will run, then the Vessel's,
       // and eventually the physics simulation.
+      StartCoroutine(
+          AdvanceAndNudgeVesselsAfterPhysicsSimulation(universal_time));
     }
   }
 
@@ -722,8 +724,14 @@ public partial class PrincipiaPluginAdapter
 
   #endregion
 
-  private void AdvanceTimeAndNudgeVesselsAfterPhysicsSimulation() {
-     double universal_time = Planetarium.GetUniversalTime();
+  private System.Collections.IEnumerator
+  AdvanceAndNudgeVesselsAfterPhysicsSimulation(double universal_time) {
+     yield return new UnityEngine.WaitForFixedUpdate();
+     // Unity's physics has just finished doing its thing.  If we correct the
+     // positions here, nobody will know that they're not the ones obtained by
+     // Unity.  Careful however: while the positions here are those of the next
+     // step, the Planetarium hasn't run yet, and still has its old time.
+     universal_time += Planetarium.TimeScale * Planetarium.fetch.fixedDeltaTime;
 
      plugin_.PrepareToReportCollisions();
 
@@ -733,9 +741,7 @@ public partial class PrincipiaPluginAdapter
      // physics simulation, which is why we report them before calling
      // |AdvanceTime|.
      foreach (Vessel vessel1 in FlightGlobals.VesselsLoaded) {
-       if (plugin_.HasVessel(vessel1.id.ToString()) &&
-           plugin_.VesselIsLoaded(vessel1.id.ToString()) &&
-           !vessel1.packed) {
+       if (plugin_.HasVessel(vessel1.id.ToString()) && !vessel1.packed) {
          if (vessel1.isEVA && vessel1.evaController.OnALadder) {
            var vessel2 = vessel1.evaController.LadderPart.vessel;
            if (vessel2 != null && plugin_.HasVessel(vessel2.id.ToString()) &&
@@ -749,9 +755,7 @@ public partial class PrincipiaPluginAdapter
              var part2 =
                  collider.gameObject.GetComponentUpwards<Part>();
              var vessel2 = part2.vessel;
-             if (vessel2 != null &&
-                 plugin_.HasVessel(vessel2.id.ToString()) &&
-                 plugin_.VesselIsLoaded(vessel1.id.ToString())) {
+             if (vessel2 != null && plugin_.HasVessel(vessel2.id.ToString())) {
                plugin_.ReportCollision(part1.flightID, part2.flightID);
              }
            }
@@ -767,19 +771,17 @@ public partial class PrincipiaPluginAdapter
                  universal_time + " plugin-universal=" +
                  (plugin_time - universal_time));
        time_is_advancing_ = false;
-       return;
+       yield break;
      } else if (plugin_time == universal_time) {
        time_is_advancing_ = false;
-       return;
+       yield break;
      }
      time_is_advancing_ = true;
 
      plugin_.FreeVesselsAndPartsAndCollectPileUps();
 
      foreach (Vessel vessel in FlightGlobals.VesselsLoaded) {
-       if (vessel.packed ||
-           !plugin_.HasVessel(vessel.id.ToString()) ||
-           !plugin_.VesselIsLoaded(vessel.id.ToString())) {
+       if (vessel.packed || !plugin_.HasVessel(vessel.id.ToString())) {
          continue;
        }
        foreach (Part part in vessel.parts) {
@@ -808,9 +810,7 @@ public partial class PrincipiaPluginAdapter
          // TODO(egg): if I understand anything, there should probably be a
          // special treatment for loaded packed vessels.  I don't understand
          // anything though.
-         if (vessel.packed ||
-             !plugin_.HasVessel(vessel.id.ToString()) ||
-             !plugin_.VesselIsLoaded(vessel.id.ToString())) {
+         if (vessel.packed || !plugin_.HasVessel(vessel.id.ToString())) {
            continue;
          }
          foreach (Part part in vessel.parts) {
@@ -855,7 +855,8 @@ public partial class PrincipiaPluginAdapter
   }
 
   private void SetBodyFramesAndPrecalculateVessels() {
-    AdvanceTimeAndNudgeVesselsAfterPhysicsSimulation();
+    Log.Info("Precalc");
+    AdvanceTime();
     SetBodyFrames();
     // Unfortunately there is no way to get scheduled between Planetarium and
     // VesselPrecalculate, so we get scheduled after VesselPrecalculate, set the
