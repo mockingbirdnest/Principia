8947fe66221e2e9d00c30460005230f7cd0e60fe
Robin Leroy <egg.robin.leroy@gmail.com>
2017-03-20 11:21:47 +0100

    I may have fixed it?

--- ksp_plugin_adapter/ksp_plugin_adapter.cs
+++ ksp_plugin_adapter/ksp_plugin_adapter.cs
@@ -1005,17 +1005,39 @@ public partial class PrincipiaPluginAdapter
                 FlightGlobals.currentMainBody.flightGlobalsIndex);
       }
       ApplyToBodyTree(body => UpdateBody(body, Planetarium.GetUniversalTime()));
+
+      Vector3d ? offset = null;
+      if (FlightGlobals.ActiveVessel != null &&
+          plugin_.HasVessel(FlightGlobals.ActiveVessel.id.ToString())) {
+        QP main_body_dof = plugin_.CelestialWorldDegreesOfFreedom(
+            FlightGlobals.ActiveVessel.mainBody.flightGlobalsIndex,
+            FlightGlobals.ActiveVessel.rootPart.flightID);
+        krakensbane.FrameVel = -(Vector3d)main_body_dof.p;
+        offset = (Vector3d)main_body_dof.q -
+                 FlightGlobals.ActiveVessel.mainBody.position;
+      }
+
       foreach (var body in FlightGlobals.Bodies) {
-        // TODO(egg): I have no idea why this |swizzle| thing makes things work.
-        // This probably really means something in terms of frames that should
-        // be done in the C++ instead---once I figure out what it is.
+        // TODO(egg): I have no idea why this |swizzle| thing makes things
+        // work. This probably really means something in terms of frames that
+        // should be done in the C++ instead---once I figure out what it is.
         var swizzly_body_world_to_world =
             ((UnityEngine.QuaternionD)plugin_.CelestialRotation(
-                 body.flightGlobalsIndex)).swizzle;
+                 body.flightGlobalsIndex))
+                .swizzle;
         body.BodyFrame = new Planetarium.CelestialFrame{
             X = swizzly_body_world_to_world * new Vector3d{x = 1, y = 0, z = 0},
             Y = swizzly_body_world_to_world * new Vector3d{x = 0, y = 1, z = 0},
             Z = swizzly_body_world_to_world * new Vector3d{x = 0, y = 0, z = 1}};
+        if (offset.HasValue) {
+          body.position += offset.Value;
+        }
+      }
+      if (offset.HasValue) {
+        foreach (Vessel vessel in FlightGlobals.Vessels.Where(
+            is_on_rails_in_space)) {
+          vessel.SetPosition(vessel.transform.position + offset.Value);
+        }
       }
     }
   }
