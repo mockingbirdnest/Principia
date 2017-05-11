7836be6016b88b5181761a642005a762091da4df
Robin Leroy <egg.robin.leroy@gmail.com>
2017-05-11 22:02:56 +0200

    the sign of NaNs

--- physics/kepler_orbit_test.cpp
+++ physics/kepler_orbit_test.cpp
@@ -15,6 +15,7 @@ namespace physics {
 namespace internal_kepler_orbit {
 
 using astronomy::ICRFJ2000Equator;
+using astronomy::J2000;
 using astronomy::JulianDate;
 using quantities::astronomy::JulianYear;
 using quantities::si::AstronomicalUnit;
@@ -25,6 +26,7 @@ using quantities::si::Milli;
 using quantities::si::Second;
 using testing_utilities::AlmostEquals;
 using ::testing::AllOf;
+using ::testing::Eq;
 using ::testing::Gt;
 using ::testing::Lt;
 
@@ -77,19 +79,19 @@ class KeplerOrbitTest : public ::testing::Test {
   KeplerianElements<ICRFJ2000Equator> SimpleEllipse() const {
     KeplerianElements<ICRFJ2000Equator> elements;
     elements.eccentricity = 0.5;
-    elements.asymptotic_true_anomaly = NaN<Angle>();
-    elements.turning_angle = NaN<Angle>();
+    elements.asymptotic_true_anomaly = -NaN<Angle>();
+    elements.turning_angle = -NaN<Angle>();
     elements.semimajor_axis = 1 * AstronomicalUnit;
     elements.specific_energy =
         -0.5 * Pow<2>(AstronomicalUnit) / Pow<2>(JulianYear);
     elements.characteristic_energy =
         -1 * Pow<2>(AstronomicalUnit) / Pow<2>(JulianYear);
     elements.period = 2 * π * JulianYear;
-    elements.mean_anomaly = 1 * Radian / JulianYear;
-    elements.hyperbolic_mean_anomaly = NaN<AngularFrequency>();
-    elements.hyperbolic_excess_velocity = NaN<Speed>();
+    elements.mean_motion = 1 * Radian / JulianYear;
+    elements.hyperbolic_mean_motion = -NaN<AngularFrequency>();
+    elements.hyperbolic_excess_velocity = -NaN<Speed>();
     elements.semiminor_axis = Sqrt(3) / 2 * AstronomicalUnit;
-    elements.impact_parameter = NaN<Length>();
+    elements.impact_parameter = -NaN<Length>();
     elements.semilatus_rectum = 0.75 * AstronomicalUnit;
     elements.specific_angular_momentum =
         Sqrt(3) / 2 * Pow<2>(AstronomicalUnit) / (JulianYear * Radian);
@@ -101,14 +103,13 @@ class KeplerOrbitTest : public ::testing::Test {
     elements.argument_of_periapsis = 0.5 * Radian;
     elements.longitude_of_periapsis = 1 * Radian;
 
-    elements.true_anomaly = π / 2;
+    elements.true_anomaly = π / 2 * Radian;
     elements.mean_anomaly = (π / 3 - Sqrt(3) / 4) * Radian;
-    elements.hyperbolic_mean_anomaly = NaN<Angle>();
+    elements.hyperbolic_mean_anomaly = -NaN<Angle>();
     return elements;
   }
 
-  GravitationalParameter const μ_ =
-      1 * Pow<3>(AstronomicalUnit) / Pow<2>(JulianYear);
+  MassiveBody const body_{1 * Pow<3>(AstronomicalUnit) / Pow<2>(JulianYear)};
 };
 
 TEST_F(KeplerOrbitTest, EarthMoon) {
@@ -194,7 +195,53 @@ TEST_F(KeplerOrbitTest, EarthMoon) {
               AlmostEquals(*MoonElements().true_anomaly, 1));
 }
 
-TEST_F(KeplerOrbitTest, CombinatorialExplosion) {
+TEST_F(KeplerOrbitTest, ConicFromEccentricityAndSemimajorAxis) {
+  KeplerianElements<ICRFJ2000Equator> elements;
+  elements.eccentricity = SimpleEllipse().eccentricity;
+  elements.semimajor_axis = SimpleEllipse().semimajor_axis;
+  // Leaving the orientation parameters and anomalies to their default values.
+  // This test does not exercise them.
+  elements.argument_of_periapsis.emplace();
+  elements.mean_anomaly.emplace();
+  elements = KeplerOrbit<ICRFJ2000Equator>(body_,
+                                           MasslessBody{},
+                                           elements,
+                                           J2000).elements_at_epoch();
+  // The inputs must not change.
+  EXPECT_THAT(*elements.eccentricity, Eq(*SimpleEllipse().eccentricity));
+  EXPECT_THAT(*elements.semimajor_axis, Eq(*SimpleEllipse().semimajor_axis));
+  // Test the conic parameters.
+  EXPECT_THAT(*elements.eccentricity,
+              AlmostEquals(*SimpleEllipse().eccentricity, 0));
+  EXPECT_THAT(*elements.asymptotic_true_anomaly,
+              AlmostEquals(*SimpleEllipse().asymptotic_true_anomaly, 0));
+  EXPECT_THAT(*elements.turning_angle,
+              AlmostEquals(*SimpleEllipse().turning_angle, 0));
+  EXPECT_THAT(*elements.semimajor_axis,
+              AlmostEquals(*SimpleEllipse().semimajor_axis, 0));
+  EXPECT_THAT(*elements.specific_energy,
+              AlmostEquals(*SimpleEllipse().specific_energy, 0));
+  EXPECT_THAT(*elements.characteristic_energy,
+              AlmostEquals(*SimpleEllipse().characteristic_energy, 0));
+  EXPECT_THAT(*elements.mean_motion,
+              AlmostEquals(*SimpleEllipse().mean_motion, 0));
+  EXPECT_THAT(*elements.period, AlmostEquals(*SimpleEllipse().period, 0));
+  EXPECT_THAT(*elements.hyperbolic_mean_motion,
+              AlmostEquals(*SimpleEllipse().hyperbolic_mean_motion, 0));
+  EXPECT_THAT(*elements.hyperbolic_excess_velocity,
+              AlmostEquals(*SimpleEllipse().hyperbolic_excess_velocity, 0));
+  EXPECT_THAT(*elements.semiminor_axis,
+              AlmostEquals(*SimpleEllipse().semiminor_axis, 0));
+  EXPECT_THAT(*elements.impact_parameter,
+              AlmostEquals(*SimpleEllipse().impact_parameter, 0));
+  EXPECT_THAT(*elements.semilatus_rectum,
+              AlmostEquals(*SimpleEllipse().semilatus_rectum, 0));
+  EXPECT_THAT(*elements.specific_angular_momentum,
+              AlmostEquals(*SimpleEllipse().specific_angular_momentum, 0));
+  EXPECT_THAT(*elements.periapsis_distance,
+              AlmostEquals(*SimpleEllipse().periapsis_distance, 0));
+  EXPECT_THAT(*elements.apoapsis_distance,
+              AlmostEquals(*SimpleEllipse().apoapsis_distance, 0));
 }
 
 }  // namespace internal_kepler_orbit
