ed05b003d80c4b947f7da159d8eac5d376059411
Robin Leroy <egg.robin.leroy@gmail.com>
2017-05-13 22:19:52 +0200

    fingerprints

---
+++ astronomy/solar_system_fingerprints.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+
+namespace principia {
+namespace astronomy {
+
+constexpr std::uint64_t KSPStockSystemFingerprint      = 0xBF078CAB88084C72;
+constexpr std::uint64_t KSPStabilizedSystemFingerprint = 0x74277F4A585899E7;
+
+}  // namespace astronomy
+}  // namespace principia
