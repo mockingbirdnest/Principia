6fa48cc55a6aeb4330c9c7765379c42f0c456e2b
Robin Leroy <egg.robin.leroy@gmail.com>
2017-05-14 15:10:16 +0200

    turns out the fingerprints haven't changed

--- astronomy/solar_system_fingerprints.hpp
+++ astronomy/solar_system_fingerprints.hpp
@@ -5,8 +5,8 @@
 namespace principia {
 namespace astronomy {
 
-constexpr std::uint64_t KSPStockSystemFingerprint      = 0xBF078CAB88084C72;
-constexpr std::uint64_t KSPStabilizedSystemFingerprint = 0x74277F4A585899E7;
+constexpr std::uint64_t KSPStockSystemFingerprint      = 0x54B6323B3376D6F3;
+constexpr std::uint64_t KSPStabilizedSystemFingerprint = 0xB57B58F9CF757C62;
 
 }  // namespace astronomy
 }  // namespace principia
