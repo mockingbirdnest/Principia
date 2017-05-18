9e9e7844c12b19a5ecb1eb580a08297fd027b962
Robin Leroy <egg.robin.leroy@gmail.com>
2017-05-18 18:33:00 +0200

    classes

---
+++ ksp_plugin_adapter/layout_utilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace principia {
+namespace ksp_plugin_adapter {
+
+internal class HorizontalLayout : IDisposable {
+  public HorizontalLayout() {
+    UnityEngine.GUILayout.BeginHorizontal();
+  }
+
+  public void Dispose() {
+    UnityEngine.GUILayout.EndHorizontal();
+  }
+}
+
+internal class VerticalLayout : IDisposable {
+  public VerticalLayout() {
+    UnityEngine.GUILayout.BeginVertical();
+  }
+
+  public void Dispose() {
+    UnityEngine.GUILayout.EndVertical();
+  }
+}
+
+}  // namespace ksp_plugin_adapter
+}  // namespace principia
