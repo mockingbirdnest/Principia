99fe729f96be6dbf94fc23c7f2fd36f179384a03
pleroy <phl+personal@google.com>
2017-06-07 23:41:36 +0200

    Method 0.

--- ksp_plugin/planetarium.cpp
+++ ksp_plugin/planetarium.cpp
@@ -2,16 +2,12 @@
 #include "ksp_plugin/planetarium.hpp"
 
 #include "geometry/point.hpp"
-#include "physics/degrees_of_freedom.hpp"
-#include "physics/rigid_motion.hpp"
 
 namespace principia {
 namespace ksp_plugin {
 namespace internal_planetarium {
 
 using geometry::Position;
-using physics::DegreesOfFreedom;
-using physics::RigidMotion;
 using quantities::Time;
 
 Planetarium::Planetarium(
@@ -22,13 +18,29 @@ Planetarium::Planetarium(
       perspective_(perspective),
       plotting_frame_(plotting_frame) {}
 
+std::vector<RP2Point<Length, Camera>> Planetarium::PlotMethod0(
+    DiscreteTrajectory<Barycentric> const& trajectory,
+    Instant const& now) const {
+  RigidMotion<Navigation, Barycentric> const inverse_rigid_motion_at_now =
+      plotting_frame_->ToThisFrameAtTime(now).Inverse();
+  std::vector<RP2Point<Length, Camera>> rp2_points;
+
+  for (auto it = trajectory.Begin(); it != trajectory.End(); ++it) {
+    AppendRP2PointIfNeeded(inverse_rigid_motion_at_now,
+                           it.time(),
+                           it.degrees_of_freedom(),
+                           rp2_points);
+  }
+
+  return rp2_points;
+}
+
 std::vector<RP2Point<Length, Camera>> Planetarium::PlotMethod1(
     Trajectory<Barycentric> const& trajectory,
     Instant const& now,
     Length const& tolerance) const {
   RigidMotion<Navigation, Barycentric> const inverse_rigid_motion_at_now =
       plotting_frame_->ToThisFrameAtTime(now).Inverse();
-
   std::vector<RP2Point<Length, Camera>> rp2_points;
 
   Instant t = trajectory.t_min();
@@ -36,29 +48,10 @@ std::vector<RP2Point<Length, Camera>> Planetarium::PlotMethod1(
   while (t <= trajectory.t_max()) {
     DegreesOfFreedom<Barycentric> const barycentric_degrees_of_freedom =
         trajectory.EvaluateDegreesOfFreedom(t);
-
-    RigidMotion<Barycentric, Navigation> const rigid_motion_at_t =
-        plotting_frame_->ToThisFrameAtTime(t);
-    RigidMotion<Barycentric, Barycentric> const rigid_motion_back_and_forth =
-        inverse_rigid_motion_at_now * rigid_motion_at_t;
-    DegreesOfFreedom<Barycentric> const plottable_degrees_of_freedom =
-        rigid_motion_back_and_forth(barycentric_degrees_of_freedom);
-
-    // TODO(phl): This is missing a precise determination of the time when the
-    // trajectory become hidden.
-    bool hidden = false;
-    for (auto const& sphere : spheres_) {
-      if (perspective_.IsHiddenBySphere(
-              plottable_degrees_of_freedom.position(), sphere)) {
-        hidden = true;
-        break;
-      }
-    }
-    if (!hidden) {
-      RP2Point<Length, Camera> const rp2_point =
-          perspective_(plottable_degrees_of_freedom.position());
-      rp2_points.push_back(rp2_point);
-    }
+    AppendRP2PointIfNeeded(inverse_rigid_motion_at_now,
+                           t,
+                           barycentric_degrees_of_freedom,
+                           rp2_points);
 
     // Don't pay any attention to the perspective, just adjust |Δt| to stay
     // within the |tolerance|.
@@ -67,6 +60,35 @@ std::vector<RP2Point<Length, Camera>> Planetarium::PlotMethod1(
   }
 
   return rp2_points;
+}
+
+void Planetarium::AppendRP2PointIfNeeded(
+    RigidMotion<Navigation, Barycentric> const& inverse_rigid_motion_at_now,
+    Instant const& t,
+    DegreesOfFreedom<Barycentric> const& barycentric_degrees_of_freedom,
+    std::vector<RP2Point<Length, Camera>>& rp2_points) const {
+  RigidMotion<Barycentric, Navigation> const rigid_motion_at_t =
+      plotting_frame_->ToThisFrameAtTime(t);
+  RigidMotion<Barycentric, Barycentric> const rigid_motion_back_and_forth =
+      inverse_rigid_motion_at_now * rigid_motion_at_t;
+  DegreesOfFreedom<Barycentric> const plottable_degrees_of_freedom =
+      rigid_motion_back_and_forth(barycentric_degrees_of_freedom);
+
+  // TODO(phl): This is missing a precise determination of the time when the
+  // trajectory become hidden.
+  bool hidden = false;
+  for (auto const& sphere : spheres_) {
+    if (perspective_.IsHiddenBySphere(plottable_degrees_of_freedom.position(),
+                                      sphere)) {
+      hidden = true;
+      break;
+    }
+  }
+  if (!hidden) {
+    RP2Point<Length, Camera> const rp2_point =
+        perspective_(plottable_degrees_of_freedom.position());
+    rp2_points.push_back(rp2_point);
+  }
 }
 
 }  // namespace internal_planetarium
--- ksp_plugin/planetarium.hpp
+++ ksp_plugin/planetarium.hpp
@@ -8,6 +8,9 @@
 #include "geometry/rp2_point.hpp"
 #include "geometry/sphere.hpp"
 #include "ksp_plugin/frames.hpp"
+#include "physics/degrees_of_freedom.hpp"
+#include "physics/discrete_trajectory.hpp"
+#include "physics/rigid_motion.hpp"
 #include "physics/trajectory.hpp"
 #include "quantities/quantities.hpp"
 
@@ -21,6 +24,9 @@ using geometry::OrthogonalMap;
 using geometry::Perspective;
 using geometry::RP2Point;
 using geometry::Sphere;
+using physics::DegreesOfFreedom;
+using physics::DiscreteTrajectory;
+using physics::RigidMotion;
 using physics::Trajectory;
 using quantities::Length;
 
@@ -31,6 +37,11 @@ class Planetarium final {
                   perspective,
               not_null<NavigationFrame*> plotting_frame);
 
+  // A no-op method that just returns all the points in the |trajectory|.
+  std::vector<RP2Point<Length, Camera>> PlotMethod0(
+      DiscreteTrajectory<Barycentric> const& trajectory,
+      Instant const& now) const;
+
   // A naïve method that doesn't pay any attention to the perspective but tries
   // to ensure that the points before the perspective are separated by less than
   // |tolerance|.
@@ -39,10 +50,16 @@ class Planetarium final {
       Instant const& now,
       Length const& tolerance) const;
 
-private:
- std::vector<Sphere<Length, Barycentric>> const spheres_;
- Perspective<Barycentric, Camera, Length, OrthogonalMap> const perspective_;
- not_null<NavigationFrame*>  const plotting_frame_;
+ private:
+  void AppendRP2PointIfNeeded(
+      RigidMotion<Navigation, Barycentric> const& inverse_rigid_motion_at_now,
+      Instant const& t,
+      DegreesOfFreedom<Barycentric> const& barycentric_degrees_of_freedom,
+      std::vector<RP2Point<Length, Camera>>& rp2_points) const;
+
+  std::vector<Sphere<Length, Barycentric>> const spheres_;
+  Perspective<Barycentric, Camera, Length, OrthogonalMap> const perspective_;
+  not_null<NavigationFrame*>  const plotting_frame_;
 };
 
 }  // namespace internal_planetarium
