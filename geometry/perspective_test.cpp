a52d7ddf635d5ff26874545b7d65f79599c09f1e
pleroy <phl+personal@google.com>
2017-06-11 11:15:44 +0200

    Returns a nullopt when a perspective is applied to a point behind the camera.

--- geometry/perspective_test.cpp
+++ geometry/perspective_test.cpp
@@ -62,11 +62,11 @@ TEST_F(PerspectiveTest, Basic) {
   Point<Displacement<World>> const p0 = camera_origin;
   Point<Displacement<World>> const p1 = camera_origin + 1 * camera_z_axis;
   Point<Displacement<World>> const p2 = camera_origin + 10 * camera_z_axis;
-  EXPECT_TRUE(perspective(p0).is_at_infinity());
-  EXPECT_THAT(perspective(p1),
+  EXPECT_TRUE(perspective(p0)->is_at_infinity());
+  EXPECT_THAT(*perspective(p1),
               Componentwise(VanishesBefore(1 * Metre, 10),
                             VanishesBefore(1 * Metre, 0)));
-  EXPECT_THAT(perspective(p2),
+  EXPECT_THAT(*perspective(p2),
               Componentwise(VanishesBefore(1 * Metre, 8),
                             VanishesBefore(1 * Metre, 4)));
 
@@ -75,16 +75,16 @@ TEST_F(PerspectiveTest, Basic) {
       Displacement<Camera>({1 * Metre, 0 * Metre, 0 * Metre}));
   Point<Displacement<World>> const p3 = p1 + 5 * camera_x_axis;
   Point<Displacement<World>> const p4 = p1 + 7 * camera_x_axis;
-  EXPECT_THAT(perspective(p3).y(), VanishesBefore(1 * Metre, 20));
-  EXPECT_THAT(perspective(p4).y(), VanishesBefore(1 * Metre, 10));
+  EXPECT_THAT(perspective(p3)->y(), VanishesBefore(1 * Metre, 20));
+  EXPECT_THAT(perspective(p4)->y(), VanishesBefore(1 * Metre, 10));
 
   // Check that points on the camera y axis get projected on the y axis of ℝP².
   Displacement<World> const camera_y_axis = world_to_camera_rotation.Inverse()(
       Displacement<Camera>({0 * Metre, 1 * Metre, 0 * Metre}));
   Point<Displacement<World>> const p5 = p1 - 11 * camera_y_axis;
   Point<Displacement<World>> const p6 = p1 + 13 * camera_y_axis;
-  EXPECT_THAT(perspective(p5).x(), VanishesBefore(1 * Metre, 120));
-  EXPECT_THAT(perspective(p6).x(), VanishesBefore(1 * Metre, 0));
+  EXPECT_THAT(perspective(p5)->x(), VanishesBefore(1 * Metre, 120));
+  EXPECT_THAT(perspective(p6)->x(), VanishesBefore(1 * Metre, 0));
 
   // Check that aligned points are aligned in ℝP².
   Point<Displacement<World>> const p7 =
@@ -96,17 +96,17 @@ TEST_F(PerspectiveTest, Basic) {
   Point<Displacement<World>> const p9 =
       camera_origin +
       Displacement<World>({19 * Metre, -19 * Metre, 19 * Metre});
-  RP2Point<Length, Camera> const q7 = perspective(p7);
-  RP2Point<Length, Camera> const q8 = perspective(p8);
-  RP2Point<Length, Camera> const q9 = perspective(p9);
-  EXPECT_THAT((q8.x() - q7.x()) * (q9.y() - q7.y()) -
-                  (q9.x() - q7.x()) * (q8.y() - q7.y()),
+  auto const q7 = perspective(p7);
+  auto const q8 = perspective(p8);
+  auto const q9 = perspective(p9);
+  EXPECT_THAT((q8->x() - q7->x()) * (q9->y() - q7->y()) -
+                  (q9->x() - q7->x()) * (q8->y() - q7->y()),
               VanishesBefore(1 * Metre * Metre, 6));
 
   // Check that the focal works as expected.
   Point<Displacement<World>> const p10 =
       camera_origin + 1 * camera_x_axis + 2 * camera_y_axis + 3 * camera_z_axis;
-  EXPECT_THAT(perspective(p10),
+  EXPECT_THAT(*perspective(p10),
               Componentwise(AlmostEquals(1.0 / 0.3 * Metre, 3),
                             AlmostEquals(2.0 / 0.3 * Metre, 2)));
 }
@@ -141,6 +141,24 @@ TEST_F(PerspectiveTest, IsHiddenBySphere) {
   EXPECT_FALSE(perspective.IsHiddenBySphere(p2, sphere));
   EXPECT_TRUE(perspective.IsHiddenBySphere(p3, sphere));
   EXPECT_FALSE(perspective.IsHiddenBySphere(p4, sphere));
+}
+
+TEST_F(PerspectiveTest, BehindCamera) {
+  Perspective<World, Camera, Length, OrthogonalMap> perspective(
+      AffineMap<World, Camera, Length, OrthogonalMap>::Identity(),
+      /*focal=*/1 * Metre);
+
+  // In front of the camera.
+  Point<Displacement<World>> const p1 =
+      World::origin +
+      Displacement<World>({1 * Metre, -2 * Metre, 3 * Metre});
+  // Behind the camera.
+  Point<Displacement<World>> const p2 =
+      World::origin +
+      Displacement<World>({-1 * Metre, 2 * Metre, -3 * Metre});
+
+  EXPECT_TRUE(perspective(p1).has_value());
+  EXPECT_FALSE(perspective(p2).has_value());
 }
 
 }  // namespace internal_perspective
