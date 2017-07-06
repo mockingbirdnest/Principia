0f0a0c3b8e778e1ecd28ba41187c30c7a681fbcf
pleroy <phl+personal@google.com>
2017-07-06 22:53:08 +0200

    H behind AB.

--- geometry/perspective_body.hpp
+++ geometry/perspective_body.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "geometry/perspective.hpp"
+#include "numerics/root_finders.hpp"
 #include "quantities/named_quantities.hpp"
 
 namespace principia {
@@ -11,6 +12,7 @@ namespace geometry {
 namespace internal_perspective {
 
 using geometry::InnerProduct;
+using numerics::SolveQuadraticEquation;
 using quantities::Product;
 
 template<typename FromFrame, typename ToFrame, typename Scalar,
@@ -143,11 +145,57 @@ Perspective<FromFrame, ToFrame, Scalar, LinearMap>::VisibleSegments(
   double const β = (KA² * KBKC - KAKB * KAKC) / determinant;
   Vector<Scalar, FromFrame> const KH = ɑ * KA + β * KB;
 
-  // The basic check: if H is outside the sphere, there is no intersection.
+  // The basic check: if H is outside the sphere, there is no intersection and
+  // thus no hiding.
   Vector<Scalar, FromFrame> const CH = KH - KC;
-  if (InnerProduct(CH, CH) >= sphere.radius²()) {
+  auto const CH² = InnerProduct(CH, CH);
+  if (CH² >= sphere.radius²()) {
     return {segment};
   }
+
+  // If ɑ + β < 1, H is between K and the line AB.
+  // If ɑ + β > 1, H is behind the line AB when seen from K.
+  // If ɑ + β == 1, H is on the line AB.
+  if (ɑ + β >= 1) {
+    // I is the intersection of the sphere with the line AB.  It is such that:
+    //   KI = KA + λ * AB
+    // where λ is computed by solving a quadratic equation.
+    Vector<Scalar, FromFrame> const AB = B - A;
+    Vector<Scalar, FromFrame> const AC = C - A;
+    auto const AB² = InnerProduct(AB, AB);
+    auto const AC² = InnerProduct(AC, AC);
+    auto const ABAC = InnerProduct(AB, AC);
+
+    std::set<double> λs = SolveQuadraticEquation(/*origin=*/0.0,
+                                                 /*a0=*/AC² - sphere.radius²(),
+                                                 /*a1=*/-2.0 * ABAC,
+                                                 /*a2=*/AB²);
+    if (λs.size() != 2) {
+      // The sphere doesn't intersect the line AB or is tangent to it.  There is
+      // no hiding.
+      return {segment};
+    }
+    double const λ1 = *λs.begin();
+    double const λ2 = *λs.rbegin();
+    if (λ1 <= 0.0 && λ2 >= 1.0) {
+      // The sphere swallows the segment.
+      return {};
+    }
+    if (λ1 > 0.0 && λ2 < 1.0) {
+      // The sphere hides the middle of the segment.
+      return {{A, A + λ1 * AB }, {A + λ2 * AB, B }};
+    }
+    if (λ1 <= 0.0) {
+      // The sphere hides the beginning of the segment.
+      return {{A + λ2 * AB, B }};
+    }
+    {
+      CHECK_GE(λ2, 1.0);
+      // The sphere hides the end of the segment.
+      return {{A, A + λ1 * AB }};
+    }
+  }
+
   return {};
 }
 
