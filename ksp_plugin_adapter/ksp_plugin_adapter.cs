9bf1b72f7e8709811bbfb73dceba42897c862c60
Robin Leroy <egg.robin.leroy@gmail.com>
2017-07-15 18:32:47 +0200

    aha there's another call with parts synchronized

--- ksp_plugin_adapter/ksp_plugin_adapter.cs
+++ ksp_plugin_adapter/ksp_plugin_adapter.cs
@@ -1293,7 +1293,8 @@ public partial class PrincipiaPluginAdapter
           plugin_.HasVessel(FlightGlobals.ActiveVessel.id.ToString())) {
         QP main_body_dof = plugin_.CelestialWorldDegreesOfFreedom(
             FlightGlobals.ActiveVessel.mainBody.flightGlobalsIndex,
-            FlightGlobals.ActiveVessel.rootPart.flightID);
+            FlightGlobals.ActiveVessel.rootPart.flightID,
+            Planetarium.GetUniversalTime());
         krakensbane.FrameVel = -(Vector3d)main_body_dof.p;
         offset = (Vector3d)main_body_dof.q -
                  FlightGlobals.ActiveVessel.mainBody.position;
