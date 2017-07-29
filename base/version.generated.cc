f2bc5b4b70acbc3e1f1afde803abf6c8e9558944
pleroy <phl+personal@google.com>
2017-07-29 17:13:01 +0200

    Bring in the generated files.

---
+++ base/version.generated.cc
@@ -0,0 +1,12 @@
+﻿
+#include "base/version.hpp"
+
+namespace principia {
+namespace base {
+
+char const BuildDate[] = "2017-07-29T11:48:45Z";
+char const Version[] =
+    u8"2017072310-Cayley-51-gc35daf6df293373e23fc44eb62f9fdba179d4eaf";
+
+}  // namespace base
+}  // namespace principia
