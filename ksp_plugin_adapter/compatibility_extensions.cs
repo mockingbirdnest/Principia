4013d9fbe0e40143ae141af8fc2b4630c6951527
Robin Leroy <egg.robin.leroy@gmail.com>
2017-08-06 14:05:52 +0200

    extension

---
+++ ksp_plugin_adapter/compatibility_extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace principia {
+namespace ksp_plugin_adapter {
+
+internal static class CompatibilityExtensions {
+  public static string DisplayName(this CelestialBody body) {
+#if KSP_VERSION_1_3
+    return body.displayName;
+#elif KSP_VERSION_1_2_2
+    return body.theName;
+#endif
+  }
+}
+
+}  // namespace ksp_plugin_adapter
+}  // namespace principia
