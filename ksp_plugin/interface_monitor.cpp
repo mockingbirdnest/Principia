04752479a519afb17a3a4cf67cb6674bd96ec00d
Robin Leroy <egg.robin.leroy@gmail.com>
2017-09-02 13:35:05 +0200

    multiple monitors

--- ksp_plugin/interface_monitor.cpp
+++ ksp_plugin/interface_monitor.cpp
@@ -2,7 +2,9 @@
 #include "ksp_plugin/interface.hpp"
 
 #include <algorithm>
+#include <array>
 #include <chrono>
+#include <type_traits>
 
 #include "quantities/quantities.hpp"
 #include "quantities/si.hpp"
@@ -16,42 +18,65 @@ using quantities::si::Nano;
 using quantities::si::Second;
 
 namespace {
-std::chrono::steady_clock::time_point monitor_start_time;
-bool is_monitor_running = false;
+constexpr int monitor_count = 64;
+constexpr int window_size = 500;
+
+struct Monitor {
+  std::string* name = nullptr;
+  std::chrono::steady_clock::time_point start_time{};
+  bool is_running = false;
+
+  int window_index = 0;
+  Time total_Δt;
+  Time min_Δt = Infinity<Time>();
+  Time max_Δt = -Infinity<Time>();
+};
+
+static_assert(
+    std::is_trivially_destructible<std::array<Monitor, monitor_count>>::value,
+    "An array of |Monitor|s should be trivially destructible");
+std::array<Monitor, monitor_count> monitors{};
 }  // namespace
 
-// No journalling to avoid overhead from that; these functions have no side
-// effects aside from logging changing and the internal state of the monitor.
+// No journalling to avoid measuring overhead from that; these functions have no
+// side effects aside from logging and changing the internal state of the
+// monitors.
 
-void principia__MonitorStart() {
-  if (!is_monitor_running) {
-    is_monitor_running = true;
-    monitor_start_time = std::chrono::steady_clock::now();
+void principia__MonitorSetName(int const i, char const* const name) {
+  Monitor& monitor = monitors[i];
+  if (monitor.name == nullptr) {
+    monitor.name = new std::string(name);
   }
 }
 
-void principia__MonitorStop() {
-  constexpr int window_size = 500;
-  static int window_index = 0;
-  static Time total_Δt;
-  static Time min_Δt = Infinity<Time>();
-  static Time max_Δt = -Infinity<Time>();
-  if (is_monitor_running) {
-    is_monitor_running = false;
+void principia__MonitorStart(int const i) {
+  Monitor& monitor = monitors[i];
+  if (!monitor.is_running) {
+    monitor.is_running = true;
+    monitor.start_time = std::chrono::steady_clock::now();
+  }
+}
+
+void principia__MonitorStop(int const i) {
+  Monitor& monitor = monitors[i];
+  if (monitor.is_running) {
+    monitor.is_running = false;
     auto const Δt =
         std::chrono::nanoseconds(
-            std::chrono::steady_clock::now() - monitor_start_time).count() *
+            std::chrono::steady_clock::now() - monitor.start_time).count() *
         Nano(Second);
-    min_Δt = std::min(min_Δt, Δt);
-    max_Δt = std::max(max_Δt, Δt);
-    total_Δt += Δt;
-    ++window_index %= window_size;
-    if (window_index == 0) {
-      LOG(INFO) << "min = " << min_Δt << ", max = " << max_Δt
-                << u8", μ = " << total_Δt / window_size;
-      min_Δt = Infinity<Time>();
-      max_Δt = -Infinity<Time>();
-      total_Δt = Time();
+    monitor.min_Δt = std::min(monitor.min_Δt, Δt);
+    monitor.max_Δt = std::max(monitor.max_Δt, Δt);
+    monitor.total_Δt += Δt;
+    ++monitor.window_index %= window_size;
+    if (monitor.window_index == 0) {
+      LOG(INFO) << "[Monitor " << i
+                <<(monitor.name == nullptr ? "" : ": " + *monitor.name) << "] "
+                << "min = " << monitor.min_Δt << ", max = " << monitor.max_Δt
+                << u8", μ = " << monitor.total_Δt / window_size;
+      monitor.min_Δt = Infinity<Time>();
+      monitor.max_Δt = -Infinity<Time>();
+      monitor.total_Δt = Time();
     }
   }
 }
