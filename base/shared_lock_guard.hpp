3ad99443813fab42f6e10ba055e84327bbf36709
pleroy <phl+personal@google.com>
2017-09-18 09:23:12 +0200

    No shared_mutex on MacOS.

--- base/shared_lock_guard.hpp
+++ base/shared_lock_guard.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
+#include "base/macros.hpp"
 #include "base/not_null.hpp"
+
+#if HAS_SHARED_MUTEX
+#include <shared_mutex>
+#else
+#include <mutex>
+#endif
 
 namespace principia {
 namespace base {
+
+#if HAS_SHARED_MUTEX
+
+using shared_mutex = std::shared_mutex;
 
 // A helper class that the language designer didn't think useful of providing.
 template<typename Mutex>
@@ -15,6 +26,13 @@ class shared_lock_guard final {
  private:
   not_null<Mutex*> const mutex_;
 };
+
+#else
+
+using shared_mutex = std::mutex;
+using shared_lock_guard = std::lock_guard;
+
+#endif
 
 }  // namespace base
 }  // namespace principia
