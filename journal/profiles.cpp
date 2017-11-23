f29e062b79e03754676da9b10deac7856e7750da
pleroy <phl+personal@google.com>
2017-11-23 22:53:49 +0100

    Conditional check for equality of results.

--- journal/profiles.cpp
+++ journal/profiles.cpp
@@ -56,7 +56,12 @@ std::uint64_t SerializePointer(T* t) {
 
 }  // namespace
 
+#define PRINCIPIA_CHECK_EQ(a, b)
+//CHECK((a) == (b))
+
 #include "journal/profiles.generated.cc"
+
+#undef PRINCIPIA_CHECK_EQ
 
 }  // namespace journal
 }  // namespace principia
