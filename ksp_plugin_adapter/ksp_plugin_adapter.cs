4b4ed94c57197134becd5ee67ed28c0c32587e67
Robin Leroy <egg.robin.leroy@gmail.com>
2017-12-10 13:56:14 +0100

    Fix #1643.

--- ksp_plugin_adapter/ksp_plugin_adapter.cs
+++ ksp_plugin_adapter/ksp_plugin_adapter.cs
@@ -2113,9 +2113,6 @@ public partial class PrincipiaPluginAdapter
       ToggleableSection(name   : "Logging Settings",
                         show   : ref show_logging_settings_,
                         render : LoggingSettings);
-      ToggleableSection(name   : "Reset Principia",
-                        show   : ref show_reset_button_,
-                        render : ResetButton);
 #if CRASH_BUTTON
       ToggleableSection(name   : "CRASH",
                         show   : ref show_crash_options_,
@@ -2363,18 +2360,6 @@ public partial class PrincipiaPluginAdapter
       // work, we should only activate one before creating a plugin.
       journaling_ = false;
       Interface.ActivateRecorder(false);
-    }
-  }
-
-  private void ResetButton() {
-    if (PluginRunning()) {
-      if (UnityEngine.GUILayout.Button(text : "Force Stop")) {
-        Cleanup();
-      }
-    } else {
-      if (UnityEngine.GUILayout.Button(text : "Force Start")) {
-        ResetPlugin();
-      }
     }
   }
 
