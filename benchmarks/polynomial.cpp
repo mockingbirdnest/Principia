0099cd604ec376b891880440f9519f876ab3ccef
pleroy <phl+personal@google.com>
2018-01-06 19:21:16 +0100

    Benchmark multiple degrees.

--- benchmarks/polynomial.cpp
+++ benchmarks/polynomial.cpp
@@ -4,8 +4,16 @@
 
 #include "benchmark/benchmark.h"
 #include "numerics/polynomial.hpp"
+#include "quantities/quantities.hpp"
+#include "quantities/si.hpp"
 
 namespace principia {
+
+using quantities::Length;
+using quantities::Quantity;
+using quantities::SIUnit;
+using quantities::Time;
+
 namespace numerics {
 
 namespace {
@@ -13,17 +21,27 @@ int const evaluations_per_iteration = 1000;
 }  // namespace
 
 template<typename T>
-T GenerateElement(std::mt19937_64& random);
+struct ValueGenerator;
 
 template<>
-double GenerateElement(std::mt19937_64& random) {
-  return static_cast<double>(random());
-}
+struct ValueGenerator<double> {
+  static double Get(std::mt19937_64& random) {
+    return static_cast<double>(random());
+  }
+};
+
+template<typename D>
+struct ValueGenerator<Quantity<D>> {
+  static Quantity<D> Get(std::mt19937_64& random) {
+    return static_cast<double>(random()) * SIUnit<Quantity<D>>();
+  }
+};
 
 template<typename Tuple, int k, int size = std::tuple_size_v<Tuple>>
 struct RandomTupleGenerator {
   static void Fill(Tuple& t, std::mt19937_64& random) {
-    std::get<k>(t) = GenerateElement<std::tuple_element_t<k, Tuple>>(random);
+    std::get<k>(t) =
+        ValueGenerator<std::tuple_element_t<k, Tuple>>::Get(random);
     RandomTupleGenerator<Tuple, k + 1, size>::Fill(t, random);
   }
 };
@@ -33,58 +51,70 @@ struct RandomTupleGenerator<Tuple, size, size> {
   static void Fill(Tuple& t, std::mt19937_64& random) {}
 };
 
-template<typename Value, typename Argument, typename degree>
+template<typename Value, typename Argument, int degree>
 void EvaluatePolynomialInMonomialBasis(benchmark::State& state) {
   using P = PolynomialInMonomialBasis<Value, Argument, degree>;
   std::mt19937_64 random(42);
   P::Coefficients coefficients;
   RandomTupleGenerator<P::Coefficients, 0>::Fill(coefficients, random);
-  double const min = static_cast<double>(random());
-  double const max = static_cast<double>(random());
-  P4 const p4(coefficients, min, max);
+  auto const min = ValueGenerator<Argument>::Get(random);
+  auto const max = ValueGenerator<Argument>::Get(random);
+  P const p(coefficients, min, max);
 
-  double argument = min;
-  double const Δargument = (max - min) * 1e-9;
-  double result = 0.0;
+  auto argument = min;
+  auto const Δargument = (max - min) * 1e-9;
+  auto result = Value{};
 
   while (state.KeepRunning()) {
     for (int i = 0; i < evaluations_per_iteration; ++i) {
-      result += p4.Evaluate(argument);
+      result += p.Evaluate(argument);
       argument += Δargument;
     }
   }
 
   // This weird call to |SetLabel| has no effect except that it uses |result|
   // and therefore prevents the loop from being optimized away.
-  state.SetLabel(std::to_string(result).substr(0, 0));
+  std::stringstream ss;
+  ss << result;
+  state.SetLabel(ss.str().substr(0, 0));
 }
 
-void BM_EvaluatePolynomial(benchmark::State& state) {
-  using P4 = PolynomialInMonomialBasis<double, double, 4>;
-  std::mt19937_64 random(42);
-  P4::Coefficients coefficients;
-  RandomTupleGenerator<P4::Coefficients, 0>::Fill(coefficients, random);
-  double const min = static_cast<double>(random());
-  double const max = static_cast<double>(random());
-  P4 const p4(coefficients, min, max);
-
-  double argument = min;
-  double const Δargument = (max - min) * 1e-9;
-  double result = 0.0;
-
-  while (state.KeepRunning()) {
-    for (int i = 0; i < evaluations_per_iteration; ++i) {
-      result += p4.Evaluate(argument);
-      argument += Δargument;
-    }
+void BM_EvaluatePolynomialDouble(benchmark::State& state) {
+  int const degree = state.range_x();
+  switch (degree) {
+    case 4:
+      EvaluatePolynomialInMonomialBasis<double, Time, 4>(state);
+      break;
+    case 8:
+      EvaluatePolynomialInMonomialBasis<double, Time, 8>(state);
+      break;
+    case 16:
+      EvaluatePolynomialInMonomialBasis<double, Time, 16>(state);
+      break;
+    default:
+      LOG(FATAL) << "Degree " << degree << " in BM_EvaluatePolynomialDouble";
   }
-
-  // This weird call to |SetLabel| has no effect except that it uses |result|
-  // and therefore prevents the loop from being optimized away.
-  state.SetLabel(std::to_string(result).substr(0, 0));
 }
 
-BENCHMARK(BM_EvaluatePolynomial);
+void BM_EvaluatePolynomialQuantity(benchmark::State& state) {
+  int const degree = state.range_x();
+  switch (degree) {
+    case 4:
+      EvaluatePolynomialInMonomialBasis<Length, Time, 4>(state);
+      break;
+    case 8:
+      EvaluatePolynomialInMonomialBasis<Length, Time, 8>(state);
+      break;
+    case 16:
+      EvaluatePolynomialInMonomialBasis<Length, Time, 16>(state);
+      break;
+    default:
+      LOG(FATAL) << "Degree " << degree << " in BM_EvaluatePolynomialQuantity";
+  }
+}
+
+BENCHMARK(BM_EvaluatePolynomialDouble)->Arg(4)->Arg(8)->Arg(16);
+BENCHMARK(BM_EvaluatePolynomialQuantity)->Arg(4)->Arg(8)->Arg(16);
 
 }  // namespace numerics
 }  // namespace principia
