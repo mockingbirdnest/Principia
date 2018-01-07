52e94d3c1bd5433ff3361c958a874c0e7dc215c4
pleroy <phl+personal@google.com>
2018-01-07 13:57:09 +0100

    Vectors.

--- benchmarks/polynomial.cpp
+++ benchmarks/polynomial.cpp
@@ -2,7 +2,10 @@
 #include <random>
 #include <tuple>
 
+#include "astronomy/frames.hpp"
 #include "benchmark/benchmark.h"
+#include "geometry/grassmann.hpp"
+#include "geometry/named_quantities.hpp"
 #include "geometry/r3_element.hpp"
 #include "numerics/polynomial.hpp"
 #include "quantities/quantities.hpp"
@@ -10,6 +13,9 @@
 
 namespace principia {
 
+using astronomy::ICRFJ2000Ecliptic;
+using geometry::Displacement;
+using geometry::Multivector;
 using geometry::R3Element;
 using quantities::Length;
 using quantities::Quantity;
@@ -45,6 +51,13 @@ struct ValueGenerator<R3Element<S>> {
     return {ValueGenerator<S>::Get(random),
             ValueGenerator<S>::Get(random),
             ValueGenerator<S>::Get(random)};
+  }
+};
+
+template<typename S, typename F, int r>
+struct ValueGenerator<Multivector<S, F, r>> {
+  static Multivector<S, F, r> Get(std::mt19937_64& random) {
+    return Multivector<S, F, r>(ValueGenerator<R3Element<S>>::Get(random));
   }
 };
 
@@ -99,6 +112,9 @@ void BM_EvaluatePolynomialInMonomialBasisDouble(benchmark::State& state) {
     case 8:
       EvaluatePolynomialInMonomialBasis<double, Time, 8>(state);
       break;
+    case 12:
+      EvaluatePolynomialInMonomialBasis<double, Time, 12>(state);
+      break;
     case 16:
       EvaluatePolynomialInMonomialBasis<double, Time, 16>(state);
       break;
@@ -116,6 +132,9 @@ void BM_EvaluatePolynomialInMonomialBasisQuantity(benchmark::State& state) {
       break;
     case 8:
       EvaluatePolynomialInMonomialBasis<Length, Time, 8>(state);
+      break;
+    case 12:
+      EvaluatePolynomialInMonomialBasis<Length, Time, 12>(state);
       break;
     case 16:
       EvaluatePolynomialInMonomialBasis<Length, Time, 16>(state);
@@ -136,6 +155,9 @@ void BM_EvaluatePolynomialInMonomialBasisR3ElementDouble(
     case 8:
       EvaluatePolynomialInMonomialBasis<R3Element<double>, Time, 8>(state);
       break;
+    case 12:
+      EvaluatePolynomialInMonomialBasis<R3Element<double>, Time, 12>(state);
+      break;
     case 16:
       EvaluatePolynomialInMonomialBasis<R3Element<double>, Time, 16>(state);
       break;
@@ -145,12 +167,78 @@ void BM_EvaluatePolynomialInMonomialBasisR3ElementDouble(
   }
 }
 
+void BM_EvaluatePolynomialInMonomialBasisVectorDouble(benchmark::State& state) {
+  int const degree = state.range_x();
+  switch (degree) {
+    case 4:
+      EvaluatePolynomialInMonomialBasis<
+          Multivector<double, ICRFJ2000Ecliptic, 1>,
+          Time,
+          4>(state);
+      break;
+    case 8:
+      EvaluatePolynomialInMonomialBasis<
+          Multivector<double, ICRFJ2000Ecliptic, 1>,
+          Time,
+          8>(state);
+      break;
+    case 12:
+      EvaluatePolynomialInMonomialBasis<
+          Multivector<double, ICRFJ2000Ecliptic, 1>,
+          Time,
+          12>(state);
+      break;
+    case 16:
+      EvaluatePolynomialInMonomialBasis<
+          Multivector<double, ICRFJ2000Ecliptic, 1>,
+          Time,
+          16>(state);
+      break;
+    default:
+      LOG(FATAL) << "Degree " << degree
+                 << " in BM_EvaluatePolynomialInMonomialBasisVectorDouble";
+  }
+}
+
+void BM_EvaluatePolynomialInMonomialBasisDisplacement(benchmark::State& state) {
+  int const degree = state.range_x();
+  switch (degree) {
+    case 4:
+      EvaluatePolynomialInMonomialBasis<Displacement<ICRFJ2000Ecliptic>,
+                                        Time,
+                                        4>(state);
+      break;
+    case 8:
+      EvaluatePolynomialInMonomialBasis<Displacement<ICRFJ2000Ecliptic>,
+                                        Time,
+                                        8>(state);
+      break;
+    case 12:
+      EvaluatePolynomialInMonomialBasis<Displacement<ICRFJ2000Ecliptic>,
+                                        Time,
+                                        12>(state);
+      break;
+    case 16:
+      EvaluatePolynomialInMonomialBasis<Displacement<ICRFJ2000Ecliptic>,
+                                        Time,
+                                        16>(state);
+      break;
+    default:
+      LOG(FATAL) << "Degree " << degree
+                 << " in BM_EvaluatePolynomialInMonomialBasisDisplacement";
+  }
+}
+
 BENCHMARK(BM_EvaluatePolynomialInMonomialBasisDouble)
-    ->Arg(4)->Arg(8)->Arg(16);
+    ->Arg(4)->Arg(8)->Arg(12)->Arg(16);
 BENCHMARK(BM_EvaluatePolynomialInMonomialBasisQuantity)
-    ->Arg(4)->Arg(8)->Arg(16);
+    ->Arg(4)->Arg(8)->Arg(12)->Arg(16);
 BENCHMARK(BM_EvaluatePolynomialInMonomialBasisR3ElementDouble)
-    ->Arg(4)->Arg(8)->Arg(16);
+    ->Arg(4)->Arg(8)->Arg(12)->Arg(16);
+BENCHMARK(BM_EvaluatePolynomialInMonomialBasisVectorDouble)
+    ->Arg(4)->Arg(8)->Arg(12)->Arg(16);
+BENCHMARK(BM_EvaluatePolynomialInMonomialBasisDisplacement)
+    ->Arg(4)->Arg(8)->Arg(12)->Arg(16);
 
 }  // namespace numerics
 }  // namespace principia
