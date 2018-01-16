933da0d783a7302457fe2d85bca5562633af59dd
pleroy <phl+personal@google.com>
2018-01-16 22:58:02 +0100

    Let's copy the other one.

--- numerics/newhall.hpp
+++ numerics/newhall.hpp
@@ -4,6 +4,8 @@
 
 #include "geometry/named_quantities.hpp"
 #include "numerics/чебышёв_series.hpp"
+#include "numerics/polynomial.hpp"
+#include "quantities/quantities.hpp"
 #include "quantities/named_quantities.hpp"
 
 namespace principia {
@@ -11,6 +13,7 @@ namespace numerics {
 namespace internal_newhall {
 
 using geometry::Instant;
+using quantities::Time;
 using quantities::Variation;
 
 // Computes a Newhall approximation of the given |degree| in the Чебышёв basis.
@@ -24,10 +27,20 @@ template<typename Vector>
     Instant const& t_min,
     Instant const& t_max);
 
+//TODO(phl):comment.
+template<typename Vector, int degree,
+         template<typename, typename, int> class Evaluator>
+PolynomialInMonomialBasis<Vector, Time, degree, Evaluator>
+ApproximationInMonomialBasis(std::vector<Vector> const& q,
+                             std::vector<Variation<Vector>> const& v,
+                             Instant const& t_min,
+                             Instant const& t_max);
+
 }  // namespace internal_newhall
 
 namespace newhall {
 using internal_newhall::ApproximationInЧебышёвBasis;
+using internal_newhall::ApproximationInMonomialBasis;
 }  // namespace newhall
 
 }  // namespace numerics
