9283b0ecdf2c4d0855479912065c557643649a3f
pleroy <phl+personal@google.com>
2018-01-20 19:31:40 +0100

    A new matcher.

---
+++ testing_utilities/is_near_test.cpp
@@ -0,0 +1,33 @@
+﻿
+#include "testing_utilities/is_near.hpp"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "quantities/bipm.hpp"
+#include "quantities/numbers.hpp"
+#include "quantities/si.hpp"
+
+namespace principia {
+namespace testing_utilities {
+
+using quantities::Speed;
+using quantities::bipm::Knot;
+using quantities::si::Metre;
+using quantities::si::Second;
+
+class IsNearTest : public testing::Test {};
+
+TEST_F(IsNearTest, Dimensionless) {
+  double const y = e;
+  EXPECT_THAT(y, IsNear(e, 1.0));
+  EXPECT_THAT(y, Not(IsNear(3.0, 1.01)));
+}
+
+TEST_F(IsNearTest, Quantity) {
+  Speed v = 1 * Knot;
+  EXPECT_THAT(v, IsNear(0.514 * Metre / Second, 1.002));
+  EXPECT_THAT(v, Not(IsNear(3.0 * Metre / Second, 1.01)));
+}
+
+}  // namespace testing_utilities
+}  // namespace principia
