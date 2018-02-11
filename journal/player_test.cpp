e3d737e1654550ea93d7e6176750eb012e5054aa
pleroy <phl+personal@google.com>
2018-02-11 12:34:08 +0100

    Journal for Cohen.

--- journal/player_test.cpp
+++ journal/player_test.cpp
@@ -21,7 +21,7 @@ namespace journal {
 void BM_PlayForReal(benchmark::State& state) {
   while (state.KeepRunning()) {
     Player player(
-        R"(P:\Public Mockingbird\Principia\Journals\JOURNAL.20171216-145412)");
+        R"(P:\Public Mockingbird\Principia\Journals\JOURNAL.20180211-103321)");
     int count = 0;
     while (player.Play()) {
       ++count;
