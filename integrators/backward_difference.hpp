39ec9eb34ffa8f007ee5ecc78fa22c0ff6c8b117
pleroy <phl+personal@google.com>
2018-02-24 23:34:14 +0100

    Coefficients for CHO and fix for the order of the backward difference method.

--- integrators/backward_difference.hpp
+++ integrators/backward_difference.hpp
@@ -1,4 +1,5 @@
-﻿#pragma once
+﻿
+#pragma once
 
 #include "numerics/fixed_arrays.hpp"
 
@@ -9,12 +10,13 @@ namespace internal_backward_difference {
 using numerics::FixedVector;
 
 // Definition of a backward difference formula.  |order_| is the order of the
-// approximation.  The formula requires |order_ + 1| points.  numerators[0]
-// corresponds to f(x₀), numerators[1] to f(x₋₁), etc.
+// approximation, that is, the error on the derivative is h^(order + 1).  The
+// formula requires |order_ + 2| positions.  numerators[0] corresponds to f(x₀),
+// numerators[1] to f(x₋₁), etc.
 template<int order_>
 struct BackwardDifference final {
   static constexpr int order = order_;
-  FixedVector<double, order + 1> numerators;
+  FixedVector<double, order + 2> numerators;
   double denominator;
 };
 
---
+++ integrators/cohen_hubbard_oesterwinter.hpp
@@ -0,0 +1,34 @@
+﻿
+#pragma once
+
+#include "numerics/fixed_arrays.hpp"
+
+namespace principia {
+namespace integrators {
+namespace internal_cohen_hubbard_oesterwinter {
+
+using numerics::FixedVector;
+
+// Definition of a Cohen-Hubbard-Oesterwinter formula.  |order_| is the order of
+// the approximation, that is, the error on the derivative is h^(order + 1).
+// The formula requires |order_| values of the acceleration.  numerators[0]
+// corresponds to f"(x₀), numerators[1] to f"(x₋₁), etc.
+// TODO(phl): This struct shows up in many places.  Factor it out.
+template<int order_>
+struct CohenHubbardOesterwinter final {
+  static constexpr int order = order_;
+  FixedVector<double, order> numerators;
+  double denominator;
+};
+
+}  // namespace internal_cohen_hubbard_oesterwinter
+
+using internal_cohen_hubbard_oesterwinter::CohenHubbardOesterwinter;
+
+template<int order>
+CohenHubbardOesterwinter<order> const& CohenHubbardOesterwinterOrder();
+
+}  // namespace integrators
+}  // namespace principia
+
+#include "integrators/backward_difference_body.hpp"
