4d1d982a9d2c3c27971fe5f0be9b41bdc01b6da1
Robin Leroy <egg.robin.leroy@gmail.com>
2018-02-25 13:19:03 +0100

    moo

--- ksp_plugin_test/interface_external_test.cpp
+++ ksp_plugin_test/interface_external_test.cpp
@@ -6,24 +6,34 @@
 #include "ksp_plugin_test/mock_planetarium.hpp"
 #include "ksp_plugin_test/mock_plugin.hpp"
 #include "ksp_plugin_test/mock_renderer.hpp"
+#include "ksp_plugin_test/mock_vessel.hpp"
 
 namespace principia {
 namespace interface {
 
+using base::make_not_null_unique;
+using ksp_plugin::FlightPlan;
 using ksp_plugin::MockPlanetarium;
 using ksp_plugin::MockPlugin;
+using ksp_plugin::MockVessel;
 using ::testing::StrictMock;
 
 class InterfaceExternalTest : public ::testing::Test {
  protected:
-  InterfaceExternalTest()
-      : plugin_(make_not_null_unique<StrictMock<MockPlugin>>()),
-        const_plugin_(plugin_.get()) {}
+  InterfaceExternalTest() : flight_plan_(1 * Tonne, t0_,) {
+    ON_CALL()
+  }
 
-  not_null<std::unique_ptr<StrictMock<MockPlugin>>> const plugin_;
-  StrictMock<MockPlugin> const* const const_plugin_;
+  MockPlugin const plugin_;
+  MockVessel const vessel_;
   Instant const t0_;
+  std::unique_ptr<Ephemeris
+  std::unique_ptr<FlightPlan> flight_plan_;
 };
+
+TEST_F(InterfaceExternalTest, GetNearestPlannedCoastDegreesOfFreedom) {
+  
+}
 
 }  // namespace interface
 }  // namespace principia
