094ca883a5915727c4ae4e67145314d6509f9c9a
Robin Leroy <egg.robin.leroy@gmail.com>
2018-02-25 19:02:27 +0100

    test plugin

---
+++ ksp_plugin_test/test_plugin.hpp
@@ -0,0 +1,31 @@
+
+#pragma once
+
+#include "astronomy/frames.hpp"
+#include "ksp_plugin/plugin.hpp"
+#include "physics/solar_system.hpp"
+
+namespace principia {
+namespace ksp_plugin {
+namespace internal_test_plugin {
+
+using astronomy::ICRFJ2000Equator;
+using physics::KeplerianElements;
+using physics::SolarSystem;
+
+class TestPlugin : public Plugin {
+  TestPlugin(SolarSystem<ICRFJ2000Equator> const& solar_system);
+
+  Vessel& AddVesselInEarthOrbit(GUID const& vessel_id,
+                                std::string const& vessel_name,
+                                PartId part_id,
+                                std::string const& part_name,
+                                KeplerianElements<Barycentric> const& elements);
+};
+
+}  // namespace internal_test_plugin
+
+using internal_test_plugin::TestPlugin;
+
+}  // namespace ksp_plugin
+}  // namespace principia
