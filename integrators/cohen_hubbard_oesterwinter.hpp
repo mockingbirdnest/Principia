c5bfeeaf4ccff727721aa04149850431deed2682
pleroy <phl+personal@google.com>
2018-02-25 14:15:18 +0100

    After egg's review.

--- integrators/cohen_hubbard_oesterwinter.hpp
+++ integrators/cohen_hubbard_oesterwinter.hpp
@@ -9,10 +9,17 @@ namespace internal_cohen_hubbard_oesterwinter {
 
 using numerics::FixedVector;
 
-// Definition of a Cohen-Hubbard-Oesterwinter formula.  |order_| is the order of
-// the approximation, that is, the error on the derivative is h^(order + 1).
-// The formula requires |order_| values of the acceleration.  numerators[0]
-// corresponds to f"(x₀), numerators[1] to f"(x₋₁), etc.
+// Definition of a modified Cohen-Hubbard-Oesterwinter formula, see Cohen,
+// Hubbard, Oesterwinter (1973), Astronomical papers prepared for the use of the
+// American ephemeris and nautical almanac Volume XXII Part I, p. 21.  Our
+// formula is:
+//   f′(x₀) ≅ f(x₀) - f(x₋₁) + h Σ(ηᵢ f″(x₋ᵢ))
+// where the sum is for i going from 0 to order_ - 1.  The ηᵢ are computed
+// using Mathematica, see cohen_hubbard_oesterwinter.wl.
+// |order_| is the order of the approximation, that is, the error on the
+// derivative is 𝒪(h^(order_ + 1)).  The formula requires |order_| values of the
+// acceleration.  numerators[0] corresponds to f″(x₀), numerators[1] to f″(x₋₁),
+// etc.
 // TODO(phl): This struct shows up in many places.  Factor it out.
 template<int order_>
 struct CohenHubbardOesterwinter final {
