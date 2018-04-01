453b0913bf1bdc7d5ef8d4b5df17b97a50fff5b0
Robin Leroy <egg.robin.leroy@gmail.com>
2018-04-01 14:39:07 +0200

    benchmark

--- benchmarks/planetarium.cpp
+++ benchmarks/planetarium.cpp
@@ -6,70 +6,249 @@
 #include <random>
 #include <vector>
 
+#include "astronomy/time_scales.hpp"
 #include "benchmark/benchmark.h"
+#include "physics/body_centred_non_rotating_dynamic_frame.hpp"
+#include "physics/solar_system.hpp"
+#include "testing_utilities/solar_system_factory.hpp"
 
 namespace principia {
 namespace geometry {
 
+using astronomy::operator""_UT1;
+using astronomy::operator""_TT;
 using base::make_not_null_unique;
 using base::not_null;
+using geometry::Bivector;
+using geometry::Perspective;
+using geometry::RigidTransformation;
+using geometry::RP2Lines;
+using geometry::Vector;
 using geometry::Velocity;
+using integrators::SymmetricLinearMultistepIntegrator;
+using integrators::methods::Quinlan1999Order8A;
+using integrators::methods::QuinlanTremaine1990Order12;
+using ksp_plugin::Camera;
 using ksp_plugin::Barycentric;
+using ksp_plugin::Navigation;
+using ksp_plugin::NavigationFrame;
 using ksp_plugin::Planetarium;
+using physics::BodyCentredNonRotatingDynamicFrame;
 using physics::DegreesOfFreedom;
 using physics::DiscreteTrajectory;
+using physics::Ephemeris;
+using physics::KeplerianElements;
+using physics::KeplerOrbit;
+using physics::MassiveBody;
+using physics::MasslessBody;
+using physics::SolarSystem;
 using quantities::Angle;
 using quantities::Cos;
+using quantities::Infinity;
+using quantities::Length;
 using quantities::Sin;
 using quantities::Time;
+using quantities::si::ArcMinute;
+using quantities::si::Day;
+using quantities::si::Degree;
+using quantities::si::Kilo;
 using quantities::si::Metre;
+using quantities::si::Milli;
+using quantities::si::Minute;
 using quantities::si::Radian;
 using quantities::si::Second;
+using testing_utilities::SolarSystemFactory;
 
 namespace {
 
-not_null<std::unique_ptr<DiscreteTrajectory<Barycentric>>>
-NewCircularTrajectory(Time const& period, Time const& step, Time const& last) {
-  auto discrete_trajectory =
-      make_not_null_unique<DiscreteTrajectory<Barycentric>>();
-  constexpr Instant t0;
-  for (Time t; t <= last; t += step) {
-    Angle const ɑ = 2 * π * t * Radian / period;
-      DegreesOfFreedom<Barycentric> const degrees_of_freedom(
-          Barycentric::origin +
-              Displacement<Barycentric>({10 * Metre * Sin(ɑ),
-                                         10 * Metre * Cos(ɑ),
-                                         0 * Metre}),
-          Velocity<Barycentric>({(20 * π / period) * Metre * Cos(ɑ),
-                                 -(20 * π / period) * Metre * Sin(ɑ),
-                                 0 * Metre / Second}));
-      discrete_trajectory->Append(t0 + t, degrees_of_freedom);
-  }
-  return discrete_trajectory;
+constexpr Length near = 40'000 * Kilo(Metre);
+constexpr Length far = 400'000 * Kilo(Metre);
+constexpr Length focal = 1 * Metre;
+
+Perspective<Navigation, Camera> PolarPerspective(
+    Length const distance_from_earth) {
+  return {
+      RigidTransformation<Navigation, Camera>(
+          Navigation::origin + Displacement<Navigation>(
+                                   {0 * Metre, 0 * Metre, distance_from_earth}),
+          Camera::origin,
+          Rotation<Navigation, Camera>(Vector<double, Navigation>({1, 0, 0}),
+                                       Vector<double, Navigation>({0, 0, 1}),
+                                       Bivector<double, Navigation>({0, -1, 0}))
+              .Forget()),
+      focal};
 }
 
-Planetarium PlanetariumLookingAlongY() {
-  // No dark area, human visual acuity, wide field of view.
-  Planetarium::Parameters parameters(
-      /*sphere_radius_multiplier=*/1,
-      /*angular_resolution=*/0.4 * ArcMinute,
-      /*field_of_view=*/90 * Degree);
-  Planetarium planetarium(
-      parameters, perspective_, &ephemeris_, &plotting_frame_);
+Perspective<Navigation, Camera> EquatorialPerspective(
+    Length const distance_from_earth) {
+  return {
+      RigidTransformation<Navigation, Camera>(
+          Navigation::origin + Displacement<Navigation>(
+                                   {0 * Metre, distance_from_earth, 0 * Metre}),
+          Camera::origin,
+          Rotation<Navigation, Camera>(Vector<double, Navigation>({1, 0, 0}),
+                                       Vector<double, Navigation>({0, 0, 1}),
+                                       Bivector<double, Navigation>({0, -1, 0}))
+              .Forget()),
+      focal};
 }
+
+class Satellites {
+ public:
+  Satellites()
+      : solar_system_(make_not_null_unique<SolarSystem<Barycentric>>(
+            SOLUTION_DIR / "astronomy" / "sol_gravity_model.proto.txt",
+            SOLUTION_DIR / "astronomy" /
+                "sol_initial_state_jd_2451545_000000000.proto.txt",
+            /*ignore_frame=*/true)),
+        ephemeris_(
+            solar_system_->MakeEphemeris(/*fitting_tolerance=*/1 * Milli(Metre),
+                                         EphemerisParameters())),
+        earth_(solar_system_->massive_body(
+            *ephemeris_,
+            SolarSystemFactory::name(SolarSystemFactory::Earth))),
+        earth_centred_inertial_(
+            make_not_null_unique<
+                BodyCentredNonRotatingDynamicFrame<Barycentric, Navigation>>(
+                ephemeris_.get(),
+                earth_)) {
+    // Two-line elements for GOES-8:
+    // 1 23051U 94022A   00004.06628221 -.00000243  00000-0  00000-0 0  9630
+    // 2 23051   0.4232  97.7420 0004776 192.8349 121.5613  1.00264613 28364
+    constexpr Instant goes_8_epoch = "JD2451548.56628221"_UT1;
+    KeplerianElements<Barycentric> goes_8_elements;
+    goes_8_elements.inclination = 0.4232 * Degree;
+    goes_8_elements.longitude_of_ascending_node = 97.7420 * Degree;
+    goes_8_elements.eccentricity = 0.0004776;
+    goes_8_elements.argument_of_periapsis = 192.8349 * Degree;
+    goes_8_elements.mean_anomaly = 121.5613 * Degree;
+    goes_8_elements.mean_motion = 1.00264613 * (2 * π * Radian / Day);
+    KeplerOrbit<Barycentric> const goes_8_orbit(
+        earth_, MasslessBody{}, goes_8_elements, goes_8_epoch);
+    goes_8_trajectory_.Append(
+        goes_8_epoch,
+        ephemeris_->trajectory(earth_)->EvaluateDegreesOfFreedom(goes_8_epoch) +
+            goes_8_orbit.StateVectors(goes_8_epoch));
+
+    ephemeris_->Prolong(goes_8_epoch);
+    auto goes_8_instance = ephemeris_->NewInstance(
+        {&goes_8_trajectory_},
+        Ephemeris<Barycentric>::NoIntrinsicAccelerations,
+        HistoryParameters());
+    ephemeris_->FlowWithFixedStep(goes_8_epoch + 100 * Day, goes_8_instance)
+  }
+
+  DiscreteTrajectory<Barycentric> const& GOES8Trajectory() const {
+    return goes_8_trajectory_;
+  }
+
+  Planetarium MakePlanetarium(
+      Perspective<Navigation, Camera> const& perspective) const {
+    // No dark area, human visual acuity, wide field of view.
+    Planetarium::Parameters parameters(
+        /*sphere_radius_multiplier=*/1,
+        /*angular_resolution=*/0.4 * ArcMinute,
+        /*field_of_view=*/90 * Degree);
+    Planetarium planetarium(
+        parameters, perspective, &ephemeris_, &earth_centred_inertial_);
+  }
+
+ private:
+  Ephemeris<Barycentric>::FixedStepParameters EphemerisParameters() {
+    return Ephemeris<Barycentric>::FixedStepParameters(
+        SymmetricLinearMultistepIntegrator<QuinlanTremaine1990Order12,
+                                           Position<Barycentric>>(),
+        /*step=*/10 * Minute);
+  }
+
+  Ephemeris<Barycentric>::FixedStepParameters HistoryParameters() {
+    return Ephemeris<Barycentric>::FixedStepParameters(
+        SymmetricLinearMultistepIntegrator<Quinlan1999Order8A,
+                                           Position<Barycentric>>(),
+        /*step=*/10 * Second);
+  }
+
+  not_null<std::unique_ptr<SolarSystem<Barycentric>>> const solar_system_;
+  not_null<std::unique_ptr<Ephemeris<Barycentric>>> const ephemeris_;
+  not_null<MassiveBody const*> const earth_;
+  not_null<std::unique_ptr<NavigationFrame>> const earth_centred_inertial_;
+  DiscreteTrajectory<Barycentric> goes_8_trajectory_;
+
+};
 
 }  // namespace
 
-void BM_PlanetariumPlotMethod2DenseCircleEdgeOn(benchmark::State& state) {
+void RunBenchmark(benchmark::State& state,
+                  Perspective<Navigation, Camera> const& perspective) {
+  Satellites satellites;
+  Planetarium planetarium = satellites.MakePlanetarium(perspective);
+  RP2Lines<Length, Camera> lines;
+  int total_lines = 0;
+  int total_points = 0;
+  int iterations = 0;
+  constexpr Instant now = "2000-01-21T04:41:30,5"_TT;
   while (state.KeepRunning()) {
-    for (auto const& segment : segments) {
-      auto const visible_segments =
-          perspective.VisibleSegments(segment, sphere);
-      ++visible_segments_count;
-      visible_segments_size += visible_segments.size();
+    lines = planetarium.PlotMethod2(satellites.GOES8Trajectory().Begin(),
+                                    satellites.GOES8Trajectory().End(),
+                                    now,
+                                    /*reverse=*/false);
+    state.PauseTiming();
+    total_lines += lines.size();
+    for (auto const& line : lines) {
+      total_points += line.size();
+    }
+    ++iterations;
+    state.ResumeTiming();
+  }
+  Length min_x = Infinity<Length>();
+  Length min_y = Infinity<Length>();
+  Length max_x = Infinity<Length>();
+  Length max_y = Infinity<Length>();
+  for (auto const& line : lines) {
+    for (auto const& point : line) {
+      min_x = std::min(min_x, point.x());
+      min_y = std::min(min_y, point.y());
+      max_x = std::max(max_x, point.x());
+      max_y = std::max(max_y, point.y());
     }
   }
+  state.SetLabel(std::to_string(total_points / iterations) + " points in " +
+                 std::to_string(total_lines / iterations) + " within [" +
+                 DebugString(min_x) + ", " + DebugString(max_x) + "] × [" +
+                 DebugString(min_y) + ", " + DebugString(max_y) + "]")
 }
+
+void BM_PlanetariumPlotMethod2NearPolarPerspective(
+    benchmark::State& state,
+    Perspective<Navigation, Camera> const& perspective) {
+  RunBenchmark(state, PolarPerspective(near));
+}
+
+BENCHMARK(BM_PlanetariumPlotMethod2NearPolarPerspective);
+
+void BM_PlanetariumPlotMethod2FarPolarPerspective(
+    benchmark::State& state,
+    Perspective<Navigation, Camera> const& perspective) {
+  RunBenchmark(state, PolarPerspective(far));
+}
+
+BENCHMARK(BM_PlanetariumPlotMethod2FarPolarPerspective);
+
+void BM_PlanetariumPlotMethod2NearEquatorialPerspective(
+    benchmark::State& state,
+    Perspective<Navigation, Camera> const& perspective) {
+  RunBenchmark(state, EquatorialPerspective(near));
+}
+
+BENCHMARK(BM_PlanetariumPlotMethod2NearEquatorialPerspective);
+
+void BM_PlanetariumPlotMethod2FarEquatorialPerspective(
+    benchmark::State& state,
+    Perspective<Navigation, Camera> const& perspective) {
+  RunBenchmark(state, EquatorialPerspective(far));
+}
+
+BENCHMARK(BM_PlanetariumPlotMethod2FarEquatorialPerspective);
 
 }  // namespace geometry
 }  // namespace principia
