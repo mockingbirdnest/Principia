460e423ba54942ab92bb14e1bde487f847bde1b9
Robin Leroy <egg.robin.leroy@gmail.com>
2018-05-01 13:11:49 +0200

    Cbrt

--- numerics/cbrt_test.cpp
+++ numerics/cbrt_test.cpp
@@ -23,44 +23,44 @@ class CubeRootTest : public ::testing::Test {
 };
 
 TEST_F(CubeRootTest, Cbrt2) {
-  EXPECT_THAT(cbrt(2), Eq(0x1.4'28A2'F98D'728Bp0));
-  EXPECT_THAT(cbrt(2) * cbrt(2) * cbrt(2), Eq(2));
+  EXPECT_THAT(Cbrt(2), Eq(0x1.4'28A2'F98D'728Bp0));
+  EXPECT_THAT(Cbrt(2) * Cbrt(2) * Cbrt(2), Eq(2));
 }
 
 TEST_F(CubeRootTest, Rescaling) {
-  EXPECT_THAT(0x1p-340 * cbrt(0x1p1021), Eq(cbrt(2)));
-  EXPECT_THAT(0x1p341 * cbrt(0x1p-1022), Eq(cbrt(2)));
-  EXPECT_THAT(0x1p358 * cbrt(0x1p-1073), Eq(cbrt(2)));
+  EXPECT_THAT(0x1p-340 * Cbrt(0x1p1021), Eq(Cbrt(2)));
+  EXPECT_THAT(0x1p341 * Cbrt(0x1p-1022), Eq(Cbrt(2)));
+  EXPECT_THAT(0x1p358 * Cbrt(0x1p-1073), Eq(Cbrt(2)));
 }
 
 TEST_F(CubeRootTest, SpecialValues) {
-  EXPECT_THAT(bits(cbrt(0)), Eq(0));
-  EXPECT_THAT(bits(cbrt(-0.0)), Eq(0x8000'0000'0000'0000));
-  EXPECT_THAT(cbrt(std::numeric_limits<double>::infinity()),
+  EXPECT_THAT(bits(Cbrt(0)), Eq(0));
+  EXPECT_THAT(bits(Cbrt(-0.0)), Eq(0x8000'0000'0000'0000));
+  EXPECT_THAT(Cbrt(std::numeric_limits<double>::infinity()),
               Eq(std::numeric_limits<double>::infinity()));
-  EXPECT_THAT(cbrt(-std::numeric_limits<double>::infinity()),
+  EXPECT_THAT(Cbrt(-std::numeric_limits<double>::infinity()),
               Eq(-std::numeric_limits<double>::infinity()));
-  EXPECT_THAT(cbrt(std::numeric_limits<double>::quiet_NaN()),
+  EXPECT_THAT(Cbrt(std::numeric_limits<double>::quiet_NaN()),
               Truly(&std::isnan<double>));
-  EXPECT_THAT(cbrt(-std::numeric_limits<double>::quiet_NaN()),
+  EXPECT_THAT(Cbrt(-std::numeric_limits<double>::quiet_NaN()),
               Truly(&std::isnan<double>));
 }
 
 TEST_F(CubeRootTest, BoundsOfTheRescalingRange) {
-  EXPECT_THAT(cbrt(0x1p-225), Eq(0x1p-75));
-  EXPECT_THAT(cbrt(0x1.0'0000'0000'0002p-225),
-              Eq(0x1p-75 * cbrt(0x1.0'0000'0000'0002p0)));
-  EXPECT_THAT(cbrt(0x1p237), Eq(0x1p79));
-  EXPECT_THAT(cbrt(0x1.F'FFFF'FFFF'FFFFp236),
-              Eq(0x1p79 * cbrt(0x1.F'FFFF'FFFF'FFFFp-1)));
+  EXPECT_THAT(Cbrt(0x1p-225), Eq(0x1p-75));
+  EXPECT_THAT(Cbrt(0x1.0'0000'0000'0002p-225),
+              Eq(0x1p-75 * Cbrt(0x1.0'0000'0000'0002p0)));
+  EXPECT_THAT(Cbrt(0x1p237), Eq(0x1p79));
+  EXPECT_THAT(Cbrt(0x1.F'FFFF'FFFF'FFFFp236),
+              Eq(0x1p79 * Cbrt(0x1.F'FFFF'FFFF'FFFFp-1)));
 }
 
 TEST_F(CubeRootTest, Sign) {
-  EXPECT_THAT(cbrt(-2), Eq(-cbrt(2)));
+  EXPECT_THAT(Cbrt(-2), Eq(-Cbrt(2)));
 }
 
 TEST_F(CubeRootTest, ParticularlyBadRounding) {
-  EXPECT_THAT(cbrt(0x1.14E35E87EA5DFp0), Eq(0x1.06C80FCCA8E18p0));
+  EXPECT_THAT(Cbrt(0x1.14E35E87EA5DFp0), Eq(0x1.06C80FCCA8E18p0));
 }
 
 }  // namespace numerics
