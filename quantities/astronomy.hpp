45b618c6afd752d4e891d765c23007fb992f8d5f
pleroy <phl+personal@google.com>
2018-06-09 12:42:26 +0200

    Physical characteristics of the Trappist bodies.

--- quantities/astronomy.hpp
+++ quantities/astronomy.hpp
@@ -11,13 +11,15 @@ namespace principia {
 namespace quantities {
 
 // This namespace contains units commonly used in astronomy that are not
-// accepted for use with the SI.
+// accepted for use with the SI.  See "Résolutions de la 29e Assemblée Générale
+// de l’Union Astronomique Internationale de 2015".
 namespace astronomy {
 
 constexpr Mass   SolarMass     = 1.98855e30 * si::Kilogram;
 constexpr Mass   JupiterMass   = 1.8986e27 * si::Kilogram;
 constexpr Mass   EarthMass     = 5.9742e24 * si::Kilogram;
 constexpr Time   JulianYear    = 365.25 * si::Day;
+constexpr Length EarthRadius   = 6.3781e6 * si::Metre;
 constexpr Length SolarRadius   = 6.957e8 * si::Metre;
 constexpr Length Parsec        = 648000 / π * si::AstronomicalUnit;
 constexpr Length LightYear     = constants::SpeedOfLight * JulianYear;
--- quantities/parser_body.hpp
+++ quantities/parser_body.hpp
@@ -114,15 +114,19 @@ inline Unit ParseUnit(std::string const& s) {
     return Unit(si::Metre);
   } else if (s == "km") {
     return Unit(si::Kilo(si::Metre));
+  } else if (s == u8"R🜨") {
+    return Unit(astronomy::EarthRadius);
+  } else if (s == u8"R☉") {
+    return Unit(astronomy::SolarRadius);
   } else if (s == "au") {
     return Unit(si::AstronomicalUnit);
   // Units of mass.
   } else if (s == "kg") {
     return Unit(si::Kilogram);
-  } else if (s == u8"M☉") {
-    return Unit(astronomy::SolarMass);
   } else if (s == u8"M🜨") {
     return Unit(astronomy::EarthMass);
+  } else if (s == u8"M☉") {
+    return Unit(astronomy::SolarMass);
   // Units of time.
   } else if (s == "ms") {
     return Unit(si::Milli(si::Second));
