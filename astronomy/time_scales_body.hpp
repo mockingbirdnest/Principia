294eb55e78beca793cae99041e030f8f09a49790
pleroy <phl+personal@google.com>
2018-07-07 18:18:26 +0200

    Remove Bulletin C.

--- astronomy/time_scales_body.hpp
+++ astronomy/time_scales_body.hpp
@@ -58,7 +58,7 @@ constexpr Instant FromTAI(quantities::Time const& tai) {
 
 // Utilities for modern UTC (since 1972).
 
-constexpr std::array<int, (2019 - 1972) * 2> leap_seconds = {{
+constexpr std::array<int, (2018 - 1972) * 2 + 1> leap_seconds = {{
     +1, +1,  // 1972
     +0, +1,  // 1973
     +0, +1,  // 1974
@@ -105,7 +105,7 @@ constexpr std::array<int, (2019 - 1972) * 2> leap_seconds = {{
     +1, +0,  // 2015
     +0, +1,  // 2016
     +0, +0,  // 2017
-    +0, +0,  // 2018
+    +0,      // 2018
 }};
 
 // Returns +1 if a positive leap second was inserted at the end of the given
