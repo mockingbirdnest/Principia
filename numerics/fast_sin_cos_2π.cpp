67ba5b408629be5593c6f1e1c40d562d7c3feb78
Robin Leroy <egg.robin.leroy@gmail.com>
2018-08-09 23:53:09 +0200

    a bit of renaming

--- numerics/fast_sin_cos_2π.cpp
+++ numerics/fast_sin_cos_2π.cpp
@@ -33,13 +33,13 @@ using P3 = PolynomialInMonomialBasis</*Value=*/double,
 // The coefficients are scaled to accept arguments scaled to [0, 1/2].
 
 // The sine polynomial uses a custom evaluation, so the individual coefficients
-// are named.  The polynomial for Sin(2 π x/4) is s₁ x + s₃ x³ + s₅ x⁵.
+// are named.  The polynomial for Sin(2 π y/4) is s₁ y + s₃ y³ + s₅ y⁵.
 double const s₁ = 6.28315387593158874093559349802 / 4;
 double const s₃ = -41.3255673715186216778612605095 / (4 * 16);
 double const s₅ = 79.5314110676979262924240784281 / (4 * 16 * 16);
 
-// The cosine polynomial for 16 x² ⟼ (Cos(2 π x) - 1)/(16 x²) is turned into a
-// polynomial for 16 x² ⟼ Cos(2 π x) by multiplying by the argument and adding
+// The cosine polynomial for 16 z² ⟼ (Cos(2 π z) - 1)/(16 z²) is turned into a
+// polynomial for 16 z² ⟼ Cos(2 π z) by multiplying by the argument and adding
 // 1, i.e., prepending 1 to the list of coefficients.
 P3 cos_polynomial(P3::Coefficients{
     1.0,
@@ -70,26 +70,26 @@ Decomposition Decompose(double const x) {
 }  // namespace
 
 void FastSinCos2π(double const cycles, double& sin, double& cos) {
-  // Argument reduction.  cycles_reduced is in [-1/2, 1/2] and quadrant goes
-  // from 0 to 3, with 0 indicating the principal quadrant and the others
-  // numbered in the trigonometric direction.  These quantities are computed by
-  // extracting the integer and fractional parts of 4 * cycles.
+  // Argument reduction.
+  // - quadrant goes from 0 to 3, with 0 indicating the principal quadrant and
+  //   the others numbered in the trigonometric direction;
+  // - y is in [-1/2, 1/2], corresponding to reduced angles of [-π/4, π/4] in
+  //   the quadrant.
+  // These quantities are computed by extracting the integer and fractional
+  // parts of 4 * cycles.
   Decomposition const decomposition = Decompose(4.0 * cycles);
-  // TODO(egg): cycles_reduced is no longer an appropriate name.
-  // This now counts a fraction of a right angle/quarter cycle/quadrant.
-  double const cycles_reduced = decomposition.fractional_part;
-  double const cycles_reduced² = cycles_reduced * cycles_reduced;
-  double const cycles_reduced³ = cycles_reduced² * cycles_reduced;
+  double const y = decomposition.fractional_part;
+  double const y² = y * y;
+  double const y³ = y² * y;
   std::int64_t const quadrant = decomposition.integer_part & 0b11;
 
   // The custom evaluation, compared to Estrin followed by multiplication by the
-  // argument, i.e., x * (s₁ + s₃ * x² + s₅ * (x² * x²)), avoids having a
-  // multiplication by x at the end.
-  double const s =
-      s₁ * cycles_reduced + (s₃ + s₅ * cycles_reduced²) * cycles_reduced³;
+  // argument, i.e., y * (s₁ + s₃ * y² + s₅ * (y² * y²)), avoids having a
+  // multiplication by y at the end.
+  double const s = s₁ * y + (s₃ + s₅ * y²) * y³;
   // TODO(egg): Considering the above, I'm not sure this is more readable than
   // just spelling out the Estrin evaluation.
-  double const c = cos_polynomial.Evaluate(cycles_reduced²);
+  double const c = cos_polynomial.Evaluate(y²);
 
   switch (quadrant) {
     case 0:
