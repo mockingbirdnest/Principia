4fb29660f78d4f8e86bd65853ce6b0485d21d546
Robin Leroy <egg.robin.leroy@gmail.com>
2018-08-09 23:39:48 +0200

    comments

--- numerics/fast_sin_cos_2π.cpp
+++ numerics/fast_sin_cos_2π.cpp
@@ -32,9 +32,8 @@ using P3 = PolynomialInMonomialBasis</*Value=*/double,
 // 1/24 respectively.  This is where the maximum error is found.
 // The coefficients are scaled to accept arguments scaled to [0, 1/2].
 
-// The sine polynomial uses a custom Estrin-like evaluation, so the individual
-// coefficients are named.
-// The polynomial for Sin(2 π x/4) is s₁ x + s₃ x³ + s₅ x⁵.
+// The sine polynomial uses a custom evaluation, so the individual coefficients
+// are named.  The polynomial for Sin(2 π x/4) is s₁ x + s₃ x³ + s₅ x⁵.
 double const s₁ = 6.28315387593158874093559349802 / 4;
 double const s₃ = -41.3255673715186216778612605095 / (4 * 16);
 double const s₅ = 79.5314110676979262924240784281 / (4 * 16 * 16);
@@ -83,6 +82,9 @@ void FastSinCos2π(double const cycles, double& sin, double& cos) {
   double const cycles_reduced³ = cycles_reduced² * cycles_reduced;
   std::int64_t const quadrant = decomposition.integer_part & 0b11;
 
+  // The custom evaluation, compared to Estrin followed by multiplication by the
+  // argument, i.e., x * (s₁ + s₃ * x² + s₅ * (x² * x²)), avoids having a
+  // multiplication by x at the end.
   double const s =
       s₁ * cycles_reduced + (s₃ + s₅ * cycles_reduced²) * cycles_reduced³;
   // TODO(egg): Considering the above, I'm not sure this is more readable than
