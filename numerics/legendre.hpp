65c5cc9a48f603189845e709ae6dbdf29141eacb
pleroy <phl+personal@google.com>
2018-08-18 17:56:00 +0200

    New class.

---
+++ numerics/legendre.hpp
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include "numerics/polynomial.hpp"
+
+namespace principia {
+namespace numerics {
+namespace internal_legendre {
+
+template<int degree_, template<typename, typename, int> class Evaluator>
+class LegendrePolynomial : public 
+  PolynomialInMonomialBasis<double, double, degree_, Evaluator> {
+ public:
+  LegendrePolynomial();
+};
+
+}  // namespace internal_legendre
+}  // namespace numerics
+}  // namespace principia
