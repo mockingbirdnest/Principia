8ee8b0fea02074a4d0c4f321e9f228a0fd190125
pleroy <phl+personal@google.com>
2018-08-23 22:34:22 +0200

    After egg's review.

--- geometry/cartesian_product.hpp
+++ geometry/cartesian_product.hpp
@@ -12,11 +12,13 @@ namespace internal_cartesian_product {
 // This struct exports two functions:
 //
 //   static constexpr ... Add(LTuple const& left, RTuple const& right);
-//   static constexpr... Subtract(LTuple const& left, RTuple const& right);
+//   static constexpr ... Subtract(LTuple const& left, RTuple const& right);
 //
 // where the return types are appropriate for the operation.  The two tuples may
 // be of different types (including different sizes) but corresponding element
-// types must have an addition and a subtraction.
+// types must have an addition and a subtraction.  In case of different sizes,
+// the smaller tuple is zero-extended, where zero is the neutral element of
+// addition on the relevant types.
 template<typename LTuple,
          typename RTuple,
          typename = std::make_integer_sequence<
