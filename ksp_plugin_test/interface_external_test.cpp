ddada3aa814e864709f4873881d99301f4c3fa31
pleroy <phl+personal@google.com>
2018-09-01 13:17:48 +0200

    More of the same, but with some matcher mess.

--- ksp_plugin_test/interface_external_test.cpp
+++ ksp_plugin_test/interface_external_test.cpp
@@ -24,6 +24,7 @@ using quantities::si::Hour;
 using quantities::si::Kilo;
 using quantities::si::Kilogram;
 using quantities::si::Metre;
+using quantities::si::Micro;
 using quantities::si::Milli;
 using quantities::si::Newton;
 using quantities::si::Tonne;
@@ -31,6 +32,7 @@ using testing_utilities::Componentwise;
 using testing_utilities::IsOk;
 using testing_utilities::IsNear;
 using testing_utilities::SolarSystemFactory;
+using ::testing::AnyOf;
 using ::testing::Eq;
 
 namespace {
@@ -100,12 +102,14 @@ TEST_F(InterfaceExternalTest, GetNearestPlannedCoastDegreesOfFreedom) {
   // the apoapsis.
   EXPECT_THAT(
       barycentric_result,
-      Componentwise(Componentwise(IsNear(-12'000 * Kilo(Metre)),
-                                  IsNear(-120 * Kilo(Metre)),
-                                  IsNear(-35 * Metre)),
-                    Componentwise(IsNear(-6.6 * Metre / Second),
-                                  IsNear(-4.9 * Kilo(Metre) / Second),
-                                  IsNear(6.6 * Milli(Metre) / Second))));
+      Componentwise(
+          Componentwise(IsNear(-12'000 * Kilo(Metre)),
+                        IsNear(-120 * Kilo(Metre)),
+                        AnyOf(IsNear(2.2 * Metre), IsNear(-35 * Metre))),
+          Componentwise(IsNear(-6.6 * Metre / Second),
+                        IsNear(-4.9 * Kilo(Metre) / Second),
+                        AnyOf(IsNear(54 * Micro(Metre) / Second),
+                              IsNear(6.6 * Milli(Metre) / Second)))));
 }
 
 }  // namespace interface
