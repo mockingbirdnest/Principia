0c3722858e89ea8ba197c6a3224bd350f0570005
Pascal Leroy <phl+personal@google.com>
2018-09-21 17:17:29 +0200

    A first test.

--- physics/body_test.cpp
+++ physics/body_test.cpp
@@ -1,38 +1,57 @@
 ﻿
 #include "physics/body.hpp"
 
+#include "astronomy/epoch.hpp"
+#include "astronomy/frames.hpp"
 #include "geometry/named_quantities.hpp"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "integrators/symmetric_linear_multistep_integrator.hpp"
 #include "physics/massive_body.hpp"
 #include "physics/massless_body.hpp"
 #include "physics/oblate_body.hpp"
 #include "physics/rotating_body.hpp"
+#include "physics/solar_system.hpp"
 #include "quantities/si.hpp"
 #include "serialization/geometry.pb.h"
 #include "testing_utilities/almost_equals.hpp"
+#include "testing_utilities/is_near.hpp"
 
 namespace principia {
 namespace physics {
 namespace internal_body {
 
+using astronomy::ICRS;
+using astronomy::J2000;
+using geometry::AngleBetween;
 using geometry::AngularVelocity;
+using geometry::Bivector;
+using geometry::Displacement;
 using geometry::Frame;
 using geometry::Instant;
 using geometry::Normalize;
+using geometry::OrientedAngleBetween;
+using geometry::Position;
 using geometry::RadiusLatitudeLongitude;
 using geometry::Vector;
+using integrators::SymmetricLinearMultistepIntegrator;
+using integrators::methods::QuinlanTremaine1990Order12;
 using quantities::Angle;
 using quantities::AngularFrequency;
 using quantities::Degree2SphericalHarmonicCoefficient;
 using quantities::GravitationalParameter;
 using quantities::Length;
 using quantities::SIUnit;
+using quantities::si::Day;
 using quantities::si::Degree;
+using quantities::si::Hour;
 using quantities::si::Metre;
+using quantities::si::Milli;
+using quantities::si::Minute;
 using quantities::si::Radian;
 using quantities::si::Second;
 using testing_utilities::AlmostEquals;
+using testing_utilities::IsNear;
 using ::testing::IsNull;
 using ::testing::NotNull;
 
@@ -316,6 +335,55 @@ TEST_F(BodyTest, AllFrames) {
   TestRotatingBody<serialization::Frame::TestTag,
                    serialization::Frame::THROUGH>();
   TestRotatingBody<serialization::Frame::TestTag, serialization::Frame::TO>();
+}
+
+TEST_F(BodyTest, SurfaceFrame) {
+  struct SurfaceFrame;
+  const Vector<double, SurfaceFrame> x({1, 0, 0});
+  const Bivector<double, SurfaceFrame> z({0, 0, 1});
+
+  SolarSystem<ICRS> solar_system_j2000(
+      SOLUTION_DIR / "astronomy" / "sol_gravity_model.proto.txt",
+      SOLUTION_DIR / "astronomy" /
+          "sol_initial_state_jd_2451545_000000000.proto.txt");
+  auto const ephemeris = solar_system_j2000.MakeEphemeris(
+      /*fitting_tolerance=*/5 * Milli(Metre),
+      Ephemeris<ICRS>::FixedStepParameters(
+          SymmetricLinearMultistepIntegrator<QuinlanTremaine1990Order12,
+                                             Position<ICRS>>(),
+          /*step=*/10 * Minute));
+  ephemeris->Prolong(J2000 + 1 * Day);
+
+  auto const earth = solar_system_j2000.rotating_body(*ephemeris, "Earth");
+  auto const sun = solar_system_j2000.rotating_body(*ephemeris, "Sun");
+  auto const earth_trajectory = ephemeris->trajectory(earth);
+  auto const sun_trajectory = ephemeris->trajectory(sun);
+
+  auto const from_surface_frame_j2000 =
+      earth->FromSurfaceFrame<SurfaceFrame>(J2000);
+  auto const earth_centre_j2000 = earth_trajectory->EvaluatePosition(J2000);
+  auto const sun_centre_j2000 = sun_trajectory->EvaluatePosition(J2000);
+  auto const x_j2000 = from_surface_frame_j2000(x);
+  auto const earth_sun_j2000 = sun_centre_j2000 - earth_centre_j2000;
+  Displacement<ICRS> const projected_earth_sun_j2000(
+      {earth_sun_j2000.coordinates().x,
+       earth_sun_j2000.coordinates().y,
+       0 * Metre});
+  EXPECT_THAT(AngleBetween(x_j2000, projected_earth_sun_j2000),
+              IsNear(1.1 * Degree));
+
+  Instant const j2000_3h = J2000 + 3 * Hour;
+  auto const from_surface_frame_j2000_3h =
+      earth->FromSurfaceFrame<SurfaceFrame>(j2000_3h);
+  auto const earth_centre_j2000_3h =
+      earth_trajectory->EvaluatePosition(j2000_3h);
+  auto const sun_centre_j2000_3h = sun_trajectory->EvaluatePosition(j2000_3h);
+  auto const x_j2000_3h = from_surface_frame_j2000_3h(x);
+  auto const earth_sun_j2000_3h = sun_centre_j2000_3h - earth_centre_j2000_3h;
+  EXPECT_THAT(
+      OrientedAngleBetween(
+          earth_sun_j2000_3h, x_j2000_3h, from_surface_frame_j2000_3h(z)),
+      IsNear(48.3 * Degree));
 }
 
 }  // namespace internal_body
