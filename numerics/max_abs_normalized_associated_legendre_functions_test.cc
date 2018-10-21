a33b08386536ea162987fec3f6d8f5f8103f75f6
Robin Leroy <egg.robin.leroy@gmail.com>
2018-10-21 15:39:04 +0200

    tables

---
+++ numerics/max_abs_normalized_associated_legendre_functions_test.cc
@@ -0,0 +1,13 @@
+
+#include "numerics/max_abs_normalized_associated_legendre_function.mathematica.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace principia {
+namespace numerics {
+
+class MaxAbsNormalizedAssociatedLegendreFunctionTest : testing::Test {};
+
+}  // namespace numerics
+}  // namespace principia
