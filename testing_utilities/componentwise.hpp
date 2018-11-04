68030764486db610c6c7f3a216b23a42f0997a59
pleroy <phl+personal@google.com>
2018-11-04 11:14:32 +0100

    Change the declarations.

--- testing_utilities/componentwise.hpp
+++ testing_utilities/componentwise.hpp
@@ -18,6 +18,8 @@ namespace testing_utilities {
 namespace internal_componentwise {
 
 using geometry::R3Element;
+using ::testing::Matcher;
+using ::testing::MatcherInterface;
 
 template<typename T1Matcher, typename T2Matcher>
 class ComponentwiseMatcher2;
@@ -39,15 +41,65 @@ Componentwise(XMatcher const& x_matcher,
 template<typename T1Matcher, typename T2Matcher>
 class ComponentwiseMatcher2 final {
  public:
-  explicit ComponentwiseMatcher2(T1Matcher const& t1_matcher,
-                                 T2Matcher const& t2_matcher);
+  ComponentwiseMatcher2(T1Matcher const& t1_matcher,
+                        T2Matcher const& t2_matcher);
+
+  template <typename PairType>
+  operator Matcher<PairType> () const;
+
+ private:
+  T1Matcher const t1_matcher_;
+  T2Matcher const t2_matcher_;
+};
+
+template<typename XMatcher, typename YMatcher, typename ZMatcher>
+class ComponentwiseMatcher3 final {
+ public:
+  ComponentwiseMatcher3(XMatcher const& x_matcher,
+                        YMatcher const& y_matcher,
+                        ZMatcher const& z_matcher);
+
+  template <typename TripleType>
+  operator Matcher<TripleType> () const;
+
+ private:
+  XMatcher const x_matcher_;
+  YMatcher const y_matcher_;
+  ZMatcher const z_matcher_;
+};
+
+template<typename PairType>
+class ComponentwiseMatcher2Impl;
+
+template<typename T1, typename T2>
+class ComponentwiseMatcher2Impl<geometry::Pair<T1, T2>> final
+    : public MatcherInterface<geometry::Pair<T1, T2>> {
+ public:
+  template<typename T1Matcher, typename T2Matcher>
+  ComponentwiseMatcher2(T1Matcher const& t1_matcher,
+                        T2Matcher const& t2_matcher);
 
   // Note that at this point this is only useful for vector/vector pairs as we
   // don't have matchers for |Point|.
-  template<typename T1, typename T2>
   bool MatchAndExplain(geometry::Pair<T1, T2> const& actual,
                        testing::MatchResultListener* listener) const;
-  template<typename Scalar, typename Frame>
+
+  void DescribeTo(std::ostream* out) const;
+  void DescribeNegationTo(std::ostream* out) const;
+
+ private:
+  Matcher<T1> const t1_matcher_;
+  Matcher<T2> const t2_matcher_;
+};
+
+template<typename Scalar, typename Frame>
+class ComponentwiseMatcher2Impl<geometry::RP2Point<Scalar, Frame>> final
+    : public MatcherInterface<geometry::RP2Point<Scalar, Frame>> {
+ public:
+  template<typename XMatcher, typename YMatcher>
+  ComponentwiseMatcher2(T1Matcher const& x_matcher,
+                        T2Matcher const& x_matcher);
+
   bool MatchAndExplain(geometry::RP2Point<Scalar, Frame> const& actual,
                        testing::MatchResultListener* listener) const;
 
@@ -55,24 +107,64 @@ class ComponentwiseMatcher2 final {
   void DescribeNegationTo(std::ostream* out) const;
 
  private:
-  T1Matcher const& t1_matcher_;
-  T2Matcher const& t2_matcher_;
+  Matcher<Scalar> const t1_matcher_;
+  Matcher<Scalar> const t2_matcher_;
 };
 
-template<typename XMatcher, typename YMatcher, typename ZMatcher>
-class ComponentwiseMatcher3 final {
- public:
-  explicit ComponentwiseMatcher3(XMatcher const& x_matcher,
-                                 YMatcher const& y_matcher,
-                                 ZMatcher const& z_matcher);
+template<typename TripleType>
+class ComponentwiseMatcher3Impl;
 
-  template<typename Scalar>
+template<typename Scalar>
+class ComponentwiseMatcher3Impl<geometry::R3Element<Scalar>> final
+    : public MatcherInterface<geometry::R3Element<Scalar>> {
+ public:
+  template<typename XMatcher, typename YMatcher, typename ZMatcher>
+  ComponentwiseMatcher3(XMatcher const& x_matcher,
+                        YMatcher const& y_matcher,
+                        ZMatcher const& z_matcher);
+
   bool MatchAndExplain(geometry::R3Element<Scalar> const& actual,
                        testing::MatchResultListener* listener) const;
-  template<typename Scalar, typename Frame>
+
+  void DescribeTo(std::ostream* out) const;
+  void DescribeNegationTo(std::ostream* out) const;
+
+ private:
+  Matcher<Scalar> const x_matcher_;
+  Matcher<Scalar> const y_matcher_;
+  Matcher<Scalar> const z_matcher_;
+};
+
+template<typename Scalar, typename Frame>
+class ComponentwiseMatcher3Impl<geometry::Vector<Scalar, Frame>> final
+    : public MatcherInterface<geometry::Vector<Scalar, Frame>> {
+ public:
+  template<typename XMatcher, typename YMatcher, typename ZMatcher>
+  ComponentwiseMatcher3(XMatcher const& x_matcher,
+                        YMatcher const& y_matcher,
+                        ZMatcher const& z_matcher);
+
   bool MatchAndExplain(geometry::Vector<Scalar, Frame> const& actual,
                        testing::MatchResultListener* listener) const;
-  template<typename Scalar, typename Frame>
+
+  void DescribeTo(std::ostream* out) const;
+  void DescribeNegationTo(std::ostream* out) const;
+
+ private:
+  Matcher<Scalar> const x_matcher_;
+  Matcher<Scalar> const y_matcher_;
+  Matcher<Scalar> const z_matcher_;
+};
+
+template<typename Scalar, typename Frame>
+class ComponentwiseMatcher3Impl<geometry::Bivector<Scalar, Frame>> final
+    : public MatcherInterface<geometry::Bivector<Scalar, Frame>> {
+ public:
+  template<typename XMatcher, typename YMatcher, typename ZMatcher>
+  ComponentwiseMatcher3(XMatcher const& x_matcher,
+                        YMatcher const& y_matcher,
+                        ZMatcher const& z_matcher);
+
   bool MatchAndExplain(geometry::Bivector<Scalar, Frame> const& actual,
                        testing::MatchResultListener* listener) const;
 
@@ -80,9 +172,9 @@ class ComponentwiseMatcher3 final {
   void DescribeNegationTo(std::ostream* out) const;
 
  private:
-  XMatcher const& x_matcher_;
-  YMatcher const& y_matcher_;
-  ZMatcher const& z_matcher_;
+  Matcher<Scalar> const x_matcher_;
+  Matcher<Scalar> const y_matcher_;
+  Matcher<Scalar> const z_matcher_;
 };
 
 }  // namespace internal_componentwise
