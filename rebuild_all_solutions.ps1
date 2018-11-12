4764c182649d31c284aa643c2405aa8036adbf63
Robin Leroy <egg.robin.leroy@gmail.com>
2018-11-12 20:01:18 +0100

    clone and pull deps

--- rebuild_all_solutions.ps1
+++ rebuild_all_solutions.ps1
@@ -7,6 +7,31 @@ $dependencies = @(".\Google\glog\google-glog.sln",
                   ".\Google\gipfeli\msvc\gipfeli.sln",
                   ".\Google\abseil-cpp\msvc\abseil-cpp.sln")
 
+push-location -path "Google"
+
+
+foreach ($repository in @("glog", "googletest", "protobuf", "benchmark",
+                          "gipfeli", "abseil-cpp")) {
+  if (!(test-path -path $repository)) {
+    git clone ("https://github.com/mockingbirdnest/" + $repository + ".git")
+  }
+  push-location $repository
+  git checkout master
+  git pull
+  if (!$?) {
+    if ($args[0] -eq "--force") {
+      git reset --hard origin/master
+      git clean -fdx
+    } else {
+      pop-location
+      pop-location
+      exit 1
+    }
+  }
+  pop-location
+}
+pop-location
+
 function build_solutions($solutions) {
   foreach ($configuration in "Debug", "Release") {
     foreach ($platform in "x64") {
