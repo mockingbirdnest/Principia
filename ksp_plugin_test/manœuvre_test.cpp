378e98a86579b5b802e7a27dc0dbdb1b28da220e
pleroy <phl+personal@google.com>
2018-12-05 16:55:31 +0100

    New bugs are appearing.

--- ksp_plugin_test/manœuvre_test.cpp
+++ ksp_plugin_test/manœuvre_test.cpp
@@ -104,7 +104,11 @@ TEST_F(ManœuvreTest, TimedBurn) {
   EXPECT_EQ(1 * Newton, manœuvre.thrust());
   EXPECT_EQ(2 * Kilogram, manœuvre.initial_mass());
   EXPECT_EQ(1 * Metre / Second, manœuvre.specific_impulse());
+#if PRINCIPIA_COMPILER_MSVC && _MSC_FULL_VER == 191627024
+  EXPECT_TRUE(e_y == manœuvre.direction());
+#else
   EXPECT_EQ(e_y, manœuvre.direction());
+#endif
   EXPECT_EQ(1 * Kilogram / Second, manœuvre.mass_flow());
 
   manœuvre.set_duration(1 * Second);
@@ -156,7 +160,11 @@ TEST_F(ManœuvreTest, TargetΔv) {
   EXPECT_EQ(1 * Newton, manœuvre.thrust());
   EXPECT_EQ(2 * Kilogram, manœuvre.initial_mass());
   EXPECT_EQ(1 * Metre / Second, manœuvre.specific_impulse());
+#if PRINCIPIA_COMPILER_MSVC && _MSC_FULL_VER == 191627024
+  EXPECT_TRUE(e_y == manœuvre.direction());
+#else
   EXPECT_EQ(e_y, manœuvre.direction());
+#endif
   EXPECT_EQ(1 * Kilogram / Second, manœuvre.mass_flow());
 
   manœuvre.set_Δv(1 * Metre / Second);
@@ -364,7 +372,11 @@ TEST_F(ManœuvreTest, Serialization) {
   EXPECT_EQ(1 * Newton, manœuvre_read.thrust());
   EXPECT_EQ(2 * Kilogram, manœuvre_read.initial_mass());
   EXPECT_EQ(1 * Metre / Second, manœuvre_read.specific_impulse());
-  EXPECT_EQ(e_y, manœuvre_read.direction());
+#if PRINCIPIA_COMPILER_MSVC && _MSC_FULL_VER == 191627024
+  EXPECT_TRUE(e_y == manœuvre.direction());
+#else
+  EXPECT_EQ(e_y, manœuvre.direction());
+#endif
   EXPECT_EQ(1 * Kilogram / Second, manœuvre_read.mass_flow());
 
   EXPECT_EQ(1 * Metre / Second, manœuvre.Δv());
