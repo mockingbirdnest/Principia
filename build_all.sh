eaf174343158ab2a3972c955e61a611413ae6fb7
Maarten Maathuis <madman2003@shikahr.net>
2018-12-06 22:02:16 +0100

    Add assemblypath logic also to build_all script

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Please specify the KSP_MANAGED_FOLDER variable if the location is different from the default in the script
+# Typically this is something like: "${LOCATION_OF_GAMES}/Kerbal Space Program/KSP_Data/Managed/"
+# This folder contains the KSP and unity C# assembly to build against
+
+if [ -z ${KSP_MANAGED_FOLDER} ]; then
+	KSP_MANAGED_FOLDER="${HOME}/.steam/steam/steamapps/common/Kerbal Space Program/KSP_Data/Managed/"
+fi
 
 # This test will fail if any of the following us true:
 # The compiler doesn't recognize the C++ standard
@@ -19,7 +26,7 @@ filesystem_support_test=$(expr "$?" == "0")
 # Build C++ and C# code
 if [ "${filesystem_support_test}" -eq "1" ]
 then
-	CXX_SUPPORTS_STD_FILESYSTEM=1 make -j8
+	AssemblySearchPaths="/usr/lib/mono/3.5-api/;/usr/lib/mono/2.0-api/;${KSP_MANAGED_FOLDER}" CXX_SUPPORTS_STD_FILESYSTEM=1 make -j8
 else
-	make -j8
+	AssemblySearchPaths="/usr/lib/mono/3.5-api/;/usr/lib/mono/2.0-api/;${KSP_MANAGED_FOLDER}" make -j8
 fi
