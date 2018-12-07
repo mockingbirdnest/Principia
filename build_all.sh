bb413a123502c9641e48e35fa10140f04331a1a4
Maarten Maathuis <madman2003@shikahr.net>
2018-12-07 07:36:55 +0100

    add option to skip (re)building dependencies

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+# Pass the option "--skip-deps" if you don't want to rebuild the dependencies.
+#
 # Please specify the KSP_MANAGED_FOLDER variable if the location is different from the default in the script
 # Typically this is something like: "${LOCATION_OF_GAMES}/Kerbal Space Program/KSP_Data/Managed/"
 # This folder contains the KSP and unity C# assembly to build against
 
+build_deps=1
+
 if [ -z ${KSP_MANAGED_FOLDER} ]; then
 	KSP_MANAGED_FOLDER="${HOME}/.steam/steam/steamapps/common/Kerbal Space Program/KSP_Data/Managed/"
 fi
+
+# Read command line arguments
+for i in "$@"
+do
+case $i in
+	--skip-deps)
+	build_deps=0
+	shift
+	;;
+	*)
+	echo "Unknown option ${i} passed"
+	exit 1
+	;;
+esac
+done
 
 # This test will fail if any of the following us true:
 # The compiler doesn't recognize the C++ standard
@@ -21,7 +40,10 @@ _TESTFILE
 filesystem_support_test=$(expr "$?" == "0")
 
 # Build dependencies of C++ code
-./build_deps.sh
+if [ ${build_deps} -eq 1 ]
+then
+	./build_deps.sh
+fi
 
 # Build C++ and C# code
 if [ "${filesystem_support_test}" -eq "1" ]
