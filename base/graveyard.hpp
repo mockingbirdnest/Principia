0b44e1e456020a9dc8a21f45bd64ab33b80c2df6
Pascal Leroy <phl+personal@google.com>
2018-12-21 18:03:56 +0100

    Graveyard implementation.

---
+++ base/graveyard.hpp
@@ -0,0 +1,26 @@
+
+#pragma once
+
+#include <cstdint>s
+#include <memory>
+
+#include "base/thread_pool.hpp"
+
+namespace principia {
+namespace base {
+
+template<typename T>
+class Graveyard {
+ public:
+  explicit Graveyard(std::int64_t number_of_threads);
+
+  void Bury(std::unique_ptr<T> t);
+
+ private:
+  ThreadPool<void> gravedigger_;
+};
+
+}  // namespace base
+}  // namespace principia
+
+#include "base/graveyard_body.hpp"
---
+++ base/graveyard_body.hpp
@@ -0,0 +1,21 @@
+
+#pragma once
+
+#include "base/graveyard.hpp"
+
+namespace principia {
+namespace base {
+
+template<typename T>
+Graveyard<T>::Graveyard(std::int64_t const number_of_threads)
+    : gravedigger_(number_of_threads) {}
+
+template<typename T>
+void Graveyard<T>::Bury(std::unique_ptr<T> t) {
+  gravedigger_.Add([corpse = std::move(t)]() {
+    corpse.reset();
+  });
+}
+
+}  // namespace base
+}  // namespace principia
