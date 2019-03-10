25e254b73fbbfbc9a5726e2028f906970345fbbc
Pascal Leroy <phl+personal@google.com>
2019-03-10 15:09:01 +0700

    Check the result of FlowPrediction.

--- ksp_plugin/plugin.cpp
+++ ksp_plugin/plugin.cpp
@@ -897,7 +897,7 @@ void Plugin::ComputeAndRenderClosestApproaches(
   DiscreteTrajectory<Barycentric> apoapsides_trajectory;
   DiscreteTrajectory<Barycentric> periapsides_trajectory;
   auto& target_vessel = renderer_->GetTargetVessel();
-  target_vessel.FlowPrediction(current_time_);
+  CHECK_OK(target_vessel.FlowPrediction(current_time_));
   ComputeApsides(target_vessel.prediction(),
                  begin,
                  end,
