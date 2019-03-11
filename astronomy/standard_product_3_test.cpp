b9bc9cf5a535c373b1a49d93df689de741a8c83a
Robin Leroy <egg.robin.leroy@gmail.com>
2019-03-12 01:09:42 +0700

    a directory for SP3 files

--- astronomy/standard_product_3_test.cpp
+++ astronomy/standard_product_3_test.cpp
@@ -20,23 +20,23 @@ using StandardProduct3DeathTest = StandardProduct3Test;
 TEST_F(StandardProduct3Test, PositionOnly) {
   // SP3-a file from the European Space Operations Centre (European
   // Space Agency).
-  StandardProduct3 sp3a(SOLUTION_DIR / "astronomy" / "esa11802.eph",
+  StandardProduct3 sp3a(SOLUTION_DIR / "astronomy" / "sp3_orbits" / "esa11802.eph",
                         StandardProduct3::Dialect::Standard);
 
   // SP3-b file from the laser operational and analysis centre (Лазерный
   // Операционно-Аналитический Центр) Mission Control Centre (Центр Управления
   // Полётами).
-  StandardProduct3 sp3b(SOLUTION_DIR / "astronomy" / "mcc14000.sp3",
+  StandardProduct3 sp3b(SOLUTION_DIR / "astronomy" / "sp3_orbits" / "mcc14000.sp3",
                         StandardProduct3::Dialect::Standard);
 
   // SP3-c and SP3-d file from the Center for Orbit Determination
   // in Europe (Astronomical Institute of the University of Bern).
   // These files are from the Multi-GNSS EXperiment (MGEX).
-  StandardProduct3 sp3c(SOLUTION_DIR / "astronomy" /
-                        "COD0MGXFIN_20181260000_01D_05M_ORB.SP3",
+  StandardProduct3 sp3c(SOLUTION_DIR / "astronomy" / "sp3_orbits" /
+                            "COD0MGXFIN_20181260000_01D_05M_ORB.SP3",
                         StandardProduct3::Dialect::Standard);
-  StandardProduct3 sp3d(SOLUTION_DIR / "astronomy" /
-                        "COD0MGXFIN_20183640000_01D_05M_ORB.SP3",
+  StandardProduct3 sp3d(SOLUTION_DIR / "astronomy" / "sp3_orbits" /
+                            "COD0MGXFIN_20183640000_01D_05M_ORB.SP3",
                         StandardProduct3::Dialect::Standard);
 
   EXPECT_THAT(sp3a.version(), Eq(StandardProduct3::Version::A));
@@ -48,15 +48,19 @@ TEST_F(StandardProduct3Test, PositionOnly) {
 // Conformant position and velocity files of versions a and c.
 TEST_F(StandardProduct3Test, PositionAndVelocity) {
   // SP3-a file from the National Geospatial-intelligence Agency.
-  StandardProduct3 sp3a(SOLUTION_DIR / "astronomy" / "nga20342.eph",
-                        StandardProduct3::Dialect::Standard);
+  StandardProduct3 sp3a(
+      SOLUTION_DIR / "astronomy" / "sp3_orbits" / "nga20342.eph",
+      StandardProduct3::Dialect::Standard);
 
   // SP3-c file from the Centro di Geodesia Spaziale (Agenzia Spaziale
   // Italiana), an ILRS analysis centre.
   // Orbit for ЭТАЛОН-2.
-  StandardProduct3 sp3c(
-      SOLUTION_DIR / "astronomy" / "asi.orb.etalon2.171209.v70.sp3",
-      StandardProduct3::Dialect::Standard);
+  StandardProduct3 sp3c(SOLUTION_DIR / "astronomy" / "sp3_orbits" /
+                            "asi.orb.etalon2.171209.v70.sp3",
+                        StandardProduct3::Dialect::Standard);
+
+  EXPECT_THAT(sp3a.version(), Eq(StandardProduct3::Version::A));
+  EXPECT_THAT(sp3c.version(), Eq(StandardProduct3::Version::C));
 }
 
 }  // namespace astronomy
