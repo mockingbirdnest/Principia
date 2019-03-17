89ea9b73d62e94fb4cabb4e2d584e49aba7deb8a
pleroy <phl+personal@google.com>
2019-03-17 10:58:33 +0100

    Make the rectangle a member and centre it.

--- ksp_plugin_adapter/dialog.cs
+++ ksp_plugin_adapter/dialog.cs
@@ -7,52 +7,44 @@ namespace principia {
 namespace ksp_plugin_adapter {
 
 internal class Dialog : IConfigNode {
-
   public void Show(String message) {
-    UnityEngine.Rect dialog_window_rectangle = UnityEngine.Rect.zero;
-
     UnityEngine.GUI.skin = null;
-    dialog_window_rectangle.xMin = x_;
-    dialog_window_rectangle.yMin = y_;
-    dialog_window_rectangle = UnityEngine.GUILayout.Window(
+    rectangle_ = UnityEngine.GUILayout.Window(
         id         : this.GetHashCode(),
-        screenRect : dialog_window_rectangle,
+        screenRect : rectangle_,
         func       : (int id) => {
           using (new VerticalLayout())
           {
             UnityEngine.GUILayout.TextArea(message);
           }
-          UnityEngine.GUI.DragWindow(
-              position: new UnityEngine.Rect(x      : 0f,
-                                             y      : 0f,
-                                             width  : 10000f,
-                                             height : 10000f));
+          UnityEngine.GUI.DragWindow();
         },
-        text: "Principia",
-        options: UnityEngine.GUILayout.MinWidth(500));
-    WindowUtilities.EnsureOnScreen(ref dialog_window_rectangle);
-    x_ = dialog_window_rectangle.xMin;
-    y_ = dialog_window_rectangle.yMin;
+        text       : "Principia");
+    WindowUtilities.EnsureOnScreen(ref rectangle_);
   }
 
   void IConfigNode.Load(ConfigNode node) {
     String x_value = node.GetAtMostOneValue("x");
     if (x_value != null) {
-      x_ = System.Convert.ToSingle(x_value);
+      rectangle_.x = System.Convert.ToSingle(x_value);
     }
     String y_value = node.GetAtMostOneValue("y");
     if (y_value != null) {
-      y_ = System.Convert.ToSingle(y_value);
+      rectangle_.y = System.Convert.ToSingle(y_value);
     }
   }
 
   void IConfigNode.Save(ConfigNode node) {
-    node.SetValue("x", x_, createIfNotFound: true);
-    node.SetValue("y", y_, createIfNotFound: true);
+    node.SetValue("x", rectangle_.x, createIfNotFound: true);
+    node.SetValue("y", rectangle_.y, createIfNotFound: true);
   }
 
-  private Single x_ = UnityEngine.Screen.width / 2;
-  private Single y_ = UnityEngine.Screen.height / 3;
+  private static readonly float min_width_ = 500;
+  private UnityEngine.Rect rectangle_ =
+      new UnityEngine.Rect(x      : (UnityEngine.Screen.width - min_width_) / 2,
+                           y      : UnityEngine.Screen.height / 3,
+                           width  : min_width_,
+                           height :0);
 }
 
 }  // namespace ksp_plugin_adapter
