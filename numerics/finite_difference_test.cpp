7bf18144965f35f27f0c6fccf57f49de31bca669
Robin Leroy <egg.robin.leroy@gmail.com>
2019-03-20 01:31:53 +0100

    multiple orders

--- numerics/finite_difference_test.cpp
+++ numerics/finite_difference_test.cpp
@@ -14,27 +14,35 @@ namespace internal_finite_difference {
 using quantities::Sqrt;
 using quantities::Pow;
 
-class FiniteDifferenceTest : public ::testing::Test {};
+class FiniteDifferenceTest : public ::testing::Test {
+ protected:
+  template<int n>
+  std::vector<std::vector<double>> estimate() const {
+    std::vector<std::vector<double>> estimates;
+    auto const f = [](double x) {
+      return 1e9 + 1729 * std::sin(x / 1024) - 163 * std::cos(x / 128 + 3) -
+             x * x / 1024;
+    };
+    for (IMPL = 1; IMPL <= 6; ++IMPL) {
+      estimates.emplace_back();
+      for (double step = 0x1p64; 1 + step != 1; step /= 2) {
+        FixedVector<double, n> values;
+        for (int i = 0; i < n; ++i) {
+          values[i] = f(1 + i * step);
+        }
+        estimates.back().push_back(FiniteDifference(values, step, 0));
+      }
+    }
+    return estimates;
+  }
+};
 
 TEST_F(FiniteDifferenceTest, Meow) {
-  std::vector<std::vector<double>> estimates;
-  auto const f = [](double x) {
-    return 1e9 + std::sin(x / 1024) - std::cos(x / 128 + 3);
-  };
-  for (IMPL = 1; IMPL <= 6; ++IMPL) {
-    estimates.emplace_back();
-    for (double step = 0x1p64; 1 + step != 1; step /= 2) {
-      FixedVector<double, 6> values = {f(1),
-                                       f(1 + step),
-                                       f(1 + 2 * step),
-                                       f(1 + 3 * step),
-                                       f(1 + 4 * step),
-                                       f(1 + 5 * step)};
-      estimates.back().push_back(FiniteDifference(values, step, 0));
-    }
-  }
   base::OFStream file(SOLUTION_DIR / "finite_difference");
-  file << mathematica::Assign("estimates", estimates);
+  file << mathematica::Assign("estimates2", estimate<2>());
+  file << mathematica::Assign("estimates5", estimate<5>());
+  file << mathematica::Assign("estimates8", estimate<8>());
+  file << mathematica::Assign("estimates16", estimate<16>());
 }
 
 }  // namespace internal_finite_difference
