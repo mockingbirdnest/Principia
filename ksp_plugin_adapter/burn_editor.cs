cfd6a0713d28e74e9fff43540ad5a093b4c262ba
pleroy <pascal.h.leroy@gmail.com>
2019-04-12 19:47:04 +0200

    An attempt that doesn't work.

--- ksp_plugin_adapter/burn_editor.cs
+++ ksp_plugin_adapter/burn_editor.cs
@@ -66,13 +66,13 @@ class BurnEditor : ScalingRenderer {
     bool changed = false;
     using (new UnityEngine.GUILayout.HorizontalScope()) {
       UnityEngine.GUILayout.Label(header);
-      string frame_warning = "";
-      if (!reference_frame_selector_.FrameParameters().Equals(
+      if (manoeuvre_warning_ == "" &
+          !reference_frame_selector_.FrameParameters().Equals(
               adapter_.plotting_frame_selector_.FrameParameters())) {
-        frame_warning = "Manœuvre frame differs from plotting frame";
+        manoeuvre_warning_ = "Manœuvre frame differs from plotting frame";
       }
       UnityEngine.GUILayout.Label(
-          frame_warning,
+          manoeuvre_warning_,
           Style.RightAligned(Style.Warning(UnityEngine.GUI.skin.label)));
     }
     using (new UnityEngine.GUILayout.VerticalScope()) {
@@ -146,8 +146,16 @@ class BurnEditor : ScalingRenderer {
     Δv_tangent_.value = burn.delta_v.x;
     Δv_normal_.value = burn.delta_v.y;
     Δv_binormal_.value = burn.delta_v.z;
-    previous_coast_duration_.value =
-        burn.initial_time - time_base;
+    if (previous_coast_duration_.value != burn.initial_time - time_base) {
+      //UnityEngine.Debug.LogError(previous_coast_duration_.value+" "+
+      //burn.initial_time+" "+time_base+" "+
+      //(previous_coast_duration_.value -( burn.initial_time - time_base)));
+      previous_coast_duration_.value = burn.initial_time - time_base;
+      manoeuvre_warning_ =
+          "Manoeuvre corrected to be consistent with the flight plan";
+    } else {
+      manoeuvre_warning_ = "";
+    }
     reference_frame_selector_.SetFrameParameters(burn.frame);
     is_inertially_fixed_ = burn.is_inertially_fixed;
     duration_ = manoeuvre.duration;
@@ -292,6 +300,7 @@ class BurnEditor : ScalingRenderer {
   private double initial_mass_in_tonnes_;
 
   private bool first_time_rendering = true;
+  private string manoeuvre_warning_ = "";
 
   private const double Log10ΔvLowerRate = -3.0;
   private const double Log10ΔvUpperRate = 3.5;
