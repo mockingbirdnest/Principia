33138270f1a96f9b85fdcf75f01d65b617673749
pleroy <pascal.h.leroy@gmail.com>
2019-05-15 22:53:18 +0200

    API changes.

--- ksp_plugin/manœuvre.hpp
+++ ksp_plugin/manœuvre.hpp
@@ -41,15 +41,33 @@ using quantities::Variation;
 template<typename InertialFrame, typename Frame>
 class Manœuvre {
  public:
+  // Characterization of intensity.  All members for exactly one of the groups
+  // must be supplied.
+  struct Intensity {
+    // Group 1.
+    std::optional<Vector<double, Frenet<Frame>>> direction;
+    std::optional<Time> duration;
+    // Group 2.
+    std::optional<Velocity<Frenet<Frame>>> Δv;
+  };
+
+  // Characterization of timing.  All members for exactly one of the groups
+  // must be supplied.
+  struct Timing {
+    // Group 1.
+    std::optional<Instant> initial_time;
+    // Group 2.
+    std::optional<Instant> time_of_half_Δv;
+  };
+
   Manœuvre(Force const& thrust,
            Mass const& initial_mass,
            SpecificImpulse const& specific_impulse,
-           Vector<double, Frenet<Frame>> const& direction,
+           Intensity const& intensity,
+           Timing const& timing,
            not_null<std::shared_ptr<DynamicFrame<InertialFrame, Frame> const>>
                frame,
            bool is_inertially_fixed);
-  Manœuvre(Manœuvre&&) = default;
-  Manœuvre& operator=(Manœuvre&&) = default;
   virtual ~Manœuvre() = default;
 
   Force const& thrust() const;
@@ -61,39 +79,26 @@ class Manœuvre {
   // by the sum of the individual mass flows (where each mass flow is the
   // individual thrust divided by the exhaust velocity).
   SpecificImpulse const& specific_impulse() const;
-  Vector<double, Frenet<Frame>> const& direction() const;
   not_null<std::shared_ptr<DynamicFrame<InertialFrame, Frame> const>> frame()
       const;
+  // If true, the direction of the burn remains fixed in a nonrotating frame.
+  // Otherwise, the direction of the burn remains fixed in the Frenet frame of
+  // the trajectory.
+  bool is_inertially_fixed() const;
 
-  // Equivalent characterizations of intensity.  Only one of the mutators may be
-  // called, and only once.
-  Time duration() const;
-  void set_duration(Time const& duration);
-  Speed Δv() const;
-  void set_Δv(Speed const& Δv);
+  // All the fields returned by these selectors are filled.
+  Intensity const& intensity() const;
+  Timing const& timing() const;
 
   // Intensity must have been set.  Returns true if Δv is NaN or infinite.
   bool IsSingular() const;
 
-  // Equivalent characterizations of timing.  Only one of the mutators may be
-  // called, and only once.
-  Instant initial_time() const;
-  void set_initial_time(Instant const& initial_time);
-  // Intensity and timing must have been set.
-  Instant time_of_half_Δv() const;
-  // |Δv| or |duration| must have been set.
-  void set_time_of_half_Δv(Instant const& time_of_half_Δv);
-
   // Derived quantities.
   Variation<Mass> mass_flow() const;
-  // Intensity must have been set.
   Mass final_mass() const;
-  // Intensity must have been set.
   Time time_to_half_Δv() const;
-  // Intensity and timing must have been set.
   Instant final_time() const;
 
-  // Intensity and timing must have been set.
   // Returns true if and only if [initial_time, final_time] ⊆ ]begin, end[.
   bool FitsBetween(Instant const& begin, Instant const& end) const;
 
@@ -103,18 +108,11 @@ class Manœuvre {
   void set_coasting_trajectory(
       not_null<DiscreteTrajectory<InertialFrame> const*> trajectory);
 
-  // If true, the direction of the burn remains fixed in a nonrotating frame.
-  // Otherwise, the direction of the burn remains fixed in the Frenet frame of
-  // the trajectory.
-  bool is_inertially_fixed() const;
-
-  // Intensity, timing and coasting trajectory must have been set.  This
-  // manœuvre must be inertially fixed.
+  // This manœuvre must be inertially fixed.
   virtual Vector<double, InertialFrame> InertialDirection() const;
 
-  // Intensity, timing and coasting trajectory must have been set.  The result
-  // is valid until |*this| is destroyed.  This manœuvre must be inertially
-  // fixed.
+  // The result is valid until |*this| is destroyed.  This manœuvre must be
+  // inertially fixed.
   typename Ephemeris<InertialFrame>::IntrinsicAcceleration
   InertialIntrinsicAcceleration() const;
 
@@ -125,8 +123,7 @@ class Manœuvre {
   // Frenet frame at the beginning of the manœuvre.
   virtual OrthogonalMap<Frenet<Frame>, InertialFrame> FrenetFrame() const;
 
-  // Intensity and timing must have been set.  |coasting_trajectory| is neither
-  // written nor read.
+  // Note that |coasting_trajectory| is neither written nor read.
   void WriteToMessage(not_null<serialization::Manoeuvre*> message) const;
   static Manœuvre ReadFromMessage(
       serialization::Manoeuvre const& message,
@@ -150,9 +147,8 @@ class Manœuvre {
   Force const thrust_;
   Mass const initial_mass_;
   SpecificImpulse const specific_impulse_;
-  Vector<double, Frenet<Frame>> const direction_;
-  std::optional<Time> duration_;
-  std::optional<Instant> initial_time_;
+  Intensity const intensity_;
+  Timing const timing_;
   not_null<std::shared_ptr<DynamicFrame<InertialFrame, Frame> const>> frame_;
   bool const is_inertially_fixed_;
   DiscreteTrajectory<InertialFrame> const* coasting_trajectory_ = nullptr;
