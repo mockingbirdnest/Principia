2d37a8c100621687f38762a4dbd1f11a855c22ad
pleroy <pascal.h.leroy@gmail.com>
2019-06-12 21:59:23 +0200

    A unit test with assertions.

--- numerics/elliptic_integrals.hpp
+++ numerics/elliptic_integrals.hpp
@@ -3,8 +3,8 @@
 // This code is a straightforward translation in C++ of:
 // Fukushima, Toshio. (2018). xelbdj.txt: Fortran test driver for
 // "elbdj"/"relbdj", subroutines to compute the double/single precision general
-// incomplete elliptic integrals of all three kinds.
-// DOI: 10.13140/RG.2.2.11113.80489.
+// incomplete elliptic integrals of all three kinds,
+// DOI: 10.13140/RG.2.2.11113.80489, License: MIT.
 // Downloaded from:
 // https://www.researchgate.net/publication/322702514_xelbdjtxt_Fortran_test_driver_for_elbdjrelbdj_subroutines_to_compute_the_doublesingle_precision_general_incomplete_elliptic_integrals_of_all_three_kinds
 namespace principia {
