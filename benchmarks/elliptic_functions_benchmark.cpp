eef895bf1abcb84cd3099c9b8f690f5e753b8d73
pleroy <pascal.h.leroy@gmail.com>
2019-06-13 22:51:54 +0200

    A benchmark.

---
+++ benchmarks/elliptic_functions_benchmark.cpp
@@ -0,0 +1,45 @@
+﻿
+// .\Release\x64\benchmarks.exe --benchmark_repetitions=10 --benchmark_min_time=2 --benchmark_filter=Gscd  // NOLINT(whitespace/line_length)
+
+#include <random>
+#include <vector>
+
+#include "benchmark/benchmark.h"
+#include "numerics/elliptic_functions.hpp"
+#include "quantities/numbers.hpp"
+
+namespace principia {
+namespace numerics {
+
+void BM_Gscd(benchmark::State& state) {
+  constexpr int size = 100;
+
+  std::mt19937_64 random(42);
+  std::uniform_real_distribution<> distribution_u(-10.0, 10.0);
+  std::uniform_real_distribution<> distribution_mc(0.0, 1.0);
+  std::vector<double> us;
+  std::vector<double> mcs;
+  for (int i = 0; i < size; ++i) {
+    us.push_back(distribution_u(random));
+    mcs.push_back(distribution_mc(random));
+  }
+
+  while (state.KeepRunningBatch(size * size)) {
+    double s;
+    double c;
+    double d;
+    for (double const u : us) {
+      for (double const mc : mcs) {
+        Gscd(u, mc, s, c, d);
+      }
+    }
+    benchmark::DoNotOptimize(s);
+    benchmark::DoNotOptimize(c);
+    benchmark::DoNotOptimize(d);
+  }
+}
+
+BENCHMARK(BM_Gscd);
+
+}  // namespace numerics
+}  // namespace principia
