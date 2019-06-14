c88c4ccdb51fec1bdafe0df2be38226fd2a3adba
pleroy <pascal.h.leroy@gmail.com>
2019-06-14 09:21:58 +0200

    More linting.

--- numerics/elliptic_functions.hpp
+++ numerics/elliptic_functions.hpp
@@ -2,7 +2,7 @@
 
 // This code is a straightforward translation in C++ of:
 // Fukushima, Toshio. (2012). xgscd.txt (Fortran program package to compute the
-// Jacobian elliptic functions, sn(u|m), cn(u|m), dn(u|m)). 
+// Jacobian elliptic functions, sn(u|m), cn(u|m), dn(u|m)).
 // Downloaded from:
 // https://www.researchgate.net/publication/233903220_xgscdtxt_Fortran_program_package_to_compute_the_Jacobian_elliptic_functions_snum_cnum_dnum
 namespace principia {
