a1c7c5c3d4e82bc1ef6ad431caed81b7232c842f
pleroy <pascal.h.leroy@gmail.com>
2019-07-04 21:37:38 +0200

    Implementation.

--- numerics/elliptic_functions.hpp
+++ numerics/elliptic_functions.hpp
@@ -2,11 +2,12 @@
 
 #include "quantities/quantities.hpp"
 
-// This code is a straightforward translation in C++ of:
-// Fukushima, Toshio. (2012). xgscd.txt (Fortran program package to compute the
-// Jacobian elliptic functions, sn(u|m), cn(u|m), dn(u|m)).
-// Downloaded from:
+// This code is a derived from: Fukushima, Toshio. (2012). xgscd.txt (Fortran
+// program package to compute the Jacobian elliptic functions, sn(u|m), cn(u|m),
+// dn(u|m)).  Downloaded from:
 // https://www.researchgate.net/publication/233903220_xgscdtxt_Fortran_program_package_to_compute_the_Jacobian_elliptic_functions_snum_cnum_dnum
+// The original code has been translated into C++ and adapted to the needs of
+// this project.
 namespace principia {
 namespace numerics {
 namespace internal_elliptic_functions {
--- numerics/elliptic_integrals.hpp
+++ numerics/elliptic_integrals.hpp
@@ -2,13 +2,13 @@
 
 #include "quantities/quantities.hpp"
 
-// This code is a straightforward translation in C++ of:
-// Fukushima, Toshio. (2018). xelbdj.txt: Fortran test driver for
-// "elbdj"/"relbdj", subroutines to compute the double/single precision general
-// incomplete elliptic integrals of all three kinds,
-// DOI: 10.13140/RG.2.2.11113.80489, License: MIT.
-// Downloaded from:
+// This code is derived from: Fukushima, Toshio. (2018). xelbdj.txt: Fortran
+// test driver for "elbdj"/"relbdj", subroutines to compute the double/single
+// precision general incomplete elliptic integrals of all three kinds,
+// DOI: 10.13140/RG.2.2.11113.80489, License: MIT.  Downloaded from:
 // https://www.researchgate.net/publication/322702514_xelbdjtxt_Fortran_test_driver_for_elbdjrelbdj_subroutines_to_compute_the_doublesingle_precision_general_incomplete_elliptic_integrals_of_all_three_kinds
+// The original code has been translated into C++ and adapted to the needs of
+// this project.
 namespace principia {
 namespace numerics {
 
@@ -18,6 +18,13 @@ void FukushimaEllipticBDJ(quantities::Angle const& φ,
                           double& b,
                           double& d,
                           double& j);
+
+void EllipticEFΠ(quantities::Angle const& φ,
+                 double n,
+                 double mc,
+                 double& e,
+                 double& f,
+                 double& pi);
 
 double EllipticK(double mc);
 
