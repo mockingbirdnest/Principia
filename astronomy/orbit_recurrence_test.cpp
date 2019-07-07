599da106bfdb3db2e6c43f61da4de79266990c5e
Robin Leroy <egg.robin.leroy@gmail.com>
2019-07-07 23:27:02 +0200

    a test that passes

--- astronomy/orbit_recurrence_test.cpp
+++ astronomy/orbit_recurrence_test.cpp
@@ -19,8 +19,9 @@ using quantities::si::Degree;
 using quantities::si::Minute;
 using testing_utilities::AbsoluteError;
 using ::testing::AllOf;
-using ::testing::Property;
+using ::testing::Eq;
 using ::testing::Lt;
+using ::testing::Property;
 
 class OrbitRecurrenceTest : public ::testing::Test {
  protected:
@@ -62,12 +63,12 @@ TEST_F(OrbitRecurrenceTest, ClosestRecurrence) {
                     Property(&OrbitRecurrence::Cᴛₒ, 16)));
 
   // TOPEX/Poséidon, figure 11.16(b).
-  AngularFrequency const Ωʹ_tp = -2.076659 * Degree / Day;
+  AngularFrequency const Ωʹ_topex_poséidon = -2.076659 * Degree / Day;
   EXPECT_THAT(OrbitRecurrence::ClosestRecurrence(
-                  112.43 * Minute, Ωʹ_tp, *earth_, /*max_abs_Cᴛₒ=*/50),
-              AllOf(Property(&OrbitRecurrence::νₒ, 13),
-                    Property(&OrbitRecurrence::Dᴛₒ, -3),
-                    Property(&OrbitRecurrence::Cᴛₒ, 10)));
+          112.43 * Minute, Ωʹ_topex_poséidon, *earth_, /*max_abs_Cᴛₒ=*/50),
+      AllOf(Property(&OrbitRecurrence::νₒ, 13),
+            Property(&OrbitRecurrence::Dᴛₒ, -3),
+            Property(&OrbitRecurrence::Cᴛₒ, 10)));
 
   // Z-Earth, figure 11.16(c).
   EXPECT_THAT(OrbitRecurrence::ClosestRecurrence(
@@ -84,10 +85,32 @@ TEST_F(OrbitRecurrenceTest, ClosestRecurrence) {
 
 TEST_F(OrbitRecurrenceTest, EquatorialShift) {
   // Example 8.2: Метеор-3 № 7.
-  OrbitRecurrence const метеор_3_7_recurrence(13, +7, 71);
-  EXPECT_THAT(
-      AbsoluteError(-27.48 * Degree, метеор_3_7_recurrence.equatorial_shift()),
+  OrbitRecurrence const метеор_3_7(13, +7, 71);
+  EXPECT_THAT(AbsoluteError(-27.48 * Degree, метеор_3_7.equatorial_shift()),
       Lt(0.01 * Degree));
+}
+
+TEST_F(OrbitRecurrenceTest, GridInterval) {
+  // Example 11.8: TOPEX/Poséidon.
+  OrbitRecurrence const topex_poséidon(13, -3, 10);
+  EXPECT_THAT(AbsoluteError(2.8346 * Degree, topex_poséidon.grid_interval()),
+              Lt(0.0001 * Degree));
+  EXPECT_THAT(AbsoluteError(28.35 * Degree, topex_poséidon.base_interval()),
+              Lt(0.01 * Degree));
+}
+
+TEST_F(OrbitRecurrenceTest, Subcycle) {
+  // Example 11.9.
+  OrbitRecurrence const spot_5(14, +5, 26);
+  EXPECT_THAT(spot_5.subcycle(), Eq(5));
+  OrbitRecurrence const terra(15, -7, 16);
+  EXPECT_THAT(terra.subcycle(), Eq(7));
+  OrbitRecurrence const adeos_1(14, +11, 41);
+  EXPECT_THAT(adeos_1.subcycle(), Eq(15));
+  OrbitRecurrence const topex_poséidon(13, -3, 10);
+  EXPECT_THAT(topex_poséidon.subcycle(), Eq(3));
+  OrbitRecurrence const icesat(15, -22, 183);
+  EXPECT_THAT(icesat.subcycle(), Eq(25));
 }
 
 }  // namespace astronomy
