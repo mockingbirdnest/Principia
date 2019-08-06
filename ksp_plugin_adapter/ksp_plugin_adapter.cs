f6dcb1e53f9811a83bbefd91393bd4c5d77b297b
pleroy <pascal.h.leroy@gmail.com>
2019-08-06 22:47:02 +0200

    Do not show our UI in contexts where it's not useful.

--- ksp_plugin_adapter/ksp_plugin_adapter.cs
+++ ksp_plugin_adapter/ksp_plugin_adapter.cs
@@ -60,7 +60,10 @@ public partial class PrincipiaPluginAdapter
       "principia_override_version_check";
 
   private KSP.UI.Screens.ApplicationLauncherButton toolbar_button_;
+  // Whether the user has hidden the UI.
   private bool hide_all_gui_ = false;
+  // Whether we are in a scene/building where we wish to show our UI.
+  private bool in_principia_scene_ = true;
 
   private const int чебышёв_plotting_method_ = 2;
 
@@ -509,8 +512,32 @@ public partial class PrincipiaPluginAdapter
 
     GameEvents.onShowUI.Add(() => { hide_all_gui_ = false; });
     GameEvents.onHideUI.Add(() => { hide_all_gui_ = true; });
+    GameEvents.onGUIAdministrationFacilitySpawn.Add(() => {
+      in_principia_scene_ = false;
+    });
+    GameEvents.onGUIAdministrationFacilityDespawn.Add(() => {
+      in_principia_scene_ = true;
+    });
+    GameEvents.onGUIAstronautComplexSpawn.Add(() => {
+      in_principia_scene_ = false;
+    });
+    GameEvents.onGUIAstronautComplexDespawn.Add(() => {
+      in_principia_scene_ = true;
+    });
+    GameEvents.onGUIMissionControlSpawn.Add(() => {
+      in_principia_scene_ = false;
+    });
+    GameEvents.onGUIMissionControlDespawn.Add(() => {
+      in_principia_scene_ = true;
+    });
+    GameEvents.onGUIRnDComplexSpawn.Add(() => {
+      in_principia_scene_ = false;
+    });
+    GameEvents.onGUIRnDComplexDespawn.Add(() => {
+      in_principia_scene_ = true;
+    });
     // Timing0, -8008 on the script execution order page.
-    TimingManager.FixedUpdateAdd(TimingManager.TimingStage.ObscenelyEarly,
+        TimingManager.FixedUpdateAdd(TimingManager.TimingStage.ObscenelyEarly,
                                  ObscenelyEarly);
     // TimingPre, -101 on the script execution order page.
     TimingManager.FixedUpdateAdd(TimingManager.TimingStage.Precalc,
@@ -636,7 +663,7 @@ public partial class PrincipiaPluginAdapter
 
     if (hide_all_gui_) {
       clear_locks();
-    } else if (main_window_.Shown()) {
+    } else if (in_principia_scene_ && main_window_.Shown()) {
       render_windows();
     } else {
       clear_locks();
