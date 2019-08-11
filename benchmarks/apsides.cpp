208d0e7a3ca4c9616b4a67946004350a0dea50b0
pleroy <pascal.h.leroy@gmail.com>
2019-08-11 12:10:08 +0200

    Use the right reference frame and check that we get the expected number of apsides/nodes.

--- benchmarks/apsides.cpp
+++ benchmarks/apsides.cpp
@@ -1,14 +1,18 @@
 ﻿
+// .\Release\x64\benchmarks.exe --benchmark_repetitions=3 --benchmark_filter=Apsides --benchmark_min_time=30  // NOLINT(whitespace/line_length)
+
 #include "astronomy/epoch.hpp"
 #include "astronomy/frames.hpp"
 #include "astronomy/standard_product_3.hpp"
 #include "base/not_null.hpp"
 #include "benchmark/benchmark.h"
+#include "geometry/grassmann.hpp"
 #include "geometry/named_quantities.hpp"
 #include "integrators/embedded_explicit_runge_kutta_nyström_integrator.hpp"
 #include "integrators/methods.hpp"
 #include "integrators/symmetric_linear_multistep_integrator.hpp"
 #include "physics/apsides.hpp"
+#include "physics/body_centred_non_rotating_dynamic_frame.hpp"
 #include "physics/body_surface_dynamic_frame.hpp"
 #include "physics/ephemeris.hpp"
 #include "physics/solar_system.hpp"
@@ -17,6 +21,7 @@
 
 namespace principia {
 
+using astronomy::GCRS;
 using astronomy::ICRS;
 using astronomy::InfiniteFuture;
 using astronomy::ITRS;
@@ -24,6 +29,7 @@ using astronomy::StandardProduct3;
 using base::dynamic_cast_not_null;
 using base::not_null;
 using geometry::Position;
+using geometry::Vector;
 using integrators::EmbeddedExplicitRungeKuttaNyströmIntegrator;
 using integrators::methods::DormandالمكاوىPrince1986RKN434FM;
 using integrators::methods::QuinlanTremaine1990Order12;
@@ -54,7 +60,6 @@ class ApsidesBenchmark : public benchmark::Fixture {
       earth_ = dynamic_cast_not_null<OblateBody<ICRS> const*>(
           solar_system_2010_->massive_body(*ephemeris_, "Earth"));
       earth_trajectory_ = ephemeris_->trajectory(earth_);
-      BodySurfaceDynamicFrame<ICRS, ITRS> const itrs(ephemeris_.get(), earth_);
 
       StandardProduct3 const ilrsa_lageos2_sp3(
           SOLUTION_DIR / "astronomy" / "standard_product_3" /
@@ -67,11 +72,13 @@ class ApsidesBenchmark : public benchmark::Fixture {
           ilrsa_lageos2_sp3.orbit(lageos2_id).front();
       auto const begin = ilrsa_lageos2_trajectory_itrs->Begin();
       ephemeris_->Prolong(begin.time());
-      ilrsa_lageos2_trajectory_.Append(
+
+      BodySurfaceDynamicFrame<ICRS, ITRS> const itrs(ephemeris_.get(), earth_);
+      ilrsa_lageos2_trajectory_icrs_.Append(
           begin.time(),
           itrs.FromThisFrameAtTime(begin.time())(begin.degrees_of_freedom()));
       ephemeris_->FlowWithAdaptiveStep(
-          &ilrsa_lageos2_trajectory_,
+          &ilrsa_lageos2_trajectory_icrs_,
           Ephemeris<ICRS>::NoIntrinsicAcceleration,
           begin.time() + 1 * JulianYear,
           Ephemeris<ICRS>::AdaptiveStepParameters(
@@ -84,6 +91,16 @@ class ApsidesBenchmark : public benchmark::Fixture {
           /*max_ephemeris_steps=*/std::numeric_limits<std::int64_t>::max(),
           /*last_point_only=*/false);
 
+      BodyCentredNonRotatingDynamicFrame<ICRS, GCRS> const gcrs(
+          ephemeris_.get(), earth_);
+      for (auto it = ilrsa_lageos2_trajectory_icrs_.Begin();
+           it != ilrsa_lageos2_trajectory_icrs_.End();
+           ++it) {
+        ilrsa_lageos2_trajectory_gcrs_.Append(
+            it.time(),
+            gcrs.ToThisFrameAtTime(it.time())(it.degrees_of_freedom()));
+      }
+
       setup_done_ = true;
     }
   }
@@ -93,7 +110,8 @@ class ApsidesBenchmark : public benchmark::Fixture {
   std::unique_ptr<Ephemeris<ICRS>> ephemeris_;
   OblateBody<ICRS> const* earth_;
   ContinuousTrajectory<ICRS> const* earth_trajectory_;
-  DiscreteTrajectory<ICRS> ilrsa_lageos2_trajectory_;
+  DiscreteTrajectory<ICRS> ilrsa_lageos2_trajectory_icrs_;
+  DiscreteTrajectory<GCRS> ilrsa_lageos2_trajectory_gcrs_;
 };
 
 BENCHMARK_F(ApsidesBenchmark, ComputeApsides)(benchmark::State& state) {
@@ -101,22 +119,26 @@ BENCHMARK_F(ApsidesBenchmark, ComputeApsides)(benchmark::State& state) {
     DiscreteTrajectory<ICRS> apoapsides;
     DiscreteTrajectory<ICRS> periapsides;
     ComputeApsides(*earth_trajectory_,
-                   ilrsa_lageos2_trajectory_.Begin(),
-                   ilrsa_lageos2_trajectory_.End(),
+                   ilrsa_lageos2_trajectory_icrs_.Begin(),
+                   ilrsa_lageos2_trajectory_icrs_.End(),
                    apoapsides,
                    periapsides);
+    CHECK_EQ(2364, apoapsides.Size());
+    CHECK_EQ(2365, periapsides.Size());
   }
 }
 
 BENCHMARK_F(ApsidesBenchmark, ComputeNodes)(benchmark::State& state) {
   for (auto _ : state) {
-    DiscreteTrajectory<ICRS> ascending;
-    DiscreteTrajectory<ICRS> descending;
-    ComputeNodes(ilrsa_lageos2_trajectory_.Begin(),
-                 ilrsa_lageos2_trajectory_.End(),
-                 earth_->polar_axis(),
+    DiscreteTrajectory<GCRS> ascending;
+    DiscreteTrajectory<GCRS> descending;
+    ComputeNodes(ilrsa_lageos2_trajectory_gcrs_.Begin(),
+                 ilrsa_lageos2_trajectory_gcrs_.End(),
+                 Vector<double, GCRS>({0, 0, 1}),
                  ascending,
                  descending);
+    CHECK_EQ(2365, ascending.Size());
+    CHECK_EQ(2365, descending.Size());
   }
 }
 
