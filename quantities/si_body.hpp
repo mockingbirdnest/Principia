16f53d3fea353722cf7b3d5f43a55f4ccc5cb1bd
pleroy <pascal.h.leroy@gmail.com>
2019-08-17 12:59:39 +0200

    Formatting of dimensions.

--- quantities/si_body.hpp
+++ quantities/si_body.hpp
@@ -8,6 +8,35 @@ namespace quantities {
 namespace si {
 
 template<typename D>
+std::string Format() {
+  auto const format_unit = [](std::string const& name,
+                              int const exponent) -> std::string {
+    switch (exponent) {
+      case 0:
+        return "";
+        break;
+      case 1:
+        return " " + name;
+      default:
+        return " " + name + "^" + std::to_string(exponent);
+    }
+  };
+
+  // This string has a leading space if it's not empty.
+  auto const format =
+      format_unit("m", D::Length) + format_unit("kg", D::Mass) +
+      format_unit("s", D::Time) + format_unit("A", D::Current) +
+      format_unit("K", D::Temperature) + format_unit("mol", D::Amount) +
+      format_unit("cd", D::LuminousIntensity) + format_unit("rad", D::Angle);
+
+  if (format.empty()) {
+    return format;
+  } else {
+    return format.substr(1, format.size() - 1);
+  }
+}
+
+template<typename D>
 constexpr Quantity<D> Yotta(Quantity<D> base) {
   return 1e24 * base;
 }
