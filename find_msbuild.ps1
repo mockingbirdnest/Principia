b1533c8be77c7795dc057948716d32e7546856ad
pleroy <pascal.h.leroy@gmail.com>
2019-08-19 22:25:10 +0200

    Migrate to Visual Studio 16.2.2.

--- find_msbuild.ps1
+++ find_msbuild.ps1
@@ -1,5 +1,12 @@
-$version = "16.2.0"
-$preview = "2"
+$version = "16.2.2"
+$preview = ""
+if ($preview.length -gt 0) {
+  $description = "version $version preview $preview"
+  $path = "VisualStudioPreview/$version-pre.$preview."
+} else {
+  $description = "version $version"
+  $path = "VisualStudio/$version+"
+}
 
 $vswhere = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
 $names = &$vswhere                        `
@@ -16,12 +23,12 @@ $msbuildpaths = &$vswhere                 `
 
 $i = 0;
 foreach ($name in $names) {
-  if ($name.startswith("VisualStudioPreview/$version-pre.$preview.")) {
+  if ($name.startswith("$path")) {
     return ($msbuildpaths | select-object -index $i)
   }
   ++$i
 }
 
 write-error(
-    "Could not find Visual Studio version $version preview $preview;" +
+    "Could not find Visual Studio $description;" +
     " found the following versions:`n$([string]::join("`n", $names))")
