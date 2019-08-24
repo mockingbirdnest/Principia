a5cb9e024c9158b5bed794ae1374926ab0e2cf19
pleroy <pascal.h.leroy@gmail.com>
2019-08-24 14:08:08 +0200

    Do not persist the message and shown state of dialogs.  Make the dialogs multiline.

--- ksp_plugin_adapter/dialog.cs
+++ ksp_plugin_adapter/dialog.cs
@@ -12,22 +12,23 @@ internal class Dialog : UnsupervisedWindowRenderer, IConfigNode {
   protected override string Title => "Principia";
 
   protected override void RenderWindow(int window_id) {
-    using (new UnityEngine.GUILayout.VerticalScope())
-    {
-      UnityEngine.GUILayout.TextArea(message_ ?? "SHOW WITHOUT MESSAGE");
+    using (new UnityEngine.GUILayout.VerticalScope()) {
+      UnityEngine.GUILayout.TextArea(message_ ?? "SHOW WITHOUT MESSAGE",
+                                     style: Style.Multiline(
+                                         UnityEngine.GUI.skin.textArea));
     }
     UnityEngine.GUI.DragWindow();
   }
 
   public new void Load(ConfigNode node) {
+    // Whether a dialog is shown or not should not be a persisted property.
+    // It's convenient to save and restore show_ in the base class, but we
+    // override this behaviour here.  For the same reason, we don't persist the
+    // message.
+    bool saved_shown = Shown();
     base.Load(node);
-    message_ = node.GetAtMostOneValue("message");
-  }
-
-  public new void Save(ConfigNode node) {
-    base.Save(node);
-    if (message_ != null) {
-      node.SetValue("message", message_, createIfNotFound : true);
+    if (saved_shown != Shown()) {
+      Toggle();
     }
   }
 
