484d46e3404d0a09e9ac932d4a0e657b81dac359
Robin Leroy <egg.robin.leroy@gmail.com>
2019-08-27 19:27:31 +0200

    mark PlayerTest.*Debug as secular

--- journal/player_test.cpp
+++ journal/player_test.cpp
@@ -84,7 +84,7 @@ TEST_F(PlayerTest, DISABLED_Benchmarks) {
   benchmark::RunSpecifiedBenchmarks();
 }
 
-TEST_F(PlayerTest, DISABLED_Debug) {
+TEST_F(PlayerTest, DISABLED_SECULAR_Debug) {
   // An example of how journaling may be used for debugging.  You must set
   // |path| and fill the |method_in| and |method_out_return| protocol buffers.
   std::string path =
