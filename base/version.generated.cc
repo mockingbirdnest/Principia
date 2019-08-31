4e342a4c283017357468f370d50ccc0112188a8d
pleroy <pascal.h.leroy@gmail.com>
2019-08-31 16:15:54 +0200

    Generated files before the change.

---
+++ base/version.generated.cc
@@ -0,0 +1,12 @@
+﻿
+#include "base/version.hpp"
+
+namespace principia {
+namespace base {
+
+char const BuildDate[] = "2019-08-29T19:59:33Z";
+char const Version[] =
+    u8"2019083011-del Ferro-7-g65ca62cf90c1aabafee9337df722ffaa926c91ac";
+
+}  // namespace base
+}  // namespace principia
