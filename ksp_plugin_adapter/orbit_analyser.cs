ce944ac0dedd5d7ec346958be56d5410f592ce0a
Robin Leroy <egg.robin.leroy@gmail.com>
2019-09-19 01:09:44 +0200

    improve elements presentation

--- ksp_plugin_adapter/orbit_analyser.cs
+++ ksp_plugin_adapter/orbit_analyser.cs
@@ -7,6 +7,92 @@ using System.Text;
 namespace principia {
 namespace ksp_plugin_adapter {
 
+internal static class Formatters {
+  // Displays an interval as midpoint±half-width.
+  public static string FormatInterval(this Interval interval) {
+    double half_width = (interval.max - interval.min) / 2;
+    double midpoint = interval.min + (interval.max - interval.min) / 2;
+    int fractional_digits =
+        Math.Max(0, 1 - (int)Math.Floor(Math.Log10(half_width)));
+    string format = $"N{fractional_digits}";
+    string formatted_midpoint = midpoint.ToString(format, Culture.culture);
+    string formatted_half_width = half_width.ToString(format, Culture.culture);
+    return $"{formatted_midpoint}±{formatted_half_width}";
+  }
+  
+  // Displays an interval of lengths as midpoint±half-width, in km if the
+  // half-width is 100 m or more.
+  public static string FormatLengthInterval(this Interval interval) {
+    double half_width = (interval.max - interval.min) / 2;
+    double midpoint = interval.min + (interval.max - interval.min) / 2;
+    string unit = "m";
+    if (half_width >= 100) {
+      half_width *= 0.01;
+      midpoint *= 0.01;
+      unit = "km";
+    }
+    int fractional_digits =
+        Math.Max(0, 1 - (int)Math.Floor(Math.Log10(half_width)));
+    string format = $"N{fractional_digits}";
+    string formatted_midpoint = midpoint.ToString(format, Culture.culture);
+    string formatted_half_width = half_width.ToString(format, Culture.culture);
+    return $"{formatted_midpoint}±{formatted_half_width} {unit}";
+  }
+
+  // Displays an interval of angles (given in radians) as midpoint°±half-width°,
+  // or the string "(precesses)" if the half-width exceeds 180°.
+  public static string FormatAngleInterval(this Interval interval) {
+    double half_width = (interval.max - interval.min) / 2;
+    double midpoint = interval.min + (interval.max - interval.min) / 2;
+    if (half_width > Math.PI) {
+      return "(precesses)";
+    }
+    double degree = Math.PI / 180;
+    int fractional_digits =
+        Math.Max(0, 1 - (int)Math.Floor(Math.Log10(half_width / degree)));
+    string format = $"N{fractional_digits}";
+    string formatted_midpoint =
+        (midpoint / degree).ToString(format, Culture.culture);
+    string formatted_half_width =
+        (half_width / degree).ToString(format, Culture.culture);
+    return $"{formatted_midpoint}°±{formatted_half_width}°";
+  }
+  
+  // Formats a duration, omitting leading components if they are 0, and omitting
+  // leading 0s on the days;
+  public static string FormatDuration(this double seconds) {
+    if (double.IsNaN(seconds)) {
+      return seconds.ToString();
+    }
+    var span = TimeSpan.FromSeconds(seconds);
+    int days = GameSettings.KERBIN_TIME ? span.Days * 4 + span.Hours / 6
+                                        : span.Days;
+    int hours = GameSettings.KERBIN_TIME ? span.Hours % 6
+                                         : span.Hours;
+    var components = new List<string>();
+    if (days > 0) {
+      components.Add(GameSettings.KERBIN_TIME ? $"{days} d6" : $"{days} d");
+    }
+    if (components.Count > 0 || hours > 0) {
+      components.Add(
+          GameSettings.KERBIN_TIME ? $"{hours:0} h" : $"{hours:00} h");
+    }
+    if (components.Count > 0 || span.Minutes > 0) {
+      components.Add($"{span.Minutes:00} min");
+    }
+    components.Add($"{span.Seconds + span.Milliseconds / 1000m:00.0} s");
+    return string.Join(" ", components.ToArray());
+  }
+
+  public static string FormatAngularFrequency(this double radians_per_second) {
+    double degree = Math.PI / 180;
+    double day = GameSettings.KERBIN_TIME ? 6 * 60 * 60 : 24 * 60 * 60;
+    string day_unit = GameSettings.KERBIN_TIME ? "d6" : "d";
+    double degrees_per_day = radians_per_second / (degree / day);
+    return $"{degrees_per_day:N2}°/{day_unit}";
+  }
+}
+
 internal class OrbitAnalyser : SupervisedWindowRenderer {
   public OrbitAnalyser(PrincipiaPluginAdapter adapter)
       : base(adapter, UnityEngine.GUILayout.MinWidth(0)) {
@@ -20,6 +106,7 @@ internal class OrbitAnalyser : SupervisedWindowRenderer {
   }
 
   protected override string Title => "Orbit analysis";
+
   protected override void RenderWindow(int window_id) {
     using (new UnityEngine.GUILayout.VerticalScope(GUILayoutWidth(8))) {
       Vessel vessel = FlightGlobals.ActiveVessel;
@@ -29,6 +116,11 @@ internal class OrbitAnalyser : SupervisedWindowRenderer {
         return;
       }
       CelestialBody primary = adapter_.plotting_frame_selector_.selected_celestial;
+
+      mission_duration_.Render(enabled : true);
+      UnityEngine.GUILayout.Label(
+          $"Analysing orbit of {vessel.name} with respect to {primary.NameWithArticle()}...");
+
       OrbitAnalysis analysis = plugin.VesselRefreshAnalysis(
           vessel.id.ToString(),
           primary.flightGlobalsIndex,
@@ -42,57 +134,43 @@ internal class OrbitAnalyser : SupervisedWindowRenderer {
         revolutions_per_cycle_ = analysis.recurrence.number_of_revolutions;
         days_per_cycle_ = analysis.recurrence.cto;
       }
-
-      mission_duration_.Render(enabled : true);
-      UnityEngine.GUILayout.Label(
-          $"Analysing orbit of {vessel.name} with respect to {primary.NameWithArticle()}...");
       UnityEngine.GUILayout.HorizontalScrollbar(
           value      : 0,
           size       : analysis.progress_percentage,
           leftValue  : 0,
           rightValue : 100);
 
-      Style.HorizontalLine();
-      UnityEngine.GUILayout.Label(
-          $@"Orbit of {vessel.name} with respect to {
-            FlightGlobals.Bodies[
-                analysis.primary_index].NameWithArticle()} over {
-            FormatDuration(analysis.mission_duration)}");
-      var elements = analysis.elements;
-      UnityEngine.GUILayout.Label(
-          analysis.elements_has_value
-              ? $@"({analysis.mission_duration /
-                     elements.anomalistic_period:N0} anomalistic rev., {
-                     analysis.mission_duration / elements.nodal_period:N0} nodal rev., {
-                     analysis.mission_duration / elements.sidereal_period:N0} sidereal rev.)".ToString(Culture.culture)
-              : "(mission duration is less than one sidereal revolution)");
-      Style.HorizontalLine();
-      UnityEngine.GUILayout.Label("Orbital elements");
-      string element_string(string s) => analysis.elements_has_value ? s : "—";
-      UnityEngine.GUILayout.Label(
-          "Sidereal period: " +
-          element_string(
-              FormatDuration(elements.sidereal_period)));
-      UnityEngine.GUILayout.Label(
-          "Nodal period: " +
-          element_string(
-              FormatDuration(elements.nodal_period)));
-      UnityEngine.GUILayout.Label(
-          "Anomalistic period: " +
-          element_string(
-              FormatDuration(analysis.elements.anomalistic_period)));
-      // TODO(egg): represent the intervals.
-      UnityEngine.GUILayout.Label(
-          $"Semimajor axis: {element_string(FormatLengthInterval(elements.mean_semimajor_axis))}");
-      UnityEngine.GUILayout.Label(
-          $"Eccentricity: {element_string(FormatInterval(elements.mean_eccentricity))}");
-      UnityEngine.GUILayout.Label(
-          $"Inclination: {element_string(FormatAngleInterval(elements.mean_inclination))}");
-      UnityEngine.GUILayout.Label(
-          $"Longitude of ascending node: {element_string(FormatAngleInterval(elements.mean_longitude_of_ascending_nodes) ?? "(precesses)")}");
-      UnityEngine.GUILayout.Label(
-          $"Argument of periapsis: {element_string(FormatAngleInterval(elements.mean_argument_of_periapsis) ?? "(precesses)")}");
+      OrbitalElements? elements = null;
+      double mission_duration = analysis.mission_duration;
+      if (analysis.elements_has_value) {
+        elements = analysis.elements;
+      }
+      primary = FlightGlobals.Bodies[analysis.primary_index];
 
+      Style.HorizontalLine();
+      string parenthetical = "";
+      if (elements.HasValue) {
+        int anomalistic_revolutions =
+            (int)(mission_duration / elements.Value.anomalistic_period);
+        int nodal_revolutions =
+            (int)(mission_duration / elements.Value.nodal_period);
+        int sidereal_revolutions =
+            (int)(mission_duration / elements.Value.sidereal_period);
+        parenthetical = $@"{
+            anomalistic_revolutions:N0} anomalistic rev., {
+            nodal_revolutions:N0} nodal rev., {
+            sidereal_revolutions:N0} sidereal rev.".ToString(Culture.culture);
+      } else {
+        parenthetical = "mission duration is shorter than one sidereal revolution";
+      }
+      string analysis_description =
+          $"Orbit of {vessel.name} with respect to {primary.NameWithArticle()}" +
+          $"\nover {mission_duration.FormatDuration()}\n({parenthetical})";
+      UnityEngine.GUILayout.Label(
+          analysis_description,
+          Style.Multiline(UnityEngine.GUI.skin.label));
+      Style.HorizontalLine();
+      RenderOrbitalElements(elements);
       Style.HorizontalLine();
       OrbitRecurrence recurrence = analysis.recurrence;
       string recurrence_string<T>(T s) => analysis.recurrence_has_value ? s.ToString() : "—";
@@ -102,6 +180,7 @@ internal class OrbitAnalyser : SupervisedWindowRenderer {
         autodetect_recurrence_ = UnityEngine.GUILayout.Toggle(autodetect_recurrence_, "Auto-detect");
       }
       using (new UnityEngine.GUILayout.HorizontalScope()) {
+        UnityEngine.GUILayout.Label("Cycle:");
         string text = UnityEngine.GUILayout.TextField(
             $"{revolutions_per_cycle_}");
         if (int.TryParse(text, out int revolutions_per_cycle) &&
@@ -131,83 +210,81 @@ internal class OrbitAnalyser : SupervisedWindowRenderer {
       }
       double half_width_km(Interval interval) => (interval.max - interval.min) / 2 * primary.Radius / 1000;
       var equatorial_crossings = analysis.ground_track.equatorial_crossings;
-      UnityEngine.GUILayout.Label($"Ascending pass: {FormatAngleInterval(equatorial_crossings.longitudes_reduced_to_ascending_pass)} (±{half_width_km(equatorial_crossings.longitudes_reduced_to_ascending_pass):N0} km)");
-      UnityEngine.GUILayout.Label($"Descending pass: {FormatAngleInterval(equatorial_crossings.longitudes_reduced_to_descending_pass)} (±{half_width_km(equatorial_crossings.longitudes_reduced_to_descending_pass):N0} km)");
+      UnityEngine.GUILayout.Label($"Ascending pass: {equatorial_crossings.longitudes_reduced_to_ascending_pass.FormatAngleInterval()} (±{half_width_km(equatorial_crossings.longitudes_reduced_to_ascending_pass):N0} km)");
+      UnityEngine.GUILayout.Label($"Descending pass: {equatorial_crossings.longitudes_reduced_to_descending_pass.FormatAngleInterval()} (±{half_width_km(equatorial_crossings.longitudes_reduced_to_descending_pass):N0} km)");
     }
     UnityEngine.GUI.DragWindow();
   }
 
-  // Displays an interval as midpoint±half-width.
-  static private string FormatInterval(Interval interval) {
-    double half_width = (interval.max - interval.min) / 2;
-    double midpoint = interval.min + (interval.max - interval.min) / 2;
-    int fractional_digits =
-        Math.Max(0, 1 - (int)Math.Floor(Math.Log10(half_width)));
-    string format = $"N{fractional_digits}";
-    string formatted_midpoint = midpoint.ToString(format, Culture.culture);
-    string formatted_half_width = half_width.ToString(format, Culture.culture);
-    return $"{formatted_midpoint}±{formatted_half_width}";
-  }
-  
-  // Displays an interval of lengths as midpoint±half-width, in km if the
-  // half-width is 100 m or more.
-  static private string FormatLengthInterval(Interval interval) {
-    double half_width = (interval.max - interval.min) / 2;
-    double midpoint = interval.min + (interval.max - interval.min) / 2;
-    string unit = "m";
-    if (half_width >= 100) {
-      half_width *= 0.01;
-      midpoint *= 0.01;
-      unit = "km";
-    }
-    int fractional_digits =
-        Math.Max(0, 1 - (int)Math.Floor(Math.Log10(half_width)));
-    string format = $"N{fractional_digits}";
-    string formatted_midpoint = midpoint.ToString(format, Culture.culture);
-    string formatted_half_width = half_width.ToString(format, Culture.culture);
-    return $"{formatted_midpoint}±{formatted_half_width} {unit}";
-  }
-
-  // Displays an interval of angles (given in radians) as midpoint°±half-width°,
-  // or null if the half-width exceeds 180°.
-  static private string FormatAngleInterval(Interval interval) {
-    double half_width = (interval.max - interval.min) / 2;
-    double midpoint = interval.min + (interval.max - interval.min) / 2;
-    if (half_width > Math.PI) {
-      return null;
-    }
-    double degree = Math.PI / 180;
-    int fractional_digits =
-        Math.Max(0, 1 - (int)Math.Floor(Math.Log10(half_width / degree)));
-    string format = $"N{fractional_digits}";
-    string formatted_midpoint =
-        (midpoint / degree).ToString(format, Culture.culture);
-    string formatted_half_width =
-        (half_width / degree).ToString(format, Culture.culture);
-    return $"{formatted_midpoint}°±{formatted_half_width}°";
-  }
-
-  // Formats a duration, omitting leading components if they are 0, and omitting
-  // leading 0s on the days;
-  private static string FormatDuration(double seconds) {
-    var span = TimeSpan.FromSeconds(seconds);
-    int days = GameSettings.KERBIN_TIME ? span.Days * 4 + span.Hours / 6
-                                        : span.Days;
-    int hours = GameSettings.KERBIN_TIME ? span.Hours % 6
-                                         : span.Hours;
-    var components = new List<string>();
-    if (days > 0) {
-      components.Add(GameSettings.KERBIN_TIME ? $"{days} d6" : $"{days} d");
-    }
-    if (components.Count > 0 || hours > 0) {
-      components.Add(
-          GameSettings.KERBIN_TIME ? $"{hours:0} h" : $"{hours:00} h");
-    }
-    if (components.Count > 0 || span.Minutes > 0) {
-      components.Add($"{span.Minutes:00} min");
-    }
-    components.Add($"{span.Seconds + span.Milliseconds / 1000m:00.0} s");
-    return string.Join(" ", components.ToArray());
+  private void RenderOrbitalElements(OrbitalElements? elements) {
+      UnityEngine.GUILayout.Label("Orbital elements");
+      using (new UnityEngine.GUILayout.HorizontalScope()) {
+        UnityEngine.GUILayout.Label("Sidereal period:");
+        UnityEngine.GUILayout.Label(
+            elements?.sidereal_period.FormatDuration() ?? em_dash,
+            Style.RightAligned(UnityEngine.GUI.skin.label));
+      }
+      using (new UnityEngine.GUILayout.HorizontalScope()) {
+        UnityEngine.GUILayout.Label("Nodal period:");
+        UnityEngine.GUILayout.Label(
+            elements?.nodal_period.FormatDuration() ?? em_dash,
+            Style.RightAligned(UnityEngine.GUI.skin.label));
+      }
+      using (new UnityEngine.GUILayout.HorizontalScope()) {
+        UnityEngine.GUILayout.Label("Anomalistic period:");
+        UnityEngine.GUILayout.Label(
+            elements?.anomalistic_period.FormatDuration() ?? em_dash,
+            Style.RightAligned(UnityEngine.GUI.skin.label));
+      }
+      using (new UnityEngine.GUILayout.HorizontalScope()) {
+        UnityEngine.GUILayout.Label(
+            "Semimajor axis:");
+        UnityEngine.GUILayout.Label(
+            elements?.mean_semimajor_axis.FormatLengthInterval() ?? em_dash,
+            Style.RightAligned(UnityEngine.GUI.skin.label));
+      }
+      using (new UnityEngine.GUILayout.HorizontalScope()) {
+        UnityEngine.GUILayout.Label(
+            "Eccentricity:");
+        UnityEngine.GUILayout.Label(
+            elements?.mean_eccentricity.FormatLengthInterval() ?? em_dash,
+            Style.RightAligned(UnityEngine.GUI.skin.label));
+      }
+      using (new UnityEngine.GUILayout.HorizontalScope()) {
+        UnityEngine.GUILayout.Label(
+            "Inclination:");
+        UnityEngine.GUILayout.Label(
+            elements?.mean_inclination.FormatAngleInterval() ?? em_dash,
+            Style.RightAligned(UnityEngine.GUI.skin.label));
+      }
+      using (new UnityEngine.GUILayout.HorizontalScope()) {
+        using (new UnityEngine.GUILayout.VerticalScope()) {
+          using (new UnityEngine.GUILayout.HorizontalScope()) {
+            UnityEngine.GUILayout.Label(
+                "Longitude of ascending node:");
+            UnityEngine.GUILayout.Label(
+                elements?.mean_longitude_of_ascending_nodes.
+                    FormatAngleInterval() ?? em_dash,
+                Style.RightAligned(UnityEngine.GUI.skin.label));
+          }
+          using (new UnityEngine.GUILayout.HorizontalScope()) {
+            UnityEngine.GUILayout.Label(
+                "Argument of periapsis:");
+            UnityEngine.GUILayout.Label(
+                elements?.mean_argument_of_periapsis.FormatAngleInterval() ??
+                    em_dash,
+                Style.RightAligned(UnityEngine.GUI.skin.label));
+          }
+        }
+        using (new UnityEngine.GUILayout.HorizontalScope()) {
+          UnityEngine.GUILayout.Label(
+              "Nodal precession:");
+          UnityEngine.GUILayout.Label(
+              elements?.nodal_precession.FormatAngularFrequency() ??
+                  em_dash,
+              Style.RightAligned(UnityEngine.GUI.skin.label));
+        }
+      }
   }
 
   static private bool TryParseMissionDuration(string str, out double value) {
@@ -218,6 +295,8 @@ internal class OrbitAnalyser : SupervisedWindowRenderer {
     value = ts.TotalSeconds;
     return true;
   }
+
+  private const string em_dash = "—";
 
   private IntPtr plugin => adapter_.Plugin();
 
@@ -233,7 +312,7 @@ internal class OrbitAnalyser : SupervisedWindowRenderer {
           FlightPlanner.FormatPositiveTimeSpan(
               TimeSpan.FromSeconds(value)),
       parser           : TryParseMissionDuration) {
-      value = 30 * 60
+      value = 7 * 24 * 60 * 60
   };
   private bool autodetect_recurrence_ = true;
   private int revolutions_per_cycle_ = 1;
