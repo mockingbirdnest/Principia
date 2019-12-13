dfd6205827a33163a847cefdefe5cf3ab5ea45af
pleroy <pascal.h.leroy@gmail.com>
2019-12-13 18:50:06 +0100

    A case macro.

--- integrators/integrators_body.hpp
+++ integrators/integrators_body.hpp
@@ -14,29 +14,142 @@
 #include "integrators/symmetric_linear_multistep_integrator.hpp"
 #include "integrators/symplectic_runge_kutta_nyström_integrator.hpp"
 
-#define PRINCIPIA_CASE_SLMS(kind, method)                      \
+#define PRINCIPIA_CASE_SLMS(kind, method, action)              \
   case serialization::FixedStepSizeIntegrator::kind:           \
-    return SymmetricLinearMultistepIntegrator<methods::method, \
-                                              typename ODE::Position>()
+    action(SymmetricLinearMultistepIntegrator<methods::method, \
+                                              typename ODE::Position>)
 
 // We do not deserialize an SPRK per se, but only when it is converted to an
 // SRKN.  The reason is that an SPRK is for a different kind of equation than
 // an SRKN, so the two would return different types.  If we ever need to do
 // this we will need to specialize ReadFromMessage somehow.
-#define PRINCIPIA_CASE_SPRK(kind, method)                                 \
-  case serialization::FixedStepSizeIntegrator::kind:                      \
-    do {                                                                  \
-      CHECK(message.has_composition_method());                            \
-      return SprkAsSrknDeserializer<                                      \
-          ODE,                                                            \
-          methods::method,                                                \
-          methods::method::first_same_as_last>::ReadFromMessage(message); \
+#define PRINCIPIA_CASE_SPRK(kind, method, action)                             \
+  case serialization::FixedStepSizeIntegrator::kind:                          \
+    do {                                                                      \
+      CHECK(message.has_composition_method());                                \
+      action(SprkAsSrknDeserializer<                                          \
+             ODE,                                                             \
+             methods::method,                                                 \
+             methods::method::first_same_as_last>::ReadFromMessage(message)); \
     } while (false)
 
 #define PRINCIPIA_CASE_SRKN(kind, method)                         \
   case serialization::FixedStepSizeIntegrator::kind:              \
-    return SymplecticRungeKuttaNyströmIntegrator<methods::method, \
-                                                 typename ODE::Position>()
+    action(SymplecticRungeKuttaNyströmIntegrator<methods::method, \
+                                                 typename ODE::Position>())
+
+#define PRINCIPIA_CASES(slms_action, sprk_action, srkn_action)                 \
+  PRINCIPIA_CASE_SPRK(BLANES_MOAN_2002_S6,                                     \
+                      BlanesMoan2002S6,                                        \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(BLANES_MOAN_2002_S10,                                    \
+                      BlanesMoan2002S10,                                       \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SRKN(BLANES_MOAN_2002_SRKN_6B,                                \
+                      BlanesMoan2002SRKN6B,                                    \
+                      srkn_action);                                            \
+  PRINCIPIA_CASE_SRKN(BLANES_MOAN_2002_SRKN_11B,                               \
+                      BlanesMoan2002SRKN11B,                                   \
+                      srkn_action);                                            \
+  PRINCIPIA_CASE_SRKN(BLANES_MOAN_2002_SRKN_14A,                               \
+                      BlanesMoan2002SRKN14A,                                   \
+                      srkn_action);                                            \
+  PRINCIPIA_CASE_SPRK(CANDY_ROZMUS_1991_FOREST_RUTH_1990,                      \
+                      CandyRozmus1991ForestRuth1990,                           \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(MCLACHLAN_1995_S2,                                       \
+                      McLachlan1995S2,                                         \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(MCLACHLAN_1995_S4,                                       \
+                      McLachlan1995S4,                                         \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(MCLACHLAN_1995_S5,                                       \
+                      McLachlan1995S5,                                         \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SRKN(MCLACHLAN_1995_SB3A_4,                                   \
+                      McLachlan1995SB3A4,                                      \
+                      srkn_action);                                            \
+  PRINCIPIA_CASE_SRKN(MCLACHLAN_1995_SB3A_5,                                   \
+                      McLachlan1995SB3A5,                                      \
+                      srkn_action);                                            \
+  PRINCIPIA_CASE_SPRK(MCLACHLAN_1995_SS5,                                      \
+                      McLachlan1995SS5,                                        \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(MCLACHLAN_1995_SS9,                                      \
+                      McLachlan1995SS9,                                        \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(MCLACHLAN_1995_SS15,                                     \
+                      McLachlan1995SS15,                                       \
+                      sprk_action;                                             \
+  PRINCIPIA_CASE_SPRK(MCLACHLAN_1995_SS17,                                     \
+                      McLachlan1995SS17,                                       \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(MCLACHLAN_ATELA_1992_ORDER_2_OPTIMAL,                    \
+                      McLachlanAtela1992Order2Optimal,                         \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(MCLACHLAN_ATELA_1992_ORDER_3_OPTIMAL,                    \
+                      McLachlanAtela1992Order3Optimal,                         \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SRKN(MCLACHLAN_ATELA_1992_ORDER_4_OPTIMAL,                    \
+                      McLachlanAtela1992Order4Optimal,                         \
+                      srkn_action);                                            \
+  PRINCIPIA_CASE_SRKN(MCLACHLAN_ATELA_1992_ORDER_5_OPTIMAL,                    \
+                      McLachlanAtela1992Order5Optimal,                         \
+                      srkn_action);                                            \
+  PRINCIPIA_CASE_SPRK(NEWTON_DELAMBRE_STORMER_VERLET_LEAPFROG,                 \
+                      NewtonDelambreStørmerVerletLeapfrog,                     \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SRKN(OKUNBOR_SKEEL_1994_ORDER_6_METHOD_13,                    \
+                      OkunborSkeel1994Order6Method13,                          \
+                      srkn_action);                                            \
+  PRINCIPIA_CASE_SLMS(QUINLAN_1999_ORDER_8A,                                   \
+                      Quinlan1999Order8A,                                      \
+                      slms_action);                                            \
+  PRINCIPIA_CASE_SLMS(QUINLAN_1999_ORDER_8B,                                   \
+                      Quinlan1999Order8B,                                      \
+                      slms_action);                                            \
+  PRINCIPIA_CASE_SLMS(QUINLAN_TREMAINE_1990_ORDER_8,                           \
+                      QuinlanTremaine1990Order8,                               \
+                      slms_action);                                            \
+  PRINCIPIA_CASE_SLMS(QUINLAN_TREMAINE_1990_ORDER_10,                          \
+                      QuinlanTremaine1990Order10,                              \
+                      slms_action);                                            \
+  PRINCIPIA_CASE_SLMS(QUINLAN_TREMAINE_1990_ORDER_12,                          \
+                      QuinlanTremaine1990Order12,                              \
+                      slms_action);                                            \
+  PRINCIPIA_CASE_SLMS(QUINLAN_TREMAINE_1990_ORDER_14,                          \
+                      QuinlanTremaine1990Order14,                              \
+                      slms_action);                                            \
+  PRINCIPIA_CASE_SPRK(RUTH_1983,                                               \
+                      Ruth1983,                                                \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(SUZUKI_1990,                                             \
+                      鈴木1990,                                                 \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(YOSHIDA_1990_ORDER_6A,                                   \
+                      吉田1990Order6A,                                          \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(YOSHIDA_1990_ORDER_6B,                                   \
+                      吉田1990Order6B,                                          \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(YOSHIDA_1990_ORDER_6C,                                   \
+                      吉田1990Order6C,                                          \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(YOSHIDA_1990_ORDER_8A,                                   \
+                      吉田1990Order8A,                                          \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(YOSHIDA_1990_ORDER_8B,                                   \
+                      吉田1990Order8B,                                          \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(YOSHIDA_1990_ORDER_8C,                                   \
+                      吉田1990Order8C,                                          \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(YOSHIDA_1990_ORDER_8D,                                   \
+                      吉田1990Order8D,                                          \
+                      sprk_action);                                            \
+  PRINCIPIA_CASE_SPRK(YOSHIDA_1990_ORDER_8E,                                   \
+                      吉田1990Order8E,                                          \
+                      sprk_action);
 
 namespace principia {
 namespace integrators {
