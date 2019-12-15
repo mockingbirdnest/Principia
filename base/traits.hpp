b263049343ba7168bb26201c31237f2ae96780dc
pleroy <pascal.h.leroy@gmail.com>
2019-12-15 13:09:50 +0100

    After egg's review.

--- base/traits.hpp
+++ base/traits.hpp
@@ -7,18 +7,18 @@ namespace base {
 namespace internal_traits {
 
 template<typename, typename = void, typename = void>
-struct has_templated_read_message : std::false_type {};
+struct has_sfinae_read_from_message : std::false_type {};
 
 template<typename T>
-struct has_templated_read_message<
+struct has_sfinae_read_from_message<
     T, std::void_t<decltype(&T::template ReadFromMessage<>)>>
     : std::true_type {};
 
 template<typename, typename = void, typename = void>
-struct has_vanilla_read_message : std::false_type {};
+struct has_unconditional_read_from_message : std::false_type {};
 
 template<typename T>
-struct has_vanilla_read_message<
+struct has_unconditional_read_from_message<
     T, std::void_t<decltype(&T::ReadFromMessage)>>
     : std::true_type {};
 
@@ -28,8 +28,8 @@ struct has_vanilla_read_message<
 // ReadFromMessage.
 template<typename T>
 inline constexpr bool is_serializable_v =
-    internal_traits::has_templated_read_message<T>::value ||
-    internal_traits::has_vanilla_read_message<T>::value;
+    internal_traits::has_sfinae_read_from_message<T>::value ||
+    internal_traits::has_unconditional_read_from_message<T>::value;
 
 }  // namespace base
 }  // namespace principia
