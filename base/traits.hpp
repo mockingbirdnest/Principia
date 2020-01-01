bc6dcef7260befbeee10e4814bbf8da252a7e6ee
Pascal Leroy <phl+personal@google.com>
2020-01-01 19:14:53 +0100

    New traits.

--- base/traits.hpp
+++ base/traits.hpp
@@ -6,6 +6,21 @@ namespace principia {
 namespace base {
 namespace internal_traits {
 
+template<template<typename...> typename T, typename U>
+struct is_instance_of : std::false_type {};
+
+template<template<typename...> class T, typename... Args>
+struct is_instance_of<T, T<Args...>> : std::true_type {};
+
+
+template<template<typename...> typename T,
+  template<typename...> typename U>
+struct is_same_template : std::false_type {};
+
+template<template<typename...> typename T>
+struct is_same_template<T, T> : std::true_type {};
+
+
 template<typename, typename = void, typename = void>
 struct has_sfinae_read_from_message : std::false_type {};
 
@@ -23,6 +38,16 @@ struct has_unconditional_read_from_message<
     : std::true_type {};
 
 }  // namespace internal_traits
+
+// True if and only if U is an instance of T.
+template<template<typename...> typename T, typename U>
+inline constexpr bool is_instance_of_v =
+    internal_traits::is_instance_of<T, U>::value;
+
+// True if and only if T and U are the same template.
+template<template<typename...> typename T, template<typename...> typename U>
+inline constexpr bool is_same_template_v =
+    internal_traits::is_same_template<T, U>::value;
 
 // True if and only if T has a (possibly templated) static member function named
 // ReadFromMessage.
