4fccaec6639efd78cda9378277ef2e528cdcfe29
pleroy <pascal.h.leroy@gmail.com>
2020-03-23 21:47:00 +0100

    Switch to 1.9.1.

--- ksp_plugin_adapter/gl_lines.cs
+++ ksp_plugin_adapter/gl_lines.cs
@@ -149,7 +149,7 @@ internal static class GLLines {
     get {
       if (line_material_ == null) {
         line_material_ = new UnityEngine.Material(
-#if KSP_VERSION_1_8_1
+#if KSP_VERSION_1_9_1
             UnityEngine.Shader.Find("KSP/Particles/Additive"));
 #elif KSP_VERSION_1_7_3
             UnityEngine.Shader.Find("Particles/Additive"));
