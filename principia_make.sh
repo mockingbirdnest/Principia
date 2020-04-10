e484eccbcf647bf4a8b55fd9c5c0f602dc604a15
Robin Leroy <egg.robin.leroy@gmail.com>
2020-04-10 16:00:18 +0200

    comments

--- principia_make.sh
+++ principia_make.sh
@@ -1,3 +1,6 @@
+# It seems that protoc really wants its dependencies to be in /usr/local/lib.
+# In some setups, e.g., Azure pipelines, this does not work, so we need to help
+# it find its dynamic libraries.
 if [ "${AGENT_OS?}" == "Darwin" ]; then
   install_name_tool \
       -change /usr/local/lib/libprotobuf.17.dylib ./deps/protobuf/src/.libs/libprotobuf.17.dylib \
