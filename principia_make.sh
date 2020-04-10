6d117bc7e93720e64ffc90d89ec6c84afa31bd8d
Robin Leroy <egg.robin.leroy@gmail.com>
2020-04-10 17:59:27 +0200

    formatting

--- principia_make.sh
+++ principia_make.sh
@@ -3,13 +3,19 @@
 # it find its dynamic libraries.
 if [ "${AGENT_OS?}" == "Darwin" ]; then
   install_name_tool \
-      -change /usr/local/lib/libprotobuf.17.dylib ./deps/protobuf/src/.libs/libprotobuf.17.dylib \
+      -change \ 
+          /usr/local/lib/libprotobuf.17.dylib \
+          ./deps/protobuf/src/.libs/libprotobuf.17.dylib \
       deps/protobuf/src/.libs/libprotoc.17.dylib
   install_name_tool \
-      -change /usr/local/lib/libprotobuf.17.dylib ./deps/protobuf/src/.libs/libprotobuf.17.dylib \
+      -change \
+          /usr/local/lib/libprotobuf.17.dylib \
+          ./deps/protobuf/src/.libs/libprotobuf.17.dylib \
       deps/protobuf/src/.libs/protoc
   install_name_tool \
-      -change /usr/local/lib/libprotoc.17.dylib ./deps/protobuf/src/.libs/libprotoc.17.dylib \
+      -change \
+          /usr/local/lib/libprotoc.17.dylib \
+          ./deps/protobuf/src/.libs/libprotoc.17.dylib \
       deps/protobuf/src/.libs/protoc
 elif [ "${AGENT_OS?}" == "Linux" ]; then
   export LD_LIBRARY_PATH="./deps/protobuf/src/.libs:$LD_LIBRARY_PATH"
