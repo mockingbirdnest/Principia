e7a3c5247197f50139e9a5b7a17bc17deeac8d14
pleroy <pascal.h.leroy@gmail.com>
2020-04-11 22:47:58 +0200

    Marshalers for optional fields.

--- ksp_plugin_adapter/optional_marshaler.cs
+++ ksp_plugin_adapter/optional_marshaler.cs
@@ -5,11 +5,49 @@ using System.Runtime.InteropServices;
 namespace principia {
 namespace ksp_plugin_adapter {
 
-internal class OptionalMarshaler<T> : MonoMarshaler where T : struct {
-  // In addition to implementing the |ICustomMarshaler| interface, custom
-  // marshalers must implement a static method called |GetInstance| that accepts
-  // a |String| as a parameter and has a return type of |ICustomMarshaler|,
-  // see https://goo.gl/wwmBTa.
+internal abstract class OptionalMarshaler<T> : MonoMarshaler where T : struct {
+  public override void CleanUpNativeDataImplementation(IntPtr native_data) {
+    Marshal.FreeHGlobal(native_data);
+  }
+
+  public object MarshalNativeToManagedImplementation(IntPtr native_data) {
+    if (native_data == IntPtr.Zero) {
+      return null;
+    } else {
+      return Marshal.PtrToStructure(native_data, typeof(T));
+    }
+  }
+}
+
+internal class OwnershipTransferOptionalMarshaler<T> : OptionalMarshaler<T>
+    where T : struct {
+  public static ICustomMarshaler GetInstance(string s) {
+    return instance_;
+  }
+
+  public override void CleanUpNativeDataImplementation(IntPtr native_data) {
+    throw Log.Fatal(
+        "use NoOwnershipTransferOptionalMarshaler for in parameters");
+  }
+
+  public override IntPtr MarshalManagedToNativeImplementation(
+      object managed_object) {
+    throw Log.Fatal(
+        "use NoOwnershipTransferOptionalMarshaler for in parameters");
+  }
+
+  public override object MarshalNativeToManaged(IntPtr native_data) {
+    var result = MarshalNativeToManagedImplementation(native_data);
+    Interface.DeleteVoid(ref native_data);
+    return result;
+  }
+
+  private static readonly OwnershipTransferOptionalMarshaler<T> instance_ =
+      new OwnershipTransferOptionalMarshaler<T>();
+}
+
+internal class NoOwnershipTransferOptionalMarshaler<T> : OptionalMarshaler<T>
+    where T : struct {
   public static ICustomMarshaler GetInstance(string s) {
     return instance_;
   }
@@ -46,15 +84,11 @@ internal class OptionalMarshaler<T> : MonoMarshaler where T : struct {
   }
 
   public override object MarshalNativeToManaged(IntPtr native_data) {
-    if (native_data == IntPtr.Zero) {
-      return null;
-    } else {
-      return Marshal.PtrToStructure(native_data, typeof(T));
-    }
+    return MarshalNativeToManagedImplementation(native_data);
   }
 
-  private static readonly OptionalMarshaler<T> instance_ =
-      new OptionalMarshaler<T>();
+  private static readonly NoOwnershipTransferOptionalMarshaler<T> instance_ =
+      new NoOwnershipTransferOptionalMarshaler<T>();
 }
 
 }  // namespace ksp_plugin_adapter
