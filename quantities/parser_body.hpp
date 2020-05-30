32d88ef77cea6f5176e8ea71761837b4006e4118
pleroy <pascal.h.leroy@gmail.com>
2020-05-30 16:10:51 +0200

    Indent.

--- quantities/parser_body.hpp
+++ quantities/parser_body.hpp
@@ -221,8 +221,8 @@ inline Unit ParseQuotientUnit(std::string const& s) {
     int const first_nonblank = s.find_first_not_of(' ', last_slash + 1);
     CHECK_NE(std::string::npos, first_nonblank);
     size_t const last_nonblank = last_slash == 0
-                                  ? std::string::npos
-                                  : s.find_last_not_of(' ', last_slash - 1);
+                                     ? std::string::npos
+                                     : s.find_last_not_of(' ', last_slash - 1);
     auto const left = last_nonblank == std::string::npos
                           ? Unit(1.0)
                           : ParseQuotientUnit(s.substr(0, last_nonblank + 1));
