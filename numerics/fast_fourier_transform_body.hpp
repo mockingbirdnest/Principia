c0dd007172b8510c766f9b934cd14c9a01ba7242
pleroy <pascal.h.leroy@gmail.com>
2020-07-09 22:49:15 +0200

    Some implementation code.

--- numerics/fast_fourier_transform_body.hpp
+++ numerics/fast_fourier_transform_body.hpp
@@ -2,14 +2,28 @@
 
 #include "numerics/fast_fourier_transform.hpp"
 
+#include "quantities/si.hpp"
+
 namespace principia {
 namespace numerics {
 namespace internal_fast_fourier_transform {
 
+using quantities::si::Unit;
+
 template<typename Container, typename Scalar, int size_>
 FastFourierTransform<Container, Scalar, size_>::FastFourierTransform(
     typename Container::const_iterator begin,
-    typename Container::const_iterator end) {}
+    typename Container::const_iterator end) {
+  int i = 0;
+
+  // Type decay and promotion to complex.
+  for (auto it = begin; it != end; ++it, ++i) {
+    transform_[i] = *it / si::Unit<Scalar>;
+  }
+  CHECK_EQ(size_, i);
+
+
+}
 
 template<typename Container, typename Scalar, int size_>
 std::array<Square<Scalar>, size_>
