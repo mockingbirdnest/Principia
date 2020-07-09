cd5147a20658f3a53a24baa08b954e09c160907b
pleroy <pascal.h.leroy@gmail.com>
2020-07-09 23:16:45 +0200

    After egg's review.

--- base/bits.hpp
+++ base/bits.hpp
@@ -1,12 +1,13 @@
+﻿
 #pragma once
 
 namespace principia {
 namespace base {
 
-// Floor log2 of n, or 0 for n = 0.  8 -> 3, 7 -> 2.
+// Floor log2 of n, or 0 for n = 0.  8 ↦ 3, 7 ↦ 2.
 constexpr int FloorLog2(int n);
 
-// Greatest power of 2 less than or equal to n.  8 -> 8, 7 -> 4.
+// Greatest power of 2 less than or equal to n.  8 ↦ 8, 7 ↦ 4.
 constexpr int PowerOf2Le(int n);
 
 }  // namespace base
