6bfea61e24b8d5634b4ec327f7e06f5709dfcaba
pleroy <pascal.h.leroy@gmail.com>
2020-07-14 20:09:46 +0200

    Next release is Galois.

--- ksp_plugin_adapter/main_window.cs
+++ ksp_plugin_adapter/main_window.cs
@@ -5,10 +5,10 @@ namespace ksp_plugin_adapter {
 
 internal class MainWindow : VesselSupervisedWindowRenderer {
   // Update this section before each release.
-  private const string next_release_name_ = "Gallai";
-  private const int next_release_lunation_number_ = 254;
+  private const string next_release_name_ = "Galois";
+  private const int next_release_lunation_number_ = 255;
   private readonly DateTimeOffset next_release_date_ =
-      new DateTimeOffset(2020, 07, 20, 17, 32, 00, TimeSpan.Zero);
+      new DateTimeOffset(2020, 08, 19, 02, 41, 00, TimeSpan.Zero);
 
   public MainWindow(PrincipiaPluginAdapter adapter,
                     FlightPlanner flight_planner,
