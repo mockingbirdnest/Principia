515915c62e9700c7adf199b554871deb9113d2c4
pleroy <pascal.h.leroy@gmail.com>
2020-08-15 14:37:23 +0200

    UnboundedVector and uninitialization.

--- numerics/unbounded_arrays_body.hpp
+++ numerics/unbounded_arrays_body.hpp
@@ -11,6 +11,50 @@ namespace internal_unbounded_arrays {
 
 using base::uninitialized;
 
+template<class T>
+template<class U, class... Args>
+inline void uninitialized_allocator<T>::construct(U* const p, Args&&... args) {}
+
+template<typename Scalar>
+UnboundedVector<Scalar>::UnboundedVector(int const size)
+    : data_(size, Scalar{}) {}
+
+template<typename Scalar>
+UnboundedVector<Scalar>::UnboundedVector(int const size, uninitialized_t)
+    : data_(size)  {}
+
+template<typename Scalar>
+void UnboundedVector<Scalar>::Extend(int const extra_size) {
+  DCHECK_LE(0, extra_size);
+  data_.resize(data_.size() + extra_size, Scalar{});
+}
+
+template<typename Scalar>
+void UnboundedVector<Scalar>::Extend(int const extra_size, uninitialized_t) {
+  DCHECK_LE(0, extra_size);
+  data_.resize(data_.size() + extra_size);
+}
+
+template<typename Scalar>
+int UnboundedVector<Scalar>::size() const {
+  return data_.size();
+}
+
+template<typename Scalar>
+bool UnboundedVector<Scalar>::operator==(UnboundedVector const& right) const {
+  return data_ == right.data_;
+}
+
+template<typename Scalar>
+Scalar& UnboundedVector<Scalar>::operator[](int const index) {
+  return data_[index];
+}
+
+template<typename Scalar>
+Scalar const& UnboundedVector<Scalar>::operator[](int const index) const {
+  return data_[index];
+}
+
 template<typename Scalar>
 UnboundedLowerTriangularMatrix<Scalar>::UnboundedLowerTriangularMatrix(
     int const rows)
@@ -22,19 +66,19 @@ UnboundedLowerTriangularMatrix<Scalar>::UnboundedLowerTriangularMatrix(
     int const rows,
     uninitialized_t)
     : rows_(rows),
-      data_(rows_ * (rows_ + 1) / 2, Scalar{uninitialized}) {}
+      data_(rows_ * (rows_ + 1) / 2) {}
 
 template<typename Scalar>
-void UnboundedLowerTriangularMatrix<Scalar>::Extend(int const rows) {
-  rows_ += rows;
+void UnboundedLowerTriangularMatrix<Scalar>::Extend(int const extra_rows) {
+  rows_ += extra_rows;
   data_.resize(rows_ * (rows_ + 1) / 2, Scalar{});
 }
 
 template<typename Scalar>
-void UnboundedLowerTriangularMatrix<Scalar>::Extend(int const rows,
+void UnboundedLowerTriangularMatrix<Scalar>::Extend(int const extra_rows,
                                                     uninitialized_t) {
-  rows_ += rows;
-  data_.resize(rows_ * (rows_ + 1) / 2, Scalar{uninitialized});
+  rows_ += extra_rows;
+  data_.resize(rows_ * (rows_ + 1) / 2);
 }
 
 template<typename Scalar>
