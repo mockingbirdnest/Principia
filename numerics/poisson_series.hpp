e6c50ac88dac4b9ab1208171254e17df5ac3503c
pleroy <pascal.h.leroy@gmail.com>
2020-08-20 08:35:05 +0200

    Lint.

--- numerics/poisson_series.hpp
+++ numerics/poisson_series.hpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <map>
+#include <string>
 #include <vector>
 
 #include "geometry/interval.hpp"
--- numerics/polynomial.hpp
+++ numerics/polynomial.hpp
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <algorithm>
+#include <string>
 #include <tuple>
 #include <utility>
 
