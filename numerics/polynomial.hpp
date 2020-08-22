46e0777fc243a771d2bcb25b7d0d8e96c029d183
Pascal Leroy <phl+personal@google.com>
2020-08-22 16:38:47 +0200

    Move the template defaults.

--- numerics/polynomial.hpp
+++ numerics/polynomial.hpp
@@ -27,7 +27,7 @@ FORWARD_DECLARE_FUNCTION_FROM(
     mathematica,
     TEMPLATE(typename Value, typename Argument, int degree_,
              template<typename, typename, int> class Evaluator,
-             typename OptionalExpressIn) std::string,
+             typename OptionalExpressIn = std::nullopt_t) std::string,
     ToMathematica,
     (numerics::
          PolynomialInMonomialBasis<Value, Argument, degree_, Evaluator> const&
