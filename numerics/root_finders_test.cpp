344d441017dfbbd5e4bbd43af8f9047fc923e924
Robin Leroy <egg.robin.leroy@gmail.com>
2020-09-23 20:50:43 +0200

    after pleroy’s review of the test

--- numerics/root_finders_test.cpp
+++ numerics/root_finders_test.cpp
@@ -73,7 +73,9 @@ TEST_F(RootFindersTest, WilkinsGuFunction) {
   double const b = 2;
 
   // We first construct the sequence of points X at which Brent will be made to
-  // evaluate the function; see section 4.1.
+  // evaluate the function; see section 4.1. We denote the sequence of points by
+  // X as in section 4.2; however, we number them upward starting at 0, instead
+  // of downward starting at m = |X|.
   std::vector<double> X;
   // The indices i such that Brent will perform a bisection at X[i].
   std::set<int> expected_bisections;
@@ -104,18 +106,23 @@ TEST_F(RootFindersTest, WilkinsGuFunction) {
   }
 
   // [WG13], equation (5).
-  auto const s = [](double xa, double xc, double xb, double fa) {
-    return fa * (xb - xc) / (xa - xc);
-  };
+  auto const s =
+      [](double const xa, double const xc, double const xb, double const fa) {
+        return fa * (xb - xc) / (xa - xc);
+      };
 
   // [WG13], equations (7) and (9).
-  auto const q =
-      [](double xa, double xd, double xc, double xb, double fa, double fb) {
-        double const α = (xa - xd) * fb + (xd - xb) * fa;
-        double const β = (xb - xd) * Pow<2>(fa) + (xd - xa) * Pow<2>(fb);
-        double const γ = fa * fb * (fb - fa) * (xc - xd);
-        return -2 * γ / (β + Sqrt(Pow<2>(β) - 4 * α * γ));
-      };
+  auto const q = [](double const xa,
+                    double const xd,
+                    double const xc,
+                    double const xb,
+                    double const fa,
+                    double const fb) {
+    double const α = (xa - xd) * fb + (xd - xb) * fa;
+    double const β = (xb - xd) * Pow<2>(fa) + (xd - xa) * Pow<2>(fb);
+    double const γ = fa * fb * (fb - fa) * (xc - xd);
+    return -2 * γ / (β + Sqrt(Pow<2>(β) - 4 * α * γ));
+  };
 
   // Capture everything but the function itself by copy so we have some
   // confidence that the function is pure.
@@ -123,11 +130,15 @@ TEST_F(RootFindersTest, WilkinsGuFunction) {
   // If non-null, |*evaluations| is incremented when the function is called.
   // If |expect_brent_calls| is true, this function checks that x is an element
   // of the expected sequence X.
+  // For x in X, this function returns f(x) as defined at the end of
+  // section 4.2.
   std::function<double(double, int*, bool)> f =
       [&f, s, q, a, b, X, expected_bisections](
           double const x,
           int* const evaluations,
           bool const expect_brent_calls) -> double {
+    // “For the initial interval to be valid, we choose f (a) = ϵ for some
+    // ϵ < 0.”
     double const f_a = -100;
     if (evaluations != nullptr) {
       ++*evaluations;
@@ -140,30 +151,33 @@ TEST_F(RootFindersTest, WilkinsGuFunction) {
     }
     // [WG13] define f only on X. We extend it as a step function, returning the
     // value for the element of X nearest to the given x.
-    int j;
+    int k;
     double min_Δx = std::numeric_limits<double>::infinity();
     for (int i = 1; i < X.size() - 1; ++i) {
       double const Δx = std::abs(x - X[i]);
       if (Δx < min_Δx) {
         min_Δx = Δx;
-        j = i;
+        k = i;
       }
     }
     if (expect_brent_calls) {
-      EXPECT_THAT(x, AlmostEquals(X[j], 0)) << j;
+      EXPECT_THAT(x, AlmostEquals(X[k], 0)) << k;
     }
 
-    if (expected_bisections.count(j) != 0) {
+    if (expected_bisections.count(k) != 0) {
+      // “Since we are bisecting the function regardless of its value at X[k] ,
+      // we may choose an arbitrary positive value for f (X[k]), such as 100.”
       return 100;
     }
     // Note that the recursion does not count as an evaluation, and is not
     // subject to expectations.
-    return q(a,
-             X[j + 1],
-             X[j],
-             X[j - 1],
-             f_a,
-             f(X[j - 1], nullptr, /*expect_brent_calls=*/false));
+    return q(
+        a,
+        X[k + 1],
+        X[k],
+        X[k - 1],
+        f_a,
+        f(X[k - 1], /*evaluations=*/nullptr, /*expect_brent_calls=*/false));
   };
 
   int evaluations = 0;
@@ -173,7 +187,7 @@ TEST_F(RootFindersTest, WilkinsGuFunction) {
                   },
                   a,
                   b),
-      AlmostEquals(a, 2));
+              AlmostEquals(a, 2));
   EXPECT_THAT(evaluations, Eq(1304));
 
   evaluations = 0;
