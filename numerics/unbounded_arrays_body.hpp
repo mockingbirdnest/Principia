84dac9a83a4716016df30b3b3c0e4f816b7201d1
pleroy <pascal.h.leroy@gmail.com>
2020-10-04 17:33:39 +0200

    Upper triangular matrix.

--- numerics/unbounded_arrays_body.hpp
+++ numerics/unbounded_arrays_body.hpp
@@ -158,6 +158,138 @@ Scalar const* UnboundedLowerTriangularMatrix<Scalar>::operator[](
 }
 
 template<typename Scalar>
+UnboundedUpperTriangularMatrix<Scalar>::UnboundedUpperTriangularMatrix(
+    int const columns)
+    : columns_(columns),
+      data_(columns_ * (columns_ + 1) / 2, Scalar{}) {}
+
+template<typename Scalar>
+UnboundedUpperTriangularMatrix<Scalar>::UnboundedUpperTriangularMatrix(
+    int const columns,
+    uninitialized_t)
+    : columns_(columns),
+      data_(columns_ * (columns_ + 1) / 2) {}
+
+template<typename Scalar>
+UnboundedUpperTriangularMatrix<Scalar>::UnboundedUpperTriangularMatrix(
+    std::initializer_list<Scalar> const& data)
+    : columns_(
+          static_cast<int>(std::lround((-1 + Sqrt(8 * data.size())) * 0.5))),
+      data_(Transpose(data,
+                      /*current_columns=*/0,
+                      /*extra_columns=*/columns_)) {
+  DCHECK_EQ(data_.size(), columns_ * (columns_ + 1) / 2);
+}
+
+template<typename Scalar>
+void UnboundedUpperTriangularMatrix<Scalar>::Extend(int const extra_columns) {
+  columns_ += extra_columns;
+  data_.resize(columns_ * (columns_ + 1) / 2, Scalar{});
+}
+
+template<typename Scalar>
+void UnboundedUpperTriangularMatrix<Scalar>::Extend(int const extra_columns,
+                                                    uninitialized_t) {
+  columns_ += extra_columns;
+  data_.resize(columns_ * (columns_ + 1) / 2);
+}
+
+template<typename Scalar>
+void UnboundedUpperTriangularMatrix<Scalar>::Extend(
+    std::initializer_list<Scalar> const& data) {
+  int const new_columns =
+      static_cast<int>(std::lround((-1 + Sqrt(8 * data.size())) * 0.5));
+  data_ = Transpose(data,
+                    /*current_columns=*/columns_,
+                    /*extra_columns=*/new_columns - columns_);
+  columns_ = new_columns;
+  DCHECK_EQ(data_.size(), columns_ * (columns_ + 1) / 2);
+}
+
+template<typename Scalar>
+void UnboundedUpperTriangularMatrix<Scalar>::EraseToEnd(
+    int const begin_column_index) {
+  columns_ = begin_row_index;
+  data_.erase(data_.begin() + begin_row_index * (begin_row_index + 1) / 2,
+              data_.end());
+}
+
+template<typename Scalar>
+int UnboundedUpperTriangularMatrix<Scalar>::columns() const {
+  return columns_;
+}
+
+template<typename Scalar>
+int UnboundedUpperTriangularMatrix<Scalar>::dimension() const {
+  return data_.size();
+}
+
+template<typename Scalar>
+bool UnboundedUpperTriangularMatrix<Scalar>::operator==(
+    UnboundedUpperTriangularMatrix const& right) const {
+  return columns_ == right.columns_ && data_ == right.data_;
+}
+
+template<typename Scalar>
+Scalar* UnboundedUpperTriangularMatrix<Scalar>::operator[](int const index) {
+  DCHECK_LT(index, columns_);
+  return &data_[index * (index + 1) / 2];
+}
+
+template<typename Scalar>
+Scalar const* UnboundedUpperTriangularMatrix<Scalar>::operator[](
+    int const index) const {
+  DCHECK_LT(index, columns_);
+  return &data_[index * (index + 1) / 2];
+}
+
+template<typename Scalar>
+auto
+UnboundedUpperTriangularMatrix<Scalar>::Transpose(
+    std::initializer_list<Scalar> const& data,
+    int const current_columns,
+    int const extra_columns) ->
+  std::vector<Scalar, uninitialized_allocator<Scalar>> {
+  // |data| is a trapezoidal slice at the end of the matrix.  This is
+  // inconvenient to index, so we start by constructing a rectangular array with
+  // |extra_columns| columns and |current_columns + extra_columns| rows padded
+  // with zeros.
+  std::vector<Scalar, uninitialized_allocator<Scalar>> padded;
+  {
+    padded.reserve(2 * data.size());  // An overestimate.
+    int row = 0;
+    int column = 0;
+    for (int i = 0; i < data.size(); ++i) {
+      if (row <= current_columns + column) {
+        padded.push_back(data[i]);
+      } else {
+        padded.emplace_back();
+      }
+      ++column;
+      if (column == extra_columns) {
+        column = 0;
+        ++row;
+      }
+    }
+  }
+
+  // Scan the padded array by column and append the part above the diagonal to
+  // the result.
+  std::vector<Scalar, uninitialized_allocator<Scalar>> result;
+  result.reserve(data.size());
+  int const number_of_rows = current_columns + extra_columns;
+  for (int column = 0; column < extra_columns; ++column) {
+    for (int row = 0; row < number_of_rows; ++row) {
+      if (row <= current_columns + column) {
+        result.push_back(padded[row * extra_columns + column]);
+      }
+    }
+  }
+
+  return result;
+}
+
+template<typename Scalar>
 std::ostream& operator<<(std::ostream& out,
                          UnboundedVector<Scalar> const& vector) {
   std::stringstream s;
@@ -175,6 +307,20 @@ std::ostream& operator<<(std::ostream& out,
   std::stringstream s;
   // TODO(phl): Triangular printout.
   s << "rows: " << matrix.rows_ << " ";
+  for (int i = 0; i < matrix.data_.size(); ++i) {
+    s << (i == 0 ? "{" : "") << matrix.data_[i]
+      << (i == matrix.data_.size() - 1 ? "}" : ", ");
+  }
+  out << s.str();
+  return out;
+}
+
+template<typename Scalar>
+std::ostream& operator<<(std::ostream& out,
+                         UnboundedUpperTriangularMatrix<Scalar> const& matrix) {
+  std::stringstream s;
+  // TODO(phl): Triangular printout.
+  s << "columns: " << matrix.columns_ << " ";
   for (int i = 0; i < matrix.data_.size(); ++i) {
     s << (i == 0 ? "{" : "") << matrix.data_[i]
       << (i == matrix.data_.size() - 1 ? "}" : ", ");
