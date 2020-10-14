ec1e395448792104612e606a83a90e261595b612
pleroy <pascal.h.leroy@gmail.com>
2020-10-14 15:38:17 +0200

    Adjust number of iterations for Linux and macOS.

--- numerics/quadrature_test.cpp
+++ numerics/quadrature_test.cpp
@@ -127,7 +127,7 @@ TEST_F(QuadratureTest, Sin10) {
                   /*max_relative_error=*/std::numeric_limits<double>::epsilon(),
                   /*max_points=*/std::nullopt),
               AlmostEquals(ʃf, 2, 20));
-  EXPECT_THAT(evaluations, AnyOf(Eq(65537), Eq(262145)));
+  EXPECT_THAT(evaluations, AnyOf(Eq(32769), Eq(65537), Eq(262145), Eq(524289)));
 }
 
 }  // namespace quadrature
