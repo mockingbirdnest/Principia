6b7c44b887280ea75aecabc11ff4e9b0ebc2bb80
pleroy <pascal.h.leroy@gmail.com>
2020-11-22 19:29:13 +0100

    Just say no to flakiness.

--- base/jthread_test.cpp
+++ base/jthread_test.cpp
@@ -8,23 +8,21 @@ namespace principia {
 namespace base {
 
 TEST(JThreadTest, StopOnDestruction) {
-  auto f = [](stop_token st, int* const value, bool* const observed_stop) {
+  auto f = [](stop_token st, int value, bool* const observed_stop) {
     while (!st.stop_requested()) {
-      std::cout << ++(*value) << ' ' << std::flush;
+      std::cout << ++value << ' ' << std::flush;
       absl::SleepFor(absl::Milliseconds(10));
     }
     std::cout << std::endl;
     *observed_stop = true;
   };
 
-  int i = 5;
   bool observed_stop = false;
   {
-    jthread thread(f, &i, &observed_stop);
+    jthread thread(f, 5, &observed_stop);
     absl::SleepFor(absl::Milliseconds(100));
   }
   EXPECT_TRUE(observed_stop);
-  EXPECT_GT(i, 14);  // May flake.
 }
 
 TEST(JThreadTest, RequestStop) {
