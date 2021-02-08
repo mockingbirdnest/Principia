e8c172d29d15bb41bceaa0df7591e775b6cbecbb
pleroy <pascal.h.leroy@gmail.com>
2021-02-08 19:45:17 +0100

    New fingerprints and local error analysis test.

--- astronomy/solar_system_fingerprints.hpp
+++ astronomy/solar_system_fingerprints.hpp
@@ -11,10 +11,10 @@ constexpr std::int64_t KSP191 = 1;
 
 constexpr std::uint64_t KSPStockSystemFingerprints[] = {
     0xC366DDFC05246F42,
-    0x63A5CF9AAB1676AE};
+    0x7A077635599E23F2};
 constexpr std::uint64_t KSPStabilizedSystemFingerprints[] = {
     0xC6E30693245BE096,
-    0x9DCFF6CCCD13551F};
+    0x4B830CBDF5E77F8D};
 
 }  // namespace astronomy
 }  // namespace principia
