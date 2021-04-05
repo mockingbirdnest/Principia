f2807516bb0b64d48c0dd19edefbedb6e10a1025
pleroy <pascal.h.leroy@gmail.com>
2021-04-05 12:35:30 +0200

    More test for the Checkpointer.

--- physics/checkpointer_test.cpp
+++ physics/checkpointer_test.cpp
@@ -2,18 +2,27 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "testing_utilities/matchers.hpp"
 
 namespace principia {
 namespace physics {
 
+using base::Error;
 using base::not_null;
 using base::Status;
 using geometry::Instant;
 using quantities::si::Second;
+using testing_utilities::StatusIs;
+using ::testing::Field;
+using ::testing::InSequence;
 using ::testing::MockFunction;
 using ::testing::Ref;
 using ::testing::Return;
 using ::testing::_;
+
+ACTION_P(SetPayload, payload) {
+  arg0->payload = payload;
+}
 
 struct Message {
   class Checkpoint {
@@ -24,6 +33,8 @@ struct Message {
     const serialization::Point& time() const {
       return time_;
     }
+
+    int payload = 0;
 
    private:
     serialization::Point time_;
@@ -52,18 +63,70 @@ TEST_F(CheckpointerTest, WriteToCheckpointIfNeeded) {
   Instant const t1 = Instant() + 10 * Second;
   EXPECT_CALL(writer_, Call(_));
   checkpointer_.WriteToCheckpoint(t1);
+  EXPECT_EQ(t1, checkpointer_.oldest_checkpoint());
 
   Instant const t2 = t1 + 8 * Second;
   EXPECT_CALL(writer_, Call(_)).Times(0);
-  checkpointer_.WriteToCheckpointIfNeeded(
+  EXPECT_FALSE(checkpointer_.WriteToCheckpointIfNeeded(
       t2,
-      /*max_time_between_checkpoints=*/10 * Second);
+      /*max_time_between_checkpoints=*/10 * Second));
+  EXPECT_EQ(t1, checkpointer_.oldest_checkpoint());
 
   EXPECT_CALL(writer_, Call(_));
   Instant const t3 = t2 + 3 * Second;
-  checkpointer_.WriteToCheckpointIfNeeded(
+  EXPECT_TRUE(checkpointer_.WriteToCheckpointIfNeeded(
       t3,
-      /*max_time_between_checkpoints=*/10 * Second);
+      /*max_time_between_checkpoints=*/10 * Second));
+  EXPECT_EQ(t1, checkpointer_.oldest_checkpoint());
+}
+
+TEST_F(CheckpointerTest, ReadFromOldestCheckpoint) {
+  EXPECT_THAT(checkpointer_.ReadFromOldestCheckpoint(),
+              StatusIs(Error::NOT_FOUND));
+
+  Instant const t1 = Instant() + 10 * Second;
+  EXPECT_CALL(writer_, Call(_));
+  checkpointer_.WriteToCheckpoint(t1);
+
+  EXPECT_CALL(reader_, Call(_))
+      .WillOnce(Return(Status::CANCELLED))
+      .WillOnce(Return(Status::OK));
+  EXPECT_THAT(checkpointer_.ReadFromOldestCheckpoint(),
+              StatusIs(Error::CANCELLED));
+  EXPECT_OK(checkpointer_.ReadFromOldestCheckpoint());
+}
+
+TEST_F(CheckpointerTest, ReadFromAllCheckpointsBackwards) {
+  Instant const t1 = Instant() + 10 * Second;
+  EXPECT_CALL(writer_, Call(_)).WillOnce(SetPayload(1));
+  checkpointer_.WriteToCheckpoint(t1);
+
+  Instant const t2 = t1 + 11 * Second;
+  EXPECT_CALL(writer_, Call(_)).WillOnce(SetPayload(2));
+  checkpointer_.WriteToCheckpoint(t2);
+
+  Instant const t3 = t2 + 11 * Second;
+  EXPECT_CALL(writer_, Call(_)).WillOnce(SetPayload(3));
+  checkpointer_.WriteToCheckpoint(t3);
+
+  {
+    InSequence s;
+    EXPECT_CALL(reader_, Call(Field(&Message::Checkpoint::payload, 3)));
+    EXPECT_CALL(reader_, Call(Field(&Message::Checkpoint::payload, 2)));
+    EXPECT_CALL(reader_, Call(Field(&Message::Checkpoint::payload, 1)));
+  }
+  EXPECT_OK(
+      checkpointer_.ReadFromAllCheckpointsBackwards(reader_.AsStdFunction()));
+
+  {
+    InSequence s;
+    EXPECT_CALL(reader_, Call(Field(&Message::Checkpoint::payload, 3)));
+    EXPECT_CALL(reader_, Call(Field(&Message::Checkpoint::payload, 2)))
+        .WillOnce(Return(Status::CANCELLED));
+  }
+  EXPECT_THAT(
+      checkpointer_.ReadFromAllCheckpointsBackwards(reader_.AsStdFunction()),
+      StatusIs(Error::CANCELLED));
 }
 
 TEST_F(CheckpointerTest, Serialization) {
