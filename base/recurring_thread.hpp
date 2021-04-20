71c00e7ae70ef9c74fc31698094f784cbee2123a
pleroy <pascal.h.leroy@gmail.com>
2021-04-20 21:25:15 +0200

    Refine the class.

--- base/recurring_thread.hpp
+++ base/recurring_thread.hpp
@@ -4,25 +4,40 @@
 #include <functional>
 #include <optional>
 
+#include "absl/synchronization/mutex.h"
 #include "base/jthread.hpp"
 
 namespace principia {
 namespace base {
 namespace internal_recurring_thread {
 
+// A stoppable thread that supports cyclical execution of an action.  It is
+// connected to two monodirectional channels that can (optionally) hold a value
+// of Input (for incoming data) or Output (for outgoing data), respectively.
+// The action is run to transform the input into the output.  This class is
+// thread-safe.
 template<typename Input, typename Output>
 class RecurringThread {
  public:
   using Action = std::function<Output(Input)>;
 
+  // Constructs a stoppable thread that executes the given |action| no more
+  // frequently than at the specified |period| (and less frequently if no input
+  // was provided).
   RecurringThread(Action action,
-                  std::chrono::duration period);
+                  std::chrono::milliseconds period);
 
+  // Overwrites the contents of the input channel.  The |input| data will be
+  // either picked by the next execution of |action|, or overwritten by the next
+  // call to |Put|.
   void Put(Input input);
 
+  // Extracts data from the output channel, if there is any.
   std::optional<Output> Get();
 
  private:
+  void RepeatedlyRunAction();
+
   Action const action_;
   std::chrono::duration const period_;
 
@@ -34,5 +49,10 @@ class RecurringThread {
 };
 
 }  // namespace internal_recurring_thread
+
+using internal_recurring_thread::RecurringThread;
+
 }  // namespace base
 }  // namespace principia
+
+#include "base/recurring_thread_body.hpp"
---
+++ base/recurring_thread_body.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "base/recurring_thread.hpp"
+
+namespace principia {
+namespace base {
+namespace internal_recurring_thread {
+
+template<typename Input, typename Output>
+RecurringThread<Input, Output>::RecurringThread(
+    Action action,
+    std::chrono::milliseconds const period)
+    : action_(std::move(action)),
+      period_(period),
+      jthread_(
+          MakeStoppableThread(std::bind(&RecurringThread::RepeatedlyRunAction),
+                              this)) {}
+
+template<typename Input, typename Output>
+void RecurringThread<Input, Output>::Put(Input input) {
+  absl::MutexLock l(&lock_);
+  input_ = std::move(input);
+}
+
+template<typename Input, typename Output>
+std::optional<Output> RecurringThread<Input, Output>::Get() {
+  absl::MutexLock l(&lock_);
+  std::optional<Output> result;
+  if (output_.has_value()) {
+    std::swap(result, output_);
+  }
+  return result;
+}
+
+template<typename Input, typename Output>
+void RecurringThread<Input, Output>::RepeatedlyRunAction() {
+  for (std::chrono::steady_clock::time_point wakeup_time;;
+       std::this_thread::sleep_until(wakeup_time)) {
+    wakeup_time = std::chrono::steady_clock::now() + period_;
+    RETURN_IF_STOPPED;
+
+    std::optional<Input> input;
+    {
+      absl::MutexLock l(&lock_);
+      if (!input_.has_value()) {
+        // No input, let's wait for it to appear.
+        continue;
+      }
+      std::swap(input, input_);
+    }
+    RETURN_IF_STOPPED;
+
+    Output output = action_(input);
+    RETURN_IF_STOPPED;
+
+    {
+      absl::MutexLock l(&lock_);
+      std::swap(output, output_);
+    }
+    RETURN_IF_STOPPED;
+  }
+}
+
+}  // namespace internal_recurring_thread
+}  // namespace base
+}  // namespace principia
