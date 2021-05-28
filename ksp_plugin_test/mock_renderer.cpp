f40a640a308d69b38c883e6ba59bde0d6429a1dc
pleroy <pascal.h.leroy@gmail.com>
2021-05-29 01:15:42 +0200

    Bring back some bits of mock_renderer.cpp.

---
+++ ksp_plugin_test/mock_renderer.cpp
@@ -0,0 +1,19 @@
+﻿#include "mock_renderer.hpp"
+
+#include "physics/mock_dynamic_frame.hpp"
+
+namespace principia {
+namespace ksp_plugin {
+namespace internal_renderer {
+
+using physics::MockDynamicFrame;
+
+MockCelestial* const sun = new MockCelestial;
+
+MockRenderer::MockRenderer()
+    : Renderer(sun,
+               std::make_unique<MockDynamicFrame<Barycentric, Navigation>>()) {}
+
+}  // namespace internal_renderer
+}  // namespace ksp_plugin
+}  // namespace principia
