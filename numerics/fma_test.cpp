93da2c5c5e9cba095fabfe6234073351083abea7
Robin Leroy <egg.robin.leroy@gmail.com>
2021-06-02 22:27:11 +0200

    SetUp

--- numerics/fma_test.cpp
+++ numerics/fma_test.cpp
@@ -13,13 +13,17 @@ using base::CPUFeatureFlags;
 using base::HasCPUFeatures;
 using testing_utilities::AlmostEquals;
 
-class FMATest : public testing::Test {};
+class FMATest : public testing::Test {
+ protected:
+  void SetUp() override {
+    // Note that we test even if |UseHardwareFMA| is false, i.e., even in debug.
+    if (!CanEmitFMAInstructions || !HasCPUFeatures(CPUFeatureFlags::FMA)) {
+      GTEST_SKIP() << "Cannot test FMA on a machine without FMA";
+    }
+  }
+};
 
 TEST_F(FMATest, FMA) {
-  // Note that we test even if |UseHardwareFMA| is false, i.e., even in debug.
-  if (!CanEmitFMAInstructions || !HasCPUFeatures(CPUFeatureFlags::FMA)) {
-    GTEST_SKIP() << "Cannot test FMA on a machine without FMA";
-  }
   EXPECT_THAT(FusedMultiplyAdd(0.2, 5.1, 1.2),
               AlmostEquals(0.2 * 5.1 + 1.2, 0));
   EXPECT_THAT(FusedMultiplySubtract(0.2, 5.1, 1.2),
