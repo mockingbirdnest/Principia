56a20821f83669f5f9d61c8b48a26e0c6ead72cc
Robin Leroy <egg.robin.leroy@gmail.com>
2021-07-05 18:02:11 +0200

    NO leap second will be introduced at the end of December 2021.

--- astronomy/time_scales_test.cpp
+++ astronomy/time_scales_test.cpp
@@ -72,9 +72,9 @@ TEST_F(TimeScalesDeathTest, WarWasBeginning) {
 }
 
 TEST_F(TimeScalesDeathTest, FirstUnknownUTC) {
-  EXPECT_DEATH("2021-12-31T23:59:60"_UTC, "leap_seconds.size");
-  EXPECT_DEATH("2021-12-31T24:00:00"_UTC, "leap_seconds.size");
-  EXPECT_DEATH("2022-01-01T00:00:00"_UTC, "leap_seconds.size");
+  EXPECT_DEATH("2022-06-30T23:59:60"_UTC, "leap_seconds.size");
+  EXPECT_DEATH("2022-06-30T24:00:00"_UTC, "leap_seconds.size");
+  EXPECT_DEATH("2022-07-01T00:00:00"_UTC, "leap_seconds.size");
 }
 
 TEST_F(TimeScalesDeathTest, StretchyLeaps) {
