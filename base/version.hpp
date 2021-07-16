0c279c2da51a915e01e53d9cad1dbc236772dcc4
pleroy <pascal.h.leroy@gmail.com>
2021-07-16 19:14:56 +0200

    Emit a message with the compiler version at compile time; this will help maintaining the Windows pipeline.

--- base/version.hpp
+++ base/version.hpp
@@ -1,11 +1,15 @@
 
 #pragma once
 
+#include "base/macros.hpp"
+
 namespace principia {
 namespace base {
 
 extern char const BuildDate[];
 extern char const Version[];
+
+#pragma message("Compiler version: " STRINGIFY_EXPANSION(_MSC_FULL_VER))
 
 }  // namespace base
 }  // namespace principia
