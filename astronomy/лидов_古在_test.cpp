cc352ad8f7346161936e77135357804f093084bd
Robin Leroy <egg.robin.leroy@gmail.com>
2021-08-23 04:03:51 +0200

    add the files…

---
+++ astronomy/лидов_古在_test.cpp
@@ -0,0 +1,158 @@
+﻿
+#include <memory>
+
+#include "astronomy/butcher_mercury_orbiter_1119.hpp"
+#include "astronomy/orbital_elements.hpp"
+#include "astronomy/frames.hpp"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "ksp_plugin/integrators.hpp"
+#include "physics/body_centred_non_rotating_dynamic_frame.hpp"
+#include "physics/solar_system.hpp"
+#include "mathematica/mathematica.hpp"
+#include "testing_utilities/matchers.hpp"
+#include "testing_utilities/is_near.hpp"
+
+namespace principia {
+namespace astronomy {
+
+using base::not_null;
+using geometry::Frame;
+using geometry::Instant;
+using geometry::Interval;
+using geometry::NonRotating;
+using geometry::Position;
+using integrators::SymmetricLinearMultistepIntegrator;
+using integrators::methods::Quinlan1999Order8A;
+using integrators::methods::QuinlanTremaine1990Order12;
+using physics::BodyCentredNonRotatingDynamicFrame;
+using physics::DiscreteTrajectory;
+using physics::Ephemeris;
+using physics::MassiveBody;
+using physics::MasslessBody;
+using physics::SolarSystem;
+using physics::Trajectory;
+using quantities::Cos;
+using quantities::Pow;
+using quantities::Sin;
+using quantities::si::Metre;
+using quantities::si::Milli;
+using quantities::si::Minute;
+using quantities::si::Radian;
+using quantities::si::Kilo;
+using quantities::si::Second;
+using testing_utilities::operator""_⑴;
+using quantities::si::Degree;
+using testing_utilities::IsNear;
+
+class Лидов古在Test : public ::testing::Test {
+ protected:
+  using MercuryCentredInertial =
+      Frame<enum class MercuryCentredInertialTag, NonRotating>;
+
+  Лидов古在Test()
+      : solar_system_1950_(
+            SOLUTION_DIR / "astronomy" / "sol_gravity_model.proto.txt",
+            SOLUTION_DIR / "astronomy" /
+                "sol_initial_state_jd_2433282_500000000.proto.txt"),
+        ephemeris_(solar_system_1950_.MakeEphemeris(
+            /*accuracy_parameters=*/{/*fitting_tolerance=*/1 * Milli(Metre),
+                                     /*geopotential_tolerance=*/0x1p-24},
+            Ephemeris<ICRS>::FixedStepParameters(
+                SymmetricLinearMultistepIntegrator<QuinlanTremaine1990Order12,
+                                                   Position<ICRS>>(),
+                /*step=*/10 * Minute))),
+        mercury_(*solar_system_1950_.massive_body(*ephemeris_, "Mercury")),
+        mercury_frame_(ephemeris_.get(), &mercury_) {}
+
+  SolarSystem<ICRS> solar_system_1950_;
+  not_null<std::unique_ptr<Ephemeris<ICRS>>> ephemeris_;
+  MassiveBody const& mercury_;
+  BodyCentredNonRotatingDynamicFrame<ICRS, MercuryCentredInertial>
+      mercury_frame_;
+};
+
+TEST_F(Лидов古在Test, MercuryOrbiter1) {
+  DiscreteTrajectory<ICRS> icrs_trajectory;
+  icrs_trajectory.Append(MercuryOrbiter1119InitialTime,
+                         MercuryOrbiter1119InitialDegreesOfFreedom<ICRS>);
+  icrs_trajectory.SetDownsampling(ksp_plugin::MaxDenseIntervals,
+                                  ksp_plugin::DownsamplingTolerance);
+  auto const instance = ephemeris_->NewInstance(
+      {&icrs_trajectory},
+      Ephemeris<ICRS>::NoIntrinsicAccelerations,
+      {SymmetricLinearMultistepIntegrator<Quinlan1999Order8A, Position<ICRS>>(),
+       /*step=*/10 * Second});
+
+  for (int year = 1967; year < 1980; ++year) {
+    Instant const t = DateTimeAsTT(date_time::DateTime::BeginningOfDay(
+        date_time::Date::Ordinal(year, 1)));
+    LOG(ERROR) << "Flowing to " << t;
+    auto const status = ephemeris_->FlowWithFixedStep(t, *instance);
+    if (!status.ok()) {
+      LOG(ERROR) << status << " at " << icrs_trajectory.back().time;
+      break;
+    }
+  }
+
+  DiscreteTrajectory<MercuryCentredInertial> mercury_centred_trajectory;
+  for (auto const& [t, dof] : icrs_trajectory) {
+    mercury_centred_trajectory.Append(t,
+                                      mercury_frame_.ToThisFrameAtTime(t)(dof));
+  }
+
+  OrbitalElements const elements = OrbitalElements::ForTrajectory(
+      mercury_centred_trajectory, mercury_, MasslessBody{}).value();
+  mathematica::Logger logger(SOLUTION_DIR / "mathematica" / "1119.generated.wl",
+                             /*make_unique=*/false);
+  // The constants c₁ and c₂ are defined in [Лид61], equations (58) and (59)
+  // respectively.
+  Interval<double> c₁;
+  Interval<double> c₂;
+  for (auto const elements : elements.mean_elements()) {
+    double const ε = 1 - Pow<2>(elements.eccentricity);
+    double const cos²_i = Pow<2>(Cos(elements.inclination));
+    double const sin²_i = Pow<2>(Sin(elements.inclination));
+    double const sin²_ω = Pow<2>(Sin(elements.inclination));
+    c₁.Include(ε * cos²_i);
+    c₂.Include((1 - ε) * (2.0 / 5.0 - sin²_i * sin²_ω));
+    logger.Append("t", elements.time, mathematica::ExpressIn(Second));
+    logger.Append("a", elements.semimajor_axis, mathematica::ExpressIn(Metre));
+    logger.Append("e", elements.eccentricity);
+    logger.Append("i", elements.inclination, mathematica::ExpressIn(Radian));
+    logger.Append(R"(\[Omega])",
+                  elements.argument_of_periapsis,
+                  mathematica::ExpressIn(Radian));
+  }
+  // The elements e, i, and ω all vary quite a lot.
+  EXPECT_THAT(elements.mean_eccentricity_interval().min, IsNear(0.40_⑴));
+  EXPECT_THAT(elements.mean_eccentricity_interval().max, IsNear(0.88_⑴));
+  EXPECT_THAT(elements.mean_inclination_interval().min, IsNear(62_⑴ * Degree));
+  EXPECT_THAT(elements.mean_inclination_interval().max, IsNear(77_⑴ * Degree));
+  EXPECT_THAT(elements.mean_argument_of_periapsis_interval().min,
+              IsNear(51_⑴ * Degree));
+  EXPECT_THAT(elements.mean_argument_of_periapsis_interval().max,
+              IsNear(129_⑴ * Degree));
+
+  // The motion preserves the “тривиального интеграла a = const” [Лид61, p. 25]
+  // very well: while the sun is nudging the orbit, it is not pumping energy
+  // into nor out of it.
+  EXPECT_THAT(elements.mean_semimajor_axis_interval().min,
+              IsNear(14'910.0_⑴ * Kilo(Metre)));
+  EXPECT_THAT(elements.mean_semimajor_axis_interval().max,
+              IsNear(14'910.3_⑴ * Kilo(Metre)));
+
+  // The integral c₁ is preserved quite well: we have a fairly clean exchange
+  // between inclination and eccentricity.
+  EXPECT_THAT(c₁.min, IsNear(0.042_⑴));
+  EXPECT_THAT(c₁.max, IsNear(0.050_⑴));
+
+  // The integral c₂ is messier.
+  // TODO(egg): Why is that?  It is not the inhomogeneity of the gravitational
+  // potential of Mercury, changing that has no appreciable effect here.
+  EXPECT_THAT(c₂.min, IsNear(-0.24_⑴));
+  EXPECT_THAT(c₂.max, IsNear(-0.079_⑴));
+}
+
+}  // namespace astronomy
+}  // namespace principia
