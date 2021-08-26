72cdb29ef1d08dd7e2eb582f15851d80976a357a
pleroy <pascal.h.leroy@gmail.com>
2021-08-26 19:37:18 +0200

    Factory functions for constructing trajectories.

--- testing_utilities/trajectory_factories_body.hpp
+++ testing_utilities/trajectory_factories_body.hpp
@@ -2,42 +2,71 @@
 
 #include "testing_utilities/trajectory_factories.hpp"
 
+#include "physics/degrees_of_freedom.hpp"
+#include "quantities/elementary_functions.hpp"
+#include "quantities/si.hpp"
+
 namespace principia {
 namespace testing_utilities {
 namespace internal_trajectory_factories {
 
+using base::make_not_null_unique;
+using geometry::Displacement;
+using geometry::Velocity;
+using physics::DegreesOfFreedom;
+using quantities::Cos;
+using quantities::Sin;
+using quantities::Speed;
+using quantities::si::Radian;
+
 template<typename Frame>
-DiscreteTrajectory<Frame> MakeCircularTrajectory(
+not_null<std::unique_ptr<DiscreteTrajectory<Frame>>> MakeCircularTrajectory(
     AngularFrequency const& ω,
     Length const& r,
     Time const& Δt,
     Instant const& t1,
     Instant const& t2) {
-  return MakeCircularTrajectory(
-      ω, r, Δt, DoublePrecision<Instant>(t1), t2, trajectory);
+  DoublePrecision<Instant> t_max;
+  return MakeCircularTrajectory<Frame>(ω,
+                                       r,
+                                       Δt,
+                                       DoublePrecision<Instant>(t1),
+                                       DoublePrecision<Instant>(t2),
+                                       t_max);
 }
 
 template<typename Frame>
-DiscreteTrajectory<Frame> AppendCircularTrajectory(
+not_null<std::unique_ptr<DiscreteTrajectory<Frame>>> MakeCircularTrajectory(
     AngularFrequency const& ω,
     Length const& r,
     Time const& Δt,
     DoublePrecision<Instant> const& t1,
     DoublePrecision<Instant> const& t2,
     DoublePrecision<Instant>& t_max) {
+  static Instant const t0;
   Speed const v = ω * r / Radian;
+  auto trajectory = make_not_null_unique<DiscreteTrajectory<Frame>>();
   auto t = t1;
-  for (; t.value <= t2; t.Increment(Δt)) {
-    DegreesOfFreedom<World> const dof = {
-        World::origin + Displacement<World>{{r * Cos(ω * (t.value - t0_)),
-                                             r * Sin(ω * (t.value - t0_)),
-                                             0 * Metre}},
-        Velocity<World>{{-v * Sin(ω * (t.value - t0_)),
-                         v * Cos(ω * (t.value - t0_)),
-                         0 * Metre / Second}}};
-    trajectory.Append(t.value, dof);
+  for (; t.value <= t2.value; t.Increment(Δt)) {
+    DegreesOfFreedom<Frame> const dof = {
+        Frame::origin + Displacement<Frame>{{r * Cos(ω * (t.value - t0)),
+                                             r * Sin(ω * (t.value - t0)),
+                                             Length{}}},
+        Velocity<Frame>{{-v * Sin(ω * (t.value - t0)),
+                         v * Cos(ω * (t.value - t0)),
+                         Speed{}}}};
+    trajectory->Append(t.value, dof);
   }
-  return t;
+  t_max = t;
+  return std::move(trajectory);
+}
+
+template<typename Frame>
+void AppendTrajectory(DiscreteTrajectory<Frame> const& from,
+                      DiscreteTrajectory<Frame>& to) {
+  for (auto const& [t, dof] : from) {
+    to.Append(t, dof);
+  }
 }
 
 }  // namespace internal_trajectory_factories
---
+++ testing_utilities/trajectory_factories_test.cpp
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include "testing_utilities/trajectory_factories.hpp"
+
+#include "geometry/frame.hpp"
+#include "geometry/named_quantities.hpp"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "quantities/si.hpp"
+#include "testing_utilities/almost_equals.hpp"
+
+namespace principia {
+namespace testing_utilities {
+
+using geometry::Frame;
+using geometry::Handedness;
+using geometry::Inertial;
+using geometry::Instant;
+using geometry::Position;
+using geometry::Velocity;
+using quantities::si::Metre;
+using quantities::si::Radian;
+using quantities::si::Second;
+
+class TrajectoryFactoriesTest : public ::testing::Test {
+ protected:
+  using World = Frame<serialization::Frame::TestTag,
+                      Inertial,
+                      Handedness::Right,
+                      serialization::Frame::TEST>;
+};
+
+TEST_F(TrajectoryFactoriesTest, MakeCircularTrajectory) {
+  auto const trajectory = MakeCircularTrajectory<World>(
+      /*ω=*/3 * Radian / Second,
+      /*r=*/2 * Metre,
+      /*Δt=*/0.1 * Second, /*t1=*/
+      Instant{} + 4 * Second,
+      /*t2=*/Instant{} + 42 * Second);
+
+  for (auto const& [time, degrees_of_freedom] : *trajectory) {
+    Position<World> const& position = degrees_of_freedom.position();
+    Velocity<World> const& velocity = degrees_of_freedom.velocity();
+
+    EXPECT_THAT((position - World::origin).Norm(),
+                AlmostEquals(2 * Metre, 0, 1));
+  }
+}
+
+}  // namespace testing_utilities
+}  // namespace principia
