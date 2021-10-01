ab2c39fbb2102af67aeca0115bb19718f5c008e1
pleroy <pascal.h.leroy@gmail.com>
2021-10-02 00:26:05 +0200

    Add a project for the stub adapter.

---
+++ ksp_plugin_adapter_stub/ksp_plugin_adapter_stub.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using KSP.Localization;
+
+namespace principia {
+namespace ksp_plugin_adapter {
+
+[KSPScenario(createOptions: ScenarioCreationOptions.AddToAllGames,
+             tgtScenes: new GameScenes[]{GameScenes.FLIGHT,
+                                         GameScenes.MAINMENU,
+                                         GameScenes.SPACECENTER,
+                                         GameScenes.TRACKSTATION})]
+public partial class PrincipiaPluginAdapterStub : ScenarioModule,
+                                                  SupervisedWindowRenderer.
+                                                  ISupervisor {
+  [KSPField(isPersistant = true)]
+  private readonly Dialog dll_stub_executed_ = new Dialog(persist_state: false);
+
+  public event Action LockClearing;
+  public event Action WindowsDisposal;
+  public event Action WindowsRendering;
+
+  PrincipiaPluginAdapterStub() {
+    dll_stub_executed_.message =
+        Localizer.Format("#Principia_DLLStubExecuted");
+    dll_stub_executed_.Show();
+  }
+
+  private void OnGUI() {
+    dll_stub_executed_.RenderWindow();
+  }
+}
+
+}  // namespace ksp_plugin_adapter
+}  // namespace principia
