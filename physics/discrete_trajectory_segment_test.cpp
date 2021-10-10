25f4121e49de63ee591797b32e0a7f76fbec8334
pleroy <pascal.h.leroy@gmail.com>
2021-10-10 12:10:41 +0200

    Tests passing.

--- physics/discrete_trajectory_segment_test.cpp
+++ physics/discrete_trajectory_segment_test.cpp
@@ -36,6 +36,14 @@ class DiscreteTrajectorySegmentTest : public ::testing::Test {
     segment_->Append(t0_ + 11 * Second, unmoving_origin_);
   }
 
+  void ForgetAfter(Instant const& t) {
+    segment_->ForgetAfter(t);
+  }
+
+  void ForgetBefore(Instant const& t) {
+    segment_->ForgetBefore(t);
+  }
+
   DiscreteTrajectorySegment<World>* segment_;
   internal_discrete_trajectory_types::Segments<World> segments_;
   Instant const t0_;
@@ -107,33 +115,33 @@ TEST_F(DiscreteTrajectorySegmentTest, EmptySize) {
 }
 
 TEST_F(DiscreteTrajectorySegmentTest, ForgetAfterExisting) {
-  segment_.ForgetAfter(t0_ + 5 * Second);
-  EXPECT_EQ(t0_ + 3 * Second, segment_.rend()->first);
+  ForgetAfter(t0_ + 5 * Second);
+  EXPECT_EQ(t0_ + 3 * Second, segment_->rbegin()->first);
 }
 
 TEST_F(DiscreteTrajectorySegmentTest, ForgetAfterNonexisting) {
-  segment_.ForgetAfter(t0_ + 6 * Second);
-  EXPECT_EQ(t0_ + 5 * Second, segment_.rend()->first);
+  ForgetAfter(t0_ + 6 * Second);
+  EXPECT_EQ(t0_ + 5 * Second, segment_->rbegin()->first);
 }
 
 TEST_F(DiscreteTrajectorySegmentTest, ForgetAfterPastTheEnd) {
-  segment_.ForgetAfter(t0_ + 29 * Second);
-  EXPECT_EQ(t0_ + 11 * Second, segment_.rend()->first);
+  ForgetAfter(t0_ + 29 * Second);
+  EXPECT_EQ(t0_ + 11 * Second, segment_->rbegin()->first);
 }
 
 TEST_F(DiscreteTrajectorySegmentTest, ForgetBeforeExisting) {
-  segment_.ForgetBefore(t0_ + 7 * Second);
-  EXPECT_EQ(t0_ + 7 * Second, segment_.begin()->first);
+  ForgetBefore(t0_ + 7 * Second);
+  EXPECT_EQ(t0_ + 7 * Second, segment_->begin()->first);
 }
 
 TEST_F(DiscreteTrajectorySegmentTest, ForgetBeforeNonexisting) {
-  segment_.ForgetBefore(t0_ + 6 * Second);
-  EXPECT_EQ(t0_ + 7 * Second, segment_.begin()->first);
+  ForgetBefore(t0_ + 6 * Second);
+  EXPECT_EQ(t0_ + 7 * Second, segment_->begin()->first);
 }
 
 TEST_F(DiscreteTrajectorySegmentTest, ForgetBeforeTheBeginning) {
-  segment_.ForgetBefore(t0_ + 1 * Second);
-  EXPECT_EQ(t0_ + 2 * Second, segment_.begin()->first);
+  ForgetBefore(t0_ + 1 * Second);
+  EXPECT_EQ(t0_ + 2 * Second, segment_->begin()->first);
 }
 
 }  // namespace physics
