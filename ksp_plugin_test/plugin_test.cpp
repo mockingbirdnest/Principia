e7dbb8888a01b481d9eff46b66ccbbc245abaad2
pleroy <pascal.h.leroy@gmail.com>
2021-11-14 12:49:02 +0100

    Minor cleanups.

--- ksp_plugin_test/plugin_test.cpp
+++ ksp_plugin_test/plugin_test.cpp
@@ -445,7 +445,6 @@ TEST_F(PluginTest, Serialization) {
   EXPECT_EQ(1, message.vessel_size());
   EXPECT_EQ(SolarSystemFactory::Earth, message.vessel(0).parent_index());
   EXPECT_TRUE(message.vessel(0).vessel().has_flight_plan());
-
   EXPECT_TRUE(message.vessel(0).vessel().has_history());
   auto const& vessel_0_history = message.vessel(0).vessel().history();
   EXPECT_EQ(7, vessel_0_history.segment(0).zfp().timeline_size());
