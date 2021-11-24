c67e9afeb821ce03f8547887d2bed097628132f9
pleroy <pascal.h.leroy@gmail.com>
2021-11-24 23:31:49 +0100

    A factory for an accelerated trajectory.

--- testing_utilities/discrete_trajectory_factories_test.cpp
+++ testing_utilities/discrete_trajectory_factories_test.cpp
@@ -22,8 +22,10 @@ using geometry::Inertial;
 using geometry::InnerProduct;
 using geometry::Instant;
 using geometry::Position;
+using geometry::Vector;
 using geometry::Velocity;
 using physics::DegreesOfFreedom;
+using quantities::Acceleration;
 using quantities::Pow;
 using quantities::Sqrt;
 using quantities::si::Metre;
@@ -86,6 +88,45 @@ TEST_F(DiscreteTrajectoryFactoriesTest, NewLinearTrajectoryTimeline) {
                              0,
                              1));
     EXPECT_THAT(velocity.Norm(), AlmostEquals(Sqrt(77) * Metre / Second, 0, 0));
+  }
+  EXPECT_THAT(timeline.begin()->time,
+              AlmostEquals(Instant() + 4 * Second, 0));
+  EXPECT_THAT(timeline.rbegin()->time,
+              AlmostEquals(Instant() + 41.9 * Second, 46));
+  EXPECT_EQ(380, timeline.size());
+}
+
+TEST_F(DiscreteTrajectoryFactoriesTest, NewAcceleratedTrajectoryTimeline) {
+  auto const timeline = NewAcceleratedTrajectoryTimeline<World>(
+      /*degrees_of_freedom=*/
+      DegreesOfFreedom<World>(
+          World::origin,
+          Velocity<World>(
+              {1 * Metre / Second, 0 * Metre / Second, 0 * Metre / Second})),
+      /*acceleration=*/
+      Vector<Acceleration, World>({2 * Metre / Second / Second,
+                                   3 * Metre / Second / Second,
+                                   4 * Metre / Second / Second}),
+      /*Δt=*/0.1 * Second,
+      /*t1=*/Instant() + 4 * Second,
+      /*t2=*/Instant() + 42 * Second);
+
+  for (auto const& [time, degrees_of_freedom] : timeline) {
+    Position<World> const& position = degrees_of_freedom.position();
+    Velocity<World> const& velocity = degrees_of_freedom.velocity();
+
+    EXPECT_THAT((position - World::origin).Norm(),
+                AlmostEquals(
+                    (time - Instant()) *
+                        Sqrt(1 + 2 * (time - Instant()) / Second +
+                             29.0 / 4.0 * Pow<2>((time - Instant()) / Second)) *
+                        Metre / Second,
+                    0, 2));
+    EXPECT_THAT(velocity.Norm(),
+                AlmostEquals(Sqrt(1 + 4 * (time - Instant()) / Second +
+                                  29 * Pow<2>((time - Instant()) / Second)) *
+                                 Metre / Second,
+                             0, 1));
   }
   EXPECT_THAT(timeline.begin()->time,
               AlmostEquals(Instant() + 4 * Second, 0));
