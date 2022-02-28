e2ab3e1f1fc2c296f75c344983a0342553736745
rnlahaye <rnlahaye@gmail.com>
2022-02-28 13:31:57 -0800

    Format macos_allocator_replacement.

--- base/macos_allocator_replacement.hpp
+++ base/macos_allocator_replacement.hpp
@@ -1,8 +1,8 @@
-// This file contains aliases of some STL containers (such as `std::vector`) in
-// the `principia::std` namespace with their default allocators overridden to
-// base new one defined here. The result of this is that if you write
-// `std::vector foo` inside the principia namespace, it will resolve to a vector
-// with the allocator defined here.
+// This file contains aliases of some STL and Abseil containers (such as
+// `std::vector`) in the `principia::std` and `principia::absl` namespaces with
+// their default allocators overridden to base new one defined here. The result
+// of this is that if you write `std::vector foo` inside the principia
+// namespace, it will resolve to a vector with the allocator defined here.
 //
 // The intended purpose of this file is to be used on macOS builds. This is
 // necessary because Unity provides overrides of `new` and `delete` which have
@@ -32,38 +32,32 @@ namespace std {
 
 using namespace ::std;
 
-template<typename T>
+template <typename T>
 using allocator = ::principia::base::MallocAllocator<T>;
 
-template<typename T, typename Allocator = allocator<T>>
+template <typename T, typename Allocator = allocator<T>>
 using vector = ::std::vector<T, Allocator>;
 
-template<typename T, typename Allocator = allocator<T>>
+template <typename T, typename Allocator = allocator<T>>
 using deque = ::std::deque<T, Allocator>;
 
-template<typename T, typename Allocator = allocator<T>>
+template <typename T, typename Allocator = allocator<T>>
 using list = ::std::list<T, Allocator>;
 
-template<typename Key,
-         typename Compare = std::less<Key>,
-         typename Allocator = allocator<Key>>
+template <typename Key, typename Compare = std::less<Key>,
+          typename Allocator = allocator<Key>>
 using set = ::std::set<Key, Compare, Allocator>;
 
-template<typename Key,
-         typename T,
-         typename Compare = std::less<Key>,
-         typename Allocator = allocator<std::pair<const Key, T>>>
+template <typename Key, typename T, typename Compare = std::less<Key>,
+          typename Allocator = allocator<std::pair<const Key, T>>>
 using map = ::std::map<Key, T, Compare, Allocator>;
 
-template<typename Key,
-         typename Compare = std::less<Key>,
-         typename Allocator = allocator<Key>>
+template <typename Key, typename Compare = std::less<Key>,
+          typename Allocator = allocator<Key>>
 using multiset = ::std::multiset<Key, Compare, Allocator>;
 
-template<typename Key,
-         typename T,
-         typename Compare = std::less<Key>,
-         typename Allocator = allocator<std::pair<const Key, T>>>
+template <typename Key, typename T, typename Compare = std::less<Key>,
+          typename Allocator = allocator<std::pair<const Key, T>>>
 using multimap = ::std::multimap<Key, T, Compare, Allocator>;
 
 }  // namespace std
