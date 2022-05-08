a7b92cacafc59df9aa280a4837012af2cdd50072
Robin Leroy <egg.robin.leroy@gmail.com>
2022-05-08 16:45:24 +0200

    StrCat

--- ksp_plugin_test/plugin_compatibility_test.cpp
+++ ksp_plugin_test/plugin_compatibility_test.cpp
@@ -75,8 +75,9 @@ class PluginCompatibilityTest : public testing::Test {
     // Write the plugin to a new file with the preferred format.
     WritePluginToFile(
         TEMP_DIR /
-            (testing::UnitTest::GetInstance()->current_test_info()->name() +
-             "_serialized_plugin.proto.b64"),
+            absl::StrCat(
+                testing::UnitTest::GetInstance()->current_test_info()->name(),
+                "_serialized_plugin.proto.b64"),
         preferred_compressor,
         preferred_encoder,
         std::move(plugin1));
@@ -84,8 +85,9 @@ class PluginCompatibilityTest : public testing::Test {
     // Read the plugin from the new file to make sure that it's fine.
     auto plugin2 = ReadPluginFromFile(
         TEMP_DIR /
-            (testing::UnitTest::GetInstance()->current_test_info()->name() +
-             "_serialized_plugin.proto.b64"),
+            absl::StrCat(
+                testing::UnitTest::GetInstance()->current_test_info()->name(),
+                "_serialized_plugin.proto.b64"),
         preferred_compressor,
         preferred_encoder);
   }
