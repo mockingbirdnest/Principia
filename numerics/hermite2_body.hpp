60a316df8e1382498adc6cf42e0f9a7740e3476d
Pascal Leroy <pascal.h.leroy@gmail.com>
2022-07-29 00:17:23 +0200

    Add Hermite2.

---
+++ numerics/hermite2_body.hpp
@@ -0,0 +1,61 @@
+﻿
+#pragma once
+
+#include "numerics/hermite2.hpp"
+
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+#include "numerics/root_finders.hpp"
+
+namespace principia {
+namespace numerics {
+namespace internal_hermite2 {
+
+using quantities::Difference;
+
+template<typename Value, typename Argument>
+Hermite2<Value, Argument>::Hermite2(
+    std::pair<Argument, Argument> arguments,
+    std::pair<Value, Value> const& values,
+    Derivative1 const& derivative_lo)
+    : arguments_(std::move(arguments)) {
+  a0_ = values.first;
+  a1_ = derivative_lo;
+  Difference<Argument> const Δargument = arguments_.second - arguments_.first;
+  // If we were given the same point twice, there is a removable singularity.
+  // Otherwise, if the arguments are the same but not the values or the
+  // derivatives, we proceed to merrily NaN away as we should.
+  if (Δargument == Difference<Argument>{} &&
+      values.first == values.second) {
+    a2_ = {};
+    return;
+  }
+  auto const one_over_Δargument = 1.0 / Δargument;
+  auto const one_over_Δargument² = one_over_Δargument * one_over_Δargument;
+  Difference<Value> const Δvalue = values.second - values.first;
+  a2_ = Δvalue * one_over_Δargument² - derivative_lo * one_over_Δargument;
+}
+
+template<typename Value, typename Argument>
+Value Hermite2<Value, Argument>::Evaluate(Argument const& argument) const {
+  Difference<Argument> const Δargument = argument - arguments_.first;
+  return ((a2_ * Δargument) + a1_) * Δargument + a0_;
+}
+
+template<typename Value, typename Argument>
+typename Hermite2<Value, Argument>::Derivative1
+Hermite2<Value, Argument>::EvaluateDerivative(Argument const& argument) const {
+  Difference<Argument> const Δargument = argument - arguments_.first;
+  return 2.0 * a2_ * Δargument + a1_;
+}
+
+template<typename Value, typename Argument>
+Argument Hermite2<Value, Argument>::FindExtremum() const {
+  return arguments_.first - a1_ / (2 * a2_);
+}
+
+}  // namespace internal_hermite2
+}  // namespace numerics
+}  // namespace principia
