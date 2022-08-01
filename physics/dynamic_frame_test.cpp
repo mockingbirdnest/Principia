f56ce4d59de36b89e0795ab8ca3c5bbedc6a2a03
Pascal Leroy <pascal.h.leroy@gmail.com>
2022-08-01 20:13:22 +0200

    Cleanup.

--- physics/dynamic_frame_test.cpp
+++ physics/dynamic_frame_test.cpp
@@ -1,18 +1,10 @@
 ﻿
 #include "physics/dynamic_frame.hpp"
 
-#include "astronomy/frames.hpp"
-#include "base/not_null.hpp"
-#include "geometry/barycentre_calculator.hpp"
 #include "geometry/frame.hpp"
 #include "geometry/named_quantities.hpp"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "integrators/methods.hpp"
-#include "integrators/symplectic_runge_kutta_nyström_integrator.hpp"
-#include "physics/mock_continuous_trajectory.hpp"
-#include "physics/mock_ephemeris.hpp"
-#include "physics/solar_system.hpp"
 #include "quantities/named_quantities.hpp"
 #include "testing_utilities/almost_equals.hpp"
 #include "testing_utilities/componentwise.hpp"
@@ -21,35 +13,25 @@ namespace principia {
 namespace physics {
 namespace internal_dynamic_frame {
 
-using astronomy::ICRS;
-using base::check_not_null;
 using geometry::AngularVelocity;
-using geometry::Barycentre;
 using geometry::DefinesFrame;
 using geometry::Displacement;
 using geometry::Frame;
 using geometry::Inertial;
 using geometry::InfiniteFuture;
 using geometry::InfinitePast;
-using geometry::InnerProduct;
 using geometry::OrthogonalMap;
 using geometry::Position;
 using geometry::RigidTransformation;
 using geometry::Velocity;
-using integrators::SymplecticRungeKuttaNyströmIntegrator;
-using integrators::methods::McLachlanAtela1992Order4Optimal;
 using quantities::AngularAcceleration;
 using quantities::GravitationalParameter;
 using quantities::Sqrt;
 using quantities::si::Metre;
-using quantities::si::Milli;
 using quantities::si::Radian;
 using quantities::si::Second;
 using testing_utilities::AlmostEquals;
 using testing_utilities::Componentwise;
-using ::testing::A;
-using ::testing::Eq;
-using ::testing::InSequence;
 using ::testing::Return;
 using ::testing::StrictMock;
 namespace si = quantities::si;
@@ -58,9 +40,6 @@ namespace {
 
 using Circular = Frame<enum class CircularTag, Inertial>;
 using Helical = Frame<enum class HelicalTag, Inertial>;
-
-char constexpr big[] = "Big";
-char constexpr small[] = "Small";
 
 Vector<Acceleration, Circular> Gravity(Instant const& t,
                                        Position<Circular> const& q) {
@@ -71,9 +50,9 @@ Vector<Acceleration, Circular> Gravity(Instant const& t,
 
 // An inertial frame.
 template<typename OtherFrame, typename ThisFrame>
-class InertialFrame : public DynamicFrame<OtherFrame, ThisFrame> {
+class FakeDynamicFrame : public DynamicFrame<OtherFrame, ThisFrame> {
  public:
-  InertialFrame(
+  FakeDynamicFrame(
       DegreesOfFreedom<OtherFrame> const& origin_degrees_of_freedom_at_epoch,
       Instant const& epoch,
       OrthogonalMap<OtherFrame, ThisFrame> const& orthogonal_map,
@@ -111,7 +90,7 @@ class InertialFrame : public DynamicFrame<OtherFrame, ThisFrame> {
 };
 
 template<typename OtherFrame, typename ThisFrame>
-InertialFrame<OtherFrame, ThisFrame>::InertialFrame(
+FakeDynamicFrame<OtherFrame, ThisFrame>::FakeDynamicFrame(
     DegreesOfFreedom<OtherFrame> const& origin_degrees_of_freedom_at_epoch,
     Instant const& epoch,
     OrthogonalMap<OtherFrame, ThisFrame> const& orthogonal_map,
@@ -125,7 +104,7 @@ InertialFrame<OtherFrame, ThisFrame>::InertialFrame(
 
 template<typename OtherFrame, typename ThisFrame>
 RigidMotion<OtherFrame, ThisFrame>
-InertialFrame<OtherFrame, ThisFrame>::ToThisFrameAtTime(
+FakeDynamicFrame<OtherFrame, ThisFrame>::ToThisFrameAtTime(
     Instant const& t) const {
   return RigidMotion<OtherFrame, ThisFrame>(
              RigidTransformation<OtherFrame, ThisFrame>(
@@ -139,12 +118,12 @@ InertialFrame<OtherFrame, ThisFrame>::ToThisFrameAtTime(
 }
 
 template<typename OtherFrame, typename ThisFrame>
-void InertialFrame<OtherFrame, ThisFrame>::WriteToMessage(
+void FakeDynamicFrame<OtherFrame, ThisFrame>::WriteToMessage(
     not_null<serialization::DynamicFrame*> message) const {}
 
 template<typename OtherFrame, typename ThisFrame>
 Vector<Acceleration, OtherFrame>
-InertialFrame<OtherFrame, ThisFrame>::GravitationalAcceleration(
+FakeDynamicFrame<OtherFrame, ThisFrame>::GravitationalAcceleration(
     Instant const& t,
     Position<OtherFrame> const& q) const {
   return gravity_(t, q);
@@ -152,7 +131,7 @@ InertialFrame<OtherFrame, ThisFrame>::GravitationalAcceleration(
 
 template<typename OtherFrame, typename ThisFrame>
 AcceleratedRigidMotion<OtherFrame, ThisFrame>
-InertialFrame<OtherFrame, ThisFrame>::MotionOfThisFrame(
+FakeDynamicFrame<OtherFrame, ThisFrame>::MotionOfThisFrame(
     Instant const& t) const {
   return AcceleratedRigidMotion<OtherFrame, ThisFrame>(
       ToThisFrameAtTime(t),
@@ -160,7 +139,6 @@ InertialFrame<OtherFrame, ThisFrame>::MotionOfThisFrame(
       /*acceleration_of_to_frame_origin=*/{});
 }
 
-//TODO(phl): Use the real MockDynamicFrame.
 template<typename InertialFrame, typename ThisFrame>
 class MockDynamicFrame : public DynamicFrame<InertialFrame, ThisFrame> {
  public:
@@ -197,49 +175,12 @@ class MockDynamicFrame : public DynamicFrame<InertialFrame, ThisFrame> {
 
 class DynamicFrameTest : public testing::Test {
  protected:
-  using MockFrame = Frame<serialization::Frame::TestTag,
-                          Arbitrary,
-                          Handedness::Right,
-                          serialization::Frame::TEST1>;
+  using InertialFrame = Frame<enum class InertialFrameTag,
+                              Inertial>;
+  using TestFrame = Frame<enum class TestFrameTag,
+                          Arbitrary>;
 
-  DynamicFrameTest()
-      : solar_system_(SOLUTION_DIR / "astronomy" /
-                          "test_gravity_model_two_bodies.proto.txt",
-                      SOLUTION_DIR / "astronomy" /
-                          "test_initial_state_two_bodies_circular.proto.txt"),
-        t0_(solar_system_.epoch()),
-        ephemeris_(solar_system_.MakeEphemeris(
-            /*accuracy_parameters=*/{/*fitting_tolerance=*/1 * Milli(Metre),
-                                     /*geopotential_tolerance=*/0x1p-24},
-            Ephemeris<ICRS>::FixedStepParameters(
-                SymplecticRungeKuttaNyströmIntegrator<
-                    McLachlanAtela1992Order4Optimal,
-                    Position<ICRS>>(),
-                /*step=*/10 * Milli(Second)))),
-        big_(solar_system_.massive_body(*ephemeris_, big)),
-        big_initial_state_(solar_system_.degrees_of_freedom(big)),
-        big_gravitational_parameter_(
-            solar_system_.gravitational_parameter(big)),
-        small_(solar_system_.massive_body(*ephemeris_, small)),
-        small_initial_state_(solar_system_.degrees_of_freedom(small)),
-        small_gravitational_parameter_(
-            solar_system_.gravitational_parameter(small)) {
-    mock_frame_ = std::make_unique<MockDynamicFrame<ICRS, MockFrame>>();
-  }
-
-  SolarSystem<ICRS> solar_system_;
-  Instant const t0_;
-  std::unique_ptr<Ephemeris<ICRS>> const ephemeris_;
-  MassiveBody const* const big_;
-  DegreesOfFreedom<ICRS> const big_initial_state_;
-  GravitationalParameter const big_gravitational_parameter_;
-  MassiveBody const* const small_;
-  DegreesOfFreedom<ICRS> const small_initial_state_;
-  GravitationalParameter const small_gravitational_parameter_;
-  StrictMock<MockEphemeris<ICRS>> mock_ephemeris_;
-  StrictMock<MockContinuousTrajectory<ICRS>> mock_big_trajectory_;
-  StrictMock<MockContinuousTrajectory<ICRS>> mock_small_trajectory_;
-  std::unique_ptr<MockDynamicFrame<ICRS, MockFrame>> mock_frame_;
+  StrictMock<MockDynamicFrame<InertialFrame, TestFrame>> mock_frame_;
 
   DegreesOfFreedom<Circular> circular_degrees_of_freedom_ = {
       Circular::origin +
@@ -247,8 +188,8 @@ class DynamicFrameTest : public testing::Test {
       Velocity<Circular>(
           {0 * Metre / Second, 1 * Metre / Second, 0 * Metre / Second})};
 
-  InertialFrame<Circular, Helical> helix_frame_ =
-      InertialFrame<Circular, Helical>(
+  FakeDynamicFrame<Circular, Helical> helix_frame_ =
+      FakeDynamicFrame<Circular, Helical>(
           {Circular::origin,
            Velocity<Circular>(
                {0 * Metre / Second, 0 * Metre / Second, 1 * Metre / Second})},
@@ -265,44 +206,45 @@ TEST_F(DynamicFrameTest, CoriolisAcceleration) {
   Instant const t;
 
   // The velocity is opposed to the motion and away from the centre.
-  DegreesOfFreedom<MockFrame> const point_dof =
-      {MockFrame::origin,
-       Velocity<MockFrame>({50 * Metre / Second,
+  DegreesOfFreedom<TestFrame> const point_dof =
+      {TestFrame::origin,
+       Velocity<TestFrame>({50 * Metre / Second,
                             -100 * Metre / Second,
                             0 * Metre / Second})};
 
-  Position<ICRS> const from_origin =
-      ICRS::origin + Displacement<ICRS>({2 * Metre,
-                                         1 * Metre,
-                                         0 * Metre});
-  Position<MockFrame> const to_origin = point_dof.position();
-  auto const rotation = Rotation<ICRS, MockFrame>::Identity();
-  RigidTransformation<ICRS, MockFrame> const rigid_transformation(
+  Position<InertialFrame> const from_origin =
+      InertialFrame::origin + Displacement<InertialFrame>({2 * Metre,
+                                                           1 * Metre,
+                                                           0 * Metre});
+  Position<TestFrame> const to_origin = point_dof.position();
+  auto const rotation = Rotation<InertialFrame, TestFrame>::Identity();
+  RigidTransformation<InertialFrame, TestFrame> const rigid_transformation(
           from_origin, to_origin, rotation.Forget<OrthogonalMap>());
-  AngularVelocity<ICRS> const angular_velocity_of_to_frame(
+  AngularVelocity<InertialFrame> const angular_velocity_of_to_frame(
       {0 * Radian / Second,
        0 * Radian / Second,
        10 * Radian / Second});
-  Velocity<ICRS> const velocity_of_to_frame_origin;
-  RigidMotion<ICRS, MockFrame> const rigid_motion(rigid_transformation,
+  Velocity<InertialFrame> const velocity_of_to_frame_origin;
+  RigidMotion<InertialFrame, TestFrame> const rigid_motion(rigid_transformation,
                                                   angular_velocity_of_to_frame,
                                                   velocity_of_to_frame_origin);
-  Bivector<AngularAcceleration, ICRS> const angular_acceleration_of_to_frame;
-  Vector<Acceleration, ICRS> const acceleration_of_to_frame_origin;
-  AcceleratedRigidMotion<ICRS, MockFrame> const accelerated_rigid_motion(
-      rigid_motion,
-      angular_acceleration_of_to_frame,
-      acceleration_of_to_frame_origin);
-  EXPECT_CALL(*mock_frame_, MotionOfThisFrame(t))
+  Bivector<AngularAcceleration, InertialFrame> const
+      angular_acceleration_of_to_frame;
+  Vector<Acceleration, InertialFrame> const acceleration_of_to_frame_origin;
+  AcceleratedRigidMotion<InertialFrame, TestFrame> const
+      accelerated_rigid_motion(rigid_motion,
+                               angular_acceleration_of_to_frame,
+                               acceleration_of_to_frame_origin);
+  EXPECT_CALL(mock_frame_, MotionOfThisFrame(t))
       .WillOnce(Return(accelerated_rigid_motion));
 
-  Vector<Acceleration, ICRS> const gravitational_acceleration;
-  EXPECT_CALL(*mock_frame_, GravitationalAcceleration(t, from_origin))
+  Vector<Acceleration, InertialFrame> const gravitational_acceleration;
+  EXPECT_CALL(mock_frame_, GravitationalAcceleration(t, from_origin))
       .WillOnce(Return(gravitational_acceleration));
 
   // The Coriolis acceleration is towards the centre and opposed to the motion.
-  EXPECT_THAT(mock_frame_->GeometricAcceleration(t, point_dof),
-              AlmostEquals(Vector<Acceleration, MockFrame>(
+  EXPECT_THAT(mock_frame_.GeometricAcceleration(t, point_dof),
+              AlmostEquals(Vector<Acceleration, TestFrame>(
                                {-2000 * Metre / Pow<2>(Second),
                                 -1000 * Metre / Pow<2>(Second),
                                 0 * Metre / Pow<2>(Second)}),
