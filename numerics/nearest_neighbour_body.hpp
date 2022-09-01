0c39c752115afcc7425e5156160299ac1a6b9a86
pleroy <pascal.h.leroy@gmail.com>
2022-09-01 22:20:43 +0200

    Quickselect is my friend.

--- numerics/nearest_neighbour_body.hpp
+++ numerics/nearest_neighbour_body.hpp
@@ -17,6 +17,7 @@ using geometry::Bivector;
 using geometry::Frame;
 using geometry::SymmetricBilinearForm;
 using geometry::SymmetricProduct;
+using geometry::Wedge;
 using quantities::Difference;
 
 template<typename T>
@@ -39,19 +40,36 @@ PrincipalComponentPartitioningTree<Value_>::PrincipalComponentPartitioningTree(
     : centroid_(Barycentre<Value, double, std::vector>(
           values,
           std::vector<double>(values.size(), 1))) {
-  std::vector<Difference<Value>> differences;
-  differences.reserve(values.size());
+  // These are members of a vector space, not |Vector| per se.
+  std::vector<Difference<Value>> vectors;
+  vectors.reserve(values.size());
   for (auto const& value : values) {
-    differences.push_back(value - centroid_);
+    vectors.push_back(value - centroid_);
   }
 
+  // Compute the "inertia" of the |vectors| and diagonalize it.  This gives us
+  // the direction of the largest eigenvalue, which defines the splitting plane.
   using Eigenframe = Frame<enum class EigenframeTag>;
 
-  auto const form = ComputePrincipalComponentForm(differences);
+  auto const form = ComputePrincipalComponentForm(vectors);
   auto const eigensystem = form.Diagonalize<Eigenframe>();
   // The first eigenvalue is the largest one.
   auto const principal_plane =
       eigensystem.rotation(Bivector<double, Eigenframe>({1, 0, 0}));
+
+  // Project the |vectors| on the principal axis.
+  std::vector<Difference<Value>> projections;
+  projections.reserve(vectors.size());
+  for (auto const& v : vectors) {
+    auto const projection_on_plane = Wedge(principal_plane, v);
+    projections.push_back(principal_plane * projection_on_plane)
+  }
+
+  // Find the median in linear time.  This is where our plane will lie.  Note
+  // that |mid| is orthogonal to the plane.
+  auto const mid = projections.begin() + projections.size() / 2;
+  std::nth_element(projections.begin(), mid, projections.end());
+  auto const anchor = *mid;
 }
 
 template<typename Value_>
