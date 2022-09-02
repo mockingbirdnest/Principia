1ae912e245c277de35fe3905305a73b6a32d50f6
pleroy <pascal.h.leroy@gmail.com>
2022-09-02 19:19:12 +0200

    Better types, and start to construct the tree.

--- numerics/nearest_neighbour.hpp
+++ numerics/nearest_neighbour.hpp
@@ -1,10 +1,24 @@
 #pragma once
 
+#include <utility>
+#include <variant>
 #include <vector>
+
+#include "base/not_null.hpp"
+#include "geometry/frame.hpp"
+#include "geometry/grassmann.hpp"
+#include "geometry/symmetric_bilinear_form.hpp"
+#include "quantities/named_quantities.hpp"
 
 namespace principia {
 namespace numerics {
 namespace internal_nearest_neighbour {
+
+using base::not_null;
+using geometry::Bivector;
+using geometry::Frame;
+using geometry::SymmetricProduct;
+using quantities::Difference;
 
 template<typename Value_>
 class PrincipalComponentPartitioningTree {
@@ -18,6 +32,44 @@ class PrincipalComponentPartitioningTree {
   Value const& FindNearest(Value const& value) const;
 
  private:
+  // A frame used to compute the principal components.
+  using PrincipalComponentsFrame =
+      Frame<enum class PrincipalComponentsFrameTag>;
+
+  // A displacement from the centroid.
+  using Displacement = Difference<Value>;
+
+  // A number of useful types for building data structures and parameters/
+  // results.
+  using DisplacementSymmetricBilinearForm =
+      decltype(SymmetricProduct(std::declval<Displacement>(),
+                                std::declval<Displacement>()));
+  using DisplacementPrincipalComponentsSystem =
+      typename DisplacementSymmetricBilinearForm::template Eigensystem<
+          PrincipalComponentsFrame>;
+  // TODO(phl): Unclear if the Bivector thingy is a good idea.
+  using PrincipalAxis =
+      decltype(std::declval<DisplacementPrincipalComponentsSystem>().rotation(
+          std::declval<Bivector<double, PrincipalComponentsFrame>>()));
+
+  struct Node;
+
+  using Children = std::pair<not_null<std::unique_ptr<Node>>,
+                             not_null<std::unique_ptr<Node>>>;
+
+  using Leaf = std::vector<Displacement>;
+
+  struct Node {
+    PrincipalAxis principal_axis;
+    Displacement anchor;
+    std::variant<Children, Leaf> tree;
+  };
+
+  static not_null<std::unique_ptr<Node>> BuildTree();
+
+  static DisplacementSymmetricBilinearForm ComputePrincipalComponentForm(
+      std::vector<Displacement> const& displacements);
+
   Value centroid_;
 };
 
--- numerics/nearest_neighbour_body.hpp
+++ numerics/nearest_neighbour_body.hpp
@@ -7,10 +7,7 @@
 #include <type_traits>
 
 #include "geometry/barycentre_calculator.hpp"
-#include "geometry/frame.hpp"
 #include "geometry/grassmann.hpp"
-#include "geometry/symmetric_bilinear_form.hpp"
-#include "quantities/named_quantities.hpp"
 
 namespace principia {
 namespace numerics {
@@ -18,25 +15,8 @@ namespace internal_nearest_neighbour {
 
 using geometry::Barycentre;
 using geometry::Bivector;
-using geometry::Frame;
 using geometry::SymmetricBilinearForm;
-using geometry::SymmetricProduct;
 using geometry::Wedge;
-using quantities::Difference;
-
-template<typename T>
-using SymmetricBilinearFormFor =
-    decltype(SymmetricProduct(std::declval<T>(), std::declval<T>()));
-
-template<typename T>
-SymmetricBilinearFormFor<T> ComputePrincipalComponentForm(
-    std::vector<T> const& ts) {
-  SymmetricBilinearFormFor<T> result;
-  for (const auto& t : ts) {
-    result += SymmetricProduct(t, t);
-  }
-  return result;
-}
 
 template<typename Value_>
 PrincipalComponentPartitioningTree<Value_>::PrincipalComponentPartitioningTree(
@@ -45,7 +25,7 @@ PrincipalComponentPartitioningTree<Value_>::PrincipalComponentPartitioningTree(
           values,
           std::vector<double>(values.size(), 1))) {
   // The displacements from the centroid.
-  std::vector<Difference<Value>> displacements;
+  std::vector<Displacement> displacements;
   displacements.reserve(values.size());
   for (auto const& value : values) {
     displacements.push_back(value - centroid_);
@@ -59,18 +39,17 @@ PrincipalComponentPartitioningTree<Value_>::PrincipalComponentPartitioningTree(
 
   // Compute the "inertia" of the |vectors| and diagonalize it.  This gives us
   // the direction of the largest eigenvalue, which defines the splitting plane.
-  using Eigenframe = Frame<enum class EigenframeTag>;
 
   auto const form = ComputePrincipalComponentForm(displacements);
-  auto const eigensystem = form.Diagonalize<Eigenframe>();
+  auto const eigensystem =
+      form.template Diagonalize<PrincipalComponentsFrame>();
   // The first eigenvalue is the largest one.
-  //TODO(phl):Bivector?
-  auto const principal_axis =
-      eigensystem.rotation(Bivector<double, Eigenframe>({1, 0, 0}));
+  auto const principal_axis = eigensystem.rotation(
+      Bivector<double, PrincipalComponentsFrame>({1, 0, 0}));
 
   // Project the |vectors| on the principal axis.
   using Projection =
-      decltype(Wedge(principal_axis, std::declval<Difference<Value>>()));
+      decltype(Wedge(principal_axis, std::declval<Displacement>()));
   std::vector<Projection> projections;
 
   auto const projection_less = [&projections](std::int32_t const left,
@@ -96,9 +75,13 @@ PrincipalComponentPartitioningTree<Value_>::PrincipalComponentPartitioningTree(
   auto const mid_lower =
       std::max_element(indices.begin(), mid_upper, projection_less);
 
-  auto const anchor = Barycentre<Difference<Value>, double>(
+  auto const anchor = Barycentre<Displacement, double>(
       {displacements[*mid_lower], displacements[*mid_upper]},
       {1, 1});
+
+  Node const node{.principal_axis = principal_axis,
+                  .anchor = anchor,
+                  .tree = std::vector<Displacement>()};
 }
 
 template<typename Value_>
@@ -107,6 +90,68 @@ void PrincipalComponentPartitioningTree<Value_>::Add(Value const& value) {}
 template<typename Value_>
 Value_ const& PrincipalComponentPartitioningTree<Value_>::FindNearest(
     Value const& value) const {}
+
+template<typename Value_>
+not_null<std::unique_ptr<
+typename PrincipalComponentPartitioningTree<Value_>::Node>>
+PrincipalComponentPartitioningTree<Value_>::BuildTree() {
+  // Compute the "inertia" of the |vectors| and diagonalize it.  This gives us
+  // the direction of the largest eigenvalue, which defines the splitting plane.
+
+  auto const form = ComputePrincipalComponentForm(displacements);
+  auto const eigensystem =
+      form.template Diagonalize<PrincipalComponentsFrame>();
+  // The first eigenvalue is the largest one.
+  auto const principal_axis = eigensystem.rotation(
+      Bivector<double, PrincipalComponentsFrame>({1, 0, 0}));
+
+  // Project the |vectors| on the principal axis.
+  using Projection =
+      decltype(Wedge(principal_axis, std::declval<Displacement>()));
+  std::vector<Projection> projections;
+
+  auto const projection_less = [&projections](std::int32_t const left,
+                                              std::int32_t const right) {
+    return projections[left].coordinates() < projections[right].coordinates();
+  };
+
+  projections.reserve(displacements.size());
+  for (auto const& displacement : displacements) {
+    projections.push_back(Wedge(principal_axis, displacement));
+  }
+
+  // Find the median of the projections on the principal axis.  Cost: 3.4 * N.
+  auto const mid_upper = indices.begin() + indices.size() / 2;
+  std::nth_element(indices.begin(), mid_upper, indices.end(), projection_less);
+
+  // Here |mid_upper| denotes an index that denotes the point that is "in the
+  // middle" of the projections on the principal axis.  We do not wish to anchor
+  // our separator plane on a point, because that would make it more likely that
+  // queries have to dig into two branches of the tree.  Instead, we look for
+  // the maximum projection in the "lower" half space, and anchor our plane
+  // halfway between these points.  Cost: 0.25 * N.
+  auto const mid_lower =
+      std::max_element(indices.begin(), mid_upper, projection_less);
+
+  auto const anchor = Barycentre<Displacement, double>(
+      {displacements[*mid_lower], displacements[*mid_upper]}, {1, 1});
+
+  Node const node{.principal_axis = principal_axis,
+                  .anchor = anchor,
+                  .tree = std::vector<Displacement>()};
+}
+
+template<typename Value_>
+typename PrincipalComponentPartitioningTree<Value_>::
+DisplacementSymmetricBilinearForm
+PrincipalComponentPartitioningTree<Value_>::ComputePrincipalComponentForm(
+    std::vector<Displacement> const& displacements) {
+  DisplacementSymmetricBilinearForm result;
+  for (const auto& displacement : displacements) {
+    result += SymmetricProduct(displacement, displacement);
+  }
+  return result;
+}
 
 }  // namespace internal_nearest_neighbour
 }  // namespace numerics
