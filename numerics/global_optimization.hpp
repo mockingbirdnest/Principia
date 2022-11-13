550ab0d78598fc0e0708097cc4542b4e12c96668
pleroy <pascal.h.leroy@gmail.com>
2022-11-13 14:22:06 +0100

    Start implementing the real thing.

--- numerics/global_optimization.hpp
+++ numerics/global_optimization.hpp
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <functional>
 #include <random>
@@ -13,6 +13,7 @@ namespace numerics {
 namespace internal_global_optimization {
 
 using geometry::Hilbert;
+using quantities::Cube;
 using quantities::Difference;
 using quantities::Length;
 using quantities::Product;
@@ -28,8 +29,9 @@ template<typename Scalar, typename Argument>
 using Gradient =
     Product<Scalar,
             Quotient<Difference<Argument>,
-                     typename Hilbert<Difference<Argument>>::Norm�Type>>;
+                     typename Hilbert<Difference<Argument>>::Norm²Type>>;
 
+//TODO(phl): Could this be a self-standing function?
 template<typename Scalar, typename Argument>
 class MultiLevelSingleLinkage {
  public:
@@ -43,16 +45,24 @@ class MultiLevelSingleLinkage {
   MultiLevelSingleLinkage(
       Box const& box,
       Field<Scalar, Argument> const& f,
-      Field<Gradient<Scalar, Argument>, Argument> const& grad_f,
-      std::int64_t values_per_round);
+      Field<Gradient<Scalar, Argument>, Argument> const& grad_f);
+
+  void FindGlobalMinimum(std::int64_t values_per_round,
+                         std::int64_t number_of_rounds) const;
 
  private:
-  // The distribution must have bounds [-1, 1].  Returns a vector of size
-  // |values_per_round|.
-  std::vector<Argument> GenerateArguments(
-      Box const& box,
-      std::int64_t values_per_round,
-      std::uniform_real_distribution<>& distribution);
+  // Returns a vector of size |values_per_round|.
+  std::vector<Argument> GenerateArguments(Box const& box,
+                                          std::int64_t values_per_round);
+
+  // Returns the radius rₖ from [] specialized for 3 dimensions.
+  static typename Hilbert<Difference<Argument>>::NormType rₖ(double σ,
+                                                             std::int64_t kN);
+
+  Box const box_;
+  Cube<typename Hilbert<Difference<Argument>>::NormType> const box_measure_;
+  Field<Scalar, Argument> const f_;
+  Field<Gradient<Scalar, Argument>, Argument> const grad_f_;
 
   std::mt19937_64 random_;
   std::uniform_real_distribution<> distribution_;
--- numerics/global_optimization_body.hpp
+++ numerics/global_optimization_body.hpp
@@ -1,9 +1,11 @@
-#pragma once
+﻿#pragma once
+
+#include "numerics/global_optimization.hpp"
 
 #include <utility>
 
 #include "geometry/barycentre_calculator.hpp"
-#include "numerics/global_optimization.hpp"
+#include "numerics/cbrt.hpp"
 
 namespace principia {
 namespace numerics {
@@ -15,17 +17,31 @@ template<typename Scalar, typename Argument>
 MultiLevelSingleLinkage<Scalar, Argument>::MultiLevelSingleLinkage(
     Box const& box,
     Field<Scalar, Argument> const& f,
-    Field<Gradient<Scalar, Argument>, Argument> const& grad_f,
-    std::int64_t const values_per_round)
-    : random_(42),
+    Field<Gradient<Scalar, Argument>, Argument> const& grad_f)
+    : box_(box),
+      box_measure_(8 * Wedge(box_.vertices[0],
+                             Wedge(box_.vertices[1], box_.vertices[2])).Norm()),
+      f_(f),
+      grad_f_(grad_f),
+      random_(42),
       distribution_(-1.0, 1.0) {}
+
+template<typename Scalar, typename Argument>
+void MultiLevelSingleLinkage<Scalar, Argument>::FindGlobalMinimum(
+    std::int64_t const values_per_round,
+    std::int64_t const number_of_rounds) const {
+  std::vector<Argument> arguments;
+  for (std::int64_t k = 0; k < number_of_rounds; ++k) {
+    std::vector<Argument> const arguments_k =
+        GenerateArguments(box_, values_per_round);
+  }
+}
 
 template<typename Scalar, typename Argument>
 std::vector<Argument>
 MultiLevelSingleLinkage<Scalar, Argument>::GenerateArguments(
     Box const& box,
-    std::int64_t const values_per_round,
-    std::uniform_real_distribution<>& distribution) {
+    std::int64_t const values_per_round) {
   std::vector<Argument> result;
   for (std::int64_t i = 0; i < values_per_round; ++i) {
     Argument argument = box.centre;
@@ -35,6 +51,13 @@ MultiLevelSingleLinkage<Scalar, Argument>::GenerateArguments(
     result.push_back(argument);
   }
   return result;
+}
+
+template<typename Scalar, typename Argument>
+typename Hilbert<Difference<Argument>>::NormType
+MultiLevelSingleLinkage<Scalar, Argument>::rₖ(double const σ,
+                                              std::int64_t const kN) {
+  return Cbrt(3.0 * box_measure_ * std::log(kN) / (4.0 * π * kN));
 }
 
 }  // namespace internal_global_optimization
