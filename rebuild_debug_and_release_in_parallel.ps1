ff90c0af86673d8965550e52eecab15d54c76096
pleroy <pascal.h.leroy@gmail.com>
2023-02-22 20:15:14 +0100

    Nugget.

--- rebuild_debug_and_release_in_parallel.ps1
+++ rebuild_debug_and_release_in_parallel.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 $msbuild = &".\find_msbuild.ps1"
 
 &$msbuild            `
-    /t:"Clean;Build" `
+    /t:"Clean;Restore;Build" `
     /m               `
     .\debug_and_release_in_parallel.xml
 
