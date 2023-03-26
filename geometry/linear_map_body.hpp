bb3873b5e6cf19ca2a773232facc1aa78d210d67
pleroy <pascal.h.leroy@gmail.com>
2023-03-26 14:43:28 +0200

    A contract for LinearMaps.

--- geometry/linear_map_body.hpp
+++ geometry/linear_map_body.hpp
@@ -8,6 +8,25 @@ namespace _linear_map {
 namespace internal {
 
 template<typename Map, typename FromFrame, typename ToFrame>
+static Map LinearMap<Map, FromFrame, ToFrame>::Identity() {
+  return Map::Identity();
+}
+
+template<typename Map, typename FromFrame, typename ToFrame>
+template<typename Scalar>
+Vector<Scalar, ToFrame> LinearMap<Map, FromFrame, ToFrame>::operator()(
+    Vector<Scalar, FromFrame> const& vector) const {
+  return static_cast<Map>(*this)(vector);
+}
+
+template<typename Map, typename FromFrame, typename ToFrame>
+template<typename T>
+typename base::Mappable<Map, T>::type
+LinearMap<Map, FromFrame, ToFrame>::operator()(T const& t) const {
+  return base::Mappable<Map, T>::Do(static_cast<Map>(*this), t);
+}
+
+template<typename Map, typename FromFrame, typename ToFrame>
 void LinearMap<Map, FromFrame, ToFrame>::WriteToMessage(
     not_null<serialization::LinearMap*> const message) {
   FromFrame::WriteToMessage(message->mutable_from_frame());
