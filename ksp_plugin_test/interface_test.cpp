084c61eec013a88d0abe569858c95e0aea0e55ac
Robin Leroy <egg.robin.leroy@gmail.com>
2023-07-09 14:23:23 +0200

    per-file errors

--- ksp_plugin_test/interface_test.cpp
+++ ksp_plugin_test/interface_test.cpp
@@ -41,7 +41,6 @@
 #include "quantities/named_quantities.hpp"
 #include "quantities/quantities.hpp"
 #include "quantities/si.hpp"
-#include "testing_utilities/almost_equals.hpp"
 #include "testing_utilities/matchers.hpp"
 #include "testing_utilities/serialization.hpp"
 
