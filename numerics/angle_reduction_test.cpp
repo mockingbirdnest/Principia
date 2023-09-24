2b89245641751ef0374e927737f5f236727f70a6
pleroy <pascal.h.leroy@gmail.com>
2023-09-24 15:02:01 +0200

    Even more tests.

--- numerics/angle_reduction_test.cpp
+++ numerics/angle_reduction_test.cpp
@@ -12,6 +12,33 @@ using namespace principia::quantities::_quantities;
 using namespace principia::quantities::_si;
 using namespace principia::testing_utilities::_almost_equals;
 
+TEST(AngleReductionTest, SingleMinusπOver2ToπOver2) {
+  Angle fractional_part;
+  std::int64_t integer_part;
+
+  ReduceAngle<-π / 2, π / 2>(1 * Radian, fractional_part, integer_part);
+  EXPECT_THAT(fractional_part, AlmostEquals(1 * Radian, 0));
+  EXPECT_EQ(0, integer_part);
+
+  ReduceAngle<-π / 2, π / 2>(-1 * Radian, fractional_part, integer_part);
+  EXPECT_THAT(fractional_part, AlmostEquals(-1 * Radian, 0));
+  EXPECT_EQ(0, integer_part);
+
+  ReduceAngle<-π / 2, π / 2>(2 * Radian, fractional_part, integer_part);
+  EXPECT_THAT(fractional_part, AlmostEquals((2 - π) * Radian, 0));
+  EXPECT_EQ(1, integer_part);
+
+  ReduceAngle<-π / 2, π / 2>(-2 * Radian, fractional_part, integer_part);
+  EXPECT_THAT(fractional_part, AlmostEquals((π - 2) * Radian, 0));
+  EXPECT_EQ(-1, integer_part);
+
+  // 355/113 is hard, let's go shopping.
+  ReduceAngle<-π / 2, π / 2>(355 * Radian, fractional_part, integer_part);
+  EXPECT_THAT(fractional_part,
+              AlmostEquals((355 - 113 * π) * Radian, 9451283));
+  EXPECT_EQ(113, integer_part);
+}
+
 TEST(AngleReductionTest, SingleMinusπToπ) {
   Angle fractional_part;
   std::int64_t integer_part;
