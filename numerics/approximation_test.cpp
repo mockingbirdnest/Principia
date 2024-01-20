dd30c03b02fb95b88ee7010981efab3b7ade4825
pleroy <pascal.h.leroy@gmail.com>
2024-01-20 15:30:43 +0100

    Adaptive subdivision.

--- numerics/approximation_test.cpp
+++ numerics/approximation_test.cpp
@@ -2,9 +2,11 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "numerics/чебышёв_series.hpp"
 #include "quantities/elementary_functions.hpp"
 #include "quantities/named_quantities.hpp"
 #include "quantities/si.hpp"
+#include "testing_utilities/almost_equals.hpp"
 #include "testing_utilities/approximate_quantity.hpp"
 #include "testing_utilities/is_near.hpp"
 #include "testing_utilities/numerics_matchers.hpp"
@@ -13,15 +15,20 @@ namespace principia {
 namespace numerics {
 namespace _approximation {
 
+using ::testing::AllOf;
+using ::testing::ElementsAre;
+using ::testing::Ge;
+using ::testing::Lt;
+using ::testing::Property;
+using ::testing::SizeIs;
+using namespace principia::numerics::_чебышёв_series;
 using namespace principia::quantities::_elementary_functions;
 using namespace principia::quantities::_named_quantities;
 using namespace principia::quantities::_si;
+using namespace principia::testing_utilities::_almost_equals;
 using namespace principia::testing_utilities::_approximate_quantity;
 using namespace principia::testing_utilities::_is_near;
 using namespace principia::testing_utilities::_numerics_matchers;
-using ::testing::AllOf;
-using ::testing::Ge;
-using ::testing::Lt;
 
 TEST(ApproximationTest, SinInverse) {
   auto const f = [](double const x) { return Sin(1 * Radian / x); };
@@ -55,6 +62,84 @@ TEST(ApproximationTest, Exp) {
   for (double x = 0.01; x < 3; x += 0.01) {
     EXPECT_THAT(approximation.Evaluate(x),
                 AbsoluteErrorFrom(f(x), AllOf(Lt(7.2e-15), Ge(0))));
+  }
+}
+
+TEST(ApproximationTest, AdaptiveSinInverse) {
+  auto const f = [](double const x) { return Sin(1 * Radian / x); };
+  double error_estimate;
+  auto const approximations =
+      AdaptiveЧебышёвPolynomialInterpolant<8>(f,
+                                              /*lower_bound=*/0.1,
+                                              /*upper_bound=*/10.0,
+                                              /*max_error=*/1e-6,
+                                              &error_estimate);
+  EXPECT_THAT(error_estimate, IsNear(7.1e-7_(1)));
+  EXPECT_THAT(approximations, SizeIs(11));
+  EXPECT_THAT(
+      approximations,
+      ElementsAre(AllOf(Property(&ЧебышёвSeries<double, double>::lower_bound,
+                                 AlmostEquals(0.1, 0)),
+                        Property(&ЧебышёвSeries<double, double>::upper_bound,
+                                 AlmostEquals(0.1 + 9.9 / 512, 0)),
+                        Property(&ЧебышёвSeries<double, double>::degree, 8)),
+                  AllOf(Property(&ЧебышёвSeries<double, double>::lower_bound,
+                                 AlmostEquals(0.1 + 9.9 / 512, 0)),
+                        Property(&ЧебышёвSeries<double, double>::upper_bound,
+                                 AlmostEquals(0.1 + 9.9 / 256, 0)),
+                        Property(&ЧебышёвSeries<double, double>::degree, 8)),
+                  AllOf(Property(&ЧебышёвSeries<double, double>::lower_bound,
+                                 AlmostEquals(0.1 + 9.9 / 256, 0)),
+                        Property(&ЧебышёвSeries<double, double>::upper_bound,
+                                 AlmostEquals(0.1 + 9.9 / 128, 0)),
+                        Property(&ЧебышёвSeries<double, double>::degree, 8)),
+                  AllOf(Property(&ЧебышёвSeries<double, double>::lower_bound,
+                                 AlmostEquals(0.1 + 9.9 / 128, 0)),
+                        Property(&ЧебышёвSeries<double, double>::upper_bound,
+                                 AlmostEquals(0.1 + 9.9 / 64, 0)),
+                        Property(&ЧебышёвSeries<double, double>::degree, 8)),
+                  AllOf(Property(&ЧебышёвSeries<double, double>::lower_bound,
+                                 AlmostEquals(0.1 + 9.9 / 64, 0)),
+                        Property(&ЧебышёвSeries<double, double>::upper_bound,
+                                 AlmostEquals(0.1 + 9.9 / 32, 1)),
+                        Property(&ЧебышёвSeries<double, double>::degree, 8)),
+                  AllOf(Property(&ЧебышёвSeries<double, double>::lower_bound,
+                                 AlmostEquals(0.1 + 9.9 / 32, 1)),
+                        Property(&ЧебышёвSeries<double, double>::upper_bound,
+                                 AlmostEquals(0.1 + 9.9 * 3 / 64, 0)),
+                        Property(&ЧебышёвSeries<double, double>::degree, 8)),
+                  AllOf(Property(&ЧебышёвSeries<double, double>::lower_bound,
+                                 AlmostEquals(0.1 + 9.9 * 3 / 64, 0)),
+                        Property(&ЧебышёвSeries<double, double>::upper_bound,
+                                 AlmostEquals(0.1 + 9.9 / 16, 0)),
+                        Property(&ЧебышёвSeries<double, double>::degree, 8)),
+                  AllOf(Property(&ЧебышёвSeries<double, double>::lower_bound,
+                                 AlmostEquals(0.1 + 9.9 / 16, 0)),
+                        Property(&ЧебышёвSeries<double, double>::upper_bound,
+                                 AlmostEquals(0.1 + 9.9 / 8, 1)),
+                        Property(&ЧебышёвSeries<double, double>::degree, 8)),
+                  AllOf(Property(&ЧебышёвSeries<double, double>::lower_bound,
+                                 AlmostEquals(0.1 + 9.9 / 8, 1)),
+                        Property(&ЧебышёвSeries<double, double>::upper_bound,
+                                 AlmostEquals(0.1 + 9.9 / 4, 1)),
+                        Property(&ЧебышёвSeries<double, double>::degree, 8)),
+                  AllOf(Property(&ЧебышёвSeries<double, double>::lower_bound,
+                                 AlmostEquals(0.1 + 9.9 / 4, 1)),
+                        Property(&ЧебышёвSeries<double, double>::upper_bound,
+                                 AlmostEquals(0.1 + 9.9 / 2, 0)),
+                        Property(&ЧебышёвSeries<double, double>::degree, 8)),
+                  AllOf(Property(&ЧебышёвSeries<double, double>::lower_bound,
+                                 AlmostEquals(0.1 + 9.9 / 2, 0)),
+                        Property(&ЧебышёвSeries<double, double>::upper_bound,
+                                 AlmostEquals(10.0, 0)),
+                        Property(&ЧебышёвSeries<double, double>::degree, 8))));
+  for (auto const& approximation : approximations) {
+    for (double x = approximation.lower_bound();
+         x < approximation.upper_bound();
+         x += 0.01) {
+      EXPECT_THAT(approximation.Evaluate(x),
+                  AbsoluteErrorFrom(f(x), AllOf(Lt(6.2e-7), Ge(2.7e-17))));
+    }
   }
 }
 
