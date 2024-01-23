33e869c7c514d8f6bcc51ce9d11cf03cf7ae7921
pleroy <pascal.h.leroy@gmail.com>
2024-01-23 22:18:04 +0100

    Termination predicate instead of max_error.

--- numerics/approximation.hpp
+++ numerics/approximation.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <type_traits>
 #include <vector>
 
@@ -17,35 +18,43 @@ using namespace principia::quantities::_named_quantities;
 template<typename Argument, typename Function>
 using Value = std::invoke_result_t<Function, Argument>;
 
+// A function that returns true iff the interpolant search should terminate
+// because it has reached an acceptable solution.  A simple predicate can just
+// check if |error_estimate| is below some threshold.
+template<typename Value, typename Argument>
+using TerminationPredicate =
+    std::function<bool(ЧебышёвSeries<Value, Argument> const& interpolant,
+                       Difference<Value> const& error_estimate)>;
+
 // Returns a Чебышёв polynomial interpolant of f over
-// [lower_bound, upper_bound].  Stops if the absolute error is estimated to be
-// below |max_error| or if |max_degree| has been reached.  If |error_estimate|
-// is nonnull, it receives the estimate of the L∞ error.
+// [lower_bound, upper_bound].  Stops if the termination predicate |done|
+// returns true or if |max_degree| has been reached.  If |error_estimate| is
+// nonnull, it receives the estimate of the L∞ error.
 template<int max_degree, typename Argument, typename Function>
 ЧебышёвSeries<Value<Argument, Function>, Argument> ЧебышёвPolynomialInterpolant(
     Function const& f,
     Argument const& lower_bound,
     Argument const& upper_bound,
-    Difference<Value<Argument, Function>> const& max_error,
+    TerminationPredicate<Value<Argument, Function>, Argument> const& done,
     Difference<Value<Argument, Function>>* error_estimate = nullptr);
 
 // Returns an ordered vector of Чебышёв polynomial interpolants of f, which
 // together cover [lower_bound, upper_bound].  Subdivides the interval so that
-// the degree of each approximant doesn't exceed |max_degree|.  The final
-// (estimated) absolute error is guaranteed to be below |max_error|.
+// the degree of each approximant doesn't exceed |max_degree|.
 template<int max_degree, typename Argument, typename Function>
 std::vector<ЧебышёвSeries<Value<Argument, Function>, Argument>>
 AdaptiveЧебышёвPolynomialInterpolant(
     Function const& f,
     Argument const& lower_bound,
     Argument const& upper_bound,
-    Difference<Value<Argument, Function>> const& max_error,
+    TerminationPredicate<Value<Argument, Function>, Argument> const& done,
     Difference<Value<Argument, Function>>* error_estimate = nullptr);
 
 }  // namespace internal
 
 using internal::AdaptiveЧебышёвPolynomialInterpolant;
 using internal::ЧебышёвPolynomialInterpolant;
+using internal::TerminationPredicate;///REMOVE
 
 }  // namespace _approximation
 }  // namespace numerics
