9bc26e7a0047e00b281e499008f41fb92bb9ec98
pleroy <pascal.h.leroy@gmail.com>
2024-01-27 14:30:44 +0100

    A benchmark.

---
+++ benchmarks/approximation.cpp
@@ -0,0 +1,102 @@
+// .\Release\x64\benchmarks.exe --benchmark_repetitions=3 --benchmark_filter=ApproximationBenchmark --benchmark_min_time=30  // NOLINT(whitespace/line_length)
+
+#include "numerics/approximation.hpp"
+
+#include <random>
+
+#include "benchmark/benchmark.h"
+#include "geometry/instant.hpp"
+#include "numerics/чебышёв_series.hpp"
+#include "quantities/named_quantities.hpp"
+#include "quantities/quantities.hpp"
+#include "quantities/si.hpp"
+
+namespace principia {
+namespace numerics {
+
+using namespace principia::geometry::_instant;
+using namespace principia::numerics::_approximation;
+using namespace principia::numerics::_чебышёв_series;
+using namespace principia::quantities::_elementary_functions;
+using namespace principia::quantities::_named_quantities;
+using namespace principia::quantities::_quantities;
+using namespace principia::quantities::_si;
+
+constexpr int64_t number_of_angular_frequencies = 5;
+
+template<int max_degree>
+class ApproximationBenchmark : public benchmark::Fixture {
+ protected:
+  void SetUp(benchmark::State& state) override {
+    std::mt19937_64 random(42);
+    std::uniform_real_distribution<> coefficients(0.0, 10.0);
+    std::uniform_real_distribution<> angular_frequency(0.0, 100.0);
+    for (std::int64_t i = 0; i < number_of_angular_frequencies; ++i) {
+      cos_coefficients_.push_back(coefficients(random) * Metre);
+      sin_coefficients_.push_back(coefficients(random) * Metre);
+      angular_frequencies_.push_back(angular_frequency(random) * Radian /
+                                     Second);
+    }
+  }
+
+  Length ComputeAltitude(Instant const& t) {
+    Length altitude{};
+    for (std::int64_t i = 0; i < number_of_angular_frequencies; ++i) {
+      auto const& angular_frequency = angular_frequencies_[i];
+      altitude += cos_coefficients_[i] * Cos(angular_frequency * (t - t0_)) +
+                  sin_coefficients_[i] * Sin(angular_frequency * (t - t0_));
+    }
+    return altitude;
+  }
+
+  void ComputeAdaptiveЧебышёвPolynomialInterpolant(benchmark::State& state) {
+    auto const altitude = [this](Instant const& t) {
+      return ComputeAltitude(t);
+    };
+    SubdivisionPredicate<Length, Instant> const subdivide =
+        [](auto const& interpolant, Length const& error_estimate) -> bool {
+      return interpolant.MayHaveRealRoots(error_estimate);
+    };
+
+    std::vector<ЧебышёвSeries<Length, Instant>> interpolants;
+    for (auto _ : state) {
+      interpolants = AdaptiveЧебышёвPolynomialInterpolant<max_degree>(
+          altitude,
+          /*lower_bound=*/t0_,
+          /*upper_bound=*/t0_ + 1000.0 * Second,
+          /*max_error=*/1.0 * Metre,
+          subdivide,
+          /*error_estimate=*/nullptr);
+      benchmark::DoNotOptimize(interpolants);
+    }
+  }
+
+  Instant const t0_;
+  std::vector<Length> cos_coefficients_;
+  std::vector<Length> sin_coefficients_;
+  std::vector<AngularFrequency> angular_frequencies_;
+};
+
+BENCHMARK_TEMPLATE_F(ApproximationBenchmark,
+                     AdaptiveЧебышёвPolynomialInterpolant4,
+                     4)(benchmark::State& state) {
+  ComputeAdaptiveЧебышёвPolynomialInterpolant(state);
+}
+BENCHMARK_TEMPLATE_F(ApproximationBenchmark,
+                     AdaptiveЧебышёвPolynomialInterpolant8,
+                     8)(benchmark::State& state) {
+  ComputeAdaptiveЧебышёвPolynomialInterpolant(state);
+}
+BENCHMARK_TEMPLATE_F(ApproximationBenchmark,
+                     AdaptiveЧебышёвPolynomialInterpolant16,
+                     16)(benchmark::State& state) {
+  ComputeAdaptiveЧебышёвPolynomialInterpolant(state);
+}
+BENCHMARK_TEMPLATE_F(ApproximationBenchmark,
+                     AdaptiveЧебышёвPolynomialInterpolant32,
+                     32)(benchmark::State& state) {
+  ComputeAdaptiveЧебышёвPolynomialInterpolant(state);
+}
+
+}  // namespace numerics
+}  // namespace principia
