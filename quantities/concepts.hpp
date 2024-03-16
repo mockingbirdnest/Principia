120a89b6b1cffdb86d2a646044fef9ca0da89dab
Robin Leroy <egg.robin.leroy@gmail.com>
2024-03-16 19:01:59 +0100

    remove_const

--- quantities/concepts.hpp
+++ quantities/concepts.hpp
@@ -16,7 +16,7 @@ using namespace quantities::_quantities;
 
 template<typename T>
 concept quantity = std::integral<T> || std::floating_point<T> ||
-                   is_instance_of_v<Quantity, T>;
+                   is_instance_of_v<Quantity, std::remove_const_t<T>>;
 
 }  // namespace internal
 
