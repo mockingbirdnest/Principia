97a2aefc99c5c2cb078ee87d4bf0849aca2159b6
Robin Leroy <egg.robin.leroy@gmail.com>
2024-03-27 03:09:46 +0100

    more tests

--- quantities/concepts_test.cpp
+++ quantities/concepts_test.cpp
@@ -27,9 +27,11 @@ TEST(Traits, IsQuantityV) {
 }
 
 TEST(Traits, Algebra) {
+  static_assert(!affine<std::string>);
   static_assert(affine<std::byte*>);
   static_assert(affine<FixedMatrix<double, 2, 3>>);
   static_assert(affine<FixedMatrix<double, 3, 3>>);
+  static_assert(affine<FixedMatrix<Length, 3, 3>>);
   static_assert(affine<Length>);
   static_assert(affine<int>);
   static_assert(affine<double>);
@@ -38,28 +40,45 @@ TEST(Traits, Algebra) {
   static_assert(!additive_group<std::byte*>);
   static_assert(additive_group<FixedMatrix<double, 2, 3>>);
   static_assert(additive_group<FixedMatrix<double, 3, 3>>);
+  static_assert(additive_group<FixedMatrix<Length, 3, 3>>);
   static_assert(additive_group<Length>);
   static_assert(additive_group<int>);
   static_assert(additive_group<double>);
 
   static_assert(!homogeneous_ring<FixedMatrix<double, 2, 3>>);
   static_assert(homogeneous_ring<FixedMatrix<double, 3, 3>>);
+  static_assert(homogeneous_ring<FixedMatrix<Length, 3, 3>>);
   static_assert(homogeneous_ring<Length>);
   static_assert(homogeneous_ring<int>);
   static_assert(homogeneous_ring<double>);
 
   static_assert(ring<FixedMatrix<double, 3, 3>>);
+  static_assert(!ring<FixedMatrix<Length, 3, 3>>);
   static_assert(!ring<Length>);
   static_assert(ring<int>);
   static_assert(ring<double>);
 
   static_assert(!homogeneous_field<FixedMatrix<double, 3, 3>>);
+  static_assert(!homogeneous_field<FixedMatrix<Length, 3, 3>>);
   static_assert(homogeneous_field<Length>);
   static_assert(!homogeneous_field<int>);
   static_assert(homogeneous_field<double>);
 
   static_assert(!field<Length>);
   static_assert(field<double>);
+}
+
+TEST(Traits, LinearAgebra) {
+  static_assert(!homogeneous_vector_space<FixedMatrix<double, 3, 3>,
+                                          FixedMatrix<double, 3, 3>>);
+  static_assert(homogeneous_vector_space<Length, Length>);
+  static_assert(!vector_space<Length, Length>);
+  static_assert(vector_space<double, double>);
+
+  static_assert(homogeneous_vector_space<FixedMatrix<Length, 3, 3>, Time>);
+  static_assert(real_vector_space<FixedMatrix<Length, 3, 3>>);
+  static_assert(!vector_space<FixedMatrix<Length, 3, 3>, Time>);
+  static_assert(!real_vector_space<int>);
 }
 
 }  // namespace quantities
