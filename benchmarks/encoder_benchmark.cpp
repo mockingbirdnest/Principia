94f9c3299350ee56dd3932ac00d0627d23f11bd3
Robin Leroy <egg.robin.leroy@gmail.com>
2024-03-31 00:37:08 +0100

    Down with benchmark_min_time

--- benchmarks/encoder_benchmark.cpp
+++ benchmarks/encoder_benchmark.cpp
@@ -1,4 +1,4 @@
-// .\Release\x64\benchmarks.exe --benchmark_min_time=2 --benchmark_repetitions=10 --benchmark_filter=(Encode|Decode)  // NOLINT(whitespace/line_length)
+// .\Release\x64\benchmarks.exe  --benchmark_repetitions=10 --benchmark_filter=(Encode|Decode)  // NOLINT(whitespace/line_length)
 
 #include <cstdint>
 #include <random>
