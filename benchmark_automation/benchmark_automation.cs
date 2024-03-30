e127e49e8e906a4631fba43e8e990ac7ef0fb082
Robin Leroy <egg.robin.leroy@gmail.com>
2024-03-30 13:19:34 +0100

    Useless using

--- benchmark_automation/benchmark_automation.cs
+++ benchmark_automation/benchmark_automation.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 
 namespace principia {
 namespace benchmark_automation {
