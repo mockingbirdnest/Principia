05627bdb38746f8e9ceecc39ca9f50c0c2977002
Robin Leroy <egg.robin.leroy@gmail.com>
2024-04-01 19:09:29 +0200

    format

--- testing_utilities/check_well_formedness.hpp
+++ testing_utilities/check_well_formedness.hpp
@@ -5,29 +5,26 @@
 #define PRINCIPIA_CONCATENATE_EXPANSION(s1, s2) PRINCIPIA_CONCATENATE(s1, s2)
 
 #define PRINCIPIA_CHECK_WELL_FORMEDNESS(unique_concept_name,                  \
-                                         expected_well_formedness,                    \
-                                         expected_well_formedness_description,        \
-                                         expression, \
-                                         ...)                           \
-  template<template<typename> typename WITH = std::type_identity_t>               \
-  concept unique_concept_name =                                                \
-      requires(__VA_ARGS__) { \
-        (expression);                                                          \
-      };                                                                       \
-  static_assert(expected_well_formedness unique_concept_name<>,             \
-                "Expected\n  " #expression   \
+                                        expected_well_formedness,             \
+                                        expected_well_formedness_description, \
+                                        expression,                           \
+                                        ...)                                  \
+  template<template<typename> typename WITH = std::type_identity_t>           \
+  concept unique_concept_name = requires(__VA_ARGS__) { (expression); };      \
+  static_assert(expected_well_formedness unique_concept_name<>,               \
+                "Expected\n  " #expression                                    \
                 "\n" expected_well_formedness_description)
-#define PRINCIPIA_CHECK_ILL_FORMED(expression, ...) \
-  PRINCIPIA_CHECK_WELL_FORMEDNESS(                                   \
-      PRINCIPIA_CONCATENATE_EXPANSION(test_concept_, __COUNTER__),    \
-      !,                                                              \
-      "not to compile",                                               \
-      expression,                                                     \
-      __VA_ARGS__)
-#define PRINCIPIA_CHECK_WELL_FORMED(expression, ...) \
+#define PRINCIPIA_CHECK_ILL_FORMED(expression, ...)                \
   PRINCIPIA_CHECK_WELL_FORMEDNESS(                                 \
-      PRINCIPIA_CONCATENATE_EXPANSION(test_concept_, __COUNTER__),  \
-      ,                                                             \
-      "to compile",                                                 \
-      expression, \
+      PRINCIPIA_CONCATENATE_EXPANSION(test_concept_, __COUNTER__), \
+      !,                                                           \
+      "not to compile",                                            \
+      expression,                                                  \
+      __VA_ARGS__)
+#define PRINCIPIA_CHECK_WELL_FORMED(expression, ...)               \
+  PRINCIPIA_CHECK_WELL_FORMEDNESS(                                 \
+      PRINCIPIA_CONCATENATE_EXPANSION(test_concept_, __COUNTER__), \
+      ,                                                            \
+      "to compile",                                                \
+      expression,                                                  \
       __VA_ARGS__)
