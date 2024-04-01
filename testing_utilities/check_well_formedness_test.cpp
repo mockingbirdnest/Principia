f83d132dfcd8be8e5dab25176393841d4f4bb280
Robin Leroy <egg.robin.leroy@gmail.com>
2024-04-01 18:54:27 +0200

    A macro to check that some constructs are ill-formed.

---
+++ testing_utilities/check_well_formedness_test.cpp
@@ -0,0 +1,8 @@
+#include "testing_utilities/check_well_formedness.hpp"
+
+#include <string>
+
+PRINCIPIA_CHECK_ILL_FORMED(s + 3, WITH<std::string> s);
+PRINCIPIA_CHECK_ILL_FORMED(s + "3", WITH<std::pair<double, double>> s);
+PRINCIPIA_CHECK_WELL_FORMED(s + t, WITH<std::string> s, WITH<std::string> t);
+PRINCIPIA_CHECK_ILL_FORMED(s - t, WITH<std::string> s, WITH<std::string> t);
