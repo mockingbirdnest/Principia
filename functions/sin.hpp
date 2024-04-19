4e4912ec8c7af96114066aafbf34e645b922fc71
pleroy <pascal.h.leroy@gmail.com>
2024-04-19 19:28:49 +0200

    A header and a test.

---
+++ functions/sin.hpp
@@ -0,0 +1,14 @@
+namespace principia {
+namespace functions {
+namespace _sin {
+namespace internal {
+
+double cr_sin(double x);
+
+}  // namespace internal
+
+using internal::cr_sin;
+
+}  // namespace _sin
+}  // namespace functions
+}  // namespace principia
