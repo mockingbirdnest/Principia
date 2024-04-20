6562b409cf1961e36e80d2c0b4f9e7d9526d47d2
pleroy <pascal.h.leroy@gmail.com>
2024-04-20 09:45:19 +0200

    Fix compilation errors and add a header.

---
+++ functions/cos.hpp
@@ -0,0 +1,14 @@
+namespace principia {
+namespace functions {
+namespace _cos {
+namespace internal {
+
+double __cdecl cr_cos(double x);
+
+}  // namespace internal
+
+using internal::cr_cos;
+
+}  // namespace _cos
+}  // namespace functions
+}  // namespace principia
