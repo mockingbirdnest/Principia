a8516c2690b3c71c038b73776b0e4066e9328c52
Pascal Leroy <pascal.h.leroy@gmail.com>
2024-08-04 12:18:32 +0200

    Revert "An implementation of Nguyễn and Stehlé's L2 algorithm for lattice reduction"

--- numerics/lattices_test.cpp
+++ numerics/lattices_test.cpp
@@ -17,7 +17,7 @@ using namespace principia::testing_utilities::_almost_equals;
 class LatticesTest : public ::testing::Test {};
 
 // [HPS14], example 7.75.
-TEST_F(LatticesTest, LLL_Example_7_75) {
+TEST_F(LatticesTest, Example_7_75) {
   FixedMatrix<double, 6, 6> l({19, 15, 43, 20,  0, 48,
                                 2, 42, 15, 44, 48, 33,
                                32, 11,  0, 44, 35, 32,
@@ -37,7 +37,7 @@ TEST_F(LatticesTest, LLL_Example_7_75) {
                   0));
 }
 
-TEST_F(LatticesTest, LLL_Rational) {
+TEST_F(LatticesTest, Rational) {
   FixedMatrix<cpp_rational, 5, 4> l({
       45,         0, 214695880217044191, 401754430875619365,
        0, 188743680,          187081485,           -6248472,
@@ -47,50 +47,10 @@ TEST_F(LatticesTest, LLL_Rational) {
 
   auto const reduced = LenstraLenstraLovász(l);
   EXPECT_EQ(reduced,
-            (FixedMatrix<cpp_rational, 5, 4>({  45,     6, -18,   15,    0,
-                                                45, -1200, 348,    0,    0,
-                                                 0,     0,   0, 1083,  336,
+            (FixedMatrix<cpp_rational, 5, 4>({  45,     6, -18,   15,   0,
+                                                45, -1200, 348,    0,   0,
+                                                 0,     0,   0, 1083, 336,
                                               -660,     0, 165, -180, 1263})));
-}
-
-TEST_F(LatticesTest, NS_Example_7_75) {
-  FixedMatrix<double, 6, 6> l({19, 15, 43, 20,  0, 48,
-                                2, 42, 15, 44, 48, 33,
-                               32, 11,  0, 44, 35, 32,
-                               46,  0, 24,  0, 16,  9,
-                                3,  3,  4, 18, 31,  1,
-                               33, 24, 16, 15, 31, 29});
-
-  auto const reduced = NguyễnStehlé(l);
-  // Note that, even taking into account permutations and sign changes, this
-  // reduction is not the same as the one obtained by LLL: the last vector
-  // differs.  It appears to be a little longer and somewhat more orthogonal
-  // than the LLL one.
-  EXPECT_THAT(reduced,
-              AlmostEquals(
-                  (FixedMatrix<double, 6, 6>({ -7, -20,  6,  5,  -7,  -9,
-                                               12,   4,  7,  2,  -4, -19,
-                                                8,  -9, 20, 33,   9,   8,
-                                               -4,  16, 21,  0,  11,   6,
-                                              -19,  13, -8, 15,  -1, -29,
-                                               -9,  16, 12, -9, -31,  10})),
-                  0));
-}
-
-TEST_F(LatticesTest, NS_Int) {
-  FixedMatrix<cpp_int, 5, 4> l({
-      45,         0, 214695880217044191, 401754430875619365,
-       0, 188743680,          187081485,           -6248472,
-       0,         0,                  0,                  0,
-       0,         0,                  3,                  0,
-       0,         0,                  0,                  3});
-
-  auto const reduced = NguyễnStehlé(l);
-  EXPECT_EQ(reduced,
-            (FixedMatrix<cpp_int, 5, 4>({  45,     6, -18,   15,    0,
-                                           45, -1200, 348,    0,    0,
-                                            0,     0,   0, 1083,  336,
-                                         -660,     0, 165, -180, 1263})));
 }
 
 }  // namespace _lattices
