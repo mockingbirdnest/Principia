52fb8511da9bc85441ca95d89d49b848e8ec7567
pleroy <pascal.h.leroy@gmail.com>
2024-11-24 10:30:50 +0100

    A test that reproduces the problem.

--- base/push_pull_callback_test.cpp
+++ base/push_pull_callback_test.cpp
@@ -40,55 +40,25 @@ TEST(PushPullCallback, Test) {
   EXPECT_EQ(8, executor.get());
 }
 
-TEST(PushPullCallback, Repeat) {
-  std::mt19937_64 random(42);
-  std::uniform_real_distribution<> ordinate(-1.0, 1.0);
-
-  double y1;
-  double y2;
-
-  auto f = [&y1, &y2](double const x) {
-    // x = -1.0 -> y1; x = 1.0 -> y2.
-    return (x + 1.0) * (y2 - y1) / 2.0 + y1;
-  };
-
+TEST(PushPullCallback, 4136) {
   auto task =
-      [](std::function<double(double const x, void* const unused)> const& f)
+      [](std::function<double(int const x)> const& f)
       -> std::optional<double> {
-    static constexpr std::int64_t n = 10000;
-    double const f0 = f(0, nullptr);
-    for (std::int64_t i = 1; i <= n; ++i) {
-      double const fi = f(i / static_cast<double>(n), nullptr);
-      if ((f0 > 0 && fi < 0) || (f0 < 0 && fi > 0)) {
-        return i / static_cast<double>(n);
-      }
-    }
     return std::nullopt;
   };
 
-  for (std::int64_t attempt = 0; attempt < 1000; ++ attempt) {
-    y1 = ordinate(random);
-    y2 = ordinate(random);
-
+  for (std::int64_t attempt = 0; attempt < 100'000; ++attempt) {
     auto* const executor =
-        new PushPullExecutor<std::optional<double>, double, double, void*>(
-            task);
+        new PushPullExecutor<std::optional<double>, double, int>(task);
     for (;;) {
-      double x = -2;
-      void* unused;
-      bool const more = executor->callback().Pull(x, unused);
+      int x;
+      bool const more = executor->callback().Pull(x);
       if (!more) {
         auto const result = executor->get();
-        if (result.has_value()) {
-          LOG(ERROR) << attempt << ": " << result.value();
-        } else {
-          LOG(ERROR) << attempt << ": no result";
-        }
         delete executor;
         break;
       }
-      double y = f(x);
-      executor->callback().Push(y);
+      executor->callback().Push(1.0);
     }
   }
 }
