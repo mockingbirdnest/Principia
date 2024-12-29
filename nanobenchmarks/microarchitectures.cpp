c8ad1ebea9f0db4f6fbfca8d585bf3d086d80571
Robin Leroy <egg.robin.leroy@gmail.com>
2024-12-29 20:10:48 +0100

    μarch

---
+++ nanobenchmarks/microarchitectures.cpp
@@ -0,0 +1,58 @@
+#include "nanobenchmarks/microarchitectures.hpp"
+
+#include <regex>
+#include <vector>
+
+#include "base/cpuid.hpp"
+#include "glog/logging.h"
+
+BENCHMARK_EXTERN_C_FUNCTION(identity);
+BENCHMARK_EXTERN_C_FUNCTION(sqrtps_xmm0_xmm0);
+BENCHMARK_EXTERN_C_FUNCTION(sqrtsd_xmm0_xmm0);
+BENCHMARK_EXTERN_C_FUNCTION(mulsd_xmm0_xmm0);
+BENCHMARK_EXTERN_C_FUNCTION(mulsd_xmm0_xmm0_4x);
+
+namespace principia {
+namespace nanobenchmarks {
+namespace _microarchitectures {
+namespace internal {
+
+using namespace principia::base::_cpuid;
+
+namespace {
+static std::vector<
+    std::pair<std::regex, std::map<BenchmarkedFunction, int>>> const&
+    microarchitectures = *new std::vector{
+        // Skylake, Cascade Lake, Coffee Lake, Cannon Lake, Ice Lake, Tiger
+        // Lake, Golden Cove(?).
+        std::pair{std::regex(R"(((6|7|9|10|11|12)th Gen Intel\(R\) Core\(TM\))"
+                             R"(|Intel\(R\) Xeon\(R\) W-[23]).*)"),
+                  std::map{std::pair{&identity, 0},
+                           std::pair{&mulsd_xmm0_xmm0, 4},
+                           std::pair{&mulsd_xmm0_xmm0_4x, 4 * 4},
+                           std::pair{&sqrtps_xmm0_xmm0, 12}}},
+        // Zen3.
+        std::pair{std::regex("AMD Ryzen Threadripper PRO 5.*"),
+                  std::map{std::pair{&identity, 0},
+                           std::pair{&mulsd_xmm0_xmm0, 3},
+                           std::pair{&mulsd_xmm0_xmm0_4x, 4 * 3},
+                           std::pair{&sqrtps_xmm0_xmm0, 14}}}};
+}
+
+std::map<BenchmarkedFunction, int> const& ReferenceCycleCounts() {
+  static std::map<BenchmarkedFunction, int> const& result = [] {
+    for (auto const& [regex, architecture] : microarchitectures) {
+      if (std::regex_match(ProcessorBrandString(), regex)) {
+        return architecture;
+      }
+    }
+    LOG(FATAL) << "Unknown architecture " << CPUVendorIdentificationString()
+               << " " << ProcessorBrandString();
+  }();
+  return result;
+}
+
+}  // namespace internal
+}  // namespace _microarchitectures
+}  // namespace nanobenchmarks
+}  // namespace principia
---
+++ nanobenchmarks/microarchitectures.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <map>
+#include <string_view>
+
+#include "nanobenchmarks/function_registry.hpp"
+
+namespace principia {
+namespace nanobenchmarks {
+namespace _microarchitectures {
+namespace internal {
+
+using namespace principia::nanobenchmarks::_function_registry;
+
+std::map<BenchmarkedFunction, int> const& ReferenceCycleCounts();
+
+}  // namespace internal
+
+using internal::ReferenceCycleCounts;
+
+}  // namespace _microarchitectures
+}  // namespace nanobenchmarks
+}  // namespace principia
