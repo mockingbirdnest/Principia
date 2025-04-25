62a923549c469ea235e5df1c4e873f63408ea913
pleroy <pascal.h.leroy@gmail.com>
2025-04-25 17:06:20 +0200

    Next release is Lagrange.

--- ksp_plugin_adapter/main_window.cs
+++ ksp_plugin_adapter/main_window.cs
@@ -8,10 +8,10 @@ namespace ksp_plugin_adapter {
 
 internal class MainWindow : VesselSupervisedWindowRenderer {
   // Update this section before each release.
-  private const string next_release_name = "Lagny";
-  private const int next_release_lunation_number = 313;
+  private const string next_release_name = "Lagrange";
+  private const int next_release_lunation_number = 314;
   private readonly DateTimeOffset next_release_date_ =
-      new DateTimeOffset(2025, 04, 27, 19, 31, 09, TimeSpan.Zero);
+      new DateTimeOffset(2025, 05, 27, 03, 02, 21, TimeSpan.Zero);
 
   public MainWindow(PrincipiaPluginAdapter adapter,
                     FlightPlanner flight_planner,
