92ddac4e58d15efff6ee5a79589c7311a0aedcbf
pleroy <pascal.h.leroy@gmail.com>
2025-07-27 18:51:33 +0200

    c > 0.

--- numerics/double_precision.hpp
+++ numerics/double_precision.hpp
@@ -67,29 +67,29 @@ DoublePrecision<Product<T, U>> Scale(T const& scale,
 template<FMAPolicy fma_policy = FMAPolicy::Auto, typename T, typename U>
 DoublePrecision<Product<T, U>> TwoProduct(T const& a, U const& b);
 
-// Returns the exact value of `a * b + c` iff `c / 2 <= a * b + c <= 2 * c`.
-// See [SZ05], section 2.1.
+// Returns the exact value of `a * b + c` iff `c / 2 <= a * b + c <= 2 * c`,
+// assuming `c > 0`.  See [SZ05], section 2.1.
 template<FMAPolicy fma_policy = FMAPolicy::Auto, typename T, typename U>
 DoublePrecision<Product<T, U>> TwoProductAdd(T const& a,
                                              U const& b,
                                              Product<T, U> const& c);
 
-// Returns the exact value of `a * b - c` iff `-c / 2 <= a * b - c <= -2 * c`.
-// See [SZ05], section 2.1.
+// Returns the exact value of `a * b - c` iff `-2 * c <= a * b - c <= -c / 2`,
+// assuming `c > 0`.  See [SZ05], section 2.1.
 template<FMAPolicy fma_policy = FMAPolicy::Auto, typename T, typename U>
 DoublePrecision<Product<T, U>> TwoProductSubtract(T const& a,
                                                   U const& b,
                                                   Product<T, U> const& c);
 
-// Returns the exact value of `-a * b + c` iff `c / 2 <= -a * b + c <= 2 * c`.
-// See [SZ05], section 2.1.
+// Returns the exact value of `-a * b + c` iff `c / 2 <= -a * b + c <= 2 * c`,
+// assuming `c > 0`.  See [SZ05], section 2.1.
 template<FMAPolicy fma_policy = FMAPolicy::Auto, typename T, typename U>
 DoublePrecision<Product<T, U>> TwoProductNegatedAdd(T const& a,
                                                     U const& b,
                                                     Product<T, U> const& c);
 
-// Returns the exact value of `-a * b - c` iff `-c / 2 <= -a * b - c <= -2 * c`.
-// See [SZ05], section 2.1.
+// Returns the exact value of `-a * b - c` iff `-2 * c <= -a * b - c <= -c / 2`,
+// assuming `c > 0`.  See [SZ05], section 2.1.
 template<FMAPolicy fma_policy = FMAPolicy::Auto, typename T, typename U>
 DoublePrecision<Product<T, U>>
 TwoProductNegatedSubtract(T const& a,
