25dc0ab814d525c426aa9cf855291882cf8e83ef
pleroy <pascal.h.leroy@gmail.com>
2025-08-13 07:36:20 +0200

    Cantorian concepts.

--- quantities/concepts.hpp
+++ quantities/concepts.hpp
@@ -21,24 +21,25 @@ concept cpp_bin_float = is_number<T>::value &&
                         number_category<T>::value == number_kind_floating_point;
 
 template<typename T>
+concept discrete =
+    std::integral<T> || std::same_as<T, boost::multiprecision::cpp_int>;
+
+template<typename T>
 concept countable =
-    std::integral<T> || std::same_as<T, boost::multiprecision::cpp_int> ||
-    std::same_as<T, boost::multiprecision::cpp_rational>;
+    discrete<T> || std::same_as<T, boost::multiprecision::cpp_rational>;
 
 template<typename T>
-concept continuum =
-    instance<T, Quantity> || std::floating_point<T> || cpp_bin_float<T>;
+concept continuum = std::floating_point<T> || cpp_bin_float<T>;
 
 template<typename T>
-concept quantity = instance<T, Quantity> || std::same_as<T, double>;
+concept quantity = instance<T, Quantity> || continuum<T>;
 
 // std::integral || std::floating_point rather than
 // std::convertible_to<double, T> because
 // the latter introduces ambiguities on Sign * Vector.
 template<typename T>
 concept convertible_to_quantity =
-    quantity<std::remove_cvref_t<T>> ||
-    std::integral<std::remove_cvref_t<T>> ||
+    quantity<std::remove_cvref_t<T>> || std::integral<std::remove_cvref_t<T>> ||
     std::floating_point<std::remove_cvref_t<T>>;
 
 }  // namespace internal
