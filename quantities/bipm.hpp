8fec39127e014c6e6727911510c1740857ebc1d8
pleroy <pascal.h.leroy@gmail.com>
2025-08-17 20:20:48 +0200

    Inadvertent polluting of quantities.

--- quantities/bipm.hpp
+++ quantities/bipm.hpp
@@ -6,6 +6,8 @@
 
 namespace principia {
 namespace quantities {
+namespace _bipm {
+namespace internal {
 
 using namespace principia::quantities::_named_quantities;
 using namespace principia::quantities::_quantities;
@@ -14,8 +16,6 @@ using namespace principia::quantities::_si;
 // This namespace contains the other non-SI units listed in the BIPM's
 // SI brochure 8, section 4.1, table 8,
 // http://www.bipm.org/en/si/si_brochure/chapter4/table8.html.
-namespace _bipm {
-namespace internal {
 
 constexpr Pressure Bar                 = 1e5 * Pascal;
 constexpr Pressure MillimetreOfMercury = 133.322 * Pascal;
