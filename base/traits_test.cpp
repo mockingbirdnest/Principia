59185a981c94807258ca54dbf9887ca5a9de6e8a
pleroy <pascal.h.leroy@gmail.com>
2025-09-10 20:54:04 +0200

    A trait that works with aliases.

--- base/traits_test.cpp
+++ base/traits_test.cpp
@@ -11,15 +11,30 @@ struct T1;
 template<typename T>
 struct T2;
 
+template<typename T1, typename T2>
+struct T3;
+
 template<typename T>
 using AliasT1 = T1<T>;
 
+static_assert(internal::can_be_instantiated<AliasT1, double>::value);
+static_assert(!internal::can_be_instantiated<AliasT1, double, double>::value);
+
+static_assert(
+    internal::matches_instantiation<AliasT1<double>, true, AliasT1, double>::
+        value);
+static_assert(
+    internal::matches_instantiation<T1<double>, true, AliasT1, double>::value);
+static_assert(
+    internal::matches_instantiation<AliasT1<double>, true, T1, double>::value);
+static_assert(
+    internal::matches_instantiation<T1<double>, true, T1, double>::value);
+
 static_assert(is_instance_of_v<T1, T1<int>>);
+static_assert(is_instance_of_v<AliasT1, T1<int>>);
+static_assert(!is_instance_of_v<AliasT1, double>);
 static_assert(!is_instance_of_v<T2, T1<int>>);
-
-// Note that `is_instance_of` does not work on alias templates.
-static_assert(!is_instance_of_v<AliasT1, T1<int>>);
-
+static_assert(!is_instance_of_v<AliasT1, T3<double, double>>);
 
 }  // namespace base
 }  // namespace principia
