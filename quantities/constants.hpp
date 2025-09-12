fd74a86ddc1de33ccd63ceee988649e15e49fad6
pleroy <pascal.h.leroy@gmail.com>
2025-09-12 12:56:59 +0200

    What do we say to the God of CODATA?  Not today.

--- quantities/constants.hpp
+++ quantities/constants.hpp
@@ -25,13 +25,15 @@ constexpr Charge ElementaryCharge = 1.602'176'634e-19 * Coulomb;
 constexpr Entropy BoltzmannConstant =  1.380'649e-23 * (Joule / Kelvin);
 constexpr Inverse<Amount> AvogadroConstant = 6.022'140'76e23 * (1 / Mole);
 
-// We use the 2022 CODATA recommended values.  We do not support uncertainties.
-constexpr double FineStructureConstant = 7.297'352'5643e-3;
+// We use the 2014 CODATA recommended values.  We do not support uncertainties.
+// TODO(phl): Use the 2022 values.  But this requires changing many tests that
+// use mass instead of gravitational parameter.
+constexpr double FineStructureConstant = 7.297'352'5664e-3;
 constexpr Quotient<GravitationalParameter, Mass> GravitationalConstant =
-    6.674'30e-11 * Newton * Pow<2>(Metre) / Pow<2>(Kilogram);
+    6.674'08e-11 * Newton * Pow<2>(Metre) / Pow<2>(Kilogram);
 
-constexpr Mass ElectronMass = 9.109'383'713'9e-31 * Kilogram;
-constexpr Mass ProtonMass   = 1.672'621'925'95e-27 * Kilogram;
+constexpr Mass ElectronMass = 9.109'382'91e-31 * Kilogram;
+constexpr Mass ProtonMass   = 1.672'621'777e-27 * Kilogram;
 
 constexpr Acceleration StandardGravity =
     9.80665 * Metre / Pow<2>(Second);
