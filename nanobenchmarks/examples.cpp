f7aba8296cc295031e9cca81af8713b78c453350
pleroy <pascal.h.leroy@gmail.com>
2025-10-06 15:08:14 +0200

    A nanobenchmark for polynomials.

--- nanobenchmarks/examples.cpp
+++ nanobenchmarks/examples.cpp
@@ -1,16 +1,28 @@
 #include <emmintrin.h>
 
+#include "geometry/frame.hpp"
+#include "geometry/grassmann.hpp"
+#include "geometry/instant.hpp"
+#include "geometry/space.hpp"
 #include "nanobenchmarks/function_registry.hpp"  // 🧙 For BENCHMARK_FUNCTION etc.
 #include "numerics/cbrt.hpp"
 #include "numerics/elementary_functions.hpp"
+#include "numerics/polynomial_in_monomial_basis.hpp"
+#include "quantities/named_quantities.hpp"
 #include "quantities/si.hpp"
 
 namespace principia {
 namespace nanobenchmarks {
 namespace _examples {
 
+using namespace principia::geometry::_frame;
+using namespace principia::geometry::_grassmann;
+using namespace principia::geometry::_instant;
+using namespace principia::geometry::_space;
 using namespace principia::numerics::_cbrt;
 using namespace principia::numerics::_elementary_functions;
+using namespace principia::numerics::_polynomial_in_monomial_basis;
+using namespace principia::quantities::_named_quantities;
 using namespace principia::quantities::_si;
 
 BENCHMARKED_FUNCTION(twice) {
@@ -81,6 +93,28 @@ BENCHMARKED_FUNCTION(principia_sin_cos) {
   // price of an extra `and` (1 cycle).
   return _mm_cvtsd_f64(
       _mm_and_pd(_mm_set_sd(values.sin), _mm_set_sd(values.cos)));
+}
+
+BENCHMARKED_FUNCTION(polynomial) {
+  using World = Frame<serialization::Frame::TestTag,
+                      Inertial,
+                      Handedness::Right,
+                      serialization::Frame::TEST>;
+  using P2A = PolynomialInMonomialBasis<Displacement<World>, Instant, 2>;
+  P2A::Coefficients const coefficients({
+            Displacement<World>({0 * Metre,
+                                 0 * Metre,
+                                 1 * Metre}),
+            Velocity<World>({0 * Metre / Second,
+                             1 * Metre / Second,
+                             0 * Metre / Second}),
+            Vector<Acceleration, World>({1 * Metre / Second / Second,
+                                         0 * Metre / Second / Second,
+                                         0 * Metre / Second / Second})});
+
+  Instant const t0 = Instant() + 0.3 * Second;
+  P2A const p(coefficients, t0);
+  return p(t0 + x * Second).Norm²() / Metre / Metre;
 }
 
 }  // namespace _examples
