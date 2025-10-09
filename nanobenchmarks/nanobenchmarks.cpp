441aeaa5b5a098481c6b062cfa5a42a0a537dabd
pleroy <pascal.h.leroy@gmail.com>
2025-10-10 00:09:17 +0200

    Use the fixture.

--- nanobenchmarks/nanobenchmarks.cpp
+++ nanobenchmarks/nanobenchmarks.cpp
@@ -21,6 +21,7 @@ using namespace principia::geometry::_grassmann;
 using namespace principia::geometry::_instant;
 using namespace principia::geometry::_space;
 using namespace principia::numerics::_cbrt;
+using namespace principia::nanobenchmarks::_function_registry;
 using namespace principia::numerics::_elementary_functions;
 using namespace principia::numerics::_polynomial_evaluators;
 using namespace principia::numerics::_polynomial_in_monomial_basis;
@@ -102,64 +103,72 @@ NANOBENCHMARKED_FUNCTION(principia_sin_cos) {
       _mm_and_pd(_mm_set_sd(values.sin), _mm_set_sd(values.cos)));
 }
 
-auto const c0 = Displacement<World>({0 * Metre, 0 * Metre, 1 * Metre});
-auto const c1 = Velocity<World>(
-    {0 * Metre / Second, 1 * Metre / Second, 0 * Metre / Second});
-auto const c2 = Vector<Acceleration, World>({1 * Metre / Second / Second,
-                                             0 * Metre / Second / Second,
-                                             0 * Metre / Second / Second});
-auto const c3 = Vector<Jerk, World>({1 * Metre / Second / Second / Second,
-                                     0 * Metre / Second / Second / Second,
-                                     0 * Metre / Second / Second / Second});
-auto const c4 =
-    Vector<Snap, World>({1 * Metre / Second / Second / Second / Second,
-                         0 * Metre / Second / Second / Second / Second,
-                         0 * Metre / Second / Second / Second / Second});
-auto const c5 = Vector<Variation<Snap>, World>(
-    {1 * Metre / Second / Second / Second / Second / Second,
-     0 * Metre / Second / Second / Second / Second / Second,
-     0 * Metre / Second / Second / Second / Second / Second});
+class PolynomialNanobenchmark : public Fixture {
+ protected:
+  PolynomialNanobenchmark()
+      : c0_({0 * Metre, 0 * Metre, 1 * Metre}),
+        c1_({0 * Metre / Second, 1 * Metre / Second, 0 * Metre / Second}),
+        c2_({1 * Metre / Second / Second,
+             0 * Metre / Second / Second,
+             0 * Metre / Second / Second}),
+        c3_({1 * Metre / Second / Second / Second,
+             0 * Metre / Second / Second / Second,
+             0 * Metre / Second / Second / Second}),
+        c4_({1 * Metre / Second / Second / Second / Second,
+             0 * Metre / Second / Second / Second / Second,
+             0 * Metre / Second / Second / Second / Second}),
+        c5_({1 * Metre / Second / Second / Second / Second / Second,
+             0 * Metre / Second / Second / Second / Second / Second,
+             0 * Metre / Second / Second / Second / Second / Second}) {};
 
-NANOBENCHMARKED_FUNCTION(polynomial1) {
+  Displacement<World> const c0_;
+  Velocity<World> const c1_;
+  Vector<Acceleration, World> const c2_;
+  Vector<Jerk, World> const c3_;
+  Vector<Snap, World> const c4_;
+  Vector<Variation<Snap>, World> const c5_;
+};
+
+NANOBENCHMARK_F(PolynomialNanobenchmark, Degree1) {
   using P1A = PolynomialInMonomialBasis<Displacement<World>, Instant, 1>;
 
-  P1A::Coefficients const coefficients({c0, c1});
+  P1A::Coefficients const coefficients({c0_, c1_});
   Instant const t0 = Instant() + 0.3 * Second;
   P1A const p(coefficients, t0, with_evaluator<Estrin>);
   return p(t0 + x * Second).Norm²() / Metre / Metre;
 }
 
-NANOBENCHMARKED_FUNCTION(polynomial2) {
+NANOBENCHMARK_F(PolynomialNanobenchmark, Degree2) {
   using P2A = PolynomialInMonomialBasis<Displacement<World>, Instant, 2>;
 
-  P2A::Coefficients const coefficients({c0, c1, c2});
+  P2A::Coefficients const coefficients({c0_, c1_, c2_});
   Instant const t0 = Instant() + 0.3 * Second;
   P2A const p(coefficients, t0, with_evaluator<Estrin>);
   return p(t0 + x * Second).Norm²() / Metre / Metre;
 }
 
-NANOBENCHMARKED_FUNCTION(polynomial3) {
+NANOBENCHMARK_F(PolynomialNanobenchmark, Degree3) {
   using P3A = PolynomialInMonomialBasis<Displacement<World>, Instant, 3>;
 
-  P3A::Coefficients const coefficients({c0, c1, c2, c3});
+  P3A::Coefficients const coefficients({c0_, c1_, c2_, c3_});
   Instant const t0 = Instant() + 0.3 * Second;
   P3A const p(coefficients, t0, with_evaluator<Estrin>);
   return p(t0 + x * Second).Norm²() / Metre / Metre;
 }
 
-NANOBENCHMARKED_FUNCTION(polynomial4) {
+NANOBENCHMARK_F(PolynomialNanobenchmark, Degree4) {
   using P4A = PolynomialInMonomialBasis<Displacement<World>, Instant, 4>;
 
-  P4A::Coefficients const coefficients({c0, c1, c2, c3, c4});
+  P4A::Coefficients const coefficients({c0_, c1_, c2_, c3_, c4_});
   Instant const t0 = Instant() + 0.3 * Second;
   P4A const p(coefficients, t0, with_evaluator<Estrin>);
   return p(t0 + x * Second).Norm²() / Metre / Metre;
 }
 
-NANOBENCHMARKED_FUNCTION(polynomial5) {
+NANOBENCHMARK_F(PolynomialNanobenchmark, Degree5) {
   using P5A = PolynomialInMonomialBasis<Displacement<World>, Instant, 5>;
 
-  P5A::Coefficients const coefficients({c0, c1, c2, c3, c4, c5});
+  P5A::Coefficients const coefficients({c0_, c1_, c2_, c3_, c4_, c5_});
   Instant const t0 = Instant() + 0.3 * Second;
   P5A const p(coefficients, t0, with_evaluator<Estrin>);
   return p(t0 + x * Second).Norm²() / Metre / Metre;
