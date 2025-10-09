500b6ee9e83e2f16ac83463496b202ea015a9e6b
pleroy <pascal.h.leroy@gmail.com>
2025-10-10 00:08:45 +0200

    Distribution table.

---
+++ nanobenchmarks/latency_distribution_table.cpp
@@ -0,0 +1,75 @@
+#include "latency_distribution_table.hpp"
+
+#include <print>
+#include <sstream>
+
+#include "absl/flags/flag.h"
+
+namespace principia {
+namespace nanobenchmarks {
+namespace _latency_distribution_table {
+namespace internal {
+
+LatencyDistributionTable::LatencyDistributionTable(
+    std::vector<double> const& quantiles)
+    : quantiles_(quantiles) {}
+
+std::vector<double> const& LatencyDistributionTable::quantiles() const {
+  return quantiles_;
+}
+
+void LatencyDistributionTable::SetSamples(std::vector<double> const& samples) {
+  min_ = samples[0];
+  for (double const q : quantiles_) {
+    measures_.push_back(samples[(samples.size() - 1) * q]);
+  }
+}
+
+std::string LatencyDistributionTable::Heading() {
+  std::stringstream& out = *new std::stringstream;
+  std::print(out, "{:>8}", "min");
+  for (double const q : quantiles_) {
+    if (q < 1e-3) {
+      std::print(out, "{:>7}‱", 10'000 * q);
+    } else if (q < 1e-2) {
+      std::print(out, "{:>7}‰", 1000 * q);
+    } else {
+      std::print(out, "{:>7}%", 100 * q);
+    }
+  }
+  return out.str();
+}
+
+inline std::string LatencyDistributionTable::Row() const {
+  std::stringstream out;
+  std::print(out, "{:8.2f}", min_);
+  for (double const measure : measures_) {
+    std::print(out, "{:+8.2f}", measure - min_);
+  }
+  return out.str();
+}
+
+LatencyDistributionTable operator*(double const a,
+                                   LatencyDistributionTable const& x) {
+  LatencyDistributionTable result(x.quantiles());
+  result.min_ = a * x.min_;
+  for (double const measure : x.measures_) {
+    result.measures_.push_back(a * measure);
+  }
+  return result;
+}
+
+LatencyDistributionTable operator+(LatencyDistributionTable const& x,
+                                   double const b) {
+  LatencyDistributionTable result(x.quantiles());
+  result.min_ = x.min_ + b;
+  for (double const measure : x.measures_) {
+    result.measures_.push_back(measure + b);
+  }
+  return result;
+}
+
+}  // namespace internal
+}  // namespace _latency_distribution_table
+}  // namespace nanobenchmarks
+}  // namespace principia
---
+++ nanobenchmarks/latency_distribution_table.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace principia {
+namespace nanobenchmarks {
+namespace _latency_distribution_table {
+namespace internal {
+
+class LatencyDistributionTable {
+ public:
+  explicit LatencyDistributionTable(std::vector<double> const& quantiles);
+
+  std::vector<double> const& quantiles() const;
+
+  void SetSamples(std::vector<double> const& samples);
+
+  std::string Heading();
+
+  std::string Row() const;
+
+ private:
+  std::vector<double> const quantiles_;
+  double min_;
+  std::vector<double> measures_;
+
+  friend LatencyDistributionTable operator*(double a,
+                                            LatencyDistributionTable const& x);
+  friend LatencyDistributionTable operator+(LatencyDistributionTable const& x,
+                                            double b);
+};
+
+LatencyDistributionTable operator*(double a, LatencyDistributionTable const& x);
+
+LatencyDistributionTable operator+(LatencyDistributionTable const& x, double b);
+
+}  // namespace internal
+
+using internal::LatencyDistributionTable;
+
+}  // namespace _latency_distribution_table
+}  // namespace nanobenchmarks
+}  // namespace principia
