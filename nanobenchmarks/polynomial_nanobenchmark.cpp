1bc6009d5f4d8ecafd993a8bb86bcb3ac91fffea
pleroy <pascal.h.leroy@gmail.com>
2025-10-11 13:12:43 +0200

    Split the nanobenchmarks.

--- nanobenchmarks/polynomial_nanobenchmark.cpp
+++ nanobenchmarks/polynomial_nanobenchmark.cpp
@@ -1,12 +1,8 @@
-#include <emmintrin.h>
-
 #include "geometry/frame.hpp"
 #include "geometry/grassmann.hpp"
 #include "geometry/instant.hpp"
 #include "geometry/space.hpp"
 #include "nanobenchmarks/function_registry.hpp"  // 🧙 For NANOBENCHMARK_*.
-#include "numerics/cbrt.hpp"
-#include "numerics/elementary_functions.hpp"
 #include "numerics/polynomial_evaluators.hpp"
 #include "numerics/polynomial_in_monomial_basis.hpp"
 #include "quantities/named_quantities.hpp"
@@ -20,9 +16,7 @@ using namespace principia::geometry::_frame;
 using namespace principia::geometry::_grassmann;
 using namespace principia::geometry::_instant;
 using namespace principia::geometry::_space;
-using namespace principia::numerics::_cbrt;
 using namespace principia::nanobenchmarks::_function_registry;
-using namespace principia::numerics::_elementary_functions;
 using namespace principia::numerics::_polynomial_evaluators;
 using namespace principia::numerics::_polynomial_in_monomial_basis;
 using namespace principia::quantities::_named_quantities;
@@ -32,76 +26,6 @@ using World = Frame<serialization::Frame::TestTag,
                     Inertial,
                     Handedness::Right,
                     serialization::Frame::TEST>;
-
-NANOBENCHMARK(twice) {
-  return 2 * x;
-}
-
-NANOBENCHMARK(thrice) {
-  return 3 * x;
-}
-
-NANOBENCHMARK(inc) {
-  return x + 1;
-}
-
-NANOBENCHMARK(multiply_4_times) {
-  return x * x * x * x * x;
-}
-
-NANOBENCHMARK(add_16_times) {
-  return x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x;
-}
-
-NANOBENCHMARK(square_root) {
-  __m128d x_0 = _mm_set_sd(x);
-  return _mm_cvtsd_f64(_mm_sqrt_sd(x_0, x_0));
-}
-
-NANOBENCHMARK(sqrt_sqrt) {
-  __m128d x_0 = _mm_set_sd(x);
-  x_0 = _mm_sqrt_sd(x_0, x_0);
-  return _mm_cvtsd_f64(_mm_sqrt_sd(x_0, x_0));
-}
-
-NANOBENCHMARK(square_root_division) {
-  __m128d x_0 = _mm_set_sd(x);
-  return _mm_cvtsd_f64(_mm_div_sd(x_0, _mm_sqrt_sd(x_0, x_0)));
-}
-NANOBENCHMARK_FUNCTION(Cbrt);
-
-using namespace principia::numerics::_cbrt::internal;
-
-NANOBENCHMARK_FUNCTION(method_3²ᴄZ5¹::Cbrt<Rounding::Faithful>);
-NANOBENCHMARK_FUNCTION(method_3²ᴄZ5¹::Cbrt<Rounding::Correct>);
-#if PRINCIPIA_COMPILER_MSVC
-NANOBENCHMARK_FUNCTION(method_5²Z4¹FMA::Cbrt<Rounding::Faithful>);
-NANOBENCHMARK_FUNCTION(method_5²Z4¹FMA::Cbrt<Rounding::Correct>);
-#endif
-
-NANOBENCHMARK(std_sin) {
-  return std::sin(x);
-}
-
-NANOBENCHMARK(principia_sin) {
-  return Sin(x * Radian);
-}
-
-NANOBENCHMARK(std_cos) {
-  return std::cos(x);
-}
-
-NANOBENCHMARK(principia_cos) {
-  return Cos(x * Radian);
-}
-
-NANOBENCHMARK(principia_sin_cos) {
-  auto const values = SinCos(x * Radian);
-  // The nanobenchmark library wants the result to be a double, so we'll pay the
-  // price of an extra `and` (1 cycle).
-  return _mm_cvtsd_f64(
-      _mm_and_pd(_mm_set_sd(values.sin), _mm_set_sd(values.cos)));
-}
 
 class PolynomialNanobenchmark : public Nanobenchmark {
  protected:
