2da6c3219243825efda69ed2f89a7bc6a8d838bb
Robin Leroy <egg.robin.leroy@gmail.com>
2025-11-15 14:01:29 +0100

    Next release is Laurent

--- ksp_plugin_adapter/main_window.cs
+++ ksp_plugin_adapter/main_window.cs
@@ -8,10 +8,10 @@ namespace ksp_plugin_adapter {
 
 internal class MainWindow : VesselSupervisedWindowRenderer {
   // Update this section before each release.
-  private const string next_release_name = "Laplace";
-  private const int next_release_lunation_number = 320;
+  private const string next_release_name = "Laurent";
+  private const int next_release_lunation_number = 321;
   private readonly DateTimeOffset next_release_date_ =
-      new DateTimeOffset(2025, 11, 20, 06, 47, 16, TimeSpan.Zero);
+      new DateTimeOffset(2025, 12, 20, 01, 43, 21, TimeSpan.Zero);
 
   public MainWindow(PrincipiaPluginAdapter adapter,
                     FlightPlanner flight_planner,
