4b88b0d49c41e540432c967cfa12d7e709358a2c
pleroy <pascal.h.leroy@gmail.com>
2025-12-16 13:51:48 +0100

    Shell syntax.

--- principia_make.sh
+++ principia_make.sh
@@ -1,7 +1,7 @@
 # It seems that protoc really wants its dependencies to be in /usr/local/lib.
 # In some setups, e.g., Azure pipelines, this does not work, so we need to help
 # it find its dynamic libraries.
-if [ "${AGENT_OS?}" == "Darwin" ]; then
+if [[ "${AGENT_OS?}" == "Darwin" ]]; then
   install_name_tool \
       -change \
           /usr/local/lib/libprotobuf.17.dylib \
@@ -17,7 +17,7 @@ if [ "${AGENT_OS?}" == "Darwin" ]; then
           /usr/local/lib/libprotoc.17.dylib \
           ./deps/protobuf/src/.libs/libprotoc.17.dylib \
       deps/protobuf/src/.libs/protoc
-elif [ "${AGENT_OS?}" == "Linux" ]; then
+elif [[ "${AGENT_OS?}" == "Linux" ]]; then
   export LD_LIBRARY_PATH="./deps/protobuf/src/.libs:$LD_LIBRARY_PATH"
 fi
 
@@ -29,7 +29,7 @@ export PRINCIPIA_TARGET=x64_AVX_FMA; make clean
 export PRINCIPIA_TARGET=x64; make -j ${PARALLELISM} each_package_test
 export PRINCIPIA_TARGET=x64_AVX_FMA; make -j ${PARALLELISM} each_package_test
 
-if [ "${AGENT_OS?}" == "Darwin" ]; then
+if [[ "${AGENT_OS?}" == "Darwin" ]]; then
   # See https://github.com/actions/virtual-environments/issues/2619#issuecomment-788397841
   # for why this is needed.
   sudo /usr/sbin/purge
