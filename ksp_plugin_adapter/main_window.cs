05f68b7fdca97daaf8f5ab5d8c62bbca8913df02
pleroy <pascal.h.leroy@gmail.com>
2025-12-31 16:19:04 +0100

    Display the platform.

--- ksp_plugin_adapter/main_window.cs
+++ ksp_plugin_adapter/main_window.cs
@@ -193,9 +193,12 @@ internal class MainWindow : VesselSupervisedWindowRenderer {
       }
       Interface.GetVersion(build_date : out string _,
                            version    : out string version,
-                           platform   : out string _);
-      UnityEngine.GUILayout.Label(version,
-                                  style : Style.Info(
+                           platform   : out string platform);
+      // Only retain the leading 20 characters of the hash to be able to display
+      // the platform.
+      UnityEngine.GUILayout.Label(version.Remove(version.Length - 20) + "-" +
+                                  platform,
+                                  style: Style.Info(
                                       UnityEngine.GUI.skin.label));
       history_length_.Render(enabled : true);
       if (FlightGlobals.ActiveVessel?.orbitTargeter != null &&
