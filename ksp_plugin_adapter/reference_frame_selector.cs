fb4ce8a63e9bb0fed1a6d691e617b332bd833822
pleroy <pascal.h.leroy@gmail.com>
2026-03-12 20:58:25 +0100

    New pinning.

--- ksp_plugin_adapter/reference_frame_selector.cs
+++ ksp_plugin_adapter/reference_frame_selector.cs
@@ -535,6 +535,9 @@ internal class
     if (focus_ == null) {
       PrincipiaPluginAdapter.LoadTextureOrDie(out focus_, "focus.png");
     }
+    if (pin_ == null) {
+      PrincipiaPluginAdapter.LoadTextureOrDie(out pin_, "pin.png");
+    }
     using (new UnityEngine.GUILayout.VerticalScope()) {
       UnityEngine.GUILayout.Label(
           L10N.CacheFormat(
@@ -669,15 +672,7 @@ internal class
       }
       UnityEngine.GUILayout.Label(celestial.StandaloneName());
       UnityEngine.GUILayout.FlexibleSpace();
-      if (celestial.is_root()) {
-        UnityEngine.GUILayout.Label(
-            L10N.CacheFormat("#Principia_ReferenceFrameSelector_Pin"),
-            UnityEngine.GUI.skin.label,
-            GUILayoutWidth(1));
-      } else if (UnityEngine.GUILayout.Toggle(pinned[celestial],
-                                              "",
-                                              GUILayoutWidth(1)) !=
-                 pinned[celestial]) {
+      if (RenderPinButton()) {
         pinned[celestial] = !pinned[celestial];
         ScheduleShrink();
       }
@@ -824,6 +819,16 @@ internal class
     }
   }
 
+  private bool RenderPinButton() {
+    return UnityEngine.GUILayout.Button(
+        new UnityEngine.GUIContent(pin_,
+                                   L10N.CacheFormat(
+                                       "#Principia_ReferenceFrameSelector_Pin")),
+        Style.Aligned(UnityEngine.TextAnchor.LowerCenter,
+                      UnityEngine.GUI.skin.button),
+        GUILayoutWidth(1));
+  }
+
   private bool SelectedFrameIs(CelestialBody celestial, FrameType type) {
     return !target_frame_selected &&
         selected_celestial == celestial && frame_type == type;
@@ -881,6 +886,7 @@ internal class
   private float tree_width_ = 0f;
   private FrameType last_orbital_type_ = FrameType.BODY_CENTRED_NON_ROTATING;
   private static UnityEngine.Texture focus_;
+  private static UnityEngine.Texture pin_;
 }
 
 }  // namespace ksp_plugin_adapter
