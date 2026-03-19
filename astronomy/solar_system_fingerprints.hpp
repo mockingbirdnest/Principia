c78dbffc916f06584c5754a85288d69b282aeb2a
pleroy <pascal.h.leroy@gmail.com>
2026-03-19 11:27:44 +0100

    After egg's review.

--- astronomy/solar_system_fingerprints.hpp
+++ astronomy/solar_system_fingerprints.hpp
@@ -14,13 +14,13 @@ constexpr std::int64_t KSP191PreLegendre = 1;
 constexpr std::int64_t KSP191 = 2;
 
 constexpr std::array KSPStockSystemFingerprints{
-    0x9F3E8BFE0E32C283ull,
-    0x7A077635599E23F2ull,
-    0xFE67F3BAEE725803ull};
+    UINT64_C(0x9F3E8BFE0E32C283),
+    UINT64_C(0x7A077635599E23F2),
+    UINT64_C(0xFE67F3BAEE725803)};
 constexpr std::array KSPStabilizedSystemFingerprints{
-    0x9F1B6D95399877C6ull,
-    0x4B830CBDF5E77F8Dull,
-    0xB1BA690A45CAD577ull};
+    UINT64_C(0x9F1B6D95399877C6),
+    UINT64_C(0x4B830CBDF5E77F8D),
+    UINT64_C(0xB1BA690A45CAD577)};
 
 }  // namespace internal
 
