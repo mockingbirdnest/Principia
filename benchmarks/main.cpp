2fbb1ef541d05a7fce20097042235a8402f796e0
Pascal Leroy <pascal.h.leroy@gmail.com>
2026-04-07 15:11:42 +0200

    Use the BENCHMARK_MAIN macro and fix the library name.

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,13 +1,3 @@
 #include "benchmark/benchmark.h"
-#include "absl/flags/parse.h"
-#include "absl/log/check.h"
-#include "absl/log/initialize.h"
-#include "absl/log/log.h"
 
-int __cdecl main(int argc, char* argv[]) {
-  google::SetLogFilenameExtension(".log");
-  absl::ParseCommandLine(argc, argv);
-  absl::InitializeLog();
-  benchmark::Initialize(&argc, argv);
-  benchmark::RunSpecifiedBenchmarks();
-}
+BENCHMARK_MAIN();
