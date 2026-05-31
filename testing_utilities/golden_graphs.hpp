39a297491fe23326465af10d19dd6354568d5916
Robin Leroy <egg.robin.leroy@gmail.com>
2026-05-31 23:45:48 +0200

    Move most of it to a function

--- testing_utilities/golden_graphs.hpp
+++ testing_utilities/golden_graphs.hpp
@@ -4,33 +4,56 @@
 #include <filesystem>
 #include <fstream>
 #include <vector>
+#include <string_view>
 
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "graphics/graph.hpp"
 #include "lodepng/lodepng.h"
 
-#define EXPECT_GOLDEN_GRAPH(graph, suffix)                                   \
-  [&] {                                                                      \
-    auto const image_path = std::filesystem::path(__FILE__)                  \
-                                .replace_extension()                         \
-                                .concat("_" suffix)                          \
-                                .replace_extension(".png");                  \
-    std::vector<std::uint8_t> golden;                                        \
-    std::ifstream in(image_path, std::ios::binary | std::ios::in);           \
-    while (in.good()) {                                                      \
-      in.read(reinterpret_cast<char*>(&golden.emplace_back()), 1);           \
-      if (in.eof()) {                                                        \
-        golden.pop_back();                                                   \
-        break;                                                               \
-      }                                                                      \
-    }                                                                        \
-    std::vector<std::uint8_t> actual;                                        \
-    lodepng::encode(                                                         \
-        actual,                                                              \
-        reinterpret_cast<std::uint8_t const*>((graph).pixels().data()),      \
-        (graph).width(),                                                     \
-        (graph).height());                                                   \
-    EXPECT_THAT(actual, ::testing::Eq(golden))                               \
-        << image_path << " has changed";                                     \
-    std::ofstream(image_path, std::ios::binary | std::ios::out)              \
-        .write(reinterpret_cast<char const*>(actual.data()), actual.size()); \
-  }()
+#define EXPECT_GOLDEN_GRAPH(graph, suffix)                            \
+  (::principia::testing_utilities::_golden_graphs::ExpectGoldenGraph( \
+      (graph), (suffix), __FILE__))
+namespace principia {
+namespace testing_utilities {
+namespace _golden_graphs {
+namespace internal {
+
+using namespace principia::graphics::_graph;
+using ::testing::Eq;
+
+template<typename Abscissa, typename Ordinate>
+void ExpectGoldenGraph(Graph<Abscissa, Ordinate> const& graph,
+                       std::string_view suffix,
+                       std::string_view test_file) {
+  auto const image_path = std::filesystem::path(test_file)
+                              .replace_extension()
+                              .concat("_")
+                              .concat(suffix)
+                              .replace_extension(".png");
+  std::vector<std::uint8_t> golden;
+  std::ifstream in(image_path, std::ios::binary | std::ios::in);
+  while (in.good()) {
+    in.read(reinterpret_cast<char*>(&golden.emplace_back()), 1);
+    if (in.eof()) {
+      golden.pop_back();
+      break;
+    }
+  }
+  std::vector<std::uint8_t> actual;
+  lodepng::encode(actual,
+                  reinterpret_cast<std::uint8_t const*>(graph.pixels().data()),
+                  graph.width(),
+                  graph.height());
+  EXPECT_THAT(actual, Eq(golden)) << image_path << " has changed";
+  std::ofstream(image_path, std::ios::binary | std::ios::out)
+      .write(reinterpret_cast<char const*>(actual.data()), actual.size());
+}
+
+}  // namespace internal
+
+using internal::ExpectGoldenGraph;
+
+}  // namespace _golden_graphs
+}  // namespace testing_utilities
+}  // namespace principia
