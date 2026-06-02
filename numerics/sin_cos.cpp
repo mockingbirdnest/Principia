fc095926f75cfbc0ce6570a0a155131b8cb71a4d
Pascal Leroy <pascal.h.leroy@gmail.com>
2026-06-02 17:43:38 +0200

    More OSACA.

--- numerics/sin_cos.cpp
+++ numerics/sin_cos.cpp
@@ -50,8 +50,7 @@ using namespace principia::numerics::_polynomial_evaluators;
     /* From argument reduction. */                                           \
     constexpr double abs_x = x > 0 ? x : -x;                                 \
     constexpr std::int64_t n = static_cast<std::int64_t>(x * (2 / π) + 0.5); \
-    constexpr double k = n;                                                  \
-    constexpr double abs_k = k > 0 ? k : -k;                                 \
+    constexpr double abs_n = n > 0 ? n : -n;                                 \
     constexpr double reduction_value = x - n * cody_waite::C₁;               \
     constexpr double reduction_error = n * cody_waite::δC₁;                  \
     /* Used to determine whether a better argument reduction is needed. */   \
@@ -60,9 +59,9 @@ using namespace principia::numerics::_polynomial_evaluators;
     constexpr double abs_x_reduced_value =                                   \
         x_reduced.value > 0 ? x_reduced.value : -x_reduced.value;            \
     constexpr double two_term_x_reduced_threshold =                          \
-        abs_k * boldo_daumas_li::two_term_x_reduced_threshold_multiplier;    \
+        abs_n * boldo_daumas_li::two_term_x_reduced_threshold_multiplier;    \
     constexpr double three_term_x_reduced_threshold =                        \
-        abs_k * boldo_daumas_li::three_term_x_reduced_threshold_multiplier;  \
+        abs_n * boldo_daumas_li::three_term_x_reduced_threshold_multiplier;  \
     /* Used in Sin to detect the near-0 case. */                             \
     constexpr double abs_x̃ =                                                 \
         x_reduced.value > 0 ? x_reduced.value : -x_reduced.value;            \
