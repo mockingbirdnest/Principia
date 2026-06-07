eca5c7769a4986e52fcf76f18a7048a4f877b42d
Robin Leroy <egg.robin.leroy@gmail.com>
2026-06-07 19:22:11 +0200

    Use the C API to avoid principia::std::vector vs. ::std::vector mismatches on mac

--- testing_utilities/golden_graphs.hpp
+++ testing_utilities/golden_graphs.hpp
@@ -1,29 +1,22 @@
 #pragma once
 
-#include <cstdint>
-#include <filesystem>
-#include <fstream>
-#include <vector>
 #include <string_view>
 
 #include "base/macros.hpp"  // 🧙 For FILESYSTEM_STRING.
-#include "gmock/gmock.h"
 #include "graphics/graph.hpp"
-#include "gtest/gtest.h"
-#include "lodepng/lodepng.h"
 
 #define EXPECT_GOLDEN_GRAPH(graph, suffix)                            \
   (::principia::testing_utilities::_golden_graphs::ExpectGoldenGraph( \
       (graph),                                                        \
       FILESYSTEM_STRING_VIEW(suffix),                                 \
       FILESYSTEM_STRING_VIEW(__FILE__)))
+
 namespace principia {
 namespace testing_utilities {
 namespace _golden_graphs {
 namespace internal {
 
 using namespace principia::graphics::_graph;
-using ::testing::Eq;
 
 // This is templatized on the character type because it needs to
 // platform-appropriate for paths (UTF-16 on Windows, UTF-8 on *nix).  We take
@@ -32,31 +25,7 @@ using ::testing::Eq;
 template<typename Abscissa, typename Ordinate, typename Character>
 void ExpectGoldenGraph(Graph<Abscissa, Ordinate> const& graph,
                        std::basic_string_view<Character> const suffix,
-                       std::basic_string_view<Character> const test_file) {
-  auto const image_path = std::filesystem::path(test_file)
-                              .replace_extension()
-                              .concat("_")
-                              .concat(suffix)
-                              .replace_extension(".png");
-  std::vector<std::uint8_t> golden;
-  std::ifstream in(image_path, std::ios::binary | std::ios::in);
-  while (in.good()) {
-    char byte;
-    in.read(&byte, 1);
-    if (in.eof()) {
-      break;
-    }
-    golden.push_back(byte);
-  }
-  std::vector<std::uint8_t> actual;
-  lodepng::encode(actual,
-                  reinterpret_cast<std::uint8_t const*>(graph.pixels().data()),
-                  graph.width(),
-                  graph.height());
-  EXPECT_THAT(actual, Eq(golden)) << image_path << " has changed";
-  std::ofstream(image_path, std::ios::binary | std::ios::out)
-      .write(reinterpret_cast<char const*>(actual.data()), actual.size());
-}
+                       std::basic_string_view<Character> const test_file);
 
 }  // namespace internal
 
@@ -65,3 +34,5 @@ using internal::ExpectGoldenGraph;
 }  // namespace _golden_graphs
 }  // namespace testing_utilities
 }  // namespace principia
+
+#include "testing_utilities/golden_graphs_body.hpp"
