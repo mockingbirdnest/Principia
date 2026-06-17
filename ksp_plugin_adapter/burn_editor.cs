df2d67db9b703180a23a28134366210e701e079d
pleroy <pascal.h.leroy@gmail.com>
2026-06-17 15:45:39 +0200

    Implement useZaxis.

--- ksp_plugin_adapter/burn_editor.cs
+++ ksp_plugin_adapter/burn_editor.cs
@@ -404,14 +404,16 @@ class BurnEditor : ScalingRenderer {
     // NOTE(egg): NathanKell informs me that in >= 1.0.5, RCS has a useZaxis
     // property, that controls whether they thrust -up or -forward.  The madness
     // keeps piling up.
-    double[] thrusts = (from engine in active_rcs
-                        select engine.thrusterPower *
-                               (from transform in engine.thrusterTransforms
-                                where transform.gameObject.activeInHierarchy
-                                select Math.Max(0,
-                                                Vector3d.Dot(
-                                                    reference_direction,
-                                                    -transform.up))).Sum()).
+    double[] thrusts =
+        (from engine in active_rcs
+         select engine.thrusterPower *
+                (from transform in engine.thrusterTransforms
+                 where transform.gameObject.activeInHierarchy
+                 select Math.Max(0,
+                                 Vector3d.Dot(reference_direction,
+                                              engine.useZaxis
+                                                  ? -transform.forward
+                                                  : -transform.up))).Sum()).
         ToArray();
     thrust_in_kilonewtons_ = thrusts.Sum();
 
