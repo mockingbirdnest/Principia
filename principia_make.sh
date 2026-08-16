70a9badf11a250e5a3861ed213dc85e5a0fedce6
Robin Leroy <egg.robin.leroy@gmail.com>
2026-08-16 18:47:55 +0200

    Avoid duplicate golden-updating PRs

--- principia_make.sh
+++ principia_make.sh
@@ -8,10 +8,16 @@ if [[ "${PRINCIPIA_PLATFORM?}" != "x64" &&
   exit 1
 fi
 
+if [[ "${PRINCIPIA_PLATFORM?}" != "x64_AVX_FMA" ]]; then
+  PLATFORM_GOLDEN_SUFFIX="_fma"
+fi
+
 if [[ "${AGENT_OS?}" == "Darwin" ]]; then
+  OS_GOLDEN_SUFFIX="_macos"
   PARALLELISM=$(sysctl -n hw.ncpu)
   TARGET="each_test"
 elif [[ "${AGENT_OS?}" == "Linux" ]]; then
+  OS_GOLDEN_SUFFIX="_linux"
   export LD_LIBRARY_PATH="./deps/protobuf/src/.libs:$LD_LIBRARY_PATH"
   PARALLELISM=$(nproc --all)
   TARGET="each_package_test"
@@ -30,18 +36,50 @@ make -j ${PARALLELISM} \
 MAKE_RESULT=$?
 set -e
 
-git add *.png
-
 if git diff --quiet HEAD *.png; then
   echo same;
 else
-  git config user.email "enrico.dandolo@mockingbirdnest.com"
-  git config user.name "Enrico Dandolo"
-  git commit -m "Update goldens for ${AGENT_OS} ${PRINCIPIA_PLATFORM}"
-  BRANCH_NAME="goldens-$(date '+%Y%m%dT%H%M%S')-${AGENT_OS}-${PRINCIPIA_PLATFORM}"
-  git checkout -b ${BRANCH_NAME}
-  git push --set-upstream "https://${GH_TOKEN}@github.com/enrico-dandolo/Principia.git" ${BRANCH_NAME}
-  gh pr create --fill --head enrico-dandolo:${BRANCH_NAME}
+  git diff --compact-summary HEAD |
+      awk '/\.png/ { if ($2 == "(gone)") { print("(deleted) " $1) } else { system("sha1sum -b " $1) } }' \
+      > golden_hashes.txt
+  HASH=$(sha1sum golden_hashes.txt | awk '{print($1)}')
+  BRANCH_NAME="goldens-${HASH}-${AGENT_OS}-${PRINCIPIA_PLATFORM}"
+  git ls-remote --exit-code                                   \
+      --heads https://github.com/enrico-dandolo/Principia.git \
+      refs/heads/${BRANCH_NAME}
+  if [[$? == 2]]; then
+    git config user.email "enrico.dandolo@mockingbirdnest.com"
+    git config user.name "Enrico Dandolo"
+    git checkout -b ${BRANCH_NAME}
+    GOLDEN_SUFFIX="${OS_GOLDEN_SUFFIX}${PLATFORM_GOLDEN_SUFFIX?}"
+    FILES_CHANGED="$(git diff --compact-summary HEAD | awk '/\.png/ { $1 }')"
+    for file in ${FILES_CHANGED}; do
+      if [[ -f ${file} ]]; then
+        mv "${file}" "${file}.new"
+      fi
+      cp "$(echo "${file}" | sed "s/${GOLDEN_SUFFIX}\.png/.png/")" "${file}"
+    done
+    git add *.png
+    git commit \
+        -m "Copy default goldens over ${AGENT_OS} ${PRINCIPIA_PLATFORM} goldens"
+    for file in ${FILES_CHANGED}; do
+      if [[ -f "${file}.new" ]]; then
+        mv "${file}.new" "${file}"
+      else
+        git rm "${file}"
+      fi
+    done
+    git add *.png
+    git commit -m "Update goldens for ${AGENT_OS} ${PRINCIPIA_PLATFORM}"
+    git push --set-upstream                                           \
+        "https://${GH_TOKEN}@github.com/enrico-dandolo/Principia.git" \
+        ${BRANCH_NAME}
+    gh pr create --fill --head enrico-dandolo:${BRANCH_NAME}
+  else
+    echo "Branch ${BRANCH_NAME} already exists."
+    echo "Merge the following PR to update these goldens:"
+    gh pr list --head ${BRANCH_NAME}
+  fi;
 fi;
 
 if [[ "${MAKE_RESULT}" != 0 ]]; then
