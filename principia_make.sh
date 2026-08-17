bfaaa2772271d8a7eab65d994f477bbea295b15b
Pascal Leroy <pascal.h.leroy@gmail.com>
2026-08-17 19:24:11 +0200

    You cannot have too many traces.

--- principia_make.sh
+++ principia_make.sh
@@ -41,7 +41,7 @@ set -e
 git add *.png
 
 if git diff --quiet HEAD; then
-  echo No files changed
+  echo "No files changed"
 else
   # git diff --name-status --no-renames outputs something like
   # A       path/to/new.file
@@ -54,29 +54,32 @@ else
   cat golden_hashes.txt
   HASH=$(shasum golden_hashes.txt | awk '{print($1)}')
   BRANCH_NAME="goldens-${HASH}-${AGENT_OS}-${PRINCIPIA_PLATFORM}"
-  echo Looking for branch ${BRANCH_NAME}...
+  echo "Looking for branch ${BRANCH_NAME}..."
   git ls-remote --exit-code                                   \
       --heads https://github.com/enrico-dandolo/Principia.git \
       refs/heads/${BRANCH_NAME}
-  if [[ $? == 2 ]]; then
-    echo "Exit code is 2"
+  code=$?
+  echo "Exit code is ${code}"
+  if [[ ${code} == 2 ]]; then
     git config user.email "enrico.dandolo@mockingbirdnest.com"
     git config user.name "Enrico Dandolo"
     git checkout -b ${BRANCH_NAME}
     GOLDEN_SUFFIX="${OS_GOLDEN_SUFFIX}${PLATFORM_GOLDEN_SUFFIX?}"
     FILES_CHANGED="$(git diff --name-only HEAD | awk '/\.png/ { $1 }')"
+    echo "Files changed ${FILES_CHANGED}"
     for file in ${FILES_CHANGED}; do
       echo "File ${file} was changed"
       if [[ -f ${file} ]]; then
         echo "Moving to ${file}.new"
         mv "${file}" "${file}.new"
       fi
-      cp "$(echo "${file}" | sed "s/${GOLDEN_SUFFIX}\.png/.png/")" "${file}"
+      cp $(echo "${file}" | sed "s/${GOLDEN_SUFFIX}\.png/.png/") "${file}"
     done
     git reset
     git add *.png
     git commit \
         -m "Copy default goldens over ${AGENT_OS} ${PRINCIPIA_PLATFORM} goldens"
+    echo "Files changed ${FILES_CHANGED}"
     for file in ${FILES_CHANGED}; do
       echo "File ${file} was changed, again"
       if [[ -f "${file}.new" ]]; then
